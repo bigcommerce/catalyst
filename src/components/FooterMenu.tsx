3e164afc919927bac6352bc8c25e27d972a32193
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-02-15 12:54:02 -0500

    chore(deps): HEADLESS-00 remove zod usage and convert to TS

--- src/components/FooterMenu.tsx
+++ src/components/FooterMenu.tsx
@@ -1,33 +1,26 @@
 import React from 'react';
-import z from 'zod';
 
 import { Link } from '../../reactant/components/Link';
 
-const FooterMenuProps = z.object({
-  title: z.string(),
-  items: z.array(
-    z.object({
-      name: z.string(),
-      path: z.string(),
-    }),
-  ),
-});
+interface FooterMenuProps {
+  title: string;
+  items: Array<{
+    name: string;
+    path: string;
+  }>;
+}
 
-export const FooterMenu = (props: z.infer<typeof FooterMenuProps>) => {
-  const { title, items } = FooterMenuProps.parse(props);
-
-  return (
-    <>
-      <h4 className="font-bold mb-4">{title}</h4>
-      <ul>
-        {items.map((item) => (
-          <li className="mb-4" key={item.path}>
-            <Link className={Link.text.className} href={item.path}>
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+export const FooterMenu = ({ title, items }: FooterMenuProps) => (
+  <>
+    <h4 className="font-bold mb-4">{title}</h4>
+    <ul>
+      {items.map((item) => (
+        <li className="mb-4" key={item.path}>
+          <Link className={Link.text.className} href={item.path}>
+            {item.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </>
+);
