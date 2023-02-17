d5ce3c923170ef2bac777feaabd13faa0a143ac7
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-02-17 16:39:02 -0500

    chore(catalyst): HEADLESS-226 move Header into component and fix ProductTiles

--- src/components/ProductTiles.tsx
+++ src/components/ProductTiles.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
-
-import { Link } from '../../reactant/components/Link';
 
 interface ProductTileProps {
   title: string;
@@ -12,6 +11,7 @@ interface ProductTileProps {
       node: {
         entityId: number;
         name: string;
+        path: string;
         brand: {
           name: string;
         } | null;
@@ -29,29 +29,35 @@ interface ProductTileProps {
   };
 }
 
-export const ProductTiles = ({ title, priority = false, products }: ProductTileProps) => (
-  <div className="my-12 mx-6 sm:mx-10 md:mx-auto">
-    <h2 className="font-black text-4xl mb-10">{title}</h2>
-    <ul className="grid grid-cols-2 gap-6 md:grid-cols-4 md:gap-8">
-      {products.edges.map((edge) => (
-        <li key={`featured-${edge.node.entityId}`}>
-          {edge.node.defaultImage && (
-            <Image
-              alt={edge.node.defaultImage.altText || 'Product image'}
-              className="mb-5 aspect-square w-full"
-              height={320}
-              priority={priority}
-              src={edge.node.defaultImage.url}
-              width={320}
-            />
-          )}
-          <p className="text-slate-500">{edge.node.brand?.name}</p>
-          <h3 className="font-bold text-xl mb-3">
-            <Link className={Link.text.className}>{edge.node.name}</Link>
-          </h3>
-          <p className="text-base">{edge.node.prices.price?.formatted}</p>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+export const ProductTiles = ({ title, priority = false, products }: ProductTileProps) => {
+  if (products.edges.length < 1) {
+    return null;
+  }
+
+  return (
+    <div className="my-12 mx-6 sm:mx-10 md:mx-auto">
+      <h2 className="font-black text-4xl mb-10">{title}</h2>
+      <ul className="grid grid-cols-2 gap-6 md:grid-cols-4 md:gap-8">
+        {products.edges.map((edge) => (
+          <li key={`featured-${edge.node.entityId}`}>
+            <Link href={edge.node.path}>
+              {edge.node.defaultImage && (
+                <Image
+                  alt={edge.node.defaultImage.altText || 'Product image'}
+                  className="mb-5 aspect-square w-full"
+                  height={320}
+                  priority={priority}
+                  src={edge.node.defaultImage.url}
+                  width={320}
+                />
+              )}
+              <p className="text-slate-500">{edge.node.brand?.name}</p>
+              <h3 className="font-bold text-xl mb-3">{edge.node.name}</h3>
+              <p className="text-base">{edge.node.prices.price?.formatted}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
