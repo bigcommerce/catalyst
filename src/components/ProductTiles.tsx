56d9777dc5334a6f5c76958f4b521a6f17201179
Yurii Zusik <66325265+yurytut1993@users.noreply.github.com>
2023-03-21 16:53:26 +0200

    feat(catalyst): BCTHEME-1418 and BCTHEME-1419 related - productTiles with swatch (#32)

--- src/components/ProductTiles.tsx
+++ src/components/ProductTiles.tsx
@@ -5,48 +5,38 @@ import React from 'react';
 import { Button } from '../../reactant/components/Button';
 import { Link as ReactantLink } from '../../reactant/components/Link';
 import { H3, P, ProductTile } from '../../reactant/components/ProducTile';
+import { Swatch, SwatchGroup } from '../../reactant/components/Swatch';
 import { HeartIcon } from '../../reactant/icons/Heart';
+import { Product } from '../pages';
 
-interface ProductTileProps {
-  title: string;
+interface ProductTilesProps {
+  cols?: keyof typeof COLS;
+  title?: string;
   priority?: boolean;
   // TODO: See if we can make this more generic
   products: {
-    edges: Array<{
-      node: {
-        addToCartUrl: string;
-        showCartAction: boolean;
-        entityId: number;
-        name: string;
-        brand: {
-          name: string;
-        } | null;
-        defaultImage: {
-          url: string;
-          altText: string;
-        } | null;
-        path: string;
-        prices: {
-          price: {
-            formatted: string;
-          } | null;
-        };
-      };
-    }>;
+    edges: Product[];
   };
   productComparisonsEnabled: boolean;
 }
 
+const COLS = {
+  2: 'md:grid-cols-2',
+  3: 'md:grid-cols-3',
+  4: 'md:grid-cols-4',
+};
+
 export const ProductTiles = ({
+  cols = 4,
   title,
   priority = false,
   products,
   productComparisonsEnabled,
-}: ProductTileProps) => {
+}: ProductTilesProps) => {
   return (
     <div className="my-12 mx-6 sm:mx-10 md:mx-auto">
-      <h2 className="font-black text-4xl mb-10">{title}</h2>
-      <ul className="grid grid-cols-2 gap-6 md:grid-cols-4 md:gap-8">
+      {title ? <h2 className="font-black text-4xl mb-10">{title}</h2> : null}
+      <ul className={`grid grid-cols-2 gap-6 ${COLS[cols]} md:gap-8`}>
         {products.edges.map((edge) => (
           <li key={`featured-${edge.node.entityId}`}>
             <ProductTile className={ProductTile.default.className}>
@@ -71,6 +61,43 @@ export const ProductTiles = ({
               </ProductTile.Figure>
               <ProductTile.Body className={ProductTile.Body.default.className}>
                 <div className="py-2">
+                  {edge.node.productOptions.edges.map(({ node: options }) => {
+                    if (
+                      options.__typename === 'MultipleChoiceOption' &&
+                      options.displayStyle === 'Swatch'
+                    ) {
+                      return (
+                        <SwatchGroup
+                          className={SwatchGroup.default.className}
+                          key={options.entityId}
+                          role="radiogroup"
+                        >
+                          {options.values.edges.map(({ node: variant }) => (
+                            <Swatch className={Swatch.default.className} key={variant.entityId}>
+                              <Swatch.Label
+                                className={Swatch.Label.default.className}
+                                title={variant.label}
+                              >
+                                <Swatch.Variant
+                                  className={Swatch.Variant.default.className}
+                                  variantColor={variant.hexColors[0]}
+                                />
+                              </Swatch.Label>
+                              <Swatch.Input
+                                aria-label={variant.label}
+                                className={Swatch.Input.default.className}
+                                name={`${variant.entityId}`}
+                                type="radio"
+                                value={variant.entityId}
+                              />
+                            </Swatch>
+                          ))}
+                        </SwatchGroup>
+                      );
+                    }
+
+                    return false;
+                  })}
                   <P className={`${P.default.className} text-[#546E7A]`}>{edge.node.brand?.name}</P>
                   <H3 className={`${H3.default.className}  hover:text-[#053FB0]`}>
                     <Link href={edge.node.path}>{edge.node.name}</Link>
@@ -79,7 +106,7 @@ export const ProductTiles = ({
                 <div className="card-text relative py-1 flex items-center gap-2">
                   <P className={P.default.className}>{edge.node.prices.price?.formatted}</P>
                 </div>
-                <div className="absolute hidden flex-row justify-start pt-4 gap-x-4 group-hover/cardBody:inline-flex">
+                <div className="absolute z-10 hidden flex-row justify-start pt-4 gap-x-4 group-hover/cardBody:inline-flex">
                   {edge.node.showCartAction && (
                     <ReactantLink
                       className={`${ReactantLink.text.className} ${Button.primary.className} hover:!text-white hover:!bg-[#3071EF]`}
