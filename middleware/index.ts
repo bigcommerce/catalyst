6fb5144b4d2f1db94e2e2c4ebfaf8d24b782ce68
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2023-05-22 09:40:17 -0500

    chore: disable middleware and iron-session

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,100 +1,103 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse, URLPattern } from 'next/server';
-
-import { bigcommerceFetch } from '@client/fetcher';
-
-// import { sessionMiddleware } from './session';
-
-interface RoutesResponse {
-  site: {
-    route: {
-      node: {
-        __typename: string;
-        entityId: string;
-      } | null;
-    };
-  };
+export function middleware() {
+  // no-empty
 }
 
-export async function middleware(request: NextRequest) {
-  // TODO: Bypassing middleware for now
-  // return rewriteUrlMiddleware(sessionMiddleware(request));
-}
+// import type { NextRequest } from 'next/server';
+// import { NextResponse, URLPattern } from 'next/server';
 
-// This is a POC middleware intended to redirect all page requests to the right NextJS route.
-// TODO: Internationalization, trailing slash, etc.
-async function rewriteUrlMiddleware(
-  requestResponse: Promise<{ request: NextRequest; response: NextResponse }>,
-) {
-  const { request, response } = await requestResponse;
+// import { bigcommerceFetch } from '@client/fetcher';
 
-  const { data } = await bigcommerceFetch<RoutesResponse>({
-    query: /* GraphQL */ `
-      query Routes($path: String!) {
-        site {
-          route(path: $path) {
-            node {
-              __typename
-              ... on Product {
-                entityId
-              }
-              ... on Category {
-                entityId
-              }
-              ... on Brand {
-                entityId
-              }
-            }
-          }
-        }
-      }
-    `,
-    variables: { path: request.nextUrl.pathname },
-  });
+// // import { sessionMiddleware } from './session';
 
-  switch (data.site.route.node?.__typename) {
-    case 'Product':
-      return NextResponse.rewrite(
-        new URL(`/product/${data.site.route.node.entityId}`, request.url),
-        response,
-      );
+// interface RoutesResponse {
+//   site: {
+//     route: {
+//       node: {
+//         __typename: string;
+//         entityId: string;
+//       } | null;
+//     };
+//   };
+// }
 
-    case 'Category':
-      return NextResponse.rewrite(
-        new URL(
-          `/category/${data.site.route.node.entityId}/${request.nextUrl.search}`,
-          request.url,
-        ),
-        response,
-      );
-  }
+// export async function middleware(request: NextRequest) {
+//   return rewriteUrlMiddleware(sessionMiddleware(request));
+// }
 
-  // These are paths we want to hide from customers as it's internal to our application.
-  // We rewrite to these paths using merchants custom URLs for each entity.
-  const OBFUSCATED_PATTERNS = [
-    new URLPattern('/product/:pid(\\d+)', request.nextUrl.origin),
-    new URLPattern('/category/:cid(\\d+)', request.nextUrl.origin),
-    new URLPattern('/brand/:bid(\\d+)', request.nextUrl.origin),
-  ];
+// // This is a POC middleware intended to redirect all page requests to the right NextJS route.
+// // TODO: Internationalization, trailing slash, etc.
+// async function rewriteUrlMiddleware(
+//   requestResponse: Promise<{ request: NextRequest; response: NextResponse }>,
+// ) {
+//   const { request, response } = await requestResponse;
 
-  // Prevent customers from directly navigating to a obfuscated URL.
-  if (OBFUSCATED_PATTERNS.find((pattern) => pattern.exec(request.url))) {
-    return NextResponse.redirect(new URL('/404', request.url));
-  }
+//   const { data } = await bigcommerceFetch<RoutesResponse>({
+//     query: /* GraphQL */ `
+//       query Routes($path: String!) {
+//         site {
+//           route(path: $path) {
+//             node {
+//               __typename
+//               ... on Product {
+//                 entityId
+//               }
+//               ... on Category {
+//                 entityId
+//               }
+//               ... on Brand {
+//                 entityId
+//               }
+//             }
+//           }
+//         }
+//       }
+//     `,
+//     variables: { path: request.nextUrl.pathname },
+//   });
 
-  return response;
-}
+//   switch (data.site.route.node?.__typename) {
+//     case 'Product':
+//       return NextResponse.rewrite(
+//         new URL(`/product/${data.site.route.node.entityId}`, request.url),
+//         response,
+//       );
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-    '/',
-  ],
-};
+//     case 'Category':
+//       return NextResponse.rewrite(
+//         new URL(
+//           `/category/${data.site.route.node.entityId}/${request.nextUrl.search}`,
+//           request.url,
+//         ),
+//         response,
+//       );
+//   }
+
+//   // These are paths we want to hide from customers as it's internal to our application.
+//   // We rewrite to these paths using merchants custom URLs for each entity.
+//   const OBFUSCATED_PATTERNS = [
+//     new URLPattern('/product/:pid(\\d+)', request.nextUrl.origin),
+//     new URLPattern('/category/:cid(\\d+)', request.nextUrl.origin),
+//     new URLPattern('/brand/:bid(\\d+)', request.nextUrl.origin),
+//   ];
+
+//   // Prevent customers from directly navigating to a obfuscated URL.
+//   if (OBFUSCATED_PATTERNS.find((pattern) => pattern.exec(request.url))) {
+//     return NextResponse.redirect(new URL('/404', request.url));
+//   }
+
+//   return response;
+// }
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - api (API routes)
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico (favicon file)
+//      */
+//     '/((?!api|_next/static|_next/image|favicon.ico).*)',
+//     '/',
+//   ],
+// };
--- middleware/session.ts
+++ middleware/session.ts
@@ -1,73 +1,74 @@
-import type { IronSession, IronSessionOptions } from 'iron-session';
-import { getIronSession } from 'iron-session/edge';
-import { NextRequest, NextResponse } from 'next/server';
+export {};
+// import type { IronSession, IronSessionOptions } from 'iron-session';
+// import { getIronSession } from 'iron-session/edge';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export const sessionOptions: IronSessionOptions = {
-  password: process.env.SESSION_PASSWORD ?? '',
-  cookieName: process.env.SESSION_ID ?? '',
-  ttl: parseInt(process.env.SESSION_TTL ?? '0', 10),
-  cookieOptions: {
-    secure: true, // this will eventually change based off if sitewide https is enabled or not
-  },
-};
+// export const sessionOptions: IronSessionOptions = {
+//   password: process.env.SESSION_PASSWORD ?? '',
+//   cookieName: process.env.SESSION_ID ?? '',
+//   ttl: parseInt(process.env.SESSION_TTL ?? '0', 10),
+//   cookieOptions: {
+//     secure: true, // this will eventually change based off if sitewide https is enabled or not
+//   },
+// };
 
-export interface Customer {
-  id: number;
-}
+// export interface Customer {
+//   id: number;
+// }
 
-// This is where we specify the typings of req.session.*
-declare module 'iron-session' {
-  interface IronSessionData {
-    customer?: Customer;
-    cartId?: string;
-    initTimestamp: number;
-    initRequestUrl: string;
-    recentlyViewedProducts: number[];
-    expiry: number;
-  }
-}
+// // This is where we specify the typings of req.session.*
+// declare module 'iron-session' {
+//   interface IronSessionData {
+//     customer?: Customer;
+//     cartId?: string;
+//     initTimestamp: number;
+//     initRequestUrl: string;
+//     recentlyViewedProducts: number[];
+//     expiry: number;
+//   }
+// }
 
-// TTL for session in milliseconds
-function ttlForSession(now: number): number {
-  return now + parseInt(process.env.SESSION_TTL ?? '0', 10) * 1000;
-}
+// // TTL for session in milliseconds
+// function ttlForSession(now: number): number {
+//   return now + parseInt(process.env.SESSION_TTL ?? '0', 10) * 1000;
+// }
 
-async function generateNewSession(
-  session: IronSession,
-  now: number,
-  requestUrl: string,
-): Promise<void> {
-  session.initTimestamp = Date.now();
-  session.initRequestUrl = requestUrl;
-  session.recentlyViewedProducts = [];
-  session.expiry = ttlForSession(now);
+// async function generateNewSession(
+//   session: IronSession,
+//   now: number,
+//   requestUrl: string,
+// ): Promise<void> {
+//   session.initTimestamp = Date.now();
+//   session.initRequestUrl = requestUrl;
+//   session.recentlyViewedProducts = [];
+//   session.expiry = ttlForSession(now);
 
-  await session.save();
-}
+//   await session.save();
+// }
 
-export async function sessionMiddleware(request: NextRequest) {
-  const now = new Date().getTime();
-  const response = NextResponse.next();
-  const session = await getIronSession(request, response, sessionOptions);
+// export async function sessionMiddleware(request: NextRequest) {
+//   const now = new Date().getTime();
+//   const response = NextResponse.next();
+//   const session = await getIronSession(request, response, sessionOptions);
 
-  const sessionCookie = request.cookies.get(process.env.SESSION_ID ?? '');
+//   const sessionCookie = request.cookies.get(process.env.SESSION_ID ?? '');
 
-  // initialize new session
-  if (sessionCookie === undefined) {
-    await generateNewSession(session, now, request.nextUrl.pathname);
-  } else if (session.expiry - now < 86400000 && session.expiry - now > 0) {
-    // if we have a valid session and expiry, and it expires in less than 1 day, update TTL
-    session.expiry = ttlForSession(now);
-    await session.save();
-  } else if (session.expiry - now <= 0) {
-    // If we have a valid session cookie, but the internal session ttl has expired, destroy it and generate a new one
-    session.destroy();
+//   // initialize new session
+//   if (sessionCookie === undefined) {
+//     await generateNewSession(session, now, request.nextUrl.pathname);
+//   } else if (session.expiry - now < 86400000 && session.expiry - now > 0) {
+//     // if we have a valid session and expiry, and it expires in less than 1 day, update TTL
+//     session.expiry = ttlForSession(now);
+//     await session.save();
+//   } else if (session.expiry - now <= 0) {
+//     // If we have a valid session cookie, but the internal session ttl has expired, destroy it and generate a new one
+//     session.destroy();
 
-    await generateNewSession(session, now, request.nextUrl.pathname);
-  }
+//     await generateNewSession(session, now, request.nextUrl.pathname);
+//   }
 
-  return {
-    request,
-    response,
-  };
-}
+//   return {
+//     request,
+//     response,
+//   };
+// }
