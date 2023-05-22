76a4f6d5b33838dc2e51fd21abe2e058f1fa8946
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2023-05-22 10:37:14 -0500

    fix: override store url on client

--- lib/client/client.ts
+++ lib/client/client.ts
@@ -1,6 +1,10 @@
 import { createClient } from './generated';
 
+const domain = process.env.BIGCOMMERCE_STOREFRONT_DOMAIN ?? '';
+const url = `${domain}/graphql`;
+
 export const client = createClient({
+  url,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${process.env.BIGCOMMERCE_STOREFRONT_TOKEN ?? ''}`,
