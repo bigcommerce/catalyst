105e200eff6e399a04e5c9d1891aee99f976b220
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2023-05-22 10:47:18 -0500

    chore: error out if env var is missing

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
+if (!process.env.BIGCOMMERCE_STOREFRONT_TOKEN) {
+  throw new Error('The environment variable BIGCOMMERCE_STOREFRONT_TOKEN is missing');
+}
+
+if (!process.env.BIGCOMMERCE_STOREFRONT_DOMAIN) {
+  throw new Error('The environment variable BIGCOMMERCE_STOREFRONT_DOMAIN is missing');
+}
+
+if (!process.env.BIGCOMMERCE_STORE_HASH) {
+  throw new Error('The environment variable BIGCOMMERCE_STORE_HASH is missing');
+}
+
+if (!process.env.BIGCOMMERCE_ACCESS_TOKEN) {
+  throw new Error('The environment variable BIGCOMMERCE_ACCESS_TOKEN is missing');
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
