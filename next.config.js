76df10c8ec7466876522ea40d103195e0913ceb2
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2023-05-24 16:56:52 -0500

    chore: remove env checks from next config

--- next.config.js
+++ next.config.js
@@ -1,23 +1,3 @@
-if (!process.env.BIGCOMMERCE_STOREFRONT_TOKEN) {
-  throw new Error('The environment variable BIGCOMMERCE_STOREFRONT_TOKEN is missing');
-}
-
-if (!process.env.BIGCOMMERCE_STOREFRONT_DOMAIN) {
-  throw new Error('The environment variable BIGCOMMERCE_STOREFRONT_DOMAIN is missing');
-}
-
-if (!process.env.BIGCOMMERCE_STORE_HASH) {
-  throw new Error('The environment variable BIGCOMMERCE_STORE_HASH is missing');
-}
-
-if (!process.env.BIGCOMMERCE_ACCESS_TOKEN) {
-  throw new Error('The environment variable BIGCOMMERCE_ACCESS_TOKEN is missing');
-}
-
-if (!process.env.MAKESWIFT_SITE_API_KEY) {
-  throw new Error('The environment variable MAKESWIFT_SITE_API_KEY is missing');
-}
-
 /** @type {import('next').NextConfig} */
 const withMakeswift = require('@makeswift/runtime/next/plugin')();
 
