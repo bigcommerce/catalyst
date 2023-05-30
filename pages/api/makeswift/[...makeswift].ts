2e8317086348aa7911ef978cb072a2148723378d
Andrew Reifman <andrewr23@gmail.com>
2023-05-30 14:42:26 -0400

    feat: added local font files and registered them with Makeswift

--- pages/api/makeswift/[...makeswift].ts
+++ pages/api/makeswift/[...makeswift].ts
@@ -3,4 +3,31 @@ import { MakeswiftApiHandler } from '@makeswift/runtime/next';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 export default MakeswiftApiHandler(process.env.MAKESWIFT_SITE_API_KEY!, {
   appOrigin: process.env.MAKESWIFT_APP_ORIGIN,
+  getFonts: () => [
+    {
+      family: 'Inter',
+      variants: [
+        {
+          weight: '400',
+          style: 'normal',
+          src: '/fonts/Inter-Regular.woff2',
+        },
+        {
+          weight: '600',
+          style: 'normal',
+          src: '/fonts/Inter-SemiBold.woff2',
+        },
+        {
+          weight: '700',
+          style: 'normal',
+          src: '/fonts/Inter-Bold.woff2',
+        },
+        {
+          weight: '900',
+          style: 'normal',
+          src: '/fonts/Inter-Black.woff2',
+        },
+      ],
+    },
+  ],
 });
