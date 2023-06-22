b32baf3ed7ed7eedb8d49cac9e692c3276035446
Jorge Moya <jorge.moya@bigcommerce.com>
2023-06-22 11:23:46 -0500

    Merge components into single file

--- packages/reactant/src/components/Header/Header.tsx
+++ packages/reactant/src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { Slot } from '@radix-ui/react-slot';
 import { ComponentPropsWithRef, forwardRef } from 'react';
 
 import { cs } from '../../utils/cs';
@@ -12,6 +13,63 @@ export const Header = forwardRef<HTMLDivElement, ComponentPropsWithRef<'header'>
       >
         {children}
       </header>
+    );
+  },
+);
+
+export const HeaderLogo = forwardRef<HTMLDivElement, ComponentPropsWithRef<'div'>>(
+  ({ className, children, ...props }, ref) => {
+    return (
+      <div
+        className={cs('flex-none items-center text-h4 font-black', className)}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+
+export const HeaderNav = forwardRef<HTMLDivElement, ComponentPropsWithRef<'nav'>>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <nav aria-label="Main menu" className={cs('flex', className)} ref={ref} {...props}>
+        {children}
+      </nav>
+    );
+  },
+);
+
+export const HeaderNavList = forwardRef<HTMLUListElement, ComponentPropsWithRef<'ul'>>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <ul className={cs('flex flex-row items-center gap-5', className)} ref={ref} {...props}>
+        {children}
+      </ul>
+    );
+  },
+);
+
+interface HeaderNavLinkProps extends ComponentPropsWithRef<'a'> {
+  asChild?: boolean;
+}
+
+export const HeaderNavLink = forwardRef<HTMLAnchorElement, HeaderNavLinkProps>(
+  ({ asChild = false, children, className, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'a';
+
+    return (
+      <Comp
+        className={cs(
+          'focus:ring-primary-blue/20 p-3 font-semibold hover:text-blue-primary focus:outline-none focus:ring-4',
+          className,
+        )}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </Comp>
     );
   },
 );
--- packages/reactant/src/components/Header/index.ts
+++ packages/reactant/src/components/Header/index.ts
@@ -1,5 +1,1 @@
 export * from './Header';
-export * from './HeaderLogo';
-export * from './HeaderNav';
-export * from './HeaderNavLink';
-export * from './HeaderNavList';
