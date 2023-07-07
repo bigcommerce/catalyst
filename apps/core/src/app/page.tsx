1696f9e6dc7c60ffd57ff441d8fcb749296536a2
Jorge Moya <jorge.moya@bigcommerce.com>
2023-07-06 21:04:05 -0500

    feat: add reactant ProductCard component

--- apps/core/src/app/page.tsx
+++ apps/core/src/app/page.tsx
@@ -1,5 +1,14 @@
 import { getBestSellingProducts, getFeaturedProducts } from '@bigcommerce/catalyst-client';
 import { cs } from '@bigcommerce/reactant/cs';
+import {
+  ProductCardBadge,
+  ProductCardImage,
+  ProductCardInfo,
+  ProductCardInfoBrandName,
+  ProductCardInfoPrice,
+  ProductCardInfoProductName,
+  ProductCard as ReactantProductCard,
+} from '@bigcommerce/reactant/ProductCard';
 import Image from 'next/image';
 import Link from 'next/link';
 import { ReactNode } from 'react';
@@ -12,32 +21,28 @@ interface ProductCardProps {
 }
 
 const ProductCard = ({ product }: ProductCardProps) => (
-  <div className="group relative mx-auto flex flex-col overflow-hidden" key={product.entityId}>
-    <div className="mx-auto h-[300px] w-[300px] group-hover:opacity-75">
+  <ReactantProductCard key={product.entityId}>
+    <ProductCardImage>
       <Image
         alt={product.defaultImage?.altText ?? product.name}
-        className="h-full w-full object-cover object-center sm:h-full sm:w-full"
+        className="h-full w-full object-contain object-center sm:h-full sm:w-full"
         height={300}
         src={product.defaultImage?.url ?? ''}
         width={300}
       />
-    </div>
-
-    <div className="flex w-[400px] flex-1 flex-col justify-end space-y-2 p-4 sm:w-auto">
-      {product.brand && <p className="text-base text-gray-500">{product.brand.name}</p>}
-
-      <h3 className="text-h5">
+    </ProductCardImage>
+    <ProductCardBadge>On sale</ProductCardBadge>
+    <ProductCardInfo>
+      {product.brand && <ProductCardInfoBrandName>{product.brand.name}</ProductCardInfoBrandName>}
+      <ProductCardInfoProductName>
         <Link href={`/product/${product.entityId}`}>
-          <span aria-hidden="true" className="absolute inset-0" />
+          <span aria-hidden="true" />
           {product.name}
         </Link>
-      </h3>
-
-      <div className="flex flex-col justify-end">
-        <p className="text-base">${product.price?.value}</p>
-      </div>
-    </div>
-  </div>
+      </ProductCardInfoProductName>
+      <ProductCardInfoPrice>${product.price?.value}</ProductCardInfoPrice>
+    </ProductCardInfo>
+  </ReactantProductCard>
 );
 
 const ProductList = ({ children }: { children: ReactNode }) => (
@@ -50,9 +55,7 @@ const ProductListName = ({ children }: { children: ReactNode }) => (
 
 const ProductListGrid = ({ children }: { children: ReactNode }) => (
   <div
-    className={cs(
-      'grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4',
-    )}
+    className={cs('grid grid-cols-2 gap-y-8 gap-x-6 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4')}
   >
     {children}
   </div>
