68b9cf72c3e1f359a5c8e89028bc6af2c6927d20
Jorge Moya <jorge.moya@bigcommerce.com>
2023-07-11 09:40:25 -0500

    feat(core): add catch all page (#96)

---
+++ apps/core/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+const NotFound = () => (
+  <div className="flex h-96 max-w-[700px] flex-col justify-center gap-10 p-10 lg:p-12">
+    <h2 className="text-h2">404</h2>
+    <h3 className="text-h3"> The page you were looking for doesn't exist.</h3>
+  </div>
+);
+
+export default NotFound;
