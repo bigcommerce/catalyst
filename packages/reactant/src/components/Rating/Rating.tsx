2b7f9d6a1d36b92d2f18b5909dfa50cb8815e467
yurytut1993 <yurii.zusik@bigcommerce.com>
2023-07-11 10:36:48 +0300

    feat(reactant): add rating component

---
+++ packages/reactant/src/components/Rating/Rating.tsx
@@ -0,0 +1,71 @@
+import { LucideIcon } from 'lucide-react';
+import {
+  ComponentPropsWithoutRef,
+  ComponentPropsWithRef,
+  ElementRef,
+  FC,
+  forwardRef,
+  ReactElement,
+} from 'react';
+
+import { cs } from '../../utils/cs';
+
+import { StarEmptyIcon } from './StarIcons/StarEmpty';
+import { StarFilledIcon } from './StarIcons/StarFilled';
+import { StarHalfIcon } from './StarIcons/StarHalf';
+
+const MAX_RATING = 5;
+const roundHalf = (num: number) => {
+  return Math.round(num * 2) / 2;
+};
+
+type StarIconType = FC<ComponentPropsWithoutRef<'svg'>> | LucideIcon;
+
+interface RatingProps extends ComponentPropsWithRef<'span'> {
+  color?: string;
+  starEmptyIcon?: StarIconType;
+  starFilledIcon?: StarIconType;
+  starHalfIcon?: StarIconType;
+  size?: number;
+  strokeColor?: string;
+  value: number;
+}
+
+export const Rating = forwardRef<ElementRef<'span'>, RatingProps>(
+  (
+    {
+      className,
+      color = 'fill-blue-primary',
+      starFilledIcon,
+      starHalfIcon,
+      starEmptyIcon,
+      size = 24,
+      value,
+      ...props
+    },
+    ref,
+  ) => {
+    const stars: ReactElement[] = [];
+    const rating = roundHalf(value);
+
+    const StarHalf = starHalfIcon || StarHalfIcon;
+    const StarEmpty = starEmptyIcon || StarEmptyIcon;
+    const StarFilled = starFilledIcon || StarFilledIcon;
+
+    for (let i = 1; i <= MAX_RATING; i += 1) {
+      if (rating - i >= 0) {
+        stars.push(<StarFilled className={cs(`${color}`)} height={size} key={i} width={size} />);
+      } else if (rating - i > -1) {
+        stars.push(<StarHalf className={cs(`${color}`)} height={size} key={i} width={size} />);
+      } else {
+        stars.push(<StarEmpty className={cs(`${color}`)} height={size} key={i} width={size} />);
+      }
+    }
+
+    return (
+      <span className={cs('inline-flex', className)} ref={ref} role="img" {...props}>
+        {stars}
+      </span>
+    );
+  },
+);
