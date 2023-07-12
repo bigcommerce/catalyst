1e398f8f5c0a9f57c549df439dad67b333024124
Jorge Moya <jorge.moya@bigcommerce.com>
2023-07-12 14:36:23 -0500

    fix: disabled color

--- packages/reactant/src/components/Input/Input.tsx
+++ packages/reactant/src/components/Input/Input.tsx
@@ -5,7 +5,7 @@ import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react';
 import { cs } from '../../utils/cs';
 
 const inputVariants = cva(
-  'focus:ring-primary-blue/20 w-full border-2 border-gray-200 py-2.5 px-4 text-base placeholder:text-gray-500 focus:border-blue-primary focus:outline-none focus:ring-4 hover:border-blue-primary focus:outline-none disabled:bg-gray-100 disabled:hover:border-gray-200',
+  'peer focus:ring-primary-blue/20 w-full border-2 border-gray-200 py-2.5 px-4 text-base placeholder:text-gray-500 focus:border-blue-primary focus:outline-none focus:ring-4 hover:border-blue-primary focus:outline-none disabled:bg-gray-100 disabled:hover:border-gray-200',
   {
     variants: {
       state: {
@@ -27,10 +27,14 @@ export const Input = forwardRef<ElementRef<'div'>, InputProps>(
       <input className={cs(inputVariants({ state, className }))} type={type} {...props} />
       <span
         aria-hidden="true"
-        className={cs('pointer-events-none absolute top-0 right-4 flex h-full items-center')}
+        className={cs(
+          'pointer-events-none absolute top-0 right-4 flex h-full items-center peer-disabled:text-gray-200',
+          state === 'success' && 'text-green',
+          state === 'error' && 'text-red',
+        )}
       >
-        {state === 'success' && <Check className=" text-green" />}
-        {state === 'error' && <AlertCircle className=" text-red" />}
+        {state === 'success' && <Check />}
+        {state === 'error' && <AlertCircle />}
       </span>
     </div>
   ),
