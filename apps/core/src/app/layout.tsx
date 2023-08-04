5734d373f3f403d461c968831545589e18a34cff
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-08-04 15:10:03 -0400

    feat(core): use main element for content

--- apps/core/src/app/layout.tsx
+++ apps/core/src/app/layout.tsx
@@ -22,9 +22,9 @@ export default function RootLayout({ children }: PropsWithChildren) {
     <html className={`${inter.variable} font-sans`} lang="en">
       <body className="flex h-screen flex-col">
         <Header />
-        <div className="flex-1 px-6 sm:px-10 lg:px-12 2xl:container 2xl:mx-auto 2xl:px-0">
+        <main className="flex-1 px-6 sm:px-10 lg:px-12 2xl:container 2xl:mx-auto 2xl:px-0">
           {children}
-        </div>
+        </main>
         <Footer />
       </body>
     </html>
