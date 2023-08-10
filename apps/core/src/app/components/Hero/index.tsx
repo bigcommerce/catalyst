ccbe03b2511c46ed0cb6edf450fec894c32f70f5
Matt Volk <matt.volk@bigcommerce.com>
2023-08-10 16:13:38 -0500

    feat(core): CATALYST-20 add carousel component to hero

--- apps/core/src/app/components/Hero/index.tsx
+++ apps/core/src/app/components/Hero/index.tsx
@@ -1,11 +1,20 @@
 import { Button } from '@bigcommerce/reactant/Button';
-import React from 'react';
+import { Carousel, CarouselItem } from '@bigcommerce/reactant/Carousel';
+import Image from 'next/image';
 
-export const Hero = () => {
-  return (
-    <div className="flex h-[640px] w-full items-center bg-gray-100 p-12 md:h-[508px] lg:h-[600px]">
-      <div>
-        <h1 className="text-h1">New collection</h1>
+export const Hero = () => (
+  <Carousel interval={10_000}>
+    <CarouselItem className="duration-700">
+      <div className="absolute -z-10 h-full w-full">
+        <Image
+          alt="an assortment of brandless products against a blank background"
+          layout="fill"
+          objectFit="cover"
+          src="/carousel-bg-01.jpg"
+        />
+      </div>
+      <div className="flex h-full flex-col justify-center p-12">
+        <h1 className="text-h1">25% Off Sale</h1>
         <p className="max-w-[548px] pt-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Ut enim ad minim veniam.
@@ -14,6 +23,26 @@ export const Hero = () => {
           <a href="/#">Shop now</a>
         </Button>
       </div>
-    </div>
-  );
-};
+    </CarouselItem>
+    <CarouselItem className="flex flex-col justify-center bg-gray-100 p-12 duration-700">
+      <h1 className="text-h1">Great Deals</h1>
+      <p className="max-w-[548px] pt-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam.
+      </p>
+      <Button asChild className="mt-10 w-[141px]">
+        <a href="/#">Shop now</a>
+      </Button>
+    </CarouselItem>
+    <CarouselItem className="flex flex-col justify-center bg-gray-100 p-12 duration-700">
+      <h1 className="text-h1">Low Prices</h1>
+      <p className="max-w-[548px] pt-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam.
+      </p>
+      <Button asChild className="mt-10 w-[141px]">
+        <a href="/#">Shop now</a>
+      </Button>
+    </CarouselItem>
+  </Carousel>
+);
