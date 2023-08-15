34dd591247f775a72d4d8ee3f54786275a0e774c
Jorge Moya <jorge.moya@bigcommerce.com>
2023-08-15 18:35:57 -0500

    fix: use opacity for disabled

--- packages/reactant/src/components/Swatch/Swatch.tsx
+++ packages/reactant/src/components/Swatch/Swatch.tsx
@@ -26,40 +26,37 @@ interface SwatchVariantProps extends ComponentPropsWithRef<typeof RadioGroupPrim
 export const SwatchItem = forwardRef<
   ElementRef<typeof RadioGroupPrimitive.Item>,
   SwatchVariantProps
->(({ children, className, disabled, variantColor, ...props }, ref) => {
-  const style = !disabled
-    ? { backgroundColor: variantColor, backgroundImage: `url(${variantColor ?? ''})` }
-    : {};
-
-  return (
-    <RadioGroupPrimitive.Item
-      className={cs(
-        'focus:ring-primary-blue/20 group h-12 w-12 border-2 bg-white p-1 hover:border-blue-primary focus:outline-none focus:ring-4 disabled:border-gray-100 disabled:hover:border-gray-100',
-        'data-[state=checked]:border-blue-primary',
-        className,
-      )}
-      disabled={disabled}
-      ref={ref}
-      {...props}
-    >
-      {variantColor ? (
-        <span className={cs('block h-9 w-9 group-disabled:bg-gray-200')} style={style} />
-      ) : (
+>(({ children, className, disabled, variantColor, ...props }, ref) => (
+  <RadioGroupPrimitive.Item
+    className={cs(
+      'focus:ring-primary-blue/20 group h-12 w-12 border-2 bg-white p-1 hover:border-blue-primary focus:outline-none focus:ring-4 disabled:border-gray-100 disabled:hover:border-gray-100',
+      'data-[state=checked]:border-blue-primary',
+      className,
+    )}
+    disabled={disabled}
+    ref={ref}
+    {...props}
+  >
+    {variantColor ? (
+      <span
+        className={cs('block h-9 w-9 group-disabled:bg-gray-200 group-disabled:opacity-30')}
+        style={{ backgroundColor: variantColor, backgroundImage: `url(${variantColor})` }}
+      />
+    ) : (
+      <span
+        className={cs(
+          'relative block h-9 w-9 overflow-hidden border border-gray-200 group-disabled:border-gray-100 ',
+          className,
+        )}
+        ref={ref}
+        {...props}
+      >
         <span
           className={cs(
-            'relative block h-9 w-9 overflow-hidden border border-gray-200 group-disabled:border-gray-100',
-            className,
+            'absolute -left-px -top-[2px] w-[51px] origin-top-left rotate-45 border-t-2 border-red-100 group-disabled:opacity-30',
           )}
-          ref={ref}
-          {...props}
-        >
-          <span
-            className={cs(
-              'absolute -left-px -top-[2px] w-[51px] origin-top-left rotate-45 border-t-2 border-red-100 group-disabled:border-gray-100',
-            )}
-          />
-        </span>
-      )}
-    </RadioGroupPrimitive.Item>
-  );
-});
+        />
+      </span>
+    )}
+  </RadioGroupPrimitive.Item>
+));
