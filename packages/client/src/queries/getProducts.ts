a45020117576487ecf57fa36a5d2403700b868ca
Matt <matt.volk@bigcommerce.com>
2023-08-17 16:36:31 -0500

    feat(client): CATALYST-35 add getProducts query (#153)

---
+++ packages/client/src/queries/getProducts.ts
@@ -0,0 +1,82 @@
+import { BigCommerceResponse, FetcherInput } from '../fetcher';
+import { generateQueryOp, QueryGenqlSelection, QueryResult } from '../generated';
+
+export interface GetProductsArguments {
+  productIds: number[];
+  images: { width: number; height?: number };
+}
+
+export const getProducts = async <T>(
+  customFetch: <U>(data: FetcherInput) => Promise<BigCommerceResponse<U>>,
+  args: GetProductsArguments,
+  config: T = {} as T,
+) => {
+  const { productIds, images } = args;
+
+  const query = {
+    site: {
+      products: {
+        __args: {
+          entityIds: productIds,
+        },
+        edges: {
+          node: {
+            brand: {
+              name: true,
+            },
+            entityId: true,
+            images: {
+              edges: {
+                node: {
+                  url: {
+                    __args: {
+                      width: images.width,
+                      height: images.height,
+                    },
+                  },
+                  altText: true,
+                  isDefault: true,
+                },
+              },
+            },
+            inventory: {
+              aggregated: {
+                availableToSell: true,
+              },
+            },
+            name: true,
+            plainTextDescription: true,
+            reviewSummary: {
+              summationOfRatings: true,
+            },
+            prices: {
+              basePrice: {
+                currencyCode: true,
+                value: true,
+              },
+              price: {
+                currencyCode: true,
+                value: true,
+              },
+              retailPrice: {
+                currencyCode: true,
+                value: true,
+              },
+              salePrice: {
+                currencyCode: true,
+                value: true,
+              },
+            },
+          },
+        },
+      },
+    },
+  } satisfies QueryGenqlSelection;
+
+  const queryOp = generateQueryOp(query);
+
+  return customFetch<QueryResult<typeof query>>({
+    ...queryOp,
+    ...config,
+  });
+};
