e771e2a65dd0834c569fd8c828191e0149a341d2
Matt Volk <matt.volk@bigcommerce.com>
2023-08-17 15:44:34 -0500

    feat(core): CATALYST-35 distinct handling for not found and empty states

--- apps/core/app/(default)/compare/[...productIds]/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Compare({ params }: { params: { productIds: string[] } }) {
-  const productIds = params.productIds;
-
-  return <h1 className="text-h2">Comparing {productIds.length} products</h1>;
-}
