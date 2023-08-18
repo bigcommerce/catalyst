c8d37ead1adae42b69b4a84df7e5b7c81131f594
Jorge Moya <jorge.moya@bigcommerce.com>
2023-08-18 09:20:16 -0500

    feat(reactant): rename prop state to variant on Input (#150)

--- packages/reactant/src/components/Input/Input.tsx
+++ packages/reactant/src/components/Input/Input.tsx
@@ -8,7 +8,7 @@ const inputVariants = cva(
   'peer focus:ring-primary-blue/20 w-full border-2 border-gray-200 py-2.5 px-4 text-base placeholder:text-gray-500 focus:border-blue-primary focus:outline-none focus:ring-4 hover:border-blue-primary disabled:bg-gray-100 disabled:hover:border-gray-200',
   {
     variants: {
-      state: {
+      variant: {
         success:
           'pr-12 border-green-100 focus:border-green-100 focus:ring-green-100/20 disabled:border-gray-200 hover:border-green-200',
         error:
@@ -19,23 +19,23 @@ const inputVariants = cva(
 );
 
 export interface InputProps extends ComponentPropsWithRef<'input'> {
-  state?: 'success' | 'error';
+  variant?: 'success' | 'error';
 }
 
 export const Input = forwardRef<ElementRef<'div'>, InputProps>(
-  ({ className, state, children, type = 'text', ...props }, ref) => (
+  ({ className, variant, children, type = 'text', ...props }, ref) => (
     <div className={cs('relative')} ref={ref}>
-      <input className={cs(inputVariants({ state, className }))} type={type} {...props} />
+      <input className={cs(inputVariants({ variant, className }))} type={type} {...props} />
       <span
         aria-hidden="true"
         className={cs(
           'pointer-events-none absolute top-0 right-4 flex h-full items-center peer-disabled:text-gray-200',
-          state === 'success' && 'text-green-100',
-          state === 'error' && 'text-red-100',
+          variant === 'success' && 'text-green-100',
+          variant === 'error' && 'text-red-100',
         )}
       >
-        {state === 'success' && <Check />}
-        {state === 'error' && <AlertCircle />}
+        {variant === 'success' && <Check />}
+        {variant === 'error' && <AlertCircle />}
       </span>
     </div>
   ),
