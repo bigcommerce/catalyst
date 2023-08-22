f8444680ca5bd677ff10bf32e336a117735c3117
Alexander Saiannyi <alexander.saiannyi@bigcommerce.com>
2023-08-22 09:43:37 +0200

    feat(core): CATALYST-33 add 404 page

---
+++ apps/core/app/not-found.tsx
@@ -0,0 +1,38 @@
+import { cs } from '@bigcommerce/reactant/cs';
+import { Message } from '@bigcommerce/reactant/Message';
+
+import { SearchForm } from 'components/SearchForm';
+import client from '~/client';
+import { Footer } from '~/components/Footer/Footer';
+import { Header } from '~/components/Header';
+import { ProductCard } from '~/components/ProductCard';
+
+const NotFound = async () => {
+  const featuredProducts = await client.getFeaturedProducts();
+
+  return (
+    <>
+      <Header />
+      <main className="mx-auto mb-10 flex max-w-[835px] flex-col justify-center gap-10 px-6 sm:px-10 lg:px-0 2xl:px-0">
+        <Message className="flex-col gap-8 px-0 py-16">
+          <h2 className="text-h2">We couldn't find that page!</h2>
+          <p className="text-lg">
+            It looks like the page you requested has moved or no longer exists.
+          </p>
+        </Message>
+        <SearchForm />
+        <section className={cs('w-full')}>
+          <h3 className={cs('mb-10 text-center text-h3 sm:text-left')}>Featured Products</h3>
+          <div className={cs('grid grid-cols-2 gap-x-8 gap-y-8 md:grid-cols-4')}>
+            {featuredProducts.map((product) => (
+              <ProductCard key={product.entityId} product={product} />
+            ))}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default NotFound;
---
+++ apps/core/components/SearchForm/index.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { Button } from '@bigcommerce/reactant/Button';
+import { Field, FieldControl, Form, FormSubmit } from '@bigcommerce/reactant/Form';
+import { Input } from '@bigcommerce/reactant/Input';
+import { Loader2 as Spinner } from 'lucide-react';
+import { experimental_useFormStatus as useFormStatus } from 'react-dom';
+
+interface Props {
+  initialTerm?: string;
+}
+
+export const SearchForm = ({ initialTerm = '' }: Props) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <div className="flex flex-col gap-8">
+      <h3 className="text-h3">Search our store</h3>
+      <Form action="/search" className="flex" method="get">
+        <Field className="mr-4 w-full" name="search">
+          <FieldControl asChild>
+            <Input
+              className="grey-200 border-2 px-8 py-3 font-semibold"
+              defaultValue={initialTerm}
+              name="term"
+              placeholder="Placeholder..."
+            />
+          </FieldControl>
+        </Field>
+        <FormSubmit asChild>
+          <Button className="w-auto" disabled={pending} type="submit">
+            {pending ? (
+              <>
+                <Spinner aria-hidden="true" className="animate-spin" />
+                <span className="sr-only">Searching...</span>
+              </>
+            ) : (
+              <span>Search</span>
+            )}
+          </Button>
+        </FormSubmit>
+      </Form>
+    </div>
+  );
+};
