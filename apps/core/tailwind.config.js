23a7a305f46607a88c70d1f5c01cff893ae2b636
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2023-08-23 11:06:14 -0500

    fix(core): CATALYST-60 add components dir to tailwind config (#173)

--- apps/core/tailwind.config.js
+++ apps/core/tailwind.config.js
@@ -3,7 +3,11 @@ const reactantPreset = require('@bigcommerce/reactant/tailwind-config');
 /** @type {import('tailwindcss').Config} */
 const config = {
   presets: [reactantPreset],
-  content: ['./app/**/*.{ts,tsx}', './node_modules/@bigcommerce/reactant/dist/**/*.{js,mjs}'],
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './node_modules/@bigcommerce/reactant/dist/**/*.{js,mjs}',
+  ],
   plugins: [],
 };
 
