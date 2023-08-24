0a56801ea86375cf315e2c81ca10100877e21ae1
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-08-24 15:56:28 -0400

    feat(core): CATALYST-30 hook up facets

--- apps/core/app/(default)/category/[slug]/Facets.tsx
+++ apps/core/app/(default)/category/[slug]/Facets.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { RatingSearchFilterItem } from '@bigcommerce/catalyst-client';
 import {
   Accordion,
   AccordionContent,
@@ -10,6 +13,9 @@ import { cs } from '@bigcommerce/reactant/cs';
 import { Input } from '@bigcommerce/reactant/Input';
 import { Label } from '@bigcommerce/reactant/Label';
 import { Rating } from '@bigcommerce/reactant/Rating';
+import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { FormEvent, useRef } from 'react';
 
 import { fetchCategory } from './fetchCategory';
 
@@ -30,206 +36,278 @@ const ProductCount = ({ shouldDisplay, count }: ProductCountProps) => {
   );
 };
 
+const sortRatingsDescending = (a: RatingSearchFilterItem, b: RatingSearchFilterItem) => {
+  const valueA = parseInt(a.value, 10);
+  const valueB = parseInt(b.value, 10);
+
+  if (valueA > valueB) {
+    return -1;
+  }
+
+  if (valueA < valueB) {
+    return 1;
+  }
+
+  return 0;
+};
+
 interface Props {
   facets: Awaited<ReturnType<typeof fetchCategory>>['facets'];
 }
 
 export const Facets = ({ facets }: Props) => {
+  const ref = useRef<HTMLFormElement>(null);
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const defaultOpenFacets = facets.items
     .filter((facet) => !facet.isCollapsedByDefault)
     .map((facet) => facet.name);
 
+  const submitForm = () => {
+    ref.current?.requestSubmit();
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const sortParam = searchParams.get('sort');
+    const filteredSearchParams = Array.from(formData.entries())
+      .filter((entry): entry is [string, string] => {
+        return entry instanceof File === false;
+      })
+      .filter(([, value]) => value !== '');
+
+    const newSearchParams = new URLSearchParams(filteredSearchParams);
+
+    // We want to keep the sort param if it exists
+    if (sortParam) {
+      newSearchParams.append('sort', sortParam);
+    }
+
+    router.push(`${pathname}?${newSearchParams.toString()}`);
+  };
+
   return (
     <Accordion defaultValue={defaultOpenFacets} type="multiple">
-      {facets.items.map((facet) => {
-        if (facet.__typename === 'BrandSearchFilter') {
-          return (
-            <AccordionItem key={facet.__typename} value={facet.name}>
-              <AccordionTrigger>
-                <h3>{facet.name}</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                {facet.brands.map((brand) => (
-                  <div className="flex max-w-sm items-center py-2 pl-1" key={brand.entityId}>
-                    <Checkbox
-                      defaultChecked={brand.isSelected}
-                      id={`${brand.name}-${brand.entityId}`}
-                      name="brand"
-                      value={brand.entityId}
-                    />
-                    <Label
-                      className="cursor-pointer pl-3"
-                      htmlFor={`${brand.name}-${brand.entityId}`}
-                    >
-                      {brand.name}
-                      <ProductCount
-                        count={brand.productCount}
-                        shouldDisplay={facet.displayProductCount}
-                      />
-                    </Label>
-                  </div>
-                ))}
-              </AccordionContent>
-            </AccordionItem>
-          );
-        }
-
-        if (facet.__typename === 'ProductAttributeSearchFilter') {
-          return (
-            <AccordionItem key={facet.__typename} value={facet.name}>
-              <AccordionTrigger>
-                <h3>{facet.name}</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                {facet.attributes.map((attribute) => (
-                  <div
-                    className="flex max-w-sm items-center py-2 pl-1"
-                    key={`${facet.filterName}-${attribute.value}`}
-                  >
-                    <Checkbox
-                      defaultChecked={attribute.isSelected}
-                      id={`${facet.filterName}-${attribute.value}`}
-                      name={facet.filterName}
-                      value={attribute.value}
-                    />
-                    <Label
-                      className="cursor-pointer pl-3"
-                      htmlFor={`${facet.filterName}-${attribute.value}`}
-                    >
-                      {attribute.value}
-                      <ProductCount
-                        count={attribute.productCount}
-                        shouldDisplay={facet.displayProductCount}
-                      />
-                    </Label>
-                  </div>
-                ))}
-              </AccordionContent>
-            </AccordionItem>
-          );
-        }
-
-        if (facet.__typename === 'RatingSearchFilter') {
-          return (
-            <AccordionItem key={facet.__typename} value={facet.name}>
-              <AccordionTrigger>
-                <h3>{facet.name}</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                {facet.ratings
-                  .reverse()
-                  .filter((rating) => rating.value !== '5')
-                  .map((rating) => (
-                    <div className="flex flex-row flex-nowrap py-2" key={rating.value}>
+      <form onSubmit={handleSubmit} ref={ref}>
+        {facets.items.map((facet) => {
+          if (facet.__typename === 'BrandSearchFilter') {
+            return (
+              <AccordionItem key={facet.__typename} value={facet.name}>
+                <AccordionTrigger>
+                  <h3>{facet.name}</h3>
+                </AccordionTrigger>
+                <AccordionContent>
+                  {facet.brands.map((brand) => {
+                    return (
                       <div
-                        className={cs('flex flex-row flex-nowrap', {
-                          'text-blue-primary': rating.isSelected,
-                        })}
+                        className="flex max-w-sm items-center py-2 pl-1"
+                        key={`${brand.entityId}-${brand.isSelected.toString()}`}
                       >
-                        <Rating value={parseInt(rating.value, 10)} />
+                        <Checkbox
+                          defaultChecked={brand.isSelected}
+                          id={`${brand.name}-${brand.entityId}`}
+                          name="brand"
+                          onCheckedChange={submitForm}
+                          value={brand.entityId}
+                        />
+                        <Label
+                          className="cursor-pointer pl-3"
+                          htmlFor={`${brand.name}-${brand.entityId}`}
+                        >
+                          {brand.name}
+                          <ProductCount
+                            count={brand.productCount}
+                            shouldDisplay={facet.displayProductCount}
+                          />
+                        </Label>
                       </div>
-                      <span className="pl-2">
-                        {/* TODO: singular vs. plural */}
-                        <span className="sr-only">{rating.value} stars</span> & up
-                      </span>
-                      <ProductCount count={rating.productCount} shouldDisplay={true} />
+                    );
+                  })}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          }
+
+          if (facet.__typename === 'ProductAttributeSearchFilter') {
+            return (
+              <AccordionItem key={`${facet.__typename}-${facet.filterName}`} value={facet.name}>
+                <AccordionTrigger>
+                  <h3>{facet.name}</h3>
+                </AccordionTrigger>
+                <AccordionContent>
+                  {facet.attributes.map((attribute) => (
+                    <div
+                      className="flex max-w-sm items-center py-2 pl-1"
+                      key={`${facet.filterName}-${
+                        attribute.value
+                      }-${attribute.isSelected.toString()}`}
+                    >
+                      <Checkbox
+                        defaultChecked={attribute.isSelected}
+                        id={`${facet.filterName}-${attribute.value}`}
+                        name={facet.filterName}
+                        onCheckedChange={submitForm}
+                        value={attribute.value}
+                      />
+                      <Label
+                        className="cursor-pointer pl-3"
+                        htmlFor={`${facet.filterName}-${attribute.value}`}
+                      >
+                        {attribute.value}
+                        <ProductCount
+                          count={attribute.productCount}
+                          shouldDisplay={facet.displayProductCount}
+                        />
+                      </Label>
                     </div>
                   ))}
-              </AccordionContent>
-            </AccordionItem>
-          );
-        }
+                </AccordionContent>
+              </AccordionItem>
+            );
+          }
 
-        if (facet.__typename === 'PriceSearchFilter') {
-          return (
-            <AccordionItem key={facet.__typename} value={facet.name}>
-              <AccordionTrigger>
-                <h3>{facet.name}</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                <div className="grid grid-cols-2 gap-4 p-1">
-                  <Input
-                    aria-label="Minimum pricing"
-                    defaultValue={facet.selected?.minPrice ?? ''}
-                    placeholder="$ min"
-                  />
-                  <Input
-                    aria-label="Maximum pricing"
-                    defaultValue={facet.selected?.maxPrice ?? ''}
-                    placeholder="$ max"
-                  />
-                  <Button className="col-span-2">Update price</Button>
-                </div>
-              </AccordionContent>
-            </AccordionItem>
-          );
-        }
+          if (facet.__typename === 'RatingSearchFilter') {
+            return (
+              <AccordionItem key={facet.__typename} value={facet.name}>
+                <AccordionTrigger>
+                  <h3>{facet.name}</h3>
+                </AccordionTrigger>
+                <AccordionContent>
+                  {facet.ratings
+                    .filter((rating) => rating.value !== '5')
+                    .sort(sortRatingsDescending)
+                    .map((rating) => {
+                      const search = new URLSearchParams(searchParams);
 
-        if (facet.__typename === 'OtherSearchFilter') {
-          return (
-            <AccordionItem key={facet.__typename} value={facet.name}>
-              <AccordionTrigger>
-                <h3>{facet.name}</h3>
-              </AccordionTrigger>
-              <AccordionContent>
-                {facet.freeShipping && (
-                  <div className="flex max-w-sm items-center py-2 pl-1">
-                    <Checkbox
-                      defaultChecked={facet.freeShipping.isSelected}
-                      id="shipping-free_shipping"
-                      name="shipping"
-                      value="free_shipping"
-                    />
-                    <Label className="cursor-pointer pl-3" htmlFor="shipping-free_shipping">
-                      Free shipping
-                      <ProductCount
-                        count={facet.freeShipping.productCount}
-                        shouldDisplay={facet.displayProductCount}
-                      />
-                    </Label>
-                  </div>
-                )}
-                {facet.isFeatured && (
-                  <div className="flex max-w-sm items-center py-2 pl-1">
-                    <Checkbox
-                      defaultChecked={facet.isFeatured.isSelected}
-                      id="isFeatured"
-                      name="isFeatured"
-                      value="1"
-                    />
-                    <Label className="cursor-pointer pl-3" htmlFor="isFeatured">
-                      Is featured
-                      <ProductCount
-                        count={facet.isFeatured.productCount}
-                        shouldDisplay={facet.displayProductCount}
-                      />
-                    </Label>
-                  </div>
-                )}
-                {facet.isInStock && (
-                  <div className="flex max-w-sm items-center py-2 pl-1">
-                    <Checkbox
-                      defaultChecked={facet.isInStock.isSelected}
-                      id="stock-in_stock"
-                      name="stock"
-                      value="in_stock"
-                    />
-                    <Label className="cursor-pointer pl-3" htmlFor="stock-in_stock">
-                      In stock
-                      <ProductCount
-                        count={facet.isInStock.productCount}
-                        shouldDisplay={facet.displayProductCount}
-                      />
-                    </Label>
-                  </div>
-                )}
-              </AccordionContent>
-            </AccordionItem>
-          );
-        }
+                      search.set('minRating', rating.value);
 
-        return null;
-      })}
+                      return (
+                        <Link
+                          className="flex flex-row flex-nowrap py-2"
+                          href={{ search: `?${search.toString()}` }}
+                          key={`${facet.name}-${rating.value}-${rating.isSelected.toString()}`}
+                        >
+                          <div
+                            className={cs('flex flex-row flex-nowrap', {
+                              'text-blue-primary': rating.isSelected,
+                            })}
+                          >
+                            <Rating value={parseInt(rating.value, 10)} />
+                          </div>
+                          <span className="pl-2">
+                            {/* TODO: singular vs. plural */}
+                            <span className="sr-only">{rating.value} stars</span> & up
+                          </span>
+                          <ProductCount count={rating.productCount} shouldDisplay={true} />
+                        </Link>
+                      );
+                    })}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          }
+
+          if (facet.__typename === 'PriceSearchFilter') {
+            return (
+              <AccordionItem key={facet.__typename} value={facet.name}>
+                <AccordionTrigger>
+                  <h3>{facet.name}</h3>
+                </AccordionTrigger>
+                <AccordionContent>
+                  <div className="grid grid-cols-2 gap-4 p-1">
+                    <Input
+                      aria-label="Minimum pricing"
+                      defaultValue={facet.selected?.minPrice ?? ''}
+                      name="minPrice"
+                      placeholder="$ min"
+                    />
+                    <Input
+                      aria-label="Maximum pricing"
+                      defaultValue={facet.selected?.maxPrice ?? ''}
+                      name="maxPrice"
+                      placeholder="$ max"
+                    />
+                    <Button className="col-span-2" type="submit" variant="secondary">
+                      Update price
+                    </Button>
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            );
+          }
+
+          if (facet.__typename === 'OtherSearchFilter') {
+            return (
+              <AccordionItem key={facet.__typename} value={facet.name}>
+                <AccordionTrigger>
+                  <h3>{facet.name}</h3>
+                </AccordionTrigger>
+                <AccordionContent>
+                  {facet.freeShipping && (
+                    <div className="flex max-w-sm items-center py-2 pl-1">
+                      <Checkbox
+                        defaultChecked={facet.freeShipping.isSelected}
+                        id="shipping-free_shipping"
+                        name="shipping"
+                        onCheckedChange={submitForm}
+                        value="free_shipping"
+                      />
+                      <Label className="cursor-pointer pl-3" htmlFor="shipping-free_shipping">
+                        Free shipping
+                        <ProductCount
+                          count={facet.freeShipping.productCount}
+                          shouldDisplay={facet.displayProductCount}
+                        />
+                      </Label>
+                    </div>
+                  )}
+                  {facet.isFeatured && (
+                    <div className="flex max-w-sm items-center py-2 pl-1">
+                      <Checkbox
+                        defaultChecked={facet.isFeatured.isSelected}
+                        id="isFeatured"
+                        name="isFeatured"
+                        onCheckedChange={submitForm}
+                      />
+                      <Label className="cursor-pointer pl-3" htmlFor="isFeatured">
+                        Is featured
+                        <ProductCount
+                          count={facet.isFeatured.productCount}
+                          shouldDisplay={facet.displayProductCount}
+                        />
+                      </Label>
+                    </div>
+                  )}
+                  {facet.isInStock && (
+                    <div className="flex max-w-sm items-center py-2 pl-1">
+                      <Checkbox
+                        defaultChecked={facet.isInStock.isSelected}
+                        id="stock-in_stock"
+                        name="stock"
+                        onCheckedChange={submitForm}
+                        value="in_stock"
+                      />
+                      <Label className="cursor-pointer pl-3" htmlFor="stock-in_stock">
+                        In stock
+                        <ProductCount
+                          count={facet.isInStock.productCount}
+                          shouldDisplay={facet.displayProductCount}
+                        />
+                      </Label>
+                    </div>
+                  )}
+                </AccordionContent>
+              </AccordionItem>
+            );
+          }
+
+          return null;
+        })}
+      </form>
     </Accordion>
   );
 };
