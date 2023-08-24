9c997e29f0a70b5aa446b40f7f059394b9b06d13
yurytut1993 <yurii.zusik@bigcommerce.com>
2023-08-24 10:41:17 +0300

    feat(core): CATALYST-34 500 page

---
+++ apps/core/app/error.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function Error() {
+  return (
+    <div className="h-full px-10 py-12 lg:py-24">
+      <div className="mx-auto max-w-3xl">
+        <h1 className="mb-8 text-h3 lg:text-h2">There was a server error!</h1>
+        <p className="text-base">Please try again later.</p>
+      </div>
+    </div>
+  );
+}
