779e326bf4d3c16e5d28fc0c238d2ade998ecb2f
yurytut1993 <yurii.zusik@bigcommerce.com>
2023-09-03 20:57:46 +0300

    feat(reactant): add BlogPostCard component

---
+++ apps/core/components/BlogPostCard/index.tsx
@@ -0,0 +1,63 @@
+import {
+  BlogPostAuthor,
+  BlogPostBanner,
+  BlogPostContent,
+  BlogPostDate,
+  BlogPostImage,
+  BlogPostTitle,
+  BlogPostCard as ReactantBlogPostCard,
+} from '@bigcommerce/reactant/BlogPostCard';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import client from '~/client';
+
+interface BlogPostCardProps {
+  blogPost: NonNullable<Awaited<ReturnType<typeof client.getBlogPosts>>>['posts']['items'][number];
+}
+
+export const BlogPostCard = ({ blogPost }: BlogPostCardProps) => (
+  <ReactantBlogPostCard>
+    {blogPost.thumbnailImage ? (
+      <BlogPostImage>
+        <Link
+          className="focus:ring-primary-blue/20 block w-full focus:outline-none focus:ring-4"
+          href={`/blog/${blogPost.entityId}`}
+        >
+          <Image
+            alt={blogPost.thumbnailImage.altText}
+            className="h-full w-full object-cover object-center"
+            height={300}
+            src={blogPost.thumbnailImage.url}
+            width={300}
+          />
+        </Link>
+      </BlogPostImage>
+    ) : (
+      <BlogPostBanner>
+        <BlogPostTitle variant="inBanner">
+          <span className="line-clamp-3 text-blue-primary">{blogPost.name}</span>
+        </BlogPostTitle>
+        <BlogPostDate variant="inBanner">
+          <span className="text-blue-primary">
+            {new Intl.DateTimeFormat('en-US').format(new Date(blogPost.publishedDate.utc))}
+          </span>
+        </BlogPostDate>
+      </BlogPostBanner>
+    )}
+
+    <BlogPostTitle>
+      <Link
+        className="focus:ring-primary-blue/20 focus:outline-none focus:ring-4"
+        href={`/blog/${blogPost.entityId}`}
+      >
+        {blogPost.name}
+      </Link>
+    </BlogPostTitle>
+    <BlogPostContent>{blogPost.plainTextSummary}</BlogPostContent>
+    <BlogPostDate>
+      {new Intl.DateTimeFormat('en-US').format(new Date(blogPost.publishedDate.utc))}
+    </BlogPostDate>
+    {blogPost.author ? <BlogPostAuthor>, by {blogPost.author}</BlogPostAuthor> : null}
+  </ReactantBlogPostCard>
+);
---
+++ packages/reactant/src/components/BlogPostCard/BlogPostCard.tsx
@@ -0,0 +1,112 @@
+import { Slot } from '@radix-ui/react-slot';
+import { cva } from 'class-variance-authority';
+import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react';
+
+import { cs } from '../../utils/cs';
+
+interface BlogPostCardProps extends ComponentPropsWithRef<'li'> {
+  asChild?: boolean;
+}
+
+export const BlogPostCard = forwardRef<ElementRef<'li'>, BlogPostCardProps>(
+  ({ asChild = false, children, className, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'li';
+
+    return (
+      <Comp className={cs('group relative list-none flex-col', className)} ref={ref} {...props}>
+        {children}
+      </Comp>
+    );
+  },
+);
+
+export const BlogPostBanner = forwardRef<ElementRef<'div'>, ComponentPropsWithRef<'div'>>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <div
+        className={cs('mb-3 flex h-44 justify-between bg-blue-primary/10 p-4 lg:h-56', className)}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  },
+);
+
+export const BlogPostImage = forwardRef<ElementRef<'div'>, ComponentPropsWithRef<'div'>>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <div className={cs('mb-2 flex h-44 lg:h-56', className)} ref={ref} {...props}>
+        {children}
+      </div>
+    );
+  },
+);
+
+interface TitleProps extends ComponentPropsWithRef<'h3'> {
+  asChild?: boolean;
+  variant?: 'inBanner';
+}
+
+const titleVariants = cva('mb-2 text-h5', {
+  variants: {
+    variant: {
+      inBanner: 'mb-0 flex-none basis-1/2 self-start text-h4',
+    },
+  },
+});
+
+export const BlogPostTitle = forwardRef<ElementRef<'h3'>, TitleProps>(
+  ({ asChild = false, children, className, variant, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'h3';
+
+    return (
+      <Comp className={cs(titleVariants({ variant, className }))} ref={ref} {...props}>
+        {children}
+      </Comp>
+    );
+  },
+);
+
+export const BlogPostContent = forwardRef<ElementRef<'p'>, ComponentPropsWithRef<'p'>>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <p className={cs('mb-2 text-base', className)} {...props} ref={ref}>
+        {children}
+      </p>
+    );
+  },
+);
+
+interface DateProps extends ComponentPropsWithRef<'small'> {
+  variant?: 'inBanner';
+}
+
+const dateVariants = cva('mb-2 text-base text-gray-500', {
+  variants: {
+    variant: {
+      inBanner: 'mb-0 flex-none self-end text-h5',
+    },
+  },
+});
+
+export const BlogPostDate = forwardRef<ElementRef<'small'>, DateProps>(
+  ({ children, className, variant, ...props }, ref) => {
+    return (
+      <small className={cs(dateVariants({ variant, className }))} {...props} ref={ref}>
+        {children}
+      </small>
+    );
+  },
+);
+
+export const BlogPostAuthor = forwardRef<ElementRef<'small'>, ComponentPropsWithRef<'small'>>(
+  ({ children, className, ...props }, ref) => {
+    return (
+      <small className={cs('text-base text-gray-500', className)} {...props} ref={ref}>
+        {children}
+      </small>
+    );
+  },
+);
