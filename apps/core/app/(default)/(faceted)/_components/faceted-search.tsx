2e74e4d54a2f595f651cf7a6e22420d2fc97309f
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-09-14 16:40:30 -0400

    refactor(core): CATALYST-56 allow SubCategories to be optional (#222)

--- apps/core/app/(default)/(faceted)/_components/faceted-search.tsx
+++ apps/core/app/(default)/(faceted)/_components/faceted-search.tsx
@@ -1,25 +1,28 @@
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, PropsWithChildren } from 'react';
 
 import type { Facet } from '../types';
 
 import { Facets } from './facets';
 import { RefineBy } from './refine-by';
-import { SubCategories } from './sub-categories';
 
 interface Props extends ComponentPropsWithoutRef<'aside'> {
-  categoryId: number;
   facets: Facet[];
   headingId: string;
 }
 
-export const FacetedSearch = ({ categoryId, facets, headingId, ...props }: Props) => {
+export const FacetedSearch = ({
+  facets,
+  headingId,
+  children,
+  ...props
+}: PropsWithChildren<Props>) => {
   return (
     <aside aria-labelledby={headingId} {...props}>
       <h2 className="sr-only" id={headingId}>
         Filters
       </h2>
 
-      <SubCategories categoryId={categoryId} />
+      {children}
 
       <RefineBy facets={facets} />
 
