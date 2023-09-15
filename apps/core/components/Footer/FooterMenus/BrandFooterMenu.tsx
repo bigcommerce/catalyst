88514e6341a0d433edb6c3cbeb4c2dc6c4a7556e
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-09-15 10:13:15 -0400

    fix(core): CATALYST-56 temp fix to link to brand pages (#226)

--- apps/core/components/Footer/FooterMenus/BrandFooterMenu.tsx
+++ apps/core/components/Footer/FooterMenus/BrandFooterMenu.tsx
@@ -5,9 +5,15 @@ import { BaseFooterMenu } from './BaseFooterMenu';
 export const BrandFooterMenu = async () => {
   const brands = await client.getBrands();
 
-  if (brands.length === 0) {
+  // Temp workaround until we have the middleware that converts paths to real urls
+  const items = brands.map((item) => ({
+    ...item,
+    path: `/brand/${item.entityId}`,
+  }));
+
+  if (!items.length) {
     return null;
   }
 
-  return <BaseFooterMenu items={brands} title="Brands" />;
+  return <BaseFooterMenu items={items} title="Brands" />;
 };
