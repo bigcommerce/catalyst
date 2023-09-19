af3e90c005c3a818effa8e18f4abae93eafa53ee
Yurii Zusik <66325265+yurytut1993@users.noreply.github.com>
2023-09-19 15:05:42 +0300

    feat(reactant): add BlogPostCard component (#196)

---
+++ apps/docs/stories/BlogPostCard.stories.tsx
@@ -0,0 +1,79 @@
+import {
+  BlogPostAuthor,
+  BlogPostBanner,
+  BlogPostCard,
+  BlogPostContent,
+  BlogPostDate,
+  BlogPostImage,
+  BlogPostTitle,
+} from '@bigcommerce/reactant/BlogPostCard';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof BlogPostCard> = {
+  component: BlogPostCard,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BlogPostCard>;
+
+export const BlogPostCardWithImage: Story = {
+  render: () => (
+    <div className="mx-auto max-w-sm">
+      <BlogPostCard asChild>
+        <div>
+          <BlogPostImage>
+            <a
+              className="focus:ring-primary-blue/20 block w-full focus:outline-none focus:ring-4"
+              href="/"
+            >
+              <div className="flex h-full w-full items-center justify-center bg-gray-200 text-h4">
+                Image
+              </div>
+            </a>
+          </BlogPostImage>
+          <BlogPostTitle>
+            <a className="focus:ring-primary-blue/20 focus:outline-none focus:ring-4" href="/">
+              Blog Post Title
+            </a>
+          </BlogPostTitle>
+          <BlogPostContent>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry
+          </BlogPostContent>
+          <BlogPostDate>8/15/23</BlogPostDate>
+          <BlogPostAuthor>, by Author Name</BlogPostAuthor>
+        </div>
+      </BlogPostCard>
+    </div>
+  ),
+};
+
+export const BlogPostCardWithoutImage: Story = {
+  render: () => (
+    <div className="mx-auto max-w-sm">
+      <BlogPostCard asChild>
+        <div>
+          <BlogPostBanner>
+            <BlogPostTitle role="presentation" variant="inBanner">
+              <span className="text-blue-primary">Blog Post Title</span>
+            </BlogPostTitle>
+            <BlogPostDate variant="inBanner">
+              <span className="text-blue-primary">8/15/23</span>
+            </BlogPostDate>
+          </BlogPostBanner>
+          <BlogPostTitle>
+            <a className="focus:ring-primary-blue/20 focus:outline-none focus:ring-4" href="/">
+              Blog Post Title
+            </a>
+          </BlogPostTitle>
+          <BlogPostContent>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry
+          </BlogPostContent>
+          <BlogPostDate>8/15/23</BlogPostDate>
+          <BlogPostAuthor>, by Author Name</BlogPostAuthor>
+        </div>
+      </BlogPostCard>
+    </div>
+  ),
+};
