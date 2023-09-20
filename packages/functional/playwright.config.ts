83b784c21b689302e7ea9022078b51a6f5ff428a
avattipalli <anudeep.vattipalli@bigcommerce.com>
2023-09-20 11:34:30 -0500

    fix: remoove unused config options

--- packages/functional/playwright.config.ts
+++ packages/functional/playwright.config.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   testDir: './tests',
   timeout: 120 * 1000,
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
