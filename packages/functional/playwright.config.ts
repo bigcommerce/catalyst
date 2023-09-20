55335f1c4851ef42e44b82d42471cac8812f6f04
Anudeep Vattipalli <58529077+avattipalli@users.noreply.github.com>
2023-09-20 11:54:52 -0500

    feat: CATALYST-88 Setup playwright config (#233)
    * feat: CATALYST-88 Setup playwright config
    
    * fix: update playwright config
    
    * fix: Add a simple test
    
    * fix: updates
    
    * fix: review feedback
    
    * fix: modify gitignore
    
    * fix: relocate
    
    * fix: review feedback
    
    * fix: remoove unused config options
    
    * fix: versioning

---
+++ packages/functional/playwright.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 120 * 1000,
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'tests-chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'tests-firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'tests-webkit',
+      use: { ...devices['Desktop Safari'] },
+    }
+  ],
+});
