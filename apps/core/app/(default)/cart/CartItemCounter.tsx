75ee9357291d8b0c0d78fedd3a6a5f72cbb1370a
Alex Saiannyi <67792608+bc-alexsaiannyi@users.noreply.github.com>
2023-09-26 17:59:48 +0200

    feat(core): CATALYST-80 add counter to cart (#227)

---
+++ apps/core/app/(default)/cart/CartItemCounter.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import {
+  CartLineItemInput,
+  CartPhysicalItem,
+  UpdateCartLineItemInput,
+} from '@bigcommerce/catalyst-client';
+import { Counter } from '@bigcommerce/reactant/Counter';
+import { useState } from 'react';
+
+import { updateProductQuantity } from './_actions/updateProductQuantity';
+
+type CartItemData = Pick<CartPhysicalItem, 'quantity' | 'productEntityId' | 'variantEntityId'> & {
+  lineItemEntityId: CartPhysicalItem['entityId'];
+};
+
+interface UpdateProductQuantityData extends CartLineItemInput {
+  lineItemEntityId: UpdateCartLineItemInput['lineItemEntityId'];
+}
+
+export const CartItemCounter = ({ itemData }: { itemData: CartItemData }) => {
+  const { quantity, lineItemEntityId, productEntityId, variantEntityId } = itemData;
+  const [counterValue, setCounterValue] = useState(quantity);
+  const handleCountUpdate = async (value: number) => {
+    if (Number.isNaN(value)) {
+      return;
+    }
+
+    setCounterValue(value);
+
+    const productData: UpdateProductQuantityData = Object.assign(
+      { lineItemEntityId, productEntityId, quantity: value },
+      variantEntityId && { variantEntityId },
+    );
+
+    await updateProductQuantity(productData);
+  };
+
+  return (
+    <Counter
+      className="w-32 text-base font-bold"
+      onChange={handleCountUpdate}
+      value={counterValue}
+    />
+  );
+};
