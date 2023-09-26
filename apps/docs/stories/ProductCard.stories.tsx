e9107ce4e74d39ac622974f74d9b7f61bbdd58ce
Jorge Moya <jorge.moya@bigcommerce.com>
2023-09-26 14:28:46 -0500

    feat(reactant): remove ProductCard actions

--- apps/docs/stories/ProductCard.stories.tsx
+++ apps/docs/stories/ProductCard.stories.tsx
@@ -1,7 +1,5 @@
 import {
   ProductCard,
-  ProductCardActionButton,
-  ProductCardActions,
   ProductCardBadge,
   ProductCardImage,
   ProductCardInfo,
@@ -10,7 +8,6 @@ import {
   ProductCardInfoProductName,
 } from '@bigcommerce/reactant/ProductCard';
 import { Meta, StoryObj } from '@storybook/react';
-import { Scale, ShoppingCart } from 'lucide-react';
 
 const meta: Meta<typeof ProductCard> = {
   component: ProductCard,
@@ -26,18 +23,6 @@ export const BasicExample: Story = {
     <ProductCard>
       <ProductCardImage>
         <div className="h-[180px] w-full bg-gray-200" />
-        <ProductCardActions>
-          <ProductCardActionButton>
-            <ShoppingCart>
-              <title>Add to Cart</title>
-            </ShoppingCart>
-          </ProductCardActionButton>
-          <ProductCardActionButton>
-            <Scale>
-              <title>Add to Compare</title>
-            </Scale>
-          </ProductCardActionButton>
-        </ProductCardActions>
       </ProductCardImage>
       <ProductCardBadge>On sale</ProductCardBadge>
       <ProductCardInfo>
