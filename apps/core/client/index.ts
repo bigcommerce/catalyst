eb51bb07d0f90694029a9410da31995e8396b46d
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-09-26 10:23:48 -0400

    feat(client): CATALYST-00 enable multi-channel client

--- apps/core/client/index.ts
+++ apps/core/client/index.ts
@@ -4,6 +4,7 @@ const client = createClient({
   key: process.env.BIGCOMMERCE_CUSTOMER_IMPERSONATION_TOKEN ?? '',
   storeHash: process.env.BIGCOMMERCE_STORE_HASH ?? '',
   xAuthToken: process.env.BIGCOMMERCE_ACCESS_TOKEN ?? '',
+  channelId: process.env.BIGCOMMERCE_CHANNEL_ID,
 });
 
 export default client;
