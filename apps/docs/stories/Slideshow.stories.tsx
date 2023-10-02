023168898352e4e88a5198b338906aafe08ccd1e
Matthew Volk <matt.volk@bigcommerce.com>
2023-10-02 14:57:42 -0500

    fix(docs): CATALYST-42 more meaningful slideshow storybook (#261)

--- apps/docs/stories/Slideshow.stories.tsx
+++ apps/docs/stories/Slideshow.stories.tsx
@@ -21,81 +21,7 @@ export default meta;
 
 type Story = StoryObj<typeof Slideshow>;
 
-export const OneSlide: Story = {
-  render: () => (
-    <Slideshow>
-      <SlideshowContent>
-        <SlideshowSlide>
-          <div className="relative">
-            <img
-              alt="A plant in a glass vase against a blank background."
-              className="absolute -z-10"
-              src="/slideshow-bg-01.jpg"
-            />
-            <div className="flex flex-col gap-4 px-12 pb-48 pt-36">
-              <h2 className="text-h1">25% Off Sale</h2>
-              <p className="max-w-xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-              </p>
-              <Button asChild className="w-fit">
-                <a href="/#">Shop now</a>
-              </Button>
-            </div>
-          </div>
-        </SlideshowSlide>
-      </SlideshowContent>
-    </Slideshow>
-  ),
-};
-
-export const TwoSlides: Story = {
-  render: () => (
-    <Slideshow>
-      <SlideshowContent>
-        <SlideshowSlide>
-          <div className="relative">
-            <img
-              alt="A plant in a glass vase against a blank background."
-              className="absolute -z-10"
-              src="/slideshow-bg-01.jpg"
-            />
-            <div className="flex flex-col gap-4 px-12 pb-48 pt-36">
-              <h2 className="text-h1">25% Off Sale</h2>
-              <p className="max-w-xl">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-              </p>
-              <Button asChild className="w-fit">
-                <a href="/#">Shop now</a>
-              </Button>
-            </div>
-          </div>
-        </SlideshowSlide>
-        <SlideshowSlide>
-          <div className="flex flex-col gap-4 bg-gray-100 px-12 pb-48 pt-36">
-            <h2 className="text-h1">Great Deals</h2>
-            <p className="max-w-xl">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-            </p>
-            <Button asChild className="w-fit">
-              <a href="/#">Shop now</a>
-            </Button>
-          </div>
-        </SlideshowSlide>
-      </SlideshowContent>
-      <SlideshowControls>
-        <SlideshowAutoplayControl />
-        <SlideshowPreviousIndicator />
-        <SlideshowPagination />
-        <SlideshowNextIndicator />
-      </SlideshowControls>
-    </Slideshow>
-  ),
-};
-
-export const ThreeSlides: Story = {
+export const MultipleSlides: Story = {
   render: () => (
     <Slideshow>
       <SlideshowContent>
@@ -143,6 +69,7 @@ export const ThreeSlides: Story = {
           </div>
         </SlideshowSlide>
       </SlideshowContent>
+
       <SlideshowControls>
         <SlideshowAutoplayControl />
         <SlideshowPreviousIndicator />
@@ -153,34 +80,31 @@ export const ThreeSlides: Story = {
   ),
 };
 
-const mocked = new Array(100).fill(undefined).map((_, index) => index);
-
-export const OneHundredSlides: Story = {
+export const SingleSlide: Story = {
   render: () => (
     <Slideshow>
       <SlideshowContent>
-        {mocked.map((i) => (
-          <SlideshowSlide key={i}>
-            <div className="relative">
-              <img
-                alt="A plant in a glass vase against a blank background."
-                className="absolute -z-10"
-                src="/slideshow-bg-01.jpg"
-              />
-              <div className="flex flex-col gap-4 px-12 pb-48 pt-36">
-                <h2 className="text-h1">{i + 1}% Off Sale</h2>
-                <p className="max-w-xl">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-                </p>
-                <Button asChild className="w-fit">
-                  <a href="/#">Shop now</a>
-                </Button>
-              </div>
+        <SlideshowSlide>
+          <div className="relative">
+            <img
+              alt="A plant in a glass vase against a blank background."
+              className="absolute -z-10"
+              src="/slideshow-bg-01.jpg"
+            />
+            <div className="flex flex-col gap-4 px-12 pb-48 pt-36">
+              <h2 className="text-h1">25% Off Sale</h2>
+              <p className="max-w-xl">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+              </p>
+              <Button asChild className="w-fit">
+                <a href="/#">Shop now</a>
+              </Button>
             </div>
-          </SlideshowSlide>
-        ))}
+          </div>
+        </SlideshowSlide>
       </SlideshowContent>
+
       <SlideshowControls>
         <SlideshowAutoplayControl />
         <SlideshowPreviousIndicator />
@@ -227,6 +151,7 @@ export const CustomControlsAndInterval: Story = {
           </div>
         </SlideshowSlide>
       </SlideshowContent>
+
       <SlideshowControls>
         <SlideshowAutoplayControl className="font-semibold">
           {({ isPaused }) => (isPaused ? 'Play' : 'Pause')}
