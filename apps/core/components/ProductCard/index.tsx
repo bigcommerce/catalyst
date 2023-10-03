85aa06aa969e9ad0cdee9fb60226930654d545af
Jorge Moya <jorge.moya@bigcommerce.com>
2023-10-03 15:12:45 -0500

    fix(core): CATALYST-0 use correct size for Rating

--- apps/core/components/ProductCard/index.tsx
+++ apps/core/components/ProductCard/index.tsx
@@ -104,7 +104,7 @@ export const ProductCard = ({
                 product.reviewSummary.numberOfReviews === 0 && 'text-gray-400',
               )}
             >
-              <Rating size={13} value={product.reviewSummary.averageRating || 0} />
+              <Rating size={16} value={product.reviewSummary.averageRating || 0} />
             </p>
 
             <div className="text-sm text-gray-500" id={summaryId}>
