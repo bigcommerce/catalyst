4b259d599f10e6bf60cc849de9045572aac4ff46
Alex Saiannyi <67792608+bc-alexsaiannyi@users.noreply.github.com>
2023-10-06 18:23:27 +0200

    feat(catalyst): CATALYST-135 add query for fetching store web pages (#277)

---
+++ packages/client/src/queries/getWebPages.ts
@@ -0,0 +1,53 @@
+import { BigCommerceResponse, FetcherInput } from '../fetcher';
+import { generateQueryOp, QueryGenqlSelection, QueryResult } from '../generated';
+import { removeEdgesAndNodes } from '../utils/removeEdgesAndNodes';
+
+export const getWebPages = async <T>(
+  customFetch: <U>(data: FetcherInput) => Promise<BigCommerceResponse<U>>,
+  config: T = {} as T,
+) => {
+  const query = {
+    site: {
+      content: {
+        pages: {
+          edges: {
+            node: {
+              __typename: true,
+              entityId: true,
+              name: true,
+              isVisibleInNavigation: true,
+              parentEntityId: true,
+              on_RawHtmlPage: {
+                path: true,
+              },
+              on_ContactPage: {
+                path: true,
+              },
+              on_NormalPage: {
+                path: true,
+              },
+              on_BlogIndexPage: {
+                path: true,
+              },
+            },
+          },
+        },
+      },
+    },
+  } satisfies QueryGenqlSelection;
+
+  const queryOp = generateQueryOp(query);
+
+  const response = await customFetch<QueryResult<typeof query>>({
+    ...queryOp,
+    ...config,
+  });
+
+  const { pages } = response.data.site.content;
+
+  if (!pages.edges?.length) {
+    return [];
+  }
+
+  return removeEdgesAndNodes(pages);
+};
