2329e82d1771f7c4c273897d1b3514655310def4
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2023-10-06 15:57:20 -0500

    feat(core): add auth to internal api routes (#288)

--- apps/core/app/api/store-settings/route.ts
+++ apps/core/app/api/store-settings/route.ts
@@ -2,11 +2,15 @@ import { NextResponse } from 'next/server';
 
 import client from '~/client';
 
-export const GET = async () => {
+import { withInternalAuth } from '../_internal-auth';
+
+const handler = async () => {
   const settings = await client.getStoreSettings({ cache: null, next: { revalidate: 60 * 5 } });
 
   // Middleware is the current consumer of this endpoint. If you need to modify this, ensure middleware is updated.
   return NextResponse.json(settings);
 };
+
+export const GET = withInternalAuth(handler);
 
 export const runtime = 'edge';
