24ec1207a9113630fed002d81e4c257392f31a4a
Matthew Volk <matt.volk@bigcommerce.com>
2023-10-06 09:46:46 -0500

    feat(setup): CATALYST-112 accept access token as input (#267)

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -1,18 +1,36 @@
-import { input } from "@inquirer/prompts";
+import { input, password } from "@inquirer/prompts";
 
 /** @returns {Promise<string>} */
 const promptStoreHash = async () =>
   input({ message: "Please enter your store hash" });
 
-/** @param {{ storeHash: string }} config */
-const logEnv = ({ storeHash }) => {
+/** @returns {Promise<string>} */
+const promptAccessToken = async () => {
+  console.log("Please create an access token with the following scopes:");
+  console.log("- Carts: read-only");
+  console.log("- Sites & routes: modify");
+  console.log("- Channel settings: modify");
+  console.log("- Storefront API customer impersonation tokens: manage");
+  console.log(
+    "http://login.bigcommerce.com/deep-links/settings/api-accounts/create"
+  );
+
+  return password({
+    message: "Please enter your store's access token",
+  });
+};
+
+/** @param {{ storeHash: string, accessToken: string }} config */
+const logEnv = ({ storeHash, accessToken }) => {
   console.log(`\nBIGCOMMERCE_STORE_HASH=${storeHash}`);
+  console.log(`BIGCOMMERCE_ACCESS_TOKEN=${accessToken}`);
 };
 
 const setup = async () => {
   const storeHash = await promptStoreHash();
+  const accessToken = await promptAccessToken();
 
-  logEnv({ storeHash });
+  logEnv({ storeHash, accessToken });
 };
 
 setup();
