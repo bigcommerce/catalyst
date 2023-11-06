ceeb8d2de11fc3a1ad9abd151e7d8dbbffd69993
avattipalli <anudeep.vattipalli@bigcommerce.com>
2023-11-06 17:02:44 -0600

    fix: Lint and gh actions

--- packages/functional/tests/visual-regression/reactant/StoryBookElements.ts
+++ packages/functional/tests/visual-regression/reactant/StoryBookElements.ts
@@ -1,3 +1,3 @@
-export const storyUrl = process.env.PLAYWRIGHT_TEST_BASE_URL;
+export const storyUrl = process.env.PLAYWRIGHT_TEST_BASE_URL || 'https://catalyst-storybook.vercel.app/?path=/story';
 export const storyBookFrame = 'iframe[title="storybook-preview-iframe"]';
 export const storyBook = '#storybook-root';
