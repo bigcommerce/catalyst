39fb87477e586c2a58ceaddab53458b9c42a5730
Chancellor Clark <chancellor.clark@bigcommerce.com>
2023-11-08 13:34:29 -0500

    refactor: simplify login page layout and components

--- packages/reactant/src/components/Message/Message.tsx
+++ packages/reactant/src/components/Message/Message.tsx
@@ -4,7 +4,7 @@ import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react';
 
 import { cs } from '../../utils/cs';
 
-const messageVariants = cva('flex w-full justify-start gap-x-2.5 py-3 ps-3 text-base', {
+const messageVariants = cva('flex w-full gap-x-2.5 justify-start p-3 text-base', {
   variants: {
     variant: {
       success: 'bg-green-300/20 [&>svg]:text-green-300',
@@ -14,14 +14,14 @@ const messageVariants = cva('flex w-full justify-start gap-x-2.5 py-3 ps-3 text-
 });
 
 interface MessageProps extends ComponentPropsWithRef<'div'> {
-  variant?: 'error' | 'success';
+  readonly variant?: 'error' | 'success';
 }
 
 export const Message = forwardRef<ElementRef<'div'>, MessageProps>(
   ({ className, children, variant, ...props }, ref) => (
     <div className={cs(messageVariants({ variant, className }))} ref={ref} {...props}>
-      {variant === 'error' && <AlertCircle />}
-      {variant === 'success' && <Check />}
+      {variant === 'error' && <AlertCircle className="flex-none" />}
+      {variant === 'success' && <Check className="flex-none" />}
       {children}
     </div>
   ),
