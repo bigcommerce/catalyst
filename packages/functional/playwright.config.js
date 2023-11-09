bfcbe2d9cf7901ec68dd7fb8290cec08869663e7
avattipalli <anudeep.vattipalli@bigcommerce.com>
2023-11-09 08:38:25 -0600

    fix: cleanup gh action and conf

--- packages/functional/playwright.config.js
+++ packages/functional/playwright.config.js
@@ -19,19 +19,7 @@ export default defineConfig({
   projects: [
     {
       name: 'tests-chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        launchOptions: {
-          args: [
-            '--font-render-hinting=none',
-            '--disable-skia-runtime-opts',
-            '--disable-system-font-check',
-            '--disable-font-subpixel-positioning',
-            '--disable-lcd-text',
-            '--disable-remote-fonts',
-          ],
-        },
-      },
+      use: { ...devices['Desktop Chrome'] },
     },
 
     {
