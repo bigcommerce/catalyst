8c8a386f346be8adf7c2cf05bfd56a9563ec07f0
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2023-12-01 09:52:50 -0600

    feat(client): add query logger

--- apps/core/client/index.ts
+++ apps/core/client/index.ts
@@ -13,6 +13,7 @@ export const newClient = createNewClient({
   xAuthToken: process.env.BIGCOMMERCE_ACCESS_TOKEN ?? '',
   storeHash: process.env.BIGCOMMERCE_STORE_HASH ?? '',
   channelId: process.env.BIGCOMMERCE_CHANNEL_ID,
+  logger: process.env.NODE_ENV !== 'production' || process.env.CLIENT_LOGGER === 'true',
 });
 
 export default client;
