757652bd11cfc32f11b02fd48a4da792759f9e22
Jorge Moya <jorge.moya@bigcommerce.com>
2023-12-11 13:53:28 -0600

    fix(reactant): CATALYST-194 auto allow scrolling in Sheet component (#380)

--- packages/reactant/src/components/Sheet/Sheet.tsx
+++ packages/reactant/src/components/Sheet/Sheet.tsx
@@ -48,7 +48,7 @@ const SheetOverlay = forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  'fixed z-50 gap-4 bg-white p-6 md:p-10 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
+  'fixed z-50 gap-4 bg-white p-6 md:p-10 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 overflow-auto',
   {
     variants: {
       side: {
