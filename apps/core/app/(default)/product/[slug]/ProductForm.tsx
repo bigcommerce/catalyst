e278ed25eb7ac556276ab43528f04be5e3bb72e8
Jorge Moya <jorge.moya@bigcommerce.com>
2023-12-14 12:39:13 -0600

    fix: return early to prevent if else

--- apps/core/app/(default)/product/[slug]/ProductForm.tsx
+++ apps/core/app/(default)/product/[slug]/ProductForm.tsx
@@ -15,21 +15,23 @@ export const ProductForm = ({ children }: ComponentPropsWithoutRef<'form'>) => (
 
       if (result?.error) {
         toast.error(result.error, { icon: <AlertCircle className="text-red-100" /> });
-      } else {
-        toast.success(
-          () => (
-            <div className="flex items-center gap-3">
-              <span>
-                {quantity} {quantity === 1 ? 'Item' : 'Items'} added to{' '}
-                <Link className="font-semibold text-blue-primary" href="/cart">
-                  your cart
-                </Link>
-              </span>
-            </div>
-          ),
-          { icon: <Check className="text-green-100" /> },
-        );
+
+        return;
       }
+
+      toast.success(
+        () => (
+          <div className="flex items-center gap-3">
+            <span>
+              {quantity} {quantity === 1 ? 'Item' : 'Items'} added to{' '}
+              <Link className="font-semibold text-blue-primary" href="/cart">
+                your cart
+              </Link>
+            </span>
+          </div>
+        ),
+        { icon: <Check className="text-green-100" /> },
+      );
     }}
     className="w-full"
   >
