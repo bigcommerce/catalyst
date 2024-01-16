0ff5197fffc8bb0458d482b560f1747e7dd9569f
Matthew Volk <matt.volk@bigcommerce.com>
2024-01-16 16:40:15 -0600

    poc: move eslint-configs

--- apps/core/.eslintrc.cjs
+++ apps/core/.eslintrc.cjs
@@ -4,16 +4,30 @@
 const config = {
   root: true,
   extends: [
-    '@bigcommerce/catalyst/base',
-    '@bigcommerce/catalyst/react',
-    '@bigcommerce/catalyst/next',
-    '@bigcommerce/catalyst/prettier',
+    '@bigcommerce/eslint-config/configs/base',
+    'prettier',
+    '@bigcommerce/eslint-config/configs/react',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prefer-read-only-props': 'off',
+    'prettier/prettier': ['warn'],
     '@typescript-eslint/naming-convention': 'off',
     '@next/next/no-html-link-for-pages': 'off',
     'import/dynamic-import-chunkname': 'off',
     'no-underscore-dangle': ['error', { allow: ['__typename'] }],
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: ['@bigcommerce/eslint-config/configs/typescript'],
+    },
+  ],
+  env: {
+    es2022: true,
+    node: true,
   },
 };
 
