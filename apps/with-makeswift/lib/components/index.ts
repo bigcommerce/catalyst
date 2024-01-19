a9f415fd08f2bc51dc368cd9664a13fb4f7fe29a
Erik Christensen <erik.christensen@bigcommerce.com>
2024-01-18 16:35:39 -0800

    chore(with-makeswift): remove with-makeswift app

--- apps/with-makeswift/lib/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './Carousel';
-export * from './Footer';
-export * from './Navigation';
-export * from './ProductCard';
-export * from './ProductCards';
--- apps/with-makeswift/lib/components/makeswift.ts
+++
@@ -1,6 +0,0 @@
-import './Carousel/Carousel.makeswift';
-import './Footer/Footer.makeswift';
-import './Navigation/Navigation.makeswift';
-import './ProductCard/ProductCard.makeswift';
-import './ProductCards/ProductCards.makeswift';
-import './Text/Text.makeswift';
