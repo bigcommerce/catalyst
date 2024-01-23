db68f4197ddd5bdd1af8b59b9c30f70f3580ebf9
Nathan Booker <nathan.booker@bigcommerce.com>
2024-01-22 18:55:14 -0600

    feat(core): CATALYST-259 Add custom User-Agent header for observability of version and environment
    Co-Authored-By: Erik Christensen <32500994+christensenep@users.noreply.github.com>

---
+++ apps/core/userAgent.ts
@@ -0,0 +1,7 @@
+import packageInfo from '../../package.json';
+
+const { name, version } = packageInfo;
+
+// Add package name and version to the user agent
+// Used as part of API client instantiation
+export const backendUserAgent = `${name}/${version}`;
