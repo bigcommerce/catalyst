c8c69d43c62357c579ed4c8929401c7f770bf973
Jorge Moya <jorge.moya@bigcommerce.com>
2024-01-23 12:37:58 -0600

    feat: forward ref

--- apps/core/components/Link/index.tsx
+++ apps/core/components/Link/index.tsx
@@ -1,14 +1,17 @@
 import NextLink, { type LinkProps } from 'next/link';
+import { ElementRef, forwardRef } from 'react';
 
 type LinkType = Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
   LinkProps & {
     children?: React.ReactNode;
   } & React.RefAttributes<HTMLAnchorElement>;
 
-export const Link = ({ href, prefetch = false, children, ...rest }: LinkType) => {
-  return (
-    <NextLink href={href} prefetch={prefetch} {...rest}>
-      {children}
-    </NextLink>
-  );
-};
+export const Link = forwardRef<ElementRef<'a'>, LinkType>(
+  ({ href, prefetch = false, children, ...rest }, ref) => {
+    return (
+      <NextLink href={href} prefetch={prefetch} ref={ref} {...rest}>
+        {children}
+      </NextLink>
+    );
+  },
+);
