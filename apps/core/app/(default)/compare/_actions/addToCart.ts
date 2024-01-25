161bf052b0a1018993135051f0f5d38da858f3c1
Jorge Moya <jorge.moya@bigcommerce.com>
2024-01-25 11:39:12 -0600

    feat(core): CATALYST-190 normalize Compare add to cart action

--- apps/core/app/(default)/compare/_actions/addToCart.ts
+++ apps/core/app/(default)/compare/_actions/addToCart.ts
@@ -13,33 +13,37 @@ export const addToCart = async (data: FormData) => {
   const cartId = cookies().get('cartId')?.value;
   const cart = await getCart(cartId);
 
-  if (cart) {
-    await addCartLineItem(cart.entityId, {
-      lineItems: [
-        {
-          productEntityId,
-          quantity: 1,
-        },
-      ],
-    });
+  try {
+    if (cart) {
+      await addCartLineItem(cart.entityId, {
+        lineItems: [
+          {
+            productEntityId,
+            quantity: 1,
+          },
+        ],
+      });
+
+      revalidateTag('cart');
+
+      return;
+    }
+
+    const newCart = await createCart([{ productEntityId, quantity: 1 }]);
+
+    if (newCart) {
+      cookies().set({
+        name: 'cartId',
+        value: newCart.entityId,
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: true,
+        path: '/',
+      });
+    }
 
     revalidateTag('cart');
-
-    return;
+  } catch (e) {
+    return { error: 'Something went wrong. Please try again.' };
   }
-
-  const newCart = await createCart([{ productEntityId, quantity: 1 }]);
-
-  if (newCart) {
-    cookies().set({
-      name: 'cartId',
-      value: newCart.entityId,
-      httpOnly: true,
-      sameSite: 'lax',
-      secure: true,
-      path: '/',
-    });
-  }
-
-  revalidateTag('cart');
 };
