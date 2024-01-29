e005ac531fead4112aeb32f72830e9ac565b2d68
Yurii Zusik <66325265+yurytut1993@users.noreply.github.com>
2024-01-29 18:16:17 +0200

    feat(core): CATALYST-221 add client query and mutation for contact us form (#445)

---
+++ apps/core/client/queries/getReCaptchaSettings.ts
@@ -0,0 +1,21 @@
+import { client } from '..';
+import { graphql } from '../generated';
+
+const GET_RECAPTCHA_SETTINGS_QUERY = /* GraphQL */ `
+  query getReCaptchaSettings {
+    site {
+      settings {
+        reCaptcha {
+          siteKey
+        }
+      }
+    }
+  }
+`;
+
+export const getReCaptchaSettings = async () => {
+  const query = graphql(GET_RECAPTCHA_SETTINGS_QUERY);
+  const response = await client.fetch({ document: query });
+
+  return response.data.site.settings?.reCaptcha;
+};
