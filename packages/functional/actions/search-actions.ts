b90bd5d7d1a525ffbd3bd3fb488dc17bc1ac3533
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-01-30 11:18:10 -0600

    fix: add accessible name to quick search

--- packages/functional/actions/search-actions.ts
+++ packages/functional/actions/search-actions.ts
@@ -2,6 +2,7 @@ import { Page } from '@playwright/test';
 
 export async function searchForProduct(page: Page, productName: string) {
   await page.getByLabel('Open search popup').click();
+  await page.getByRole('dialog', { name: 'Search bar' }).isVisible();
   await page.getByPlaceholder('Search...').fill(productName);
   await page.getByPlaceholder('Search...').press('Enter');
   await page.getByRole('link').filter({ hasText: productName }).isVisible();
