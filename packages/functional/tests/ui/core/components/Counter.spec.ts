a01801dfdce9225493101ade6edc7d705854b6dd
Anudeep Vattipalli <anudeep.vattipalli@bigcommerce.com>
2024-01-31 14:16:22 -0600

    fix: Breakdown tests as per review suggestions

--- packages/functional/tests/ui/core/components/Counter.spec.ts
+++ packages/functional/tests/ui/core/components/Counter.spec.ts
@@ -2,22 +2,26 @@ import { expect, test } from '@playwright/test';
 
 import { ProductActions } from '../../../../actions/product-actions';
 
-test('Modify product quantity in cart and verify the results', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/');
   await page.getByLabel('Main').getByRole('link', { name: 'Shop All' }).click();
 
   await ProductActions.addProductToCart(page, '[Sample] Orbit Terrarium - Large');
   await page.getByRole('link').filter({ hasText: 'Cart Items1' }).click();
+});
 
+test('Increase count and verify the results', async({ page }) => {
   await expect(page.getByRole('spinbutton')).toHaveValue('1');
 
   await page.getByLabel('Increase count').click();
   await page.getByLabel('Increase count').click();
 
   await expect(page.getByRole('spinbutton')).toHaveValue('3');
+});
 
-  await page.getByLabel('Decrease count').click();
-  await page.getByLabel('Decrease count').click();
+test('Decrease count and verify the results', async({ page }) => {
+  await expect(page.getByRole('spinbutton')).toHaveValue('1');
+
   await page.getByLabel('Decrease count').click();
 
   await expect(page.getByRole('spinbutton')).toHaveValue('0');
