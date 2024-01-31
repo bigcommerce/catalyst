7605f2fee304d29ffcbed99c3935b47dff104ef4
Erik Christensen <erik.christensen@bigcommerce.com>
2024-01-31 14:44:44 -0600

    feat(core): Remove hardcoded About Us and FAQ pages

--- apps/core/app/(default)/(webpages)/faq/page.tsx
+++
@@ -1,9 +0,0 @@
-import { PageContent } from '../_components/PageContent';
-
-const mockedContent = 'Here can be answers for all popular questions from your Customers.';
-
-export default function FAQ() {
-  return <PageContent content={mockedContent} title="FAQ" />;
-}
-
-export const runtime = 'edge';
