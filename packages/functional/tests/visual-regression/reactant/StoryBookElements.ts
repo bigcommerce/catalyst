a0fd1f78d07f89d2b96b396040b6823c3031f87c
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-02-08 14:05:45 -0500

    test: fix functional visual regression tests

--- packages/functional/tests/visual-regression/reactant/StoryBookElements.ts
+++ packages/functional/tests/visual-regression/reactant/StoryBookElements.ts
@@ -1,5 +1,5 @@
 export const storyUrl = process.env.PLAYWRIGHT_TEST_BASE_URL
   ? `${process.env.PLAYWRIGHT_TEST_BASE_URL}/?path=/story`
   : 'https://catalyst-storybook.vercel.app/?path=/story';
-export const storyBookFrame = 'iframe[title="storybook-preview-iframe"]';
+export const storyBookFrame = '#storybook-preview-iframe';
 export const storyBook = '#storybook-root';
