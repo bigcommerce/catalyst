ed69d6903f982734e493895f2eac8eef5145ad93
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-02-08 13:58:47 -0500

    test: fix functional ui tests

--- packages/functional/tests/ui/core/components/Carousel.spec.ts
+++ packages/functional/tests/ui/core/components/Carousel.spec.ts
@@ -2,18 +2,32 @@ import { expect, test } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('/');
-  await page.getByRole('heading', { name: 'Best Selling Products' }).scrollIntoViewIfNeeded();
+
+  await page
+    .getByRole('region')
+    .filter({ has: page.getByRole('heading', { name: 'Best Selling Products' }) })
+    .getByRole('tablist', { name: 'Slides' })
+    .scrollIntoViewIfNeeded();
+
   await expect(page.getByRole('link', { name: '[Sample] Tiered Wire Basket' })).toBeVisible();
 });
 
 test('Navigate to next set of products', async ({ page }) => {
-  await page.getByRole('button', { name: 'Next products' }).click();
+  await page
+    .getByRole('region')
+    .filter({ has: page.getByRole('heading', { name: 'Best Selling Products' }) })
+    .getByRole('button', { name: 'Next products' })
+    .click();
 
   await expect(page.getByRole('link', { name: '[Sample] Fog Linen Chambray' })).toBeVisible();
 });
 
 test('Navigate to previous set of products', async ({ page }) => {
-  await page.getByRole('button', { name: 'Previous products' }).click();
+  await page
+    .getByRole('region')
+    .filter({ has: page.getByRole('heading', { name: 'Best Selling Products' }) })
+    .getByRole('button', { name: 'Previous products' })
+    .click();
 
   await expect(page.getByRole('link', { name: '[Sample] Fog Linen Chambray' })).toBeVisible();
 });
@@ -21,8 +35,17 @@ test('Navigate to previous set of products', async ({ page }) => {
 test('Navigation on set of products is cyclic', async ({ page }) => {
   await expect(page.getByRole('link', { name: '[Sample] Tiered Wire Basket' })).toBeVisible();
 
-  await page.getByRole('button', { name: 'Next products' }).click();
-  await page.getByRole('button', { name: 'Next products' }).click();
+  await page
+    .getByRole('region')
+    .filter({ has: page.getByRole('heading', { name: 'Best Selling Products' }) })
+    .getByRole('button', { name: 'Next products' })
+    .click();
+
+  await page
+    .getByRole('region')
+    .filter({ has: page.getByRole('heading', { name: 'Best Selling Products' }) })
+    .getByRole('button', { name: 'Next products' })
+    .click();
 
   await expect(page.getByRole('link', { name: '[Sample] Tiered Wire Basket' })).toBeVisible();
 });
