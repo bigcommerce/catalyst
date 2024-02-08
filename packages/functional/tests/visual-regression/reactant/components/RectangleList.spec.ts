9777f93eb34eb9ea97a296b124531f11273fd889
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-02-08 14:48:17 -0500

    test: fix renamed component tests (#535)

--- packages/functional/tests/visual-regression/reactant/components/RectangleList.spec.ts
+++ packages/functional/tests/visual-regression/reactant/components/RectangleList.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test';
 import * as storyBookElements from '../StoryBookElements';
 
 test('Basic rectangle list', async ({ page }) => {
-  await page.goto(`${storyBookElements.storyUrl}/rectanglelist--basic-example`);
+  await page.goto(`${storyBookElements.storyUrl}/rectangle-list--basic-example`);
   await expect(
     page.frameLocator(storyBookElements.storyBookFrame).locator(storyBookElements.storyBook),
   ).toBeVisible();
