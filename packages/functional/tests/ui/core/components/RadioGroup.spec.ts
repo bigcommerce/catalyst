00e0bd8942fba576db05e009cb3b240ae7b33b45
Anudeep Vattipalli <anudeep.vattipalli@bigcommerce.com>
2024-02-13 14:59:26 -0600

    fix: Update tests to match product changes (#550)

--- packages/functional/tests/ui/core/components/RadioGroup.spec.ts
+++ packages/functional/tests/ui/core/components/RadioGroup.spec.ts
@@ -12,21 +12,21 @@ test('Changing selection on radio button options should update query parameters'
 
   await expect(page).toHaveURL('shop-all/');
 
-  await page.getByRole('button', { name: 'Quick add' }).first().click();
+  await page.getByRole('link', { name: 'Quick add' }).first().click();
 
   await expect(page.getByRole('dialog', { name: 'Choose options' })).toBeVisible();
 
   await page.getByLabel('Radio').getByText('1').click();
 
-  await expect(page).toHaveURL('shop-all/?134=139');
+  await expect(page).toHaveURL('shop-all/?showQuickAdd=77&134=139');
 
   await expect(page.getByRole('dialog', { name: 'Choose options' })).toBeVisible();
 
   await page.getByLabel('Radio').getByText('2').click();
 
-  await expect(page).toHaveURL('shop-all/?134=140');
+  await expect(page).toHaveURL('shop-all/?showQuickAdd=77&134=140');
 
   await page.getByLabel('Radio').getByText('3').click();
 
-  await expect(page).toHaveURL('shop-all/?134=141');
+  await expect(page).toHaveURL('shop-all/?showQuickAdd=77&134=141');
 });
