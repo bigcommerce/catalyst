12c530092ccdd102d3f66f314f8f49e2547e8fc0
Matthew Volk <matt.volk@bigcommerce.com>
2024-02-14 14:57:13 -0600

    feat(cli): introduce types to commander (#558)

--- packages/create-catalyst/tsup.config.ts
+++ packages/create-catalyst/tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig, Options } from 'tsup';
 export default defineConfig((options: Options) => ({
   entry: ['src/index.ts'],
   format: ['esm'],
-  dts: true,
   clean: !options.watch,
   ...options,
 }));
