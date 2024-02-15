23d84f4bf69dd0f3a0c9be26f7bf1de047403393
Anudeep Vattipalli <anudeep.vattipalli@bigcommerce.com>
2024-02-15 11:59:17 -0600

    fix: update baseline images

--- packages/functional/tests/visual-regression/reactant/components/Footer.spec.ts
+++ packages/functional/tests/visual-regression/reactant/components/Footer.spec.ts
@@ -6,10 +6,7 @@ test('Footer', async ({ page }) => {
   await page.setExtraHTTPHeaders({ 'x-vercel-skip-toolbar': '1' });
   await page.goto(`${storyBookElements.storyUrl}/footer--example`);
   await expect(
-    page
-      .frameLocator(storyBookElements.storyBookFrame)
-      .locator(storyBookElements.storyBook)
-      .getByLabel('Footer navigation'),
+    page.frameLocator(storyBookElements.storyBookFrame).locator(storyBookElements.storyBook),
   ).toBeVisible();
   await expect(
     page.frameLocator(storyBookElements.storyBookFrame).locator(storyBookElements.storyBook),
