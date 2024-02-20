41822e07e4501dcf00d6bb0e740a0046c84b726a
Jorge Moya <jorge.moya@bigcommerce.com>
2024-02-20 13:21:08 -0600

    test: add temp header

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -70,6 +70,7 @@ class Client<FetcherRequestInit extends RequestInit = RequestInit> {
         Authorization: `Bearer ${this.config.customerImpersonationToken}`,
         'User-Agent': this.backendUserAgent,
         ...(customerId && { 'X-Bc-Customer-Id': String(customerId) }),
+        'X-BC-Test-Header': 'test',
         ...headers,
       },
       body: JSON.stringify({
