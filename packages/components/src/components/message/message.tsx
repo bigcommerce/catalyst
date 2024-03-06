ca52b77c4ef64f7d2dd600279e42d974a8217cde
Onur Yanar <onur4185@google.com>
2024-03-07 01:35:22 +0300

    Remove custom success color form message component

--- packages/components/src/components/message/message.tsx
+++ packages/components/src/components/message/message.tsx
@@ -7,7 +7,7 @@ import { cn } from '~/lib/utils';
 const messageVariants = cva('flex w-full gap-x-2.5 justify-start p-3 text-base', {
   variants: {
     variant: {
-      success: 'bg-success-light/[.15] [&>svg]:text-success',
+      success: 'bg-success/[.15] [&>svg]:text-success',
       error: 'bg-error/[.15] [&>svg]:text-error-secondary',
     },
   },
