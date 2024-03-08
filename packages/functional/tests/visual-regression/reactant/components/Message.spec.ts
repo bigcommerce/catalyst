6cd743965bc3525a4762195556c76ad997b1901f
Jorge Moya <jorge.moya@bigcommerce.com>
2024-03-08 11:27:03 -0600

    fix: title for test

--- packages/functional/tests/visual-regression/reactant/components/Message.spec.ts
+++ packages/functional/tests/visual-regression/reactant/components/Message.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 import * as storyBookElements from '../StoryBookElements';
 
-test('Primary message', async ({ page }) => {
+test('Default message', async ({ page }) => {
   await page.goto(`${storyBookElements.storyUrl}/message--default`);
   await expect(
     page.frameLocator(storyBookElements.storyBookFrame).getByText('Here is your message for Users'),
