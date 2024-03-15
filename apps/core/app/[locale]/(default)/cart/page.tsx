42d3567993cc44da6f7c86b51a410c5caf4abbba
Jorge Moya <jorge.moya@bigcommerce.com>
2024-03-15 10:03:11 -0500

    fix: revert key deletion

--- apps/core/app/[locale]/(default)/cart/page.tsx
+++ apps/core/app/[locale]/(default)/cart/page.tsx
@@ -184,6 +184,7 @@ export default async function CartPage({ params: { locale } }: Props) {
           <NextIntlClientProvider locale={locale} messages={{ Cart: messages.Cart ?? {} }}>
             <CheckoutSummary
               cart={cart}
+              key={cart.totalExtendedListPrice.value}
               shippingCosts={shippingCosts}
               shippingCountries={shippingCountries}
             />
