5af0e66e7b065ea1d158a0d062a6c3216752d5be
Jorge Moya <jorge.moya@bigcommerce.com>
2024-03-19 14:28:13 -0500

    fix(component): extends colors in tailwind config (#680)

--- packages/components/tailwind.config.js
+++ packages/components/tailwind.config.js
@@ -4,31 +4,31 @@
 const config = {
   content: ['./src/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: '#000000',
-      primary: '#053FB0',
-      secondary: '#3071EF',
-      white: '#FFFFFF',
-      error: {
-        DEFAULT: '#AD0000',
-        secondary: '#C62828',
-      },
-      success: {
-        DEFAULT: '#146622',
-        secondary: '#388E3C',
-      },
-      gray: {
-        100: '#F1F3F5',
-        200: '#CFD8DC',
-        300: '#AFBAC5',
-        400: '#90A4AE',
-        500: '#546E7A',
-        600: '#091D45',
-      },
-    },
     extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: '#000000',
+        primary: '#053FB0',
+        secondary: '#3071EF',
+        white: '#FFFFFF',
+        error: {
+          DEFAULT: '#AD0000',
+          secondary: '#C62828',
+        },
+        success: {
+          DEFAULT: '#146622',
+          secondary: '#388E3C',
+        },
+        gray: {
+          100: '#F1F3F5',
+          200: '#CFD8DC',
+          300: '#AFBAC5',
+          400: '#90A4AE',
+          500: '#546E7A',
+          600: '#091D45',
+        },
+      },
       fontFamily: {
         sans: ['var(--font-inter)'],
       },
