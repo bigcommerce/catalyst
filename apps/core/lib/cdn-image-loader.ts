5e6ee15f8276d8851620051b5a5ee4cabd4910bc
Nathan Booker <nathan.booker@bigcommerce.com>
2024-03-19 17:19:58 -0500

    Add compression=lossy param to BC CDN images
    Improves load time

--- apps/core/lib/cdn-image-loader.ts
+++ apps/core/lib/cdn-image-loader.ts
@@ -1,5 +1,13 @@
 'use client';
 
+export const addLossyCompressionParam = (url: string): string => {
+  const urlObj = new URL(url);
+
+  urlObj.searchParams.set('compression', 'lossy');
+
+  return urlObj.toString();
+};
+
 export default function bcCdnImageLoader({
   src,
   width,
@@ -9,9 +17,17 @@ export default function bcCdnImageLoader({
   width: number;
   height?: number;
 }): string {
+  let url;
+
   if (height) {
-    return src.replace('{:size}', `${width}x${height}`);
+    url = src.replace('{:size}', `${width}x${height}`);
   }
 
-  return src.replace('{:size}', `${width}w`);
+  url = src.replace('{:size}', `${width}w`);
+
+  // Add compression=lossy param so the BC CDN will compress the image
+  // This will be added into the GQL API in the future so this can be removed later
+  url = addLossyCompressionParam(url);
+
+  return url;
 }
