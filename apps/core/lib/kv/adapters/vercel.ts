9508b8ab377e61abeb5f6b630cadc2965cf99083
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2024-04-17 11:39:48 -0500

    wip

--- apps/core/lib/kv/adapters/vercel.ts
+++ apps/core/lib/kv/adapters/vercel.ts
@@ -2,14 +2,36 @@ import { kv } from '@vercel/kv';
 
 import { KvAdapter, SetCommandOptions } from '../types';
 
+import { DevKvAdapter } from './dev';
+
+const memoryKv = new DevKvAdapter();
+
 export class VercelKvAdapter implements KvAdapter {
   constructor(private adapter = kv) {}
 
   async get<Data>(key: string) {
+    const memoryValue = await memoryKv.get<Data>(key);
+
+    if (memoryValue) {
+      // eslint-disable-next-line no-console
+      console.log('======= Memory Value =======');
+
+      return memoryValue;
+    }
+
     return this.adapter.get<Data>(key);
   }
 
   async mget<Data>(...keys: string[]) {
+    const memoryValues = await memoryKv.mget<Data>(...keys);
+
+    if (memoryValues.length) {
+      // eslint-disable-next-line no-console
+      console.log('======= Memory Values =======');
+
+      return memoryValues;
+    }
+
     return this.adapter.mget<Data[]>(keys);
   }
 
@@ -18,6 +40,8 @@ export class VercelKvAdapter implements KvAdapter {
     value: Data,
     opts?: Options,
   ) {
+    await memoryKv.set(key, value, opts);
+
     const response = await this.adapter.set(key, value, opts);
 
     if (response === 'OK') {
