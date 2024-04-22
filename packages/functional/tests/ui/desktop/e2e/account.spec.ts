4e87a9447256911dd2eeedf3d00ff8dad3fb02d3
Anastasiia <anastasiia.zvierieva@bigcommerce.com>
2024-04-22 14:00:00 +0300

    feat(core): Add Account creation tests

--- packages/functional/tests/ui/desktop/e2e/account.spec.ts
+++ packages/functional/tests/ui/desktop/e2e/account.spec.ts
@@ -6,3 +6,12 @@ test('Account access is restricted for guest users', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'My Account' })).toBeVisible({ visible: false });
   await expect(page.getByRole('heading', { name: 'Log in' })).toBeVisible();
 });
+
+test('Create user Account', async ({ page }) => {
+  await page.goto('/login/');
+  // click "Create Account" button
+  // Fill out required fields
+  // Click "Submit" button
+  // Verify success message
+  // Make sure user is able to login
+})
