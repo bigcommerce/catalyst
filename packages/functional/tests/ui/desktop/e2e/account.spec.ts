f9dbd46e852029069efd5047b97fa882a6a389af
Anastasiia <anastasiia.zvierieva@bigcommerce.com>
2024-04-25 19:35:55 +0300

    feat(core): Add Account tabs tests

--- packages/functional/tests/ui/desktop/e2e/account.spec.ts
+++ packages/functional/tests/ui/desktop/e2e/account.spec.ts
@@ -11,7 +11,7 @@ test('Account access is restricted for guest users', async ({ page }) => {
 test('My Account tabs are displayed and clickable', async ({ page }) => {
   await LoginPage.loginAsShopper(page);
 
-  await expect(page).toHaveURL('/.*account/');
+  await expect(page).toHaveURL('/account/');
   await expect(page.getByRole('heading', { name: 'Orders' })).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Messages' })).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Addresses' })).toBeVisible();
@@ -20,26 +20,26 @@ test('My Account tabs are displayed and clickable', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Account settings' })).toBeVisible();
 
   await page.getByRole('heading', { name: 'Orders' }).click();
-  await expect(page).toHaveURL('/.*orders/');
+  await expect(page).toHaveURL('account/orders/');
   await expect(page.getByRole('heading', { name: 'Orders' })).toBeVisible();
 
   await page.getByRole('tab', { name: 'Messages' }).click();
-  await expect(page).toHaveURL('/.*messages/');
+  await expect(page).toHaveURL('account/messages/');
   await expect(page.getByRole('heading', { name: 'Messages' })).toBeVisible();
 
   await page.getByRole('tab', { name: 'Addresses' }).click();
-  await expect(page).toHaveURL('/.*addresses/');
+  await expect(page).toHaveURL('account/addresses/');
   await expect(page.getByRole('heading', { name: 'Addresses' })).toBeVisible();
 
   await page.getByRole('tab', { name: 'Wish lists' }).click();
-  await expect(page).toHaveURL('/.*wishlists/');
+  await expect(page).toHaveURL('account/wishlists/');
   await expect(page.getByRole('heading', { name: 'Wish lists' })).toBeVisible();
 
   await page.getByRole('tab', { name: 'Recently viewed' }).click();
-  await expect(page).toHaveURL('/.*recently-viewed/');
+  await expect(page).toHaveURL('account/recently-viewed/');
   await expect(page.getByRole('heading', { name: 'Recently viewed' })).toBeVisible();
 
   await page.getByRole('tab', { name: 'Account settings' }).click();
-  await expect(page).toHaveURL('/.*settings/');
+  await expect(page).toHaveURL('account/settings/');
   await expect(page.getByRole('heading', { name: 'Account settings' })).toBeVisible();
 })
