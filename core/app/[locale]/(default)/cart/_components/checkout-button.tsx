e7b4a59bcb01ea2a64eb47ef3496759d5d73ef59
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2024-05-13 15:23:55 -0500

    chore: fetch checkout redirect url with gql

--- core/app/[locale]/(default)/cart/_components/checkout-button.tsx
+++ core/app/[locale]/(default)/cart/_components/checkout-button.tsx
@@ -1,8 +1,27 @@
-import { getCheckoutUrl } from '~/client/management/get-checkout-url';
+import { client } from '~/client';
+import { graphql } from '~/client/graphql';
 import { Button } from '~/components/ui/button';
 
+export const CheckoutButtonMutation = graphql(`
+  mutation CheckoutButtonMutation($cartId: String!) {
+    cart {
+      createCartRedirectUrls(input: { cartEntityId: $cartId }) {
+        redirectUrls {
+          redirectedCheckoutUrl
+        }
+      }
+    }
+  }
+`);
+
 export const CheckoutButton = async ({ cartId, label }: { cartId: string; label: string }) => {
-  const checkoutUrl = await getCheckoutUrl(cartId);
+  const { data } = await client.fetch({
+    document: CheckoutButtonMutation,
+    variables: { cartId },
+    fetchOptions: { cache: 'no-store' },
+  });
+
+  const checkoutUrl = data.cart.createCartRedirectUrls.redirectUrls?.redirectedCheckoutUrl;
 
   return (
     <Button asChild className="mt-6">
