edb806703d237088225ff520de2cd309a541346b
Matthew Volk <matt.volk@bigcommerce.com>
2024-05-17 13:54:55 -0500

    fix: eslint config resolution in core (#920)

--- core/.eslintrc.cjs
+++ core/.eslintrc.cjs
@@ -1,5 +1,8 @@
 // @ts-check
 
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@bigcommerce/eslint-config/patch');
+
 /** @type {import('eslint').Linter.Config} */
 const config = {
   root: true,
