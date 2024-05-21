1cfb8a22f3b54c2316c938c88af7eaef8818f22b
“bc-yevhenii-buliuk” <yevhenii.buliuk@bigcommerce.com>
2024-05-21 21:47:04 +0300

    fix(core): update icons on the account page

--- core/app/[locale]/(default)/account/page.tsx
+++ core/app/[locale]/(default)/account/page.tsx
@@ -42,22 +42,22 @@ export default async function AccountPage({ params: { locale } }: Props) {
 
       <div className="mb-14 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         <AccountItem href="/account/orders" title={t('orders')}>
-          <Package className="me-8" height={48} width={48} />
+          <Package className="me-8" size={48} strokeWidth={1.5} />
         </AccountItem>
         <AccountItem href="/account/messages" title={t('messages')}>
-          <Mail className="me-8" height={48} width={48} />
+          <Mail className="me-8" size={48} strokeWidth={1.5} />
         </AccountItem>
         <AccountItem href="/account/addresses" title={t('addresses')}>
-          <BookUser className="me-8" height={48} width={48} />
+          <BookUser className="me-8" size={48} strokeWidth={1.5} />
         </AccountItem>
         <AccountItem href="/account/wishlists" title={t('wishlists')}>
-          <Gift className="me-8" height={48} width={48} />
+          <Gift className="me-8" size={48} strokeWidth={1.5} />
         </AccountItem>
         <AccountItem href="/account/recently-viewed" title={t('recentlyViewed')}>
-          <Eye className="me-8" height={48} width={48} />
+          <Eye className="me-8" size={48} strokeWidth={1.5} />
         </AccountItem>
         <AccountItem href="/account/settings" title={t('settings')}>
-          <Settings className="me-8" height={48} width={48} />
+          <Settings className="me-8" size={48} strokeWidth={1.5} />
         </AccountItem>
       </div>
     </div>
