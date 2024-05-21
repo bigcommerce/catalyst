8416cd068c541c9e298ca31422ba5954a59dc868
Matthew Volk <matt.volk@bigcommerce.com>
2024-05-21 11:13:10 -0500

    refactor: better node version check (#936)

---
+++ packages/create-catalyst/bin/index.cjs
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const semver = require('semver');
+
+const catalystRequiredNodeVersion = '>=20';
+const userNodeVersion = process.version;
+
+if (!semver.satisfies(userNodeVersion, catalystRequiredNodeVersion)) {
+  console.error(`\n\x1b[31mYou are using Node.js version: ${userNodeVersion}.`);
+  console.error(`Catalyst requires Node.js version: ${catalystRequiredNodeVersion}.\x1b[0m\n`);
+  process.exit(1);
+}
+
+// eslint-disable-next-line import/dynamic-import-chunkname, import/extensions
+import('../dist/index.js').catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
