ee4fa15c2b6a44089b0921a7859a0e09ee1c4887
bookernath <nathan.booker@bigcommerce.com>
2024-05-31 17:07:02 -0500

    Add sitemap proxy

--- core/middleware.ts
+++ core/middleware.ts
@@ -13,7 +13,10 @@ export const config = {
      * - _next/image (image optimization files)
      * - _vercel (vercel internals, eg: web vitals)
      * - favicon.ico (favicon file)
+     * - admin (admin panel)
+     * - sitemap.xml (sitemap route)
+     * - xmlsitemap.php (legacy sitemap route)
      */
-    '/((?!api|admin|_next/static|_next/image|_vercel|favicon.ico).*)',
+    '/((?!api|admin|_next/static|_next/image|_vercel|favicon.ico|xmlsitemap.php|sitemap.xml).*)',
   ],
 };
