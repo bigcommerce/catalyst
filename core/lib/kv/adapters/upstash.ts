bf4739d0977deb69f3bc1cf0e70f4c96b60c6d89
Jorge Moya <jorge.moya@bigcommerce.com>
2024-06-04 15:33:49 -0500

    feat(core): add upstash kv adapter (#977)
    * feat(core): add upstash kv adapter
    
    * fix: remove constructor

---
+++ core/lib/kv/adapters/upstash.ts
@@ -0,0 +1,40 @@
+import { Redis } from '@upstash/redis';
+
+import { KvAdapter, SetCommandOptions } from '../types';
+
+import { MemoryKvAdapter } from './memory';
+
+const memoryKv = new MemoryKvAdapter();
+
+export class UpstashKvAdapter implements KvAdapter {
+  private upstashKv = Redis.fromEnv();
+  private memoryKv = memoryKv;
+
+  async get<Data>(key: string) {
+    const memoryValue = await this.memoryKv.get<Data>(key);
+
+    return memoryValue ?? this.upstashKv.get<Data>(key);
+  }
+
+  async mget<Data>(...keys: string[]) {
+    const memoryValues = await this.memoryKv.mget<Data>(...keys);
+
+    return memoryValues.length ? memoryValues : this.upstashKv.mget<Data[]>(keys);
+  }
+
+  async set<Data, Options extends SetCommandOptions = SetCommandOptions>(
+    key: string,
+    value: Data,
+    opts?: Options,
+  ) {
+    await this.memoryKv.set(key, value, opts);
+
+    const response = await this.upstashKv.set(key, value, opts);
+
+    if (response === 'OK') {
+      return null;
+    }
+
+    return response;
+  }
+}
