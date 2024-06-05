30bd697a0d279c59bd52734d25c7e1ca25c1a9f4
Yurii Zusik <66325265+yurytut1993@users.noreply.github.com>
2024-06-05 23:35:31 +0300

    feat(core): add delete wishlist mutation (#975)

---
+++ core/client/mutations/delete-wishlists.ts
@@ -0,0 +1,30 @@
+import { getSessionCustomerId } from '~/auth';
+
+import { client } from '..';
+import { graphql, VariablesOf } from '../graphql';
+
+const DELETE_WISHLISTS_MUTATION = graphql(`
+  mutation DeleteWishlists($input: DeleteWishlistsInput!) {
+    wishlist {
+      deleteWishlists(input: $input) {
+        result
+      }
+    }
+  }
+`);
+
+type Variables = VariablesOf<typeof DELETE_WISHLISTS_MUTATION>;
+export type Input = Variables['input'];
+
+export const deleteWishlists = async (input: Input) => {
+  const customerId = await getSessionCustomerId();
+
+  const response = await client.fetch({
+    document: DELETE_WISHLISTS_MUTATION,
+    variables: { input },
+    customerId,
+    fetchOptions: { cache: 'no-store' },
+  });
+
+  return response.data.wishlist.deleteWishlists?.result;
+};
