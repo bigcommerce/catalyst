9cb6ac56f04e5e3efcb434d9e6004efb80f9e539
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-06-11 16:19:17 -0400

    refactor: use otel environment variables

--- core/instrumentation.ts
+++ core/instrumentation.ts
@@ -1,16 +1,5 @@
-import { OTLPHttpProtoTraceExporter, registerOTel } from '@vercel/otel';
-
-const host: string = process.env.COLLECTOR_HOST ?? 'ingest.lightstep.com';
-const port: string = process.env.COLLECTOR_PORT ?? '443';
-const otlpApiPath: string = process.env.API_PATH ?? 'v1/traces';
-
-const lsTraceExporter = new OTLPHttpProtoTraceExporter({
-  url: `https://${host}:${port}/${otlpApiPath}`,
-  headers: {
-    'lightstep-access-token': process.env.LIGHTSTEP_ACCESS_TOKEN || '',
-  },
-});
+import { registerOTel } from '@vercel/otel';
 
 export function register() {
-  registerOTel({ serviceName: 'catalyst', traceExporter: lsTraceExporter });
+  registerOTel({ serviceName: 'catalyst' });
 }
