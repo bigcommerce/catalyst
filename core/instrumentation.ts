ca2d19010d4789bd73deb81ca117910382ae2ae7
Anudeep Vattipalli <anudeep.vattipalli@bigcommerce.com>
2024-06-13 12:21:05 -0500

    fix: add trace exporter option

--- core/instrumentation.ts
+++ core/instrumentation.ts
@@ -1,5 +1,12 @@
-import { registerOTel } from '@vercel/otel';
+import { OTLPHttpProtoTraceExporter, registerOTel } from '@vercel/otel';
+
+const traceExporter = new OTLPHttpProtoTraceExporter({
+  url: 'https://ingest.lightstep.com:443',
+  headers: {
+    'lightstep-access-token': process.env.LIGHTSTEP_ACCESS_TOKEN || '',
+  },
+});
 
 export function register() {
-  registerOTel({ serviceName: 'catalyst' });
+  registerOTel({ serviceName: 'catalyst', traceExporter });
 }
