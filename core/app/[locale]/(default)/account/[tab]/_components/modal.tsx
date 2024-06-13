5d33257582103da20dbe83e633da9e745d910adb
yurytut1993 <yurii.zusik@bigcommerce.com>
2024-06-13 16:43:51 +0300

    feat(core): update customer account modal component

--- core/app/[locale]/(default)/account/[tab]/_components/modal.tsx
+++ core/app/[locale]/(default)/account/[tab]/_components/modal.tsx
@@ -1,7 +1,5 @@
-'use client';
-
 import { X } from 'lucide-react';
-import { MouseEventHandler, PropsWithChildren } from 'react';
+import { Dispatch, MouseEventHandler, PropsWithChildren, SetStateAction } from 'react';
 
 import { Button } from '~/components/ui/button';
 import {
@@ -19,9 +17,13 @@ import {
 interface Props extends PropsWithChildren {
   actionHandler?: MouseEventHandler<HTMLButtonElement>;
   title: string;
+  trigger: React.ReactNode;
   descriptionText?: string;
   confirmationText?: string;
   abortText?: string;
+  open?: boolean;
+  setOpen?: Dispatch<SetStateAction<boolean>>;
+  showCancelButton?: boolean;
 }
 
 export const Modal = ({
@@ -29,21 +31,25 @@ export const Modal = ({
   actionHandler,
   confirmationText = 'OK',
   descriptionText,
+  open,
+  setOpen,
+  showCancelButton = true,
   title,
+  trigger,
   children,
 }: Props) => {
   return (
-    <Dialog>
+    <Dialog onOpenChange={setOpen} open={open}>
       <DialogTrigger aria-controls="modal-content" asChild>
-        {children}
+        {trigger}
       </DialogTrigger>
       <DialogPortal>
         <DialogOverlay />
         <DialogContent className="w-full sm:w-8/12 md:w-6/12 lg:w-5/12" id="modal-content">
-          <div className="flex justify-between gap-4 p-6">
-            <DialogTitle>{title}</DialogTitle>
+          <div className="flex items-start justify-between gap-4 p-6">
+            <DialogTitle className="text-xl lg:text-2xl">{title}</DialogTitle>
             <DialogCancel asChild>
-              <Button className="ms-auto w-min p-2" type="button" variant="subtle">
+              <Button className="ms-auto w-min p-0" type="button" variant="subtle">
                 <X>
                   <title>{abortText}</title>
                 </X>
@@ -52,16 +58,23 @@ export const Modal = ({
           </div>
           {Boolean(descriptionText) && <DialogDescription>{descriptionText}</DialogDescription>}
           <div className="flex flex-col gap-2 p-6 lg:flex-row">
-            <DialogAction asChild>
-              <Button className="w-full lg:w-fit" onClick={actionHandler} variant="primary">
-                {confirmationText}
-              </Button>
-            </DialogAction>
-            <DialogCancel asChild>
-              <Button className="w-full lg:w-fit" variant="subtle">
-                {abortText}
-              </Button>
-            </DialogCancel>
+            {children}
+            <div className="flex flex-col lg:flex-row">
+              {actionHandler && (
+                <DialogAction asChild>
+                  <Button className="w-full lg:w-fit" onClick={actionHandler} variant="primary">
+                    {confirmationText}
+                  </Button>
+                </DialogAction>
+              )}
+              {showCancelButton && (
+                <DialogCancel asChild>
+                  <Button className="mt-2 w-full lg:ms-2 lg:mt-0 lg:w-fit" variant="subtle">
+                    {abortText}
+                  </Button>
+                </DialogCancel>
+              )}
+            </div>
           </div>
         </DialogContent>
       </DialogPortal>
