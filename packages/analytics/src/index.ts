5c31adeb3bd6aa149d54e6f24986e7f5f0547bb3
Volodymyr Krasnoshapka <volodymyr.kras@bigcommerce.com>
2024-06-20 11:31:37 +0200

    feat(core): removed initialized property

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -12,48 +12,50 @@ import {
 export { BodlProvider } from './providers/bodl';
 
 export class AnalyticsProvider implements BaseProvider {
-  private initialized = false;
-
   constructor(private providers: Provider[]) {}
 
-  private call(func: (provider: Provider) => void): void {
-    // Make sure init method is called only once before every event
-    if (!this.initialized) {
-      this.providers.forEach((provider) => provider.init());
-      this.initialized = true;
-    }
+  private call() {
+    let initialized = false;
 
-    this.providers.forEach(func);
+    return (func: (provider: Provider) => void) => {
+      // Make sure init method is called only once before every event
+      if (!initialized) {
+        this.providers.forEach((provider) => provider.init());
+        initialized = true;
+      }
+
+      this.providers.forEach(func);
+    };
   }
 
   // eslint-disable-next-line @typescript-eslint/member-ordering
   navigation = {
     search: (payload: SearchPayload) => {
-      this.call((provider) => provider.navigation.search(payload));
+      this.call()((provider) => provider.navigation.search(payload));
     },
     productViewed: (payload: ProductViewedPayload) => {
-      this.call((provider) => provider.navigation.productViewed(payload));
+      this.call()((provider) => provider.navigation.productViewed(payload));
     },
     categoryViewed: (payload: CategoryViewedPayload) => {
-      this.call((provider) => provider.navigation.categoryViewed(payload));
+      this.call()((provider) => provider.navigation.categoryViewed(payload));
     },
   };
 
   // eslint-disable-next-line @typescript-eslint/member-ordering
   cart = {
     productAdded: (payload: ProductAddedPayload) => {
-      this.call((provider) => provider.cart.productAdded(payload));
+      this.call()((provider) => provider.cart.productAdded(payload));
     },
     productRemoved: (payload: ProductRemovedPayload) => {
-      this.call((provider) => provider.cart.productRemoved(payload));
+      this.call()((provider) => provider.cart.productRemoved(payload));
     },
     cartViewed: (payload: CartViewedPayload) => {
-      this.call((provider) => provider.cart.cartViewed(payload));
+      this.call()((provider) => provider.cart.cartViewed(payload));
     },
   };
 
   // eslint-disable-next-line @typescript-eslint/member-ordering
   customEvent(payload: unknown): void {
-    this.call((provider) => provider.customEvent(payload));
+    this.call()((provider) => provider.customEvent(payload));
   }
 }
