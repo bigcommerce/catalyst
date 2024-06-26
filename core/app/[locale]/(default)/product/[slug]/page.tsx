fdfb99530358807736a8237000aae1697c9896e3
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2024-06-26 14:36:47 -0500

    fix: pre selected options on PDP page

--- core/app/[locale]/(default)/product/[slug]/page.tsx
+++ core/app/[locale]/(default)/product/[slug]/page.tsx
@@ -28,7 +28,11 @@ export async function generateMetadata({
   const productId = Number(params.slug);
   const optionValueIds = getOptionValueIds({ searchParams });
 
-  const product = await getProduct({ entityId: productId, optionValueIds });
+  const product = await getProduct({
+    entityId: productId,
+    optionValueIds,
+    useDefaultOptionSelections: optionValueIds.length === 0 ? true : undefined,
+  });
 
   if (!product) {
     return {};
@@ -66,7 +70,11 @@ export default async function Product({ params, searchParams }: ProductPageProps
 
   const optionValueIds = getOptionValueIds({ searchParams });
 
-  const product = await getProduct({ entityId: productId, optionValueIds });
+  const product = await getProduct({
+    entityId: productId,
+    optionValueIds,
+    useDefaultOptionSelections: optionValueIds.length === 0 ? true : undefined,
+  });
 
   if (!product) {
     return notFound();
