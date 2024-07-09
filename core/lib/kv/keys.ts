f4914a3c2e111be2996c19bdffd8228d9238c896
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-09 11:13:25 -0500

    fix: version const

--- core/lib/kv/keys.ts
+++ core/lib/kv/keys.ts
@@ -1,8 +1,10 @@
+const VERSION = 'v3';
+
 export const STORE_STATUS_KEY = 'storeStatus';
 
 export const kvKey = (key: string, channelId?: string) => {
   const namespace = process.env.KV_NAMESPACE ?? process.env.BIGCOMMERCE_STORE_HASH ?? 'store';
   const id = channelId ?? process.env.BIGCOMMERCE_CHANNEL_ID ?? '1';
 
-  return `${namespace}_${id}_v3_${key}`;
+  return `${namespace}_${id}_${VERSION}_${key}`;
 };
