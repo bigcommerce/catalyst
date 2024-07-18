e492efdde939ff8aebd3b0c4971db0d0d9612ad7
Drew Pledger <drew@makeswift.com>
2024-07-18 16:10:38 -0500

    refactor: tabs + usage

--- core/components/ui/tabs/tabs.tsx
+++ core/components/ui/tabs/tabs.tsx
@@ -3,44 +3,38 @@ import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react';
 
 import { cn } from '~/lib/utils';
 
-const Tabs = TabsPrimitive.Root;
+type Tab = {
+  content: React.ReactNode;
+  title: string | React.ReactNode;
+  value: string;
+};
+interface Props {
+  className?: string;
+  defaultValue?: string;
+  tabs: Tab[];
+}
 
-const TabsList = forwardRef<
-  ElementRef<typeof TabsPrimitive.List>,
-  ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    className={cn('mb-8 flex list-none items-start overflow-x-auto text-base', className)}
-    ref={ref}
-    {...props}
-  />
-));
-
-TabsList.displayName = TabsPrimitive.List.displayName;
-
-const TabsTrigger = forwardRef<
-  ElementRef<typeof TabsPrimitive.Trigger>,
-  ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    className={cn(
-      'px-4 pb-2 data-[state=active]:border-b-4 data-[state=active]:border-primary data-[state=active]:text-primary',
-      className,
-    )}
-    ref={ref}
-    {...props}
-  />
-));
-
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
-
-const TabsContent = forwardRef<
-  ElementRef<typeof TabsPrimitive.Content>,
-  ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content className={cn('', className)} ref={ref} {...props} />
-));
-
-TabsContent.displayName = TabsPrimitive.Content.displayName;
-
-export { Tabs, TabsList, TabsTrigger, TabsContent };
+export function Tabs({ className, tabs, defaultValue }: Props) {
+  return (
+    <TabsPrimitive.Root activationMode="manual" defaultValue={defaultValue} className={className}>
+      <TabsPrimitive.List className="mb-8 flex list-none items-start overflow-x-auto text-base">
+        {tabs.map((tab) => (
+          <TabsPrimitive.Trigger
+            key={tab.value}
+            className={
+              'px-4 pb-2 data-[state=active]:border-b-4 data-[state=active]:border-primary data-[state=active]:text-primary'
+            }
+            value={tab.value}
+          >
+            {tab.title}
+          </TabsPrimitive.Trigger>
+        ))}
+      </TabsPrimitive.List>
+      {tabs.map((tab) => (
+        <TabsPrimitive.Content key={tab.value} value={tab.value}>
+          {tab.content}
+        </TabsPrimitive.Content>
+      ))}
+    </TabsPrimitive.Root>
+  );
+}
