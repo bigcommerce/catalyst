3647bc972815c5286d58f393f614da25f899ade7
Drew Pledger <drew@makeswift.com>
2024-07-19 13:05:44 -0400

    fix: merge with main (#1105)

--- core/app/[locale]/(default)/account/[tab]/_components/account-tabs.tsx
+++ core/app/[locale]/(default)/account/[tab]/_components/account-tabs.tsx
@@ -4,7 +4,7 @@ import { useTranslations } from 'next-intl';
 import { PropsWithChildren, useEffect } from 'react';
 
 import { Link } from '~/components/link';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
+import { Tabs } from '~/components/ui/tabs';
 
 import { TabType } from '../layout';
 
@@ -30,23 +30,22 @@ export const AccountTabs = ({ children, activeTab, tabs }: Props) => {
   }, [activeTab, activeTabRef]);
 
   return (
-    <Tabs activationMode="manual" defaultValue={activeTab}>
-      <TabsList aria-label={t('accountTabsLabel')} className="mb-5 pb-3 pt-1">
-        {tabs.map((tab) => (
-          <TabsTrigger asChild key={tab} value={tab}>
-            <Link
-              className="whitespace-nowrap font-semibold"
-              href={`/account/${tab}`}
-              prefetch="viewport"
-              prefetchKind="full"
-              ref={activeTab === tab ? activeTabRef : null}
-            >
-              {tab === 'recently-viewed' ? t('recentlyViewed') : t(tab)}
-            </Link>
-          </TabsTrigger>
-        ))}
-      </TabsList>
-      <TabsContent value={activeTab ?? ''}>{children}</TabsContent>
-    </Tabs>
+    <Tabs
+      defaultValue={activeTab}
+      tabs={tabs.map((tab) => ({
+        value: tab,
+        title: (
+          <Link
+            className="whitespace-nowrap font-semibold"
+            href={`/account/${tab}`}
+            prefetch="viewport"
+            prefetchKind="full"
+          >
+            {tab === 'recently-viewed' ? t('recentlyViewed') : t(tab)}
+          </Link>
+        ),
+        content: children,
+      }))}
+    />
   );
 };
