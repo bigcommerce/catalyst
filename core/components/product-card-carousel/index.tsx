d839e32ad5ad1694d7a19bf527233170e69d087a
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2024-07-19 11:23:33 -0500

    refactor: carousel

--- core/components/product-card-carousel/index.tsx
+++ core/components/product-card-carousel/index.tsx
@@ -1,17 +1,7 @@
-import { useId } from 'react';
-
 import { graphql, ResultOf } from '~/client/graphql';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNextIndicator,
-  CarouselPreviousIndicator,
-} from '~/components/ui/carousel';
+import { Carousel } from '~/components/ui/carousel';
 
 import { ProductCard, ProductCardFragment } from '../product-card';
-
-import { Pagination } from './pagination';
 
 export const ProductCardCarouselFragment = graphql(
   `
@@ -37,60 +27,20 @@ export const ProductCardCarousel = ({
   showCompare?: boolean;
   showReviews?: boolean;
 }) => {
-  const id = useId();
-  const titleId = useId();
-
   if (products.length === 0) {
     return null;
   }
 
-  const groupedProducts = products.reduce<Product[][]>((batches, _, index) => {
-    if (index % 4 === 0) {
-      batches.push([]);
-    }
+  const items = products.map((product) => (
+    <ProductCard
+      imageSize="tall"
+      key={product.entityId}
+      product={product}
+      showCart={showCart}
+      showCompare={showCompare}
+      showReviews={showReviews}
+    />
+  ));
 
-    const product = products[index];
-
-    if (batches[batches.length - 1] && product) {
-      batches[batches.length - 1]?.push(product);
-    }
-
-    return batches;
-  }, []);
-
-  return (
-    <Carousel aria-labelledby={titleId} className="mb-14" opts={{ loop: true }}>
-      <div className="flex items-center justify-between">
-        <h2 className="text-3xl font-black lg:text-4xl" id={titleId}>
-          {title}
-        </h2>
-        <span className="no-wrap flex">
-          <CarouselPreviousIndicator />
-          <CarouselNextIndicator />
-        </span>
-      </div>
-      <CarouselContent>
-        {groupedProducts.map((group, index) => (
-          <CarouselItem
-            aria-label={`${index + 1} of ${groupedProducts.length}`}
-            id={`${id}-slide-${index + 1}`}
-            index={index}
-            key={index}
-          >
-            {group.map((product) => (
-              <ProductCard
-                imageSize="tall"
-                key={product.entityId}
-                product={product}
-                showCart={showCart}
-                showCompare={showCompare}
-                showReviews={showReviews}
-              />
-            ))}
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <Pagination groupedProducts={groupedProducts} id={id} />
-    </Carousel>
-  );
+  return <Carousel className="mb-14" items={items} title={title} />;
 };
