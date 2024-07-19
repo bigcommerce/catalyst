b06fe39e7bee8b9b796662f35881ac95870cb9d2
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-19 14:24:59 -0500

    refactor: button (#1116)

--- core/components/ui/button/button.tsx
+++ core/components/ui/button/button.tsx
@@ -1,64 +1,58 @@
 import { Slot } from '@radix-ui/react-slot';
-import { cva } from 'class-variance-authority';
 import { Loader2 as Spinner } from 'lucide-react';
-import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react';
+import { ComponentPropsWithoutRef } from 'react';
 
 import { cn } from '~/lib/utils';
 
-export const buttonVariants = cva(
-  'relative flex w-full justify-center items-center border-2 py-2.5 px-[30px] text-base leading-6 font-semibold border-primary disabled:border-gray-400 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary/20',
-  {
-    variants: {
-      variant: {
-        primary:
-          'bg-primary text-white hover:bg-secondary hover:border-secondary disabled:bg-gray-400 disabled:hover:bg-gray-400 disabled:hover:border-gray-400',
-        secondary:
-          'bg-transparent text-primary hover:bg-secondary hover:bg-opacity-10 hover:border-secondary hover:text-secondary disabled:text-gray-400 disabled:hover:bg-transparent disabled:hover:border-gray-400 disabled:hover:text-gray-400',
-        subtle:
-          'border-none bg-transparent text-primary hover:bg-secondary hover:bg-opacity-10 hover:text-secondary disabled:text-gray-400 disabled:hover:bg-transparent disabled:hover:text-gray-400',
-      },
-    },
-    defaultVariants: {
-      variant: 'primary',
-    },
-  },
-);
-
-export interface ButtonProps extends ComponentPropsWithRef<'button'> {
+interface Props extends ComponentPropsWithoutRef<'button'> {
   variant?: 'primary' | 'secondary' | 'subtle';
   asChild?: boolean;
   loading?: boolean;
   loadingText?: string;
 }
 
-export const Button = forwardRef<ElementRef<'button'>, ButtonProps>(
-  (
-    { asChild = false, children, className, variant, loading, loadingText, disabled, ...props },
-    ref,
-  ) => {
-    const Comp = asChild ? Slot : 'button';
+const Button = ({
+  asChild = false,
+  children,
+  className,
+  variant = 'primary',
+  loading,
+  loadingText,
+  disabled,
+  ...props
+}: Props) => {
+  const Comp = asChild ? Slot : 'button';
 
-    return (
-      <Comp
-        className={cn(buttonVariants({ variant, className }))}
-        disabled={disabled || loading}
-        ref={ref}
-        {...props}
-      >
-        {loading ? (
-          <>
-            <span className="absolute z-10 flex h-full w-full items-center justify-center">
-              <Spinner aria-hidden="true" className="animate-spin" />
-              <span className="sr-only">{loadingText}</span>
-            </span>
-            <span className={cn('invisible flex items-center')}>{children}</span>
-          </>
-        ) : (
-          children
-        )}
-      </Comp>
-    );
-  },
-);
+  return (
+    <Comp
+      className={cn(
+        'relative flex w-full items-center justify-center border-2 border-primary px-[30px] py-2.5 text-base font-semibold leading-6 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary/20 disabled:border-gray-400',
+        variant === 'primary' &&
+          'bg-primary text-white hover:border-secondary hover:bg-secondary disabled:bg-gray-400 disabled:hover:border-gray-400 disabled:hover:bg-gray-400',
+        variant === 'secondary' &&
+          'bg-transparent text-primary hover:border-secondary hover:bg-secondary hover:bg-opacity-10 hover:text-secondary disabled:text-gray-400 disabled:hover:border-gray-400 disabled:hover:bg-transparent disabled:hover:text-gray-400',
+        variant === 'subtle' &&
+          'border-none bg-transparent text-primary hover:bg-secondary hover:bg-opacity-10 hover:text-secondary disabled:text-gray-400 disabled:hover:bg-transparent disabled:hover:text-gray-400',
+        className,
+      )}
+      disabled={disabled || loading}
+      {...props}
+    >
+      {loading ? (
+        <>
+          <span className="absolute z-10 flex h-full w-full items-center justify-center">
+            <Spinner aria-hidden="true" className="animate-spin" />
+            <span className="sr-only">{loadingText}</span>
+          </span>
+          <span className="invisible flex items-center">{children}</span>
+        </>
+      ) : (
+        children
+      )}
+    </Comp>
+  );
+};
 
 Button.displayName = 'Button';
+
+export { Button };
