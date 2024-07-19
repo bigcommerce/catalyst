5731411e5c8e9db88c54a7beede516861fda5b28
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2024-07-19 13:58:29 -0500

    refactor: message component (#1112)

--- core/components/ui/message/message.tsx
+++ core/components/ui/message/message.tsx
@@ -1,32 +1,28 @@
-import { cva } from 'class-variance-authority';
 import { AlertCircle, Check, Info } from 'lucide-react';
-import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react';
+import { ComponentPropsWithoutRef } from 'react';
 
 import { cn } from '~/lib/utils';
 
-const messageVariants = cva('flex w-full gap-x-2.5 justify-start p-3 text-base', {
-  variants: {
-    variant: {
-      info: 'bg-secondary/[.15] [&>svg]:text-primary',
-      success: 'bg-success-secondary/[.15] [&>svg]:text-success',
-      error: 'bg-error-secondary/[.15] [&>svg]:text-error',
-    },
-  },
-});
-
-interface MessageProps extends ComponentPropsWithRef<'div'> {
+interface Props extends ComponentPropsWithoutRef<'div'> {
   readonly variant?: 'info' | 'error' | 'success';
 }
 
-export const Message = forwardRef<ElementRef<'div'>, MessageProps>(
-  ({ className, children, variant = 'info', ...props }, ref) => (
-    <div className={cn(messageVariants({ variant, className }))} ref={ref} {...props}>
-      {variant === 'info' && <Info className="flex-none" />}
-      {variant === 'error' && <AlertCircle className="flex-none" />}
-      {variant === 'success' && <Check className="flex-none" />}
-      {children}
-    </div>
-  ),
+const Message = ({ className, children, variant = 'info', ...props }: Props) => (
+  <div
+    className={cn(
+      'flex w-full justify-start gap-x-2.5 p-3 text-base',
+      variant === 'info' && 'bg-secondary/[.15] [&>svg]:text-primary',
+      variant === 'success' && 'bg-success-secondary/[.15] [&>svg]:text-success',
+      variant === 'error' && 'bg-error-secondary/[.15] [&>svg]:text-error',
+      className,
+    )}
+    {...props}
+  >
+    {variant === 'info' && <Info className="flex-none" />}
+    {variant === 'error' && <AlertCircle className="flex-none" />}
+    {variant === 'success' && <Check className="flex-none" />}
+    {children}
+  </div>
 );
 
-Message.displayName = 'Message';
+export { Message };
