c682e6b58c0dd38c3c516ac212364e8f4bb736ce
Drew Pledger <drew@makeswift.com>
2024-07-19 13:22:47 -0400

    refactor: badge (#1108)

--- core/components/ui/badge/badge.tsx
+++ core/components/ui/badge/badge.tsx
@@ -1,22 +1,23 @@
-import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react';
+import { ComponentPropsWithoutRef } from 'react';
 
 import { cn } from '~/lib/utils';
 
-export const Badge = forwardRef<ElementRef<'span'>, ComponentPropsWithRef<'span'>>(
-  ({ children, className, ...props }, ref) => {
-    return (
-      <span
-        className={cn(
-          'absolute end-0 top-0 min-w-[24px] rounded-[28px] border-2 border-white bg-primary px-1 py-px text-center text-xs font-bold font-normal leading-normal text-white',
-          className,
-        )}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </span>
-    );
-  },
-);
+interface Props extends ComponentPropsWithoutRef<'span'> {
+  label: string;
+}
 
-Badge.displayName = 'Badge';
+const Badge = ({ children, className, label, ...props }: Props) => {
+  return (
+    <span
+      className={cn(
+        'absolute end-0 top-0 min-w-[24px] rounded-[28px] border-2 border-white bg-primary px-1 py-px text-center text-xs font-bold font-normal leading-normal text-white',
+        className,
+      )}
+      {...props}
+    >
+      {label}
+    </span>
+  );
+};
+
+export default Badge;
