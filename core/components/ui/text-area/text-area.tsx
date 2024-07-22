3889765d073f5bada0ad7de8b87766901c908fd1
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-22 15:56:59 -0500

    refactor: textarea

--- core/components/ui/text-area/text-area.tsx
+++ core/components/ui/text-area/text-area.tsx
@@ -1,33 +1,24 @@
-import { cva } from 'class-variance-authority';
 import { ComponentPropsWithRef, ElementRef, forwardRef } from 'react';
 
 import { cn } from '~/lib/utils';
 
-const textAreaVariants = cva(
-  'focus-visible:ring-primary/20 h-[64px] w-full border-2 border-gray-200 py-2.5 px-4 hover:border-primary focus-visible:border-primary focus-visible:outline-none focus-visible:ring-4',
-  {
-    variants: {
-      variant: {
-        success:
-          'pe-12 border-success-secondary focus-visible:border-success-secondary focus-visible:ring-success-secondary/20 disabled:border-gray-200 hover:border-success',
-        error:
-          'ring-error-secondary/20 focus-visible:ring-error-secondary/20 !border-error-secondary pe-12 hover:border-error-secondary focus-visible:border-error-secondary  disabled:border-gray-200',
-      },
-    },
-  },
-);
-
-interface TextAreaProps extends ComponentPropsWithRef<'textarea'> {
+interface Props extends ComponentPropsWithRef<'textarea'> {
   variant?: 'error' | 'success';
 }
 
-export const TextArea = forwardRef<ElementRef<'textarea'>, TextAreaProps>(
-  ({ className, required, variant, ...props }, ref) => {
+const TextArea = forwardRef<ElementRef<'textarea'>, Props>(
+  ({ className, variant, ...props }, ref) => {
     return (
       <textarea
-        className={cn(textAreaVariants({ variant, className }))}
+        className={cn(
+          'h-[64px] w-full border-2 border-gray-200 px-4 py-2.5 hover:border-primary focus-visible:border-primary focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary/20',
+          variant === 'success' &&
+            'border-success-secondary pe-12 hover:border-success focus-visible:border-success-secondary focus-visible:ring-success-secondary/20 disabled:border-gray-200',
+          variant === 'error' &&
+            '!border-error-secondary pe-12 ring-error-secondary/20 hover:border-error-secondary focus-visible:border-error-secondary focus-visible:ring-error-secondary/20 disabled:border-gray-200',
+          className,
+        )}
         ref={ref}
-        required={required}
         {...props}
       />
     );
@@ -35,3 +26,5 @@ export const TextArea = forwardRef<ElementRef<'textarea'>, TextAreaProps>(
 );
 
 TextArea.displayName = 'TextArea';
+
+export { TextArea };
