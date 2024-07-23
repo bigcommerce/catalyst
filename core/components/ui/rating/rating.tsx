dc99a713d5114392faffd64238f29833fe62a91f
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-23 16:41:46 -0500

    refactor: rating

--- core/components/ui/rating/rating.tsx
+++ core/components/ui/rating/rating.tsx
@@ -1,12 +1,5 @@
 import { LucideIcon } from 'lucide-react';
-import {
-  ComponentPropsWithoutRef,
-  ComponentPropsWithRef,
-  ElementRef,
-  FC,
-  forwardRef,
-  ReactElement,
-} from 'react';
+import { ComponentPropsWithoutRef, FC, ReactElement } from 'react';
 
 import { cn } from '~/lib/utils';
 
@@ -21,40 +14,48 @@ const roundHalf = (num: number) => {
 
 type StarIconType = FC<ComponentPropsWithoutRef<'svg'>> | LucideIcon;
 
-interface RatingProps extends ComponentPropsWithRef<'img'> {
+interface Props extends ComponentPropsWithoutRef<'span'> {
+  size?: number;
   starEmptyIcon?: StarIconType;
   starFilledIcon?: StarIconType;
   starHalfIcon?: StarIconType;
-  size?: number;
   strokeColor?: string;
   value: number;
 }
 
-export const Rating = forwardRef<ElementRef<'img'>, RatingProps>(
-  ({ className, starFilledIcon, starHalfIcon, starEmptyIcon, size = 24, value, ...props }, ref) => {
-    const stars: ReactElement[] = [];
-    const rating = roundHalf(value);
+const Rating = ({
+  className,
+  size = 24,
+  starEmptyIcon,
+  starFilledIcon,
+  starHalfIcon,
+  value,
+  ...props
+}: Props) => {
+  const stars: ReactElement[] = [];
+  const rating = roundHalf(value);
 
-    const StarHalf = starHalfIcon || StarHalfIcon;
-    const StarEmpty = starEmptyIcon || StarEmptyIcon;
-    const StarFilled = starFilledIcon || StarFilledIcon;
+  const StarHalf = starHalfIcon || StarHalfIcon;
+  const StarEmpty = starEmptyIcon || StarEmptyIcon;
+  const StarFilled = starFilledIcon || StarFilledIcon;
 
-    for (let i = 1; i <= MAX_RATING; i += 1) {
-      if (rating - i >= 0) {
-        stars.push(<StarFilled height={size} key={i} width={size} />);
-      } else if (rating - i > -1) {
-        stars.push(<StarHalf height={size} key={i} width={size} />);
-      } else {
-        stars.push(<StarEmpty height={size} key={i} width={size} />);
-      }
+  for (let i = 1; i <= MAX_RATING; i += 1) {
+    if (rating - i >= 0) {
+      stars.push(<StarFilled height={size} key={i} width={size} />);
+    } else if (rating - i > -1) {
+      stars.push(<StarHalf height={size} key={i} width={size} />);
+    } else {
+      stars.push(<StarEmpty height={size} key={i} width={size} />);
     }
+  }
 
-    return (
-      <span className={cn('inline-flex fill-current', className)} ref={ref} role="img" {...props}>
-        {stars}
-      </span>
-    );
-  },
-);
+  return (
+    <span className={cn('inline-flex fill-current', className)} role="img" {...props}>
+      {stars}
+    </span>
+  );
+};
 
 Rating.displayName = 'Rating';
+
+export { Rating };
