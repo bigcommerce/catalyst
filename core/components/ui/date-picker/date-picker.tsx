d733e6b76536c09f928758809fd72fc53d72a451
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-22 19:52:06 -0500

    refactor: date picker (#1130)

--- core/components/ui/date-picker/date-picker.tsx
+++ core/components/ui/date-picker/date-picker.tsx
@@ -1,12 +1,42 @@
-'use client';
-
 import * as PopoverPrimitive from '@radix-ui/react-popover';
-import { CalendarIcon } from 'lucide-react';
-import { useState } from 'react';
-import { DayPickerSingleProps } from 'react-day-picker';
+import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
+import { ComponentPropsWithoutRef, useState } from 'react';
+import { DayPicker, DayPickerSingleProps } from 'react-day-picker';
 
-import { Calendar } from '../calendar';
 import { Input, InputProps } from '../input';
+
+const Calendar = ({ ...props }: ComponentPropsWithoutRef<typeof DayPicker>) => {
+  return (
+    <DayPicker
+      className="w-[304px] p-3 shadow-md"
+      classNames={{
+        month: 'space-y-4',
+        caption: 'flex justify-center pt-1 relative items-center',
+        caption_label: 'text-base',
+        nav: 'space-x-1 flex items-center',
+        nav_button:
+          'relative flex h-8 w-8 items-center justify-center border-2 border-none border-primary bg-transparent p-0 text-base font-semibold leading-6 text-primary hover:bg-secondary hover:bg-opacity-10 hover:text-secondary focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary/20 disabled:border-gray-400 disabled:text-gray-400 disabled:hover:bg-transparent disabled:hover:text-gray-400',
+        nav_button_previous: 'absolute start-1',
+        nav_button_next: 'absolute end-1',
+        head_row: 'flex',
+        head_cell: 'text-gray-400 w-10 text-xs font-normal font-normal',
+        row: 'flex w-full',
+        cell: 'relative flex h-10 w-10 items-center justify-center p-0 text-center text-xs font-normal focus-within:relative focus-within:z-20 focus-within:rounded focus-within:border focus-within:border-primary/20',
+        day: 'h-8 w-8 p-0 text-base hover:bg-secondary/10 focus-visible:outline-none aria-selected:bg-primary aria-selected:text-white aria-selected:hover:bg-primary aria-selected:hover:text-white',
+
+        day_today: 'bg-secondary/10',
+        day_disabled: 'text-gray-400 aria-selected:bg-gray-100 aria-selected:text-white',
+      }}
+      components={{
+        IconLeft: () => <ChevronLeftIcon className="h-6 w-6" />,
+        IconRight: () => <ChevronRightIcon className="h-6 w-6" />,
+      }}
+      {...props}
+    />
+  );
+};
+
+Calendar.displayName = 'Calendar';
 
 type Props = Omit<InputProps, 'defaultValue'> & {
   defaultValue?: string | Date;
@@ -15,7 +45,7 @@ type Props = Omit<InputProps, 'defaultValue'> & {
   disabledDays?: DayPickerSingleProps['disabled'];
 };
 
-export const DatePicker = ({
+const DatePicker = ({
   defaultValue,
   disabledDays,
   selected,
@@ -64,3 +94,7 @@ export const DatePicker = ({
     </PopoverPrimitive.Root>
   );
 };
+
+DatePicker.displayName = 'DatePicker';
+
+export { DatePicker };
