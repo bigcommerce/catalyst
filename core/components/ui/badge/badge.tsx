b2f12f5807812e645e3c0da1cd8a7ddec890badd
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-24 12:13:06 -0500

    fix: use correct font weight in badge (#1151)

--- core/components/ui/badge/badge.tsx
+++ core/components/ui/badge/badge.tsx
@@ -6,7 +6,7 @@ const Badge = ({ children, className, ...props }: ComponentPropsWithoutRef<'span
   return (
     <span
       className={cn(
-        'absolute end-0 top-0 min-w-[24px] rounded-[28px] border-2 border-white bg-primary px-1 py-px text-center text-xs font-bold font-normal leading-normal text-white',
+        'absolute end-0 top-0 min-w-[24px] rounded-[28px] border-2 border-white bg-primary px-1 py-px text-center text-xs font-semibold leading-normal text-white',
         className,
       )}
       {...props}
