ba31c51038ade18e4142d3655963b7a7c28689b1
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-24 14:14:51 -0500

    refactor: pick list props (#1146)

--- core/components/ui/pick-list/pick-list.tsx
+++ core/components/ui/pick-list/pick-list.tsx
@@ -1,16 +1,23 @@
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
-import { ComponentPropsWithRef, ElementRef, forwardRef, useId } from 'react';
+import {
+  ComponentPropsWithoutRef,
+  ComponentPropsWithRef,
+  ElementRef,
+  forwardRef,
+  useId,
+} from 'react';
 
 import { BcImage } from '~/components/bc-image';
 import { cn } from '~/lib/utils';
 
+interface Item extends ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item> {
+  label: string;
+  defaultImage: { altText: string; url: string } | null;
+  onMouseEnter?: () => void;
+}
+
 interface Props extends ComponentPropsWithRef<typeof RadioGroupPrimitive.Root> {
-  items: Array<{
-    value: string;
-    label: string;
-    defaultImage: { altText: string; url: string } | null;
-    prefetchHandler?: () => void;
-  }>;
+  items: Item[];
 }
 
 const PickList = forwardRef<ElementRef<typeof RadioGroupPrimitive.Root>, Props>(
@@ -23,33 +30,34 @@ const PickList = forwardRef<ElementRef<typeof RadioGroupPrimitive.Root>, Props>(
         ref={ref}
         {...props}
       >
-        {items.map((item) => (
-          <div
-            className="flex items-center p-4"
-            key={item.value}
-            onMouseEnter={item.prefetchHandler}
-          >
-            {Boolean(item.defaultImage) && (
-              <BcImage
-                alt={item.defaultImage?.altText || ''}
-                className="me-6"
-                height={48}
-                src={item.defaultImage?.url || ''}
-                width={48}
-              />
-            )}
-            <RadioGroupPrimitive.Item
-              className="flex h-6 w-6 shrink-0 items-center justify-center rounded-full border-2 border-gray-200 hover:border-secondary focus-visible:border-primary focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary/20 focus-visible:hover:border-secondary disabled:pointer-events-none disabled:bg-gray-100 radix-state-checked:border-primary radix-state-checked:bg-primary radix-state-checked:hover:border-secondary radix-state-checked:hover:bg-secondary radix-state-checked:disabled:border-gray-400 radix-state-checked:disabled:bg-gray-400"
-              id={`${id}-${item.value}`}
-              value={item.value}
-            >
-              <RadioGroupPrimitive.Indicator className="h-2 w-2 rounded-full bg-white" />
-            </RadioGroupPrimitive.Item>
-            <label className="w-full cursor-pointer ps-4" htmlFor={`${id}-${item.value}`}>
-              {item.label}
-            </label>
-          </div>
-        ))}
+        {items.map((item) => {
+          const { defaultImage, label, value, onMouseEnter, ...itemProps } = item;
+
+          return (
+            <div className="flex items-center p-4" key={value} onMouseEnter={onMouseEnter}>
+              {Boolean(defaultImage) && (
+                <BcImage
+                  alt={defaultImage?.altText || ''}
+                  className="me-6"
+                  height={48}
+                  src={defaultImage?.url || ''}
+                  width={48}
+                />
+              )}
+              <RadioGroupPrimitive.Item
+                {...itemProps}
+                className="flex h-6 w-6 shrink-0 items-center justify-center rounded-full border-2 border-gray-200 hover:border-secondary focus-visible:border-primary focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary/20 focus-visible:hover:border-secondary disabled:pointer-events-none disabled:bg-gray-100 radix-state-checked:border-primary radix-state-checked:bg-primary radix-state-checked:hover:border-secondary radix-state-checked:hover:bg-secondary radix-state-checked:disabled:border-gray-400 radix-state-checked:disabled:bg-gray-400"
+                id={`${id}-${value}`}
+                value={value}
+              >
+                <RadioGroupPrimitive.Indicator className="h-2 w-2 rounded-full bg-white" />
+              </RadioGroupPrimitive.Item>
+              <label className="w-full cursor-pointer ps-4" htmlFor={`${id}-${value}`}>
+                {label}
+              </label>
+            </div>
+          );
+        })}
       </RadioGroupPrimitive.Root>
     );
   },
