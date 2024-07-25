3e97c29f28dc0cf111f9cd0a95a27c4df31534be
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-25 10:15:43 -0500

    fix: forward DatePicker ref (#1155)

--- core/components/ui/date-picker/date-picker.tsx
+++ core/components/ui/date-picker/date-picker.tsx
@@ -1,6 +1,6 @@
 import * as PopoverPrimitive from '@radix-ui/react-popover';
 import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
-import { ComponentPropsWithoutRef, useState } from 'react';
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, useState } from 'react';
 import { DayPicker, DayPickerSingleProps } from 'react-day-picker';
 
 import { Input, InputProps } from '../input';
@@ -45,55 +45,61 @@ type Props = Omit<InputProps, 'defaultValue'> & {
   disabledDays?: DayPickerSingleProps['disabled'];
 };
 
-const DatePicker = ({
-  defaultValue,
-  disabledDays,
-  selected,
-  onSelect,
-  placeholder = 'MM/DD/YYYY',
-  required,
-  ...props
-}: Props) => {
-  const [date, setDate] = useState<Date | undefined>(
-    defaultValue ? new Date(defaultValue) : undefined,
-  );
+const DatePicker = forwardRef<ElementRef<'input'>, Props>(
+  (
+    {
+      defaultValue,
+      disabledDays,
+      selected,
+      onSelect,
+      placeholder = 'MM/DD/YYYY',
+      required,
+      ...props
+    },
+    ref,
+  ) => {
+    const [date, setDate] = useState<Date | undefined>(
+      defaultValue ? new Date(defaultValue) : undefined,
+    );
 
-  const formattedSelected = selected ? Intl.DateTimeFormat().format(selected) : undefined;
-  const formattedDate = date ? Intl.DateTimeFormat().format(date) : undefined;
+    const formattedSelected = selected ? Intl.DateTimeFormat().format(selected) : undefined;
+    const formattedDate = date ? Intl.DateTimeFormat().format(date) : undefined;
 
-  return (
-    <PopoverPrimitive.Root>
-      <PopoverPrimitive.Trigger asChild>
-        <Input
-          icon={<CalendarIcon />}
-          placeholder={placeholder}
-          readOnly={true}
-          required={required}
-          type="text"
-          value={formattedSelected ?? formattedDate ?? ''}
-          {...props}
-        />
-      </PopoverPrimitive.Trigger>
-
-      <PopoverPrimitive.Portal>
-        <PopoverPrimitive.Content
-          align="start"
-          className="z-50 w-auto bg-white p-0 text-base shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
-          sideOffset={4}
-        >
-          <Calendar
-            disabled={disabledDays}
-            initialFocus
-            mode="single"
-            onSelect={onSelect || setDate}
+    return (
+      <PopoverPrimitive.Root>
+        <PopoverPrimitive.Trigger asChild>
+          <Input
+            icon={<CalendarIcon />}
+            placeholder={placeholder}
+            readOnly={true}
+            ref={ref}
             required={required}
-            selected={selected ?? date}
+            type="text"
+            value={formattedSelected ?? formattedDate ?? ''}
+            {...props}
           />
-        </PopoverPrimitive.Content>
-      </PopoverPrimitive.Portal>
-    </PopoverPrimitive.Root>
-  );
-};
+        </PopoverPrimitive.Trigger>
+
+        <PopoverPrimitive.Portal>
+          <PopoverPrimitive.Content
+            align="start"
+            className="z-50 w-auto bg-white p-0 text-base shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+            sideOffset={4}
+          >
+            <Calendar
+              disabled={disabledDays}
+              initialFocus
+              mode="single"
+              onSelect={onSelect || setDate}
+              required={required}
+              selected={selected ?? date}
+            />
+          </PopoverPrimitive.Content>
+        </PopoverPrimitive.Portal>
+      </PopoverPrimitive.Root>
+    );
+  },
+);
 
 DatePicker.displayName = 'DatePicker';
 
