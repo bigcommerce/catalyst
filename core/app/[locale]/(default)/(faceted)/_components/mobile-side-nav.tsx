d8e1bdca705753d551cb68904b93303a5970f48c
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-25 10:16:28 -0500

    refactor: sheet (#1149)

--- core/app/[locale]/(default)/(faceted)/_components/mobile-side-nav.tsx
+++ core/app/[locale]/(default)/(faceted)/_components/mobile-side-nav.tsx
@@ -5,15 +5,7 @@ import { useTranslations } from 'next-intl';
 import { PropsWithChildren, useEffect, useState } from 'react';
 
 import { Button } from '~/components/ui/button';
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetHeader,
-  SheetOverlay,
-  SheetTitle,
-  SheetTrigger,
-} from '~/components/ui/sheet';
+import { Sheet } from '~/components/ui/sheet';
 
 export const MobileSideNav = ({ children }: PropsWithChildren) => {
   const [open, setOpen] = useState(false);
@@ -24,23 +16,17 @@ export const MobileSideNav = ({ children }: PropsWithChildren) => {
   }, [children]);
 
   return (
-    <Sheet onOpenChange={setOpen} open={open}>
-      <SheetTrigger asChild>
+    <Sheet
+      onOpenChange={setOpen}
+      open={open}
+      title={t('filters')}
+      trigger={
         <Button className="items-center md:w-auto lg:hidden" variant="secondary">
           <Filter className="me-3" /> <span>{t('showFilters')}</span>
         </Button>
-      </SheetTrigger>
-      <SheetOverlay className="bg-transparent, backdrop-blur-none lg:hidden">
-        <SheetContent className="lg:hidden">
-          <SheetHeader>
-            <SheetTitle asChild>
-              <h2>{t('filters')}</h2>
-            </SheetTitle>
-            <SheetClose />
-          </SheetHeader>
-          {children}
-        </SheetContent>
-      </SheetOverlay>
+      }
+    >
+      {children}
     </Sheet>
   );
 };
