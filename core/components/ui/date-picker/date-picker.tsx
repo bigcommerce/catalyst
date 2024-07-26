de7b176ff77f0482972bb07098d0af5f5b0e0595
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-26 13:30:16 -0500

    refactor: header & navigation menu (#1160)

--- core/components/ui/date-picker/date-picker.tsx
+++ core/components/ui/date-picker/date-picker.tsx
@@ -1,14 +1,14 @@
-import * as PopoverPrimitive from '@radix-ui/react-popover';
 import { CalendarIcon, ChevronLeftIcon, ChevronRightIcon } from 'lucide-react';
 import { ComponentPropsWithoutRef, ElementRef, forwardRef, useState } from 'react';
 import { DayPicker, DayPickerSingleProps } from 'react-day-picker';
 
 import { Input, InputProps } from '../input';
+import { Popover } from '../popover';
 
 const Calendar = ({ ...props }: ComponentPropsWithoutRef<typeof DayPicker>) => {
   return (
     <DayPicker
-      className="w-[304px] p-3 shadow-md"
+      className="w-[304px]"
       classNames={{
         month: 'space-y-4',
         caption: 'flex justify-center pt-1 relative items-center',
@@ -66,8 +66,9 @@ const DatePicker = forwardRef<ElementRef<'input'>, Props>(
     const formattedDate = date ? Intl.DateTimeFormat().format(date) : undefined;
 
     return (
-      <PopoverPrimitive.Root>
-        <PopoverPrimitive.Trigger asChild>
+      <Popover
+        align="start"
+        trigger={
           <Input
             icon={<CalendarIcon />}
             placeholder={placeholder}
@@ -78,25 +79,17 @@ const DatePicker = forwardRef<ElementRef<'input'>, Props>(
             value={formattedSelected ?? formattedDate ?? ''}
             {...props}
           />
-        </PopoverPrimitive.Trigger>
-
-        <PopoverPrimitive.Portal>
-          <PopoverPrimitive.Content
-            align="start"
-            className="z-50 w-auto bg-white p-0 text-base shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
-            sideOffset={4}
-          >
-            <Calendar
-              disabled={disabledDays}
-              initialFocus
-              mode="single"
-              onSelect={onSelect || setDate}
-              required={required}
-              selected={selected ?? date}
-            />
-          </PopoverPrimitive.Content>
-        </PopoverPrimitive.Portal>
-      </PopoverPrimitive.Root>
+        }
+      >
+        <Calendar
+          disabled={disabledDays}
+          initialFocus
+          mode="single"
+          onSelect={onSelect || setDate}
+          required={required}
+          selected={selected ?? date}
+        />
+      </Popover>
     );
   },
 );
