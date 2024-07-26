ab8d9c8b9d2aa214348071a38d66b12b9019f938
alexhwoods <alex@makeswift.com>
2024-07-26 12:16:40 -0400

    feat: add Makeswift env vars

--- core/app/api/makeswift/[...makeswift]/route.ts
+++ core/app/api/makeswift/[...makeswift]/route.ts
@@ -7,6 +7,8 @@ strict(process.env.MAKESWIFT_SITE_API_KEY, 'MAKESWIFT_SITE_API_KEY is required')
 
 const handler = MakeswiftApiHandler(process.env.MAKESWIFT_SITE_API_KEY, {
   runtime,
+  apiOrigin: process.env.MAKESWIFT_API_ORIGIN,
+  appOrigin: process.env.MAKESWIFT_APP_ORIGIN,
 });
 
 export { handler as GET, handler as POST };
--- core/lib/makeswift/client.ts
+++ core/lib/makeswift/client.ts
@@ -7,4 +7,5 @@ strict(process.env.MAKESWIFT_SITE_API_KEY, 'MAKESWIFT_SITE_API_KEY is required')
 
 export const client = new Makeswift(process.env.MAKESWIFT_SITE_API_KEY, {
   runtime,
+  apiOrigin: process.env.MAKESWIFT_API_ORIGIN,
 });
