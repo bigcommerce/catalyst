47f568bc6ab51ef85e92a75030ee0c8b29c1faf2
Jorge Moya <jorge.moya@bigcommerce.com>
2024-07-31 16:20:35 -0500

    feat(core): add more context when no result in search page

---
+++ .changeset/friendly-seas-wash.md
@@ -0,0 +1,5 @@
+---
+"@bigcommerce/catalyst-core": patch
+---
+
+Add more context when no result is found in search page.
--- core/components/search-form/index.tsx
+++ core/components/search-form/index.tsx
@@ -16,17 +16,19 @@ export const SearchForm = ({ initialTerm = '' }: Props) => {
   const t = useTranslations('NotFound');
 
   return (
-    <div className="flex flex-col gap-8">
-      <h3 className="text-3xl font-black lg:text-4xl">{t('searchProducts')}</h3>
+    <div className="flex flex-col gap-8 py-16">
+      {initialTerm ? (
+        <h3 className="text-3xl font-black lg:text-4xl">
+          {t('noSearchResults', { term: `"${initialTerm}"` })}
+        </h3>
+      ) : (
+        <h3 className="text-3xl font-black lg:text-4xl">{t('searchProducts')}</h3>
+      )}
+      <p>{t('checkSpelling')}</p>
       <Form action="/search" className="flex" method="get">
         <Field className="me-4 w-full" name="search">
           <FieldControl asChild>
-            <Input
-              className="grey-200 border-2 px-8 py-3 font-semibold"
-              defaultValue={initialTerm}
-              name="term"
-              placeholder={t('searchPlaceholder')}
-            />
+            <Input defaultValue={initialTerm} name="term" placeholder={t('searchPlaceholder')} />
           </FieldControl>
         </Field>
         <FormSubmit asChild>
