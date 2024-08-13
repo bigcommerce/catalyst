b47ae2a510b15aff75a08653b29c271daada4bc3
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-08-13 12:56:51 -0400

    feat: remove all usages of customer impersonation token and update docs

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -12,7 +12,6 @@ export const adminApiHostname: string =
 
 interface Config<FetcherRequestInit extends RequestInit = RequestInit> {
   storeHash: string;
-  customerImpersonationToken: string;
   storefrontToken: string;
   xAuthToken: string;
   channelId?: string;
@@ -53,7 +52,6 @@ class Client<FetcherRequestInit extends RequestInit = RequestInit> {
   async fetch<TResult, TVariables extends Record<string, unknown>>(config: {
     document: DocumentDecoration<TResult, TVariables>;
     variables: TVariables;
-    customerId?: string;
     customerAccessToken?: string;
     fetchOptions?: FetcherRequestInit;
     channelId?: string;
@@ -63,7 +61,6 @@ class Client<FetcherRequestInit extends RequestInit = RequestInit> {
   async fetch<TResult>(config: {
     document: DocumentDecoration<TResult, Record<string, never>>;
     variables?: undefined;
-    customerId?: string;
     customerAccessToken?: string;
     fetchOptions?: FetcherRequestInit;
     channelId?: string;
@@ -72,14 +69,12 @@ class Client<FetcherRequestInit extends RequestInit = RequestInit> {
   async fetch<TResult, TVariables>({
     document,
     variables,
-    customerId,
     customerAccessToken,
     fetchOptions = {} as FetcherRequestInit,
     channelId,
   }: {
     document: DocumentDecoration<TResult, TVariables>;
     variables?: TVariables;
-    customerId?: string;
     customerAccessToken?: string;
     fetchOptions?: FetcherRequestInit;
     channelId?: string;
@@ -98,7 +93,6 @@ class Client<FetcherRequestInit extends RequestInit = RequestInit> {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${this.config.storefrontToken}`,
         'User-Agent': this.backendUserAgent,
-        ...(customerId && { 'X-Bc-Customer-Id': customerId }),
         ...(customerAccessToken && { 'X-Bc-Customer-Access-Token': customerAccessToken }),
         ...(this.trustedProxySecret && { 'X-BC-Trusted-Proxy-Secret': this.trustedProxySecret }),
         ...additionalFetchHeaders,
