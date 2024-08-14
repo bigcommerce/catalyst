0d464f32f9453991e26b8150cdf3240536a72ae3
Anudeep Vattipalli <anudeep.vattipalli@bigcommerce.com>
2024-08-14 12:01:24 -0500

    fix(chore): Fetch pull request details in script (#1260)
    * fix(chore): Fetch pull request details in script
    
    * fix: address review commentsg

---
+++ .github/workflows/pull-request-data.js
@@ -0,0 +1,11 @@
+module.exports = async ({ github, context, core }) => {
+    const result = await github.rest.repos.listPullRequestsAssociatedWithCommit({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        commit_sha: context.sha,
+    });
+
+    core.setOutput('pr', result?.data[0]?.number || '');
+    core.setOutput('title', result?.data[0]?.title || '');
+    core.setOutput('url', result?.data[0]?.html_url || '');
+}
