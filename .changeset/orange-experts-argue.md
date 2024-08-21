3385a8255ec0011ea7047da4ff73ae3355b0ac76
Alex Saiannyi <67792608+bc-alexsaiannyi@users.noreply.github.com>
2024-08-21 10:36:15 +0200

    feat(core): add orders-all page for account (#1167)

---
+++ .changeset/orange-experts-argue.md
@@ -0,0 +1,5 @@
+---
+"@bigcommerce/catalyst-core": patch
+---
+
+add orders all page for account
--- core/app/[locale]/(default)/account/(tabs)/layout.tsx
+++ core/app/[locale]/(default)/account/(tabs)/layout.tsx
@@ -6,7 +6,7 @@ import { Link } from '~/components/link';
 import { LocaleType } from '~/i18n/routing';
 import { cn } from '~/lib/utils';
 
-const tabList = ['addresses', 'settings'] as const;
+const tabList = ['addresses', 'settings', 'orders'] as const;
 
 export type TabType = (typeof tabList)[number];
 
@@ -22,6 +22,7 @@ export default function AccountTabLayout({ children, params: { locale } }: Props
   const tabsTitles = {
     addresses: t('addresses'),
     settings: t('settings'),
+    orders: t('orders'),
   };
 
   return (
