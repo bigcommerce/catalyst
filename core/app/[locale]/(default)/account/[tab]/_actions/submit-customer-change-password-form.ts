bigcommerce-sae/catalyst-shapetech

refactor(core): queries, mutations, and fragments

core/app/[locale]/(default)/account/[tab]/_actions/submit-customer-change-password-form.ts
@@ -2,13 +2,46 @@
 
 import { z } from 'zod';
 
-import { ChangePasswordFieldsSchema } from '~/client/mutations/submit-change-password';
-import { submitCustomerChangePassword } from '~/client/mutations/submit-customer-change-password';
+import { getSessionCustomerId } from '~/auth';
+import { client } from '~/client';
+import { graphql } from '~/client/graphql';
 
-export const CustomerChangePasswordSchema = ChangePasswordFieldsSchema.omit({
+const ChangePasswordFieldsSchema = z.object({
+  customerId: z.string(),
+  customerToken: z.string(),
+  currentPassword: z.string().min(1),
+  newPassword: z.string().min(1),
+  confirmPassword: z.string().min(1),
+});
+
+const CustomerChangePasswordSchema = ChangePasswordFieldsSchema.omit({
   customerId: true,
   customerToken: true,
 });
+
+const CustomerChangePasswordMutation = graphql(`
+  mutation CustomerChangePassword($input: ChangePasswordInput!) {
+    customer {
+      changePassword(input: $input) {
+        errors {
+          ... on ValidationError {
+            message
+            path
+          }
+          ... on CustomerDoesNotExistError {
+            message
+          }
+          ... on CustomerPasswordError {
+            message
+          }
+          ... on CustomerNotLoggedInError {
+            message
+          }
+        }
+      }
+    }
+  }
+`);
 
 export interface State {
   status: 'idle' | 'error' | 'success';
@@ -19,6 +52,8 @@ export const submitCustomerChangePasswordForm = async (
   _previousState: unknown,
   formData: FormData,
 ) => {
+  const customerId = await getSessionCustomerId();
+
   try {
     const parsedData = CustomerChangePasswordSchema.parse({
       newPassword: formData.get('new-password'),
@@ -26,18 +61,26 @@ export const submitCustomerChangePasswordForm = async (
       confirmPassword: formData.get('confirm-password'),
     });
 
-    const response = await submitCustomerChangePassword({
-      newPassword: parsedData.newPassword,
-      currentPassword: parsedData.currentPassword,
+    const response = await client.fetch({
+      document: CustomerChangePasswordMutation,
+      variables: {
+        input: {
+          currentPassword: parsedData.currentPassword,
+          newPassword: parsedData.newPassword,
+        },
+      },
+      customerId,
     });
 
-    if (response.errors.length === 0) {
+    const result = response.data.customer.changePassword;
+
+    if (result.errors.length === 0) {
       return { status: 'success', message: 'Password has been updated successfully.' };
     }
 
     return {
       status: 'error',
-      message: response.errors.map((error) => error.message).join('\n'),
+      message: result.errors.map((error) => error.message).join('\n'),
     };
   } catch (error: unknown) {
     if (error instanceof z.ZodError) {

core/app/[locale]/(default)/account/[tab]/_actions/update-address.ts
@@ -2,10 +2,41 @@
 
 import { revalidatePath } from 'next/cache';
 
-import {
-  updateCustomerAddress,
-  UpdateCustomerAddressInput,
-} from '~/client/mutations/update-customer-address';
+import { getSessionCustomerId } from '~/auth';
+import { client } from '~/client';
+import { graphql, VariablesOf } from '~/client/graphql';
+
+const UpdateCustomerAddressMutation = graphql(`
+  mutation UpdateCustomerAddress(
+    $input: UpdateCustomerAddressInput!
+    $reCaptchaV2: ReCaptchaV2Input
+  ) {
+    customer {
+      updateCustomerAddress(input: $input, reCaptchaV2: $reCaptchaV2) {
+        errors {
+          __typename
+          ... on CustomerAddressUpdateError {
+            message
+          }
+          ... on CustomerNotLoggedInError {
+            message
+          }
+          ... on ValidationError {
+            message
+            path
+          }
+        }
+        address {
+          entityId
+          firstName
+          lastName
+        }
+      }
+    }
+  }
+`);
+
+type UpdateCustomerAddressInput = VariablesOf<typeof UpdateCustomerAddressMutation>['input'];
 
 const isUpdateCustomerAddressInput = (
   data: unknown,
@@ -26,6 +57,8 @@ export const updateAddress = async ({
   formData: FormData;
   reCaptchaToken?: string;
 }) => {
+  const customerId = await getSessionCustomerId();
+
   try {
     const parsed: unknown = [...formData.entries()].reduce<
       Record<string, FormDataEntryValue | Record<string, FormDataEntryValue>>
@@ -51,23 +84,30 @@ export const updateAddress = async ({
       };
     }
 
-    const response = await updateCustomerAddress({
-      input: {
-        addressEntityId: addressId,
-        data: parsed,
+    const response = await client.fetch({
+      document: UpdateCustomerAddressMutation,
+      customerId,
+      fetchOptions: { cache: 'no-store' },
+      variables: {
+        input: {
+          addressEntityId: addressId,
+          data: parsed,
+        },
+        ...(reCaptchaToken && { reCaptchaV2: { token: reCaptchaToken } }),
       },
-      reCaptchaToken,
     });
+
+    const result = response.data.customer.updateCustomerAddress;
 
     revalidatePath('/account/addresses', 'page');
 
-    if (response.errors.length === 0) {
+    if (result.errors.length === 0) {
       return { status: 'success', message: 'The address has been updated.' };
     }
 
     return {
       status: 'error',
-      message: response.errors
+      message: result.errors
         .map((error) => {
           if (error.__typename === 'AddressDoesNotExistError') {
             return 'Address does not exist.';

core/app/[locale]/(default)/cart/_components/coupon-code/apply-coupon-code.ts
@@ -3,7 +3,9 @@
 import { revalidateTag } from 'next/cache';
 import { z } from 'zod';
 
-import { applyCheckoutCoupon } from '~/client/mutations/apply-checkout-coupon';
+import { getSessionCustomerId } from '~/auth';
+import { client } from '~/client';
+import { graphql } from '~/client/graphql';
 import { TAGS } from '~/client/tags';
 
 const ApplyCouponCodeSchema = z.object({
@@ -11,14 +13,42 @@ const ApplyCouponCodeSchema = z.object({
   couponCode: z.string(),
 });
 
-export async function applyCouponCode(formData: FormData) {
+const ApplyCheckoutCouponMutation = graphql(`
+  mutation ApplyCheckoutCoupon($applyCheckoutCouponInput: ApplyCheckoutCouponInput!) {
+    checkout {
+      applyCheckoutCoupon(input: $applyCheckoutCouponInput) {
+        checkout {
+          entityId
+        }
+      }
+    }
+  }
+`);
+
+export const applyCouponCode = async (formData: FormData) => {
+  const customerId = await getSessionCustomerId();
+
   try {
     const parsedData = ApplyCouponCodeSchema.parse({
       checkoutEntityId: formData.get('checkoutEntityId'),
       couponCode: formData.get('couponCode'),
     });
 
-    const checkout = await applyCheckoutCoupon(parsedData.checkoutEntityId, parsedData.couponCode);
+    const response = await client.fetch({
+      document: ApplyCheckoutCouponMutation,
+      variables: {
+        applyCheckoutCouponInput: {
+          checkoutEntityId: parsedData.checkoutEntityId,
+          data: {
+            couponCode: parsedData.couponCode,
+          },
+        },
+      },
+      customerId,
+      fetchOptions: { cache: 'no-store' },
+    });
+
+    const checkout = response.data.checkout.applyCheckoutCoupon?.checkout;
 
     if (!checkout?.entityId) {
       return { status: 'error', error: 'Coupon code is invalid.' };
@@ -34,4 +64,4 @@ export async function applyCouponCode(formData: FormData) {
 
     return { status: 'error' };
   }
-}
+};

bigcommerce/catalyst

core/app/[locale]/(default)/account/[tab]/_actions/submit-customer-change-password-form.ts
@@ -2,13 +2,46 @@
 
 import { z } from 'zod';
 
-import { ChangePasswordFieldsSchema } from '~/client/mutations/submit-change-password';
-import { submitCustomerChangePassword } from '~/client/mutations/submit-customer-change-password';
+import { getSessionCustomerId } from '~/auth';
+import { client } from '~/client';
+import { graphql } from '~/client/graphql';
 
-export const CustomerChangePasswordSchema = ChangePasswordFieldsSchema.omit({
+const ChangePasswordFieldsSchema = z.object({
+  customerId: z.string(),
+  customerToken: z.string(),
+  currentPassword: z.string().min(1),
+  newPassword: z.string().min(1),
+  confirmPassword: z.string().min(1),
+});
+
+const CustomerChangePasswordSchema = ChangePasswordFieldsSchema.omit({
   customerId: true,
   customerToken: true,
 });
+
+const CustomerChangePasswordMutation = graphql(`
+  mutation CustomerChangePasswordMutation($input: ChangePasswordInput!) {
+    customer {
+      changePassword(input: $input) {
+        errors {
+          ... on ValidationError {
+            message
+            path
+          }
+          ... on CustomerDoesNotExistError {
+            message
+          }
+          ... on CustomerPasswordError {
+            message
+          }
+          ... on CustomerNotLoggedInError {
+            message
+          }
+        }
+      }
+    }
+  }
+`);
 
 export interface State {
   status: 'idle' | 'error' | 'success';
@@ -19,6 +52,8 @@ export const submitCustomerChangePasswordForm = async (
   _previousState: unknown,
   formData: FormData,
 ) => {
+  const customerId = await getSessionCustomerId();
+
   try {
     const parsedData = CustomerChangePasswordSchema.parse({
       newPassword: formData.get('new-password'),
@@ -26,18 +61,26 @@ export const submitCustomerChangePasswordForm = async (
       confirmPassword: formData.get('confirm-password'),
     });
 
-    const response = await submitCustomerChangePassword({
-      newPassword: parsedData.newPassword,
-      currentPassword: parsedData.currentPassword,
+    const response = await client.fetch({
+      document: CustomerChangePasswordMutation,
+      variables: {
+        input: {
+          currentPassword: parsedData.currentPassword,
+          newPassword: parsedData.newPassword,
+        },
+      },
+      customerId,
     });
 
-    if (response.errors.length === 0) {
+    const result = response.data.customer.changePassword;
+
+    if (result.errors.length === 0) {
       return { status: 'success', message: 'Password has been updated successfully.' };
     }
 
     return {
       status: 'error',
-      message: response.errors.map((error) => error.message).join('\n'),
+      message: result.errors.map((error) => error.message).join('\n'),
     };
   } catch (error: unknown) {
     if (error instanceof z.ZodError) {

core/app/[locale]/(default)/account/[tab]/_actions/update-address.ts
@@ -2,10 +2,42 @@
 
 import { revalidatePath } from 'next/cache';
 
-import {
-  updateCustomerAddress,
-  UpdateCustomerAddressInput,
-} from '~/client/mutations/update-customer-address';
+import { getSessionCustomerId } from '~/auth';
+import { client } from '~/client';
+import { graphql, VariablesOf } from '~/client/graphql';
+
+const UpdateCustomerAddressMutation = graphql(`
+  mutation UpdateCustomerAddressMutation(
+    $input: UpdateCustomerAddressInput!
+    $reCaptchaV2: ReCaptchaV2Input
+  ) {
+    customer {
+      updateCustomerAddress(input: $input, reCaptchaV2: $reCaptchaV2) {
+        errors {
+          __typename
+          ... on CustomerAddressUpdateError {
+            message
+          }
+          ... on CustomerNotLoggedInError {
+            message
+          }
+          ... on ValidationError {
+            message
+            path
+          }
+        }
+        address {
+          entityId
+          firstName
+          lastName
+        }
+      }
+    }
+  }
+`);
+
+type Variables = VariablesOf<typeof UpdateCustomerAddressMutation>;
+type UpdateCustomerAddressInput = Variables['input'];
 
 const isUpdateCustomerAddressInput = (
   data: unknown,
@@ -26,6 +58,8 @@ export const updateAddress = async ({
   formData: FormData;
   reCaptchaToken?: string;
 }) => {
+  const customerId = await getSessionCustomerId();
+
   try {
     const parsed: unknown = [...formData.entries()].reduce<
       Record<string, FormDataEntryValue | Record<string, FormDataEntryValue>>
@@ -51,23 +85,30 @@ export const updateAddress = async ({
       };
     }
 
-    const response = await updateCustomerAddress({
-      input: {
-        addressEntityId: addressId,
-        data: parsed,
+    const response = await client.fetch({
+      document: UpdateCustomerAddressMutation,
+      customerId,
+      fetchOptions: { cache: 'no-store' },
+      variables: {
+        input: {
+          addressEntityId: addressId,
+          data: parsed,
+        },
+        ...(reCaptchaToken && { reCaptchaV2: { token: reCaptchaToken } }),
       },
-      reCaptchaToken,
     });
+
+    const result = response.data.customer.updateCustomerAddress;
 
     revalidatePath('/account/addresses', 'page');
 
-    if (response.errors.length === 0) {
+    if (result.errors.length === 0) {
       return { status: 'success', message: 'The address has been updated.' };
     }
 
     return {
       status: 'error',
-      message: response.errors
+      message: result.errors
         .map((error) => {
           if (error.__typename === 'AddressDoesNotExistError') {
             return 'Address does not exist.';

core/app/[locale]/(default)/cart/_components/coupon-code/apply-coupon-code.ts
@@ -3,7 +3,9 @@
 import { revalidateTag } from 'next/cache';
 import { z } from 'zod';
 
-import { applyCheckoutCoupon } from '~/client/mutations/apply-checkout-coupon';
+import { getSessionCustomerId } from '~/auth';
+import { client } from '~/client';
+import { graphql } from '~/client/graphql';
 import { TAGS } from '~/client/tags';
 
 const ApplyCouponCodeSchema = z.object({
@@ -11,14 +13,42 @@ const ApplyCouponCodeSchema = z.object({
   couponCode: z.string(),
 });
 
-export async function applyCouponCode(formData: FormData) {
+const ApplyCheckoutCouponMutation = graphql(`
+  mutation ApplyCheckoutCouponMutation($applyCheckoutCouponInput: ApplyCheckoutCouponInput!) {
+    checkout {
+      applyCheckoutCoupon(input: $applyCheckoutCouponInput) {
+        checkout {
+          entityId
+        }
+      }
+    }
+  }
+`);
+
+export const applyCouponCode = async (formData: FormData) => {
+  const customerId = await getSessionCustomerId();
+
   try {
     const parsedData = ApplyCouponCodeSchema.parse({
       checkoutEntityId: formData.get('checkoutEntityId'),
       couponCode: formData.get('couponCode'),
     });
 
-    const checkout = await applyCheckoutCoupon(parsedData.checkoutEntityId, parsedData.couponCode);
+    const response = await client.fetch({
+      document: ApplyCheckoutCouponMutation,
+      variables: {
+        applyCheckoutCouponInput: {
+          checkoutEntityId: parsedData.checkoutEntityId,
+          data: {
+            couponCode: parsedData.couponCode,
+          },
+        },
+      },
+      customerId,
+      fetchOptions: { cache: 'no-store' },
+    });
+
+    const checkout = response.data.checkout.applyCheckoutCoupon?.checkout;
 
     if (!checkout?.entityId) {
       return { status: 'error', error: 'Coupon code is invalid.' };
@@ -34,4 +64,4 @@ export async function applyCouponCode(formData: FormData) {
 
     return { status: 'error' };
   }
-}
+};