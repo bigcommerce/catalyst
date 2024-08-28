218703c041aee05de29d42e1349ce44851b42eb3
Jorge Moya <jorge.moya@bigcommerce.com>
2024-08-28 14:16:03 -0500

    fix: remove unused variable

--- core/app/[locale]/(default)/account/(tabs)/_components/tab-heading.tsx
+++ core/app/[locale]/(default)/account/(tabs)/_components/tab-heading.tsx
@@ -2,11 +2,9 @@ import { getLocale, getTranslations } from 'next-intl/server';
 
 import { TabType } from '../layout';
 
-export const TabHeading = async ({ heading }: { heading: TabType | 'change_password' }) => {
+export const TabHeading = async ({ heading }: { heading: TabType }) => {
   const locale = await getLocale();
   const t = await getTranslations({ locale, namespace: 'Account.Home' });
-  const tab = heading;
-  const title = tab === 'change_password' ? 'changePassword' : tab;
 
-  return <h2 className="mb-8 text-3xl font-black lg:text-4xl">{t(title)}</h2>;
+  return <h2 className="mb-8 text-3xl font-black lg:text-4xl">{t(heading)}</h2>;
 };
