a2d8bcbf07f9c5314f8a0164c3d1e9dec2627c03
Jorge Moya <jorge.moya@bigcommerce.com>
2024-09-03 14:32:36 -0500

    feat: update middleware to redirect to /account in auth pages if logged in

--- core/middlewares/with-auth.ts
+++ core/middlewares/with-auth.ts
@@ -1,7 +1,26 @@
+import { NextResponse } from 'next/server';
+
 import { auth } from '~/auth';
 
 import { type MiddlewareFactory } from './compose-middlewares';
 
-// The `auth` function doesn't have the correct type to support it as a MiddlewareFactory.
-// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-export const withAuth: MiddlewareFactory = auth as unknown as MiddlewareFactory;
+const AUTH_PATHS = ['/login/', '/register/', '/reset/', '/change-password/'];
+
+export const withAuth: MiddlewareFactory = (next) => {
+  return async (request, event) => {
+    // @ts-expect-error: The `auth` function doesn't have the correct type to support it as a MiddlewareFactory.
+    const authWithCallback = auth(async (req) => {
+      if (req.auth && AUTH_PATHS.includes(req.nextUrl.pathname)) {
+        return NextResponse.redirect(new URL('/account', req.nextUrl.origin));
+      }
+
+      // Continue the middleware chain
+      const response = await next(req, event);
+
+      return response;
+    });
+
+    // @ts-expect-error: The `auth` function doesn't have the correct type to support it as a MiddlewareFactory.
+    return authWithCallback(request, event);
+  };
+};
