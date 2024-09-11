add5c12fe4471cd93cbdbe2b1094850a3313044d
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-09-11 11:35:13 -0400

    test(core): run address tests in serial mode (#1356)

--- core/tests/ui/desktop/e2e/addresses.spec.ts
+++ core/tests/ui/desktop/e2e/addresses.spec.ts
@@ -26,6 +26,8 @@ async function logout(page: Page) {
   await page.getByRole('menuitem', { name: 'Log out' }).click();
 }
 
+test.describe.configure({ mode: 'serial' });
+
 test('Add new address', async ({ page }) => {
   await loginWithUserAccount(page, testUserEmail, testUserPassword);
   await page.goto('/account/addresses');
