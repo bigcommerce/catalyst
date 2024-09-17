a44706027aeae9a23599775ff876f0c5f90117ee
Jorge Moya <jorge.moya@bigcommerce.com>
2024-09-17 17:41:29 -0500

    fix: remove paddings

--- core/app/[locale]/(default)/layout.tsx
+++ core/app/[locale]/(default)/layout.tsx
@@ -19,9 +19,7 @@ export default function DefaultLayout({ children, params: { locale } }: Props) {
         <Header cart={<Cart />} />
       </Suspense>
 
-      <main className="flex-1 px-4 2xl:container sm:px-10 lg:px-12 2xl:mx-auto 2xl:px-0">
-        {children}
-      </main>
+      <main>{children}</main>
 
       <Suspense>
         <Footer />
