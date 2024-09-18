98f53396d8d7b978e0cd27763b34a3321d1696e4
Jorge Moya <jorge.moya@bigcommerce.com>
2024-09-18 16:27:30 -0500

    fix: remove unused action

--- core/components/header/_actions/change-locale.ts
+++
@@ -1,8 +0,0 @@
-'use server';
-
-import { redirect } from '~/i18n/routing';
-
-// eslint-disable-next-line @typescript-eslint/require-await
-export const changeLocale = async (newLocale: string) => {
-  redirect(`/${newLocale}`);
-};
