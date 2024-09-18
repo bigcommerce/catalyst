4d6721026f1a91bcd19d9dff5a883f4fda4f8cf7
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2024-09-18 11:28:50 -0500

    refactor: wrap header and footer in suspense

--- core/components/header/index.tsx
+++ core/components/header/index.tsx
@@ -2,8 +2,11 @@ import { ShoppingCart, User } from 'lucide-react';
 import { getLocale, getTranslations } from 'next-intl/server';
 import { ReactNode, Suspense } from 'react';
 
+import { LayoutQuery } from '~/app/[locale]/(default)/query';
 import { getSessionCustomerId } from '~/auth';
-import { FragmentOf } from '~/client/graphql';
+import { client } from '~/client';
+import { readFragment } from '~/client/graphql';
+import { revalidate } from '~/client/revalidate-target';
 import { logoTransformer } from '~/data-transformers/logo-transformer';
 import { localeLanguageRegionMap } from '~/i18n/routing';
 
@@ -19,14 +22,19 @@ import { QuickSearch } from './quick-search';
 
 interface Props {
   cart: ReactNode;
-  data: FragmentOf<typeof HeaderFragment>;
 }
 
-export const Header = async ({ cart, data }: Props) => {
+export const Header = async ({ cart }: Props) => {
   const locale = await getLocale();
   const t = await getTranslations('Components.Header');
-
   const customerId = await getSessionCustomerId();
+
+  const { data: response } = await client.fetch({
+    document: LayoutQuery,
+    fetchOptions: customerId ? { cache: 'no-store' } : { next: { revalidate } },
+  });
+
+  const data = readFragment(HeaderFragment, response).site;
 
   /**  To prevent the navigation menu from overflowing, we limit the number of categories to 6.
    To show a full list of categories, modify the `slice` method to remove the limit.
@@ -97,3 +105,27 @@ export const Header = async ({ cart, data }: Props) => {
     />
   );
 };
+
+export const HeaderSkeleton = () => (
+  <header className="flex min-h-[92px] animate-pulse items-center justify-between gap-1 overflow-y-visible bg-white px-4 2xl:container sm:px-10 lg:gap-8 lg:px-12 2xl:mx-auto 2xl:px-0">
+    <div className="h-16 w-40 rounded bg-slate-200" />
+    <div className="hidden space-x-4 lg:flex">
+      <div className="h-6 w-20 rounded bg-slate-200" />
+      <div className="h-6 w-20 rounded bg-slate-200" />
+      <div className="h-6 w-20 rounded bg-slate-200" />
+      <div className="h-6 w-20 rounded bg-slate-200" />
+    </div>
+    <div className="flex items-center gap-2 lg:gap-4">
+      <div className="h-8 w-8 rounded-full bg-slate-200" />
+
+      <div className="flex gap-2 lg:gap-4">
+        <div className="h-8 w-8 rounded-full bg-slate-200" />
+        <div className="h-8 w-8 rounded-full bg-slate-200" />
+      </div>
+
+      <div className="h-8 w-20 rounded bg-slate-200" />
+
+      <div className="h-8 w-8 rounded bg-slate-200 lg:hidden" />
+    </div>
+  </header>
+);
