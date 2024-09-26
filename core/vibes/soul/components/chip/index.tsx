acac85a11828bb30d37a7e0f319020f901d0b763
Jorge Moya <jorge.moya@bigcommerce.com>
2024-09-26 12:16:22 -0500

    chore(core): add soul components for plp

---
+++ core/vibes/soul/components/chip/index.tsx
@@ -0,0 +1,24 @@
+import { clsx } from 'clsx';
+
+interface Props {
+  label: string;
+  amount: number;
+  onClick: () => void;
+  selected: boolean;
+}
+
+export const Chip = function Chip({ label, amount, onClick, selected }: Props) {
+  return (
+    <button
+      className={clsx(
+        'whitespace-nowrap rounded-full px-2 py-1 text-sm font-normal',
+        'ring-primary focus-visible:outline-0 focus-visible:ring-2',
+        'border border-transparent transition-colors',
+        selected ? 'bg-foreground text-background' : 'bg-contrast-100 hover:border-contrast-300',
+      )}
+      onClick={onClick}
+    >
+      {label} <span className="text-contrast-300">{amount}</span>
+    </button>
+  );
+};
