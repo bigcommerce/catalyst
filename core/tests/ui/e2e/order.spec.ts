4c9ce2f31e994d24eef0d4692b515f0e75421837
anastasiia.zvierieva <anastasiia.zvierieva@bigcommerce.com>
2024-10-03 11:48:48 +0200

    Add Orders test

--- core/tests/ui/e2e/order.spec.ts
+++ core/tests/ui/e2e/order.spec.ts
@@ -1,6 +1,31 @@
 import { expect, test } from '~/tests/fixtures';
 
-test('Order page is displayed', async ({ page, account, product, order }) => {
+function formatAmount(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(amount);
+}
+
+function formatDate(s: string): string {
+  return new Date(s).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+}
+test('Orders page has empty state', async ({ page, account }) => {
+  const customer = await account.create();
+
+  await customer.login();
+
+  await page.goto('/account/orders/');
+
+  await expect(page.getByRole('heading', { name: 'Orders' })).toBeVisible();
+  await expect(page.getByText('No orders yet')).toBeVisible();
+});
+
+test('Order details are visible on Orders page', async ({ page, account, product, order }) => {
   const customer = await account.create();
 
   await customer.login();
@@ -11,5 +36,16 @@ test('Order page is displayed', async ({ page, account, product, order }) => {
 
   await page.goto('/account/orders/');
 
-  await expect(page.getByRole('heading', { name: 'Orders' })).toBeVisible();
+  const formattedTotal = formatAmount(parseFloat(orderDetails.total_inc_tax));
+  const formattedProductPrice = formatAmount(orderProduct.price ?? 0);
+  const formattedDate = formatDate(orderDetails.date_created);
+
+  await expect(page.getByRole('link', { name: `Order # ${orderDetails.id}` })).toBeVisible();
+  await expect(page.getByText('Order placed')).toBeVisible();
+  await expect(page.getByText(formattedDate)).toBeVisible();
+  await expect(page.getByText('Total')).toBeVisible();
+  await expect(page.getByText(formattedTotal)).toBeVisible();
+  await expect(page.getByText(`${orderDetails.status}`)).toBeVisible();
+  await expect(page.getByRole('link', { name: `${orderProduct.name}` })).toBeVisible();
+  await expect(page.getByText(formattedProductPrice)).toBeVisible();
 });
