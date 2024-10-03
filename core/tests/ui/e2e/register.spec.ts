a463093b9ec3336f0b1acfa94a92bd11514e7fca
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-10-03 16:56:59 -0400

    fix(core): update register customer test (#1419)

--- core/tests/ui/e2e/register.spec.ts
+++ core/tests/ui/e2e/register.spec.ts
@@ -2,10 +2,10 @@ import { faker } from '@faker-js/faker';
 
 import { expect, test } from '~/tests/fixtures';
 
-// Prefix is added to ensure that the password requirements are met
-const password = faker.internet.password({ pattern: /[a-zA-Z0-9]/, prefix: '1At', length: 10 });
-
 test('Account register', async ({ page }) => {
+  // Prefix is added to ensure that the password requirements are met
+  const password = faker.internet.password({ pattern: /[a-zA-Z0-9]/, prefix: '1At', length: 10 });
+
   await page.goto('/register');
 
   await page.getByRole('heading', { name: 'New account' }).waitFor();
@@ -32,10 +32,10 @@ test('Account is not created if email is already in use', async ({ page, account
   await page.goto('/register');
 
   await page.getByLabel('Email Address').fill(customer.email);
-  await page.getByLabel('PasswordRequired', { exact: true }).fill(password);
-  await page.getByLabel('Confirm PasswordRequired').fill(password);
-  await page.getByLabel('First NameRequired').fill(faker.person.firstName());
-  await page.getByLabel('Last NameRequired').fill(faker.person.lastName());
+  await page.getByLabel('PasswordRequired', { exact: true }).fill(customer.password);
+  await page.getByLabel('Confirm PasswordRequired').fill(customer.password);
+  await page.getByLabel('First NameRequired').fill(customer.firstName);
+  await page.getByLabel('Last NameRequired').fill(customer.lastName);
   await page.getByLabel('Address Line 1Required').fill(faker.location.streetAddress());
   await page.getByLabel('Suburb/CityRequired').fill(faker.location.city());
   await page.getByLabel('Zip/PostcodeRequired').fill(faker.location.zipCode());
