4cb2bbbfa347b018516a2d1882a0dcf0e1fe0053
anastasiia.zvierieva <anastasiia.zvierieva@bigcommerce.com>
2024-10-04 17:47:32 +0200

    Remove unused code

--- core/tests/fixtures/utils/product/index.ts
+++ core/tests/fixtures/utils/product/index.ts
@@ -1,32 +1,10 @@
 import { type Page } from '@playwright/test';
 
-import { createProduct } from './create';
-import { deleteProduct } from './delete';
 import { getProduct } from './get';
 import { Product } from './product';
 
 export class ProductFactory {
-  autoCleanup = false;
-  private products: Product[] = [];
-
   constructor(private readonly page: Page) {}
-
-  async create() {
-    const productData = await createProduct();
-
-    const product = new Product(
-      this.page,
-      productData.name,
-      productData.type,
-      productData.price,
-      productData.id,
-      productData.url,
-    );
-
-    this.products.push(product);
-
-    return product;
-  }
 
   async get(productId: number) {
     const productData = await getProduct(productId);
@@ -39,9 +17,5 @@ export class ProductFactory {
       productData.id,
       productData.custom_url.url,
     );
-  }
-
-  async cleanup() {
-    await Promise.all(this.products.map(async (product) => await deleteProduct(product.id)));
   }
 }
