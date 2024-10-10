d12c0d22ec121f0effb95e1fab347a05ca84c7af
Matthew Volk <matt.volk@bigcommerce.com>
2024-10-10 16:16:36 -0500

    refactor(cli)!: make cloneCatalyst clone `bigcommerce/catalyst` (#1438)

--- packages/create-catalyst/src/utils/clone-catalyst.ts
+++ packages/create-catalyst/src/utils/clone-catalyst.ts
@@ -1,103 +1,34 @@
-import chalk from 'chalk';
-import { readFile, writeFile } from 'fs/promises';
-import { ensureDir, readJsonSync, removeSync, writeJsonSync } from 'fs-extra/esm';
-import { downloadTemplate } from 'giget';
-import merge from 'lodash.merge';
-import { join } from 'path';
-import * as z from 'zod';
+import { execSync } from 'child_process';
 
-import { spinner } from './spinner';
+import { checkoutRef } from './checkout-ref';
+import { hasGitHubSSH } from './has-github-ssh';
 
-export const cloneCatalyst = async ({
-  codeEditor,
-  includeFunctionalTests,
-  projectDir,
+export const cloneCatalyst = ({
   projectName,
-  ghRef = 'main',
+  projectDir,
+  ghRef,
 }: {
-  codeEditor: string;
-  includeFunctionalTests: boolean;
-  projectDir: string;
   projectName: string;
+  projectDir: string;
   ghRef?: string;
 }) => {
-  await spinner(
-    downloadTemplate(`github:bigcommerce/catalyst/core#${ghRef}`, {
-      dir: projectDir,
-      offline: false,
-    }),
-    {
-      text: 'Cloning Catalyst template...',
-      successText: 'Catalyst template cloned successfully',
-      failText: (err) => chalk.red(`Failed to clone Catalyst template: ${err.message}`),
-    },
-  );
+  const repositoryName = 'bigcommerce/catalyst';
+  const useSSH = hasGitHubSSH();
 
-  switch (codeEditor) {
-    case 'vscode':
-      await ensureDir(join(projectDir, '.vscode'));
+  console.log(`Cloning ${repositoryName} using ${useSSH ? 'SSH' : 'HTTPS'}...\n`);
 
-      writeJsonSync(
-        join(projectDir, '.vscode/settings.json'),
-        { 'typescript.tsdk': 'node_modules/typescript/lib' },
-        { spaces: 2 },
-      );
-      break;
+  const cloneCommand = `git clone ${
+    useSSH ? `git@github.com:${repositoryName}` : `https://github.com/${repositoryName}`
+  }.git${projectName ? ` ${projectName}` : ''}`;
 
-    default:
-      break;
-  }
+  execSync(cloneCommand, { stdio: 'inherit' });
+  console.log();
 
-  const packageJson = z
-    .object({
-      name: z.string(),
-      description: z.string(),
-      version: z.string(),
-      scripts: z.object({}).passthrough().optional(),
-      dependencies: z.object({}).passthrough(),
-      devDependencies: z.object({}).passthrough(),
-    })
-    .passthrough()
-    .parse(
-      merge({}, readJsonSync(join(projectDir, 'package.json')), {
-        name: projectName,
-        description: '',
-      }),
-    );
+  execSync('git remote rename origin upstream', { cwd: projectDir, stdio: 'inherit' });
+  console.log();
 
-  delete packageJson.dependencies['@bigcommerce/catalyst-client'];
-  delete packageJson.devDependencies['@bigcommerce/eslint-config-catalyst'];
-
-  if (!includeFunctionalTests) {
-    delete packageJson.devDependencies['@faker-js/faker'];
-    delete packageJson.devDependencies['@playwright/test'];
-  }
-
-  writeJsonSync(join(projectDir, 'package.json'), packageJson, { spaces: 2 });
-
-  if (!includeFunctionalTests) {
-    const tsConfigJson = z
-      .object({
-        include: z.array(z.string()).optional(),
-      })
-      .passthrough()
-      .parse(readJsonSync(join(projectDir, 'tsconfig.json')));
-
-    tsConfigJson.include = tsConfigJson.include?.filter(
-      (include) => !['tests/**/*', 'playwright.config.ts'].includes(include),
-    );
-
-    writeJsonSync(join(projectDir, 'tsconfig.json'), tsConfigJson, { spaces: 2 });
-  }
-
-  if (!includeFunctionalTests) {
-    const eslint = (await readFile(join(projectDir, '.eslintrc.cjs'), { encoding: 'utf-8' }))
-      .replace(/['"]playwright-report\/\*\*['"],?/, '')
-      .replace(/['"]test-results\/\*\*['"],?/, '');
-
-    await writeFile(join(projectDir, '.eslintrc.cjs'), eslint);
-
-    removeSync(join(projectDir, 'tests'));
-    removeSync(join(projectDir, 'playwright.config.ts'));
+  if (ghRef) {
+    checkoutRef(projectDir, ghRef);
+    console.log();
   }
 };
