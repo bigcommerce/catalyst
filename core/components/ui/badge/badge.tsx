d877ad8b4ab80150f5c7f92a6fb55f96bf3b12af
Kathiravan Balu <kathir@arizon.digital>
2024-10-17 16:12:01 +0530

    fixed the cart badge

--- core/components/ui/badge/badge.tsx
+++ core/components/ui/badge/badge.tsx
@@ -11,12 +11,12 @@ const Badge = ({ children, className, ...props }: Props) => {
   return (
     <span
       className={cn(
-        'absolute end-0 top-0 min-w-[24px] rounded-[28px] border-2 border-white bg-primary px-1 py-px text-center text-xs font-semibold leading-normal text-white',
+        'cart',
         className,
       )}
       {...props}
     >
-      {children}
+     ({children}) 
     </span>
   );
 };
