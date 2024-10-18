6a70ece12c697c019cbcc034b55bb3e9c56f96ae
Jorge Moya <jorge.moya@bigcommerce.com>
2024-10-18 14:21:22 -0500

    feat(client): handle GQL errors

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -1,4 +1,5 @@
-import { BigCommerceAPIError } from './error';
+import { BigCommerceAPIError } from './api-error';
+import { BigCommerceGQLError } from './gql-error';
 import { DocumentDecoration } from './types';
 import { getOperationInfo } from './utils/getOperationName';
 import { normalizeQuery } from './utils/normalizeQuery';
@@ -120,12 +121,20 @@ class Client<FetcherRequestInit extends RequestInit = RequestInit> {
     });
 
     if (!response.ok) {
-      throw await BigCommerceAPIError.createFromResponse(response);
+      throw new BigCommerceAPIError(response.status);
     }
 
     log(response);
 
-    return response.json() as Promise<BigCommerceResponse<TResult>>;
+    const result = (await response.json()) as BigCommerceResponse<TResult>;
+
+    const { errors, ...data } = result;
+
+    if (errors) {
+      throw BigCommerceGQLError.createFromResult(errors);
+    }
+
+    return data;
   }
 
   async fetchShippingZones() {
