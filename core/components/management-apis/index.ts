384b60c4c5b6f223b3a94dab4e65a15caa501b4a
Kathiravan Balu <kathir@arizon.digital>
2024-10-22 16:41:08 +0530

    P2-494 added the variant level metafield function

--- core/components/management-apis/index.ts
+++ core/components/management-apis/index.ts
@@ -20,7 +20,7 @@ export const GetCustomerById = async (entityId: Number) => {
       }
 };
 
-export const GetProductMetaFields = async (entityId: Number, nameSpace: string) => {
+export const GetProductMetaFields = async (entityId: number, nameSpace: string) => {
   let metaFieldsArray: any = [];
   let productMetaFields: any = await getMetaFieldsByProduct(entityId, nameSpace, 1);
   if(productMetaFields?.data) {
@@ -36,7 +36,23 @@ export const GetProductMetaFields = async (entityId: Number, nameSpace: string) 
   return metaFieldsArray;
 };
 
-const getMetaFieldsByProduct = async (entityId: Number, nameSpace: string = '', page = 1) => {
+export const GetProductVariantMetaFields = async (entityId: number, variantId: number, nameSpace: string) => {
+  let metaFieldsArray: any = [];
+  let productMetaFields: any = await getMetaFieldsByProductVariant(entityId, variantId, nameSpace, 1);
+  if(productMetaFields?.data) {
+    metaFieldsArray.push(...productMetaFields?.data);
+    let totalPages = productMetaFields?.meta?.pagination?.total_pages;
+    if(totalPages > 1) {
+      for(let i=2; i<= totalPages; i++) {
+        let productMeta: any = await getMetaFieldsByProductVariant(entityId, variantId, nameSpace, i);
+        metaFieldsArray.push(...productMeta?.data);
+      }
+    }
+  }
+  return metaFieldsArray;
+};
+
+const getMetaFieldsByProduct = async (entityId: number, nameSpace: string = '', page = 1) => {
   try {
     let nameSpaceValue = '';
     if(nameSpace) {
@@ -44,6 +60,35 @@ const getMetaFieldsByProduct = async (entityId: Number, nameSpace: string = '', 
     }
     let productMetaFields = await fetch(
       `https://api.bigcommerce.com/stores/${process.env.BIGCOMMERCE_STORE_HASH}/v3/catalog/products/${entityId}/metafields?page=${page}${nameSpaceValue}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Auth-Token': process.env.BIGCOMMERCE_ACCESS_TOKEN,
+        },
+        next: { 
+          revalidate: 3600 
+        }
+      },
+    ).then(res => res.json())
+    .then(jsonData => {
+        return jsonData;
+    });
+    return productMetaFields;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getMetaFieldsByProductVariant = async (entityId: Number, variantId: number, nameSpace: string = '', page = 1) => {
+  try {
+    let nameSpaceValue = '';
+    if(nameSpace) {
+      nameSpaceValue = '&namespace='+nameSpace;
+    }
+    
+    let productMetaFields = await fetch(
+      `https://api.bigcommerce.com/stores/${process.env.BIGCOMMERCE_STORE_HASH}/v3/catalog/products/${entityId}/variants/${variantId}/metafields?page=${page}${nameSpaceValue}`,
       {
         method: 'GET',
         headers: {
