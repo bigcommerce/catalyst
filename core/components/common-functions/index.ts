699cc6d624e465f65a1c8e5264205cdb3339a45e
Kathiravan Balu <kathir@arizon.digital>
2024-10-25 10:21:55 +0530

    P2-494 common functions fix

--- core/components/common-functions/index.ts
+++ core/components/common-functions/index.ts
@@ -2,19 +2,17 @@ import { removeEdgesAndNodes } from "@bigcommerce/catalyst-client";
 import { GetProductMetaFields } from "../management-apis";
 
 export const getMetaFieldsByProduct = async (product: any, metaKey: string) => {
-    let getvariantData = removeEdgesAndNodes(product?.variants);
-    let entityId = product?.entityId;
-    let variantId = 0;
-    if(getvariantData) {
-        let getvariant: any = getvariantData?.find((variant: any) => variant?.sku == product?.sku);
-        console.log('-------variant----', getvariant);
-        if(getvariant) {
-            variantId = getvariant?.entityId;
-        }
-        console.log('--variantId---', variantId);
+  let getvariantData = removeEdgesAndNodes(product?.variants);
+  let entityId = product?.entityId;
+  let variantId = 0;
+  if(getvariantData) {
+    let getvariant: any = getvariantData?.find((variant: any) => variant?.sku == product?.sku);
+    if(getvariant) {
+      variantId = getvariant?.entityId;
     }
-    let metaFields = await GetProductMetaFields(entityId, '');
-    if(metaFields) {
-        return metaFields?.find((meta:any) => meta.key == metaKey);
-    }
+  }
+  let metaFields = await GetProductMetaFields(entityId, '');
+  if(metaFields) {
+    return metaFields?.find((meta:any) => meta.key == metaKey);
+  }
 }
