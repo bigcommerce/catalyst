7d15723907b9e985f02f6e4e3d019731af8ca43a
James Q Quick <james.q.quick@gmail.com>
2024-11-06 13:59:48 -0500

    Update FeaturedProductsCarousel component (#1547)

--- core/vibes/soul/sections/featured-products-carousel/index.tsx
+++ core/vibes/soul/sections/featured-products-carousel/index.tsx
@@ -1,5 +1,6 @@
-import { CarouselProduct, ProductsCarousel } from '@/vibes/soul/primitives/products-carousel';
 import { Link } from '~/components/link';
+
+import { CarouselProduct, ProductsCarousel } from '@/vibes/soul/primitives/products-carousel';
 
 interface Link {
   label: string;
@@ -13,16 +14,11 @@ interface Props {
   products: CarouselProduct[];
 }
 
-export const FeaturedProductsCarousel = function FeaturedProductsCarousel({
-  title,
-  description,
-  cta,
-  products,
-}: Props) {
+export function FeaturedProductsCarousel({ title, description, cta, products }: Props) {
   return (
-    <section className="@container">
-      <div className="py-10 @4xl:py-24">
-        <div className="mx-auto mb-6 flex w-full max-w-screen-2xl flex-row flex-wrap justify-between gap-5 px-3 text-foreground @xl:px-6 @4xl:mb-8 @4xl:items-end @5xl:px-20">
+    <section className="overflow-hidden @container">
+      <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 @4xl:py-24">
+        <div className="mb-6 flex w-full flex-row flex-wrap justify-between gap-5 text-foreground @4xl:mb-8 @4xl:items-end">
           <div className="flex flex-col gap-5">
             <h2 className="font-heading text-2xl font-medium leading-none">{title}</h2>
             {description != null && description !== '' && (
@@ -31,8 +27,8 @@ export const FeaturedProductsCarousel = function FeaturedProductsCarousel({
           </div>
           {cta != null && cta.href !== '' && cta.label !== '' && (
             <Link
-              className="rounded-lg font-semibold text-foreground ring-primary focus-visible:outline-0 focus-visible:ring-2"
               href={cta.href}
+              className="rounded-lg font-semibold text-foreground ring-primary focus-visible:outline-0 focus-visible:ring-2"
             >
               {cta.label}
             </Link>
@@ -42,4 +38,4 @@ export const FeaturedProductsCarousel = function FeaturedProductsCarousel({
       </div>
     </section>
   );
-};
+}
