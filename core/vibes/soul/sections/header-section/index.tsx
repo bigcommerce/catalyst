09cb1f913ae374804408e8b6672e52efe158254a
Miguel Oller <maoa22@gmail.com>
2024-11-06 15:24:53 -0500

    feat: use new Soul VIBE HeaderSection

---
+++ core/vibes/soul/sections/header-section/index.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { forwardRef, useEffect, useState } from 'react';
+import Headroom from 'react-headroom';
+
+import { Banner } from '@/vibes/soul/primitives/banner';
+import { Navigation } from '@/vibes/soul/primitives/navigation';
+
+interface Props {
+  navigation: Omit<React.ComponentPropsWithoutRef<typeof Navigation>, 'onDismiss'>;
+  banner?: React.ComponentPropsWithoutRef<typeof Banner>;
+}
+
+export const HeaderSection = forwardRef<React.ComponentRef<'div'>, Props>(
+  ({ navigation, banner }, ref) => {
+    const [bannerElement, setBannerElement] = useState<HTMLElement | null>(null);
+    const [bannerHeight, setBannerHeight] = useState(0);
+
+    useEffect(() => {
+      if (bannerElement) {
+        setBannerHeight(bannerElement.getBoundingClientRect().height);
+      }
+    }, [bannerElement]);
+
+    return (
+      <div ref={ref}>
+        {banner && (
+          <Banner ref={setBannerElement} {...banner} onDismiss={() => setBannerHeight(0)} />
+        )}
+        <Headroom pinStart={bannerHeight}>
+          <div className="p-2">
+            <Navigation {...navigation} />
+          </div>
+        </Headroom>
+      </div>
+    );
+  },
+);
+
+HeaderSection.displayName = 'HeaderSection';
