dc2a48a0f6be800dbb3673da2028513c95289f74
Alan Pledger <apledger3@gmail.com>
2024-11-07 16:57:23 -0500

    feat: add generics for PDP field types

--- core/vibes/soul/sections/product-detail/index.tsx
+++ core/vibes/soul/sections/product-detail/index.tsx
@@ -17,13 +17,13 @@ type ProductDetailProduct = {
   description?: string;
 };
 
-type Props = {
+type Props<F extends Field> = {
   product: ProductDetailProduct;
-  action: ProductDetailFormAction;
-  fields: Field[];
+  action: ProductDetailFormAction<F>;
+  fields: F[];
 };
 
-export function ProductDetail({ product, action, fields }: Props) {
+export function ProductDetail<F extends Field>({ product, action, fields }: Props<F>) {
   return (
     <section className="@container">
       <div className="mx-auto grid w-full max-w-screen-xl grid-cols-1 items-stretch gap-x-8 gap-y-10 px-4 py-10 @xl:px-6 @xl:py-14 @2xl:grid-cols-2 @4xl:px-8 @4xl:py-20 @5xl:gap-x-16">
