bd2ca4bfb0fb835f508ac62ac8a1722662b6ca43
Alan Pledger <apledger3@gmail.com>
2024-11-07 14:33:46 -0500

    feat: pass fields to pdp form

--- core/vibes/soul/sections/product-detail/index.tsx
+++ core/vibes/soul/sections/product-detail/index.tsx
@@ -1,116 +1,59 @@
-'use client';
-
-import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
-import { clsx } from 'clsx';
-import { BcImage as Image } from '~/components/bc-image';
-import { useState } from 'react';
-
-import { Button } from '@/vibes/soul/primitives/button';
-import { Favorite } from '@/vibes/soul/primitives/favorite';
-import { Label } from '@/vibes/soul/primitives/label';
-import { PriceLabel } from '@/vibes/soul/primitives/price-label';
-import { CardProduct } from '@/vibes/soul/primitives/product-card';
+import { Price, PriceLabel } from '@/vibes/soul/primitives/price-label';
 import { Rating } from '@/vibes/soul/primitives/rating';
 import { ProductGallery } from '@/vibes/soul/sections/product-detail/product-gallery';
 
-type ProductDetailType = {
-  options?: string[];
-  swatches?: Array<{
-    id: string;
-    name: string;
-    image?: { src: string; alt: string };
-    hex?: string;
-  }>;
-  images?: Array<{ src: string; alt: string }>;
-} & CardProduct;
+import { ProductDetailForm, ProductDetailFormAction } from './product-detail-form';
+import { Field } from './schema';
 
-export type ProductDetailProps = {
-  product: ProductDetailType;
+type ProductDetailProduct = {
+  id: string;
+  title: string;
+  href: string;
+  images: Array<{ src: string; alt: string }>;
+  price?: Price;
+  subtitle?: string;
+  badge?: string;
+  rating?: number;
+  description?: string;
 };
 
-export function ProductDetail({ product }: ProductDetailProps) {
-  const [favorited, setFavorited] = useState(false);
-  const [selectedOption, setSelectedOption] = useState(product.options?.[0] ?? null);
-  const [selectedSwatch, setSelectedSwatch] = useState(product.swatches?.[0] ?? null);
+type Props = {
+  product: ProductDetailProduct;
+  action: ProductDetailFormAction;
+  fields: Field[];
+};
 
+export function ProductDetail({ product, action, fields }: Props) {
   return (
-    <section className="flex flex-col bg-background @container">
-      <div className="mx-auto grid h-full w-full max-w-screen-2xl flex-grow @4xl:min-h-[800px] @4xl:grid-cols-2">
-        <ProductGallery images={product.images ?? []} />
+    <section className="@container">
+      <div className="mx-auto grid w-full max-w-screen-xl grid-cols-1 items-stretch gap-x-8 gap-y-10 px-4 py-10 @xl:px-6 @xl:py-14 @2xl:grid-cols-2 @4xl:px-8 @4xl:py-20 @5xl:gap-x-16">
+        <div className="hidden @2xl:block">
+          <ProductGallery className="sticky top-8" images={product.images} />
+        </div>
 
         {/* Product Details */}
-        <div className="my-auto flex flex-col gap-4 px-3 py-10 text-foreground @xl:px-6 @4xl:py-28 @5xl:px-20">
-          <h2 className="font-heading text-3xl font-medium leading-none">{product.title}</h2>
+        <div className="text-foreground">
+          {product.subtitle != null && product.subtitle !== '' && (
+            <p className="font-mono text-sm uppercase">{product.subtitle}</p>
+          )}
+
+          <h1 className="mb-3 mt-2 font-heading text-2xl font-medium leading-none @xl:mb-4 @xl:text-3xl @4xl:text-4xl">
+            {product.title}
+          </h1>
+
           <Rating rating={product.rating ?? 0} />
-          {product.subtitle != null && product.subtitle !== '' && <p>{product.subtitle}</p>}
-          <PriceLabel className="!text-2xl" price={product.price ?? ''} />
 
-          <form className="mt-6 flex flex-col gap-4 @4xl:mt-12">
-            {/* Options */}
-            {product.options && (
-              <>
-                <Label>Size</Label>
-                <RadioGroupPrimitive.Root className="flex flex-wrap gap-2.5">
-                  {product.options.map((option, index) => (
-                    <RadioGroupPrimitive.Item
-                      className={clsx(
-                        'flex h-12 w-12 shrink-0 items-center justify-center rounded-full text-xs font-medium transition-colors duration-300',
-                        'ring-primary focus-visible:outline-0 focus-visible:ring-2',
-                        option === selectedOption
-                          ? 'bg-foreground text-background'
-                          : 'bg-contrast-100 hover:bg-contrast-200',
-                      )}
-                      key={index}
-                      onClick={() => setSelectedOption(option)}
-                      value={option}
-                    >
-                      {option}
-                    </RadioGroupPrimitive.Item>
-                  ))}
-                </RadioGroupPrimitive.Root>
-              </>
-            )}
+          <PriceLabel className="my-3 text-xl @xl:text-2xl" price={product.price ?? ''} />
 
-            {/* Swatches */}
-            {product.swatches && (
-              <>
-                {/* TODO: Restructure JSON to include group name */}
-                <Label className="mt-6">
-                  {(selectedSwatch?.name != null && selectedSwatch.name !== '') || 'Color'}
-                </Label>
-                <RadioGroupPrimitive.Root className="flex flex-wrap gap-2.5">
-                  {product.swatches.map((swatch) => (
-                    <RadioGroupPrimitive.Item
-                      className={clsx(
-                        'relative h-12 w-12 shrink-0 overflow-hidden rounded-full transition-colors duration-300',
-                        'focus-visible:outline-0 focus-visible:ring-2',
-                        swatch.id === selectedSwatch?.id ? 'ring-primary' : 'ring-transparent',
-                      )}
-                      key={swatch.id}
-                      onClick={() => setSelectedSwatch(swatch)}
-                      value={swatch.id}
-                    >
-                      {swatch.image?.src != null && swatch.image.src !== '' ? (
-                        <Image
-                          alt={swatch.image.alt}
-                          className="h-full object-cover"
-                          height={48}
-                          src={swatch.image.src}
-                          width={48}
-                        />
-                      ) : null}
-                      {/* {swatch.name} */}
-                    </RadioGroupPrimitive.Item>
-                  ))}
-                </RadioGroupPrimitive.Root>
-              </>
-            )}
+          <div className="mb-8 @2xl:hidden">
+            <ProductGallery images={product.images} />
+          </div>
 
-            <div className="mt-4 flex max-w-sm gap-2">
-              <Button className="flex-grow">Add to Cart</Button>
-              <Favorite checked={favorited} setChecked={setFavorited} />
-            </div>
-          </form>
+          {product.description != null && product.description !== '' && (
+            <p className="mb-6 text-contrast-500">{product.description}</p>
+          )}
+
+          <ProductDetailForm action={action} fields={fields} productId={product.id} />
         </div>
       </div>
     </section>
--- core/vibes/soul/sections/product-detail/product-gallery.tsx
+++ core/vibes/soul/sections/product-detail/product-gallery.tsx
@@ -2,18 +2,15 @@
 
 import { clsx } from 'clsx';
 import useEmblaCarousel from 'embla-carousel-react';
+import { BcImage as Image } from '~/components/bc-image';
 import { useEffect, useState } from 'react';
 
-import { BcImage } from '~/components/bc-image';
-
-export type ProductGalleryProps = {
-  images: Array<{
-    alt: string;
-    src: string;
-  }>;
+type Props = {
+  images: Array<{ alt: string; src: string }>;
+  className?: string;
 };
 
-export const ProductGallery = ({ images }: ProductGalleryProps) => {
+export function ProductGallery({ images, className }: Props) {
   const [previewImage, setPreviewImage] = useState(0);
   const [emblaRef, emblaApi] = useEmblaCarousel();
 
@@ -35,15 +32,12 @@ export const ProductGallery = ({ images }: ProductGalleryProps) => {
   };
 
   return (
-    <div className="relative mt-[60px] flex h-96 w-full items-center overflow-hidden bg-contrast-100 @2xl:h-[550px] @4xl:mt-0 @4xl:h-full">
-      <div className="my-auto h-full max-h-[800px] w-full overflow-hidden" ref={emblaRef}>
-        <div className="flex h-full w-full">
+    <div className={clsx('@container', className)}>
+      <div className="w-full overflow-hidden rounded-xl @xl:rounded-2xl" ref={emblaRef}>
+        <div className="flex">
           {images.map((image, idx) => (
-            <div
-              className="relative h-full w-full min-w-0 shrink-0 grow-0 basis-full items-center"
-              key={idx}
-            >
-              <BcImage
+            <div className="relative aspect-[4/5] w-full shrink-0 grow-0 basis-full" key={idx}>
+              <Image
                 alt={image.alt}
                 className="object-cover"
                 fill
@@ -56,27 +50,23 @@ export const ProductGallery = ({ images }: ProductGalleryProps) => {
         </div>
       </div>
 
-      <div className="absolute bottom-6 left-1/2 flex max-w-full -translate-x-1/2 gap-1.5 overflow-x-auto px-10 @3xl:gap-3 @4xl:bottom-10">
+      <div className="mt-2 flex max-w-full gap-2 overflow-x-auto">
         {images.map((image, index) => (
           <button
             aria-label={`View image number ${index + 1}`}
             className={clsx(
-              'h-10 w-10 shrink-0 overflow-hidden rounded-lg border transition-colors duration-300 @4xl:h-14 @4xl:w-14',
-              index === previewImage ? 'border-foreground' : 'border-transparent',
+              'relative h-12 w-12 shrink-0 overflow-hidden rounded-lg border transition-all duration-300 hover:opacity-100 @md:h-16 @md:w-16',
+              index === previewImage
+                ? 'border-foreground opacity-100'
+                : 'border-transparent opacity-50',
             )}
             key={index}
             onClick={() => selectImage(index)}
           >
-            <BcImage
-              alt={image.alt}
-              className="h-full w-full bg-contrast-100 object-cover"
-              height={256}
-              src={image.src}
-              width={256}
-            />
+            <Image alt={image.alt} className="bg-contrast-100 object-cover" fill src={image.src} />
           </button>
         ))}
       </div>
     </div>
   );
-};
+}
