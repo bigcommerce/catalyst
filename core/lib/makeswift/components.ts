84f82ceff3fafdd3e9019315acc5c6ce1d775eca
Aleksey Gurtovoy <agurtovoy@acm.org>
2024-11-07 19:31:50 -0600

    feat: editable PDP

--- core/lib/makeswift/components.ts
+++ core/lib/makeswift/components.ts
@@ -8,8 +8,8 @@ import '~/makeswift/components/button-link/button-link.makeswift';
 import '~/makeswift/components/card/card.makeswift';
 import '~/makeswift/components/carousel/carousel.makeswift';
 import '~/makeswift/components/card-carousel/card-carousel.makeswift';
+import '~/makeswift/components/product-description/register';
 import '~/makeswift/components/products-carousel/products-carousel.makeswift';
-
 import '~/makeswift/components/products-list/products-list.makeswift';
 import '~/makeswift/components/inline-email-form/inline-email-form.makeswift';
 
