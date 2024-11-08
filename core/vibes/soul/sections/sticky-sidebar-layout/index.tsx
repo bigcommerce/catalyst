a4a6584507effecc46ea022d9c7bd524afc36074
Alan Pledger <apledger3@gmail.com>
2024-11-08 17:27:59 -0500

    feat: add container size to sticky sidebar layout

--- core/vibes/soul/sections/sticky-sidebar-layout/index.tsx
+++ core/vibes/soul/sections/sticky-sidebar-layout/index.tsx
@@ -6,16 +6,27 @@ export function StickySidebarLayout({
   children,
   sidebarSize = '1/3',
   sidebarPosition = 'left',
+  containerSize = '2xl',
 }: {
   className?: string;
   sidebar: React.ReactNode;
   children: React.ReactNode;
+  containerSize?: 'lg' | 'xl' | '2xl';
   sidebarSize?: '1/4' | '1/3' | '1/2' | 'small' | 'medium' | 'large';
   sidebarPosition?: 'left' | 'right';
 }) {
   return (
     <section className={clsx('@container', className)}>
-      <div className="mx-auto flex max-w-screen-2xl flex-col items-stretch gap-x-16 gap-y-10 px-4 py-10 @xl:px-6 @xl:py-14 @2xl:flex-row @4xl:px-8 @4xl:py-20">
+      <div
+        className={clsx(
+          'mx-auto flex flex-col items-stretch gap-x-16 gap-y-10 px-4 py-10 @xl:px-6 @xl:py-14 @2xl:flex-row @4xl:px-8 @4xl:py-20',
+          {
+            lg: 'max-w-screen-lg',
+            xl: 'max-w-screen-xl',
+            '2xl': 'max-w-screen-2xl',
+          }[containerSize],
+        )}
+      >
         <div
           className={clsx(
             clsx(sidebarPosition === 'right' ? 'order-2' : 'order-1'),
