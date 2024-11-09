b30b5a07e67c7dc8bac8bc8cec962abcce6cc5aa
Alan Pledger <apledger3@gmail.com>
2024-11-09 18:37:48 -0500

    feat: add compare section empty state and skeleton

--- core/vibes/soul/primitives/compare-card/index.tsx
+++ core/vibes/soul/primitives/compare-card/index.tsx
@@ -1,9 +1,12 @@
 import clsx from 'clsx';
 
 import { Button } from '@/vibes/soul/primitives/button';
-import { CardProduct, ProductCard } from '@/vibes/soul/primitives/product-card';
-
-import { Rating } from '../rating';
+import {
+  CardProduct,
+  ProductCard,
+  ProductCardSkeleton,
+} from '@/vibes/soul/primitives/product-card';
+import { Rating } from '@/vibes/soul/primitives/rating';
 
 export type CompareProduct = CardProduct & {
   description?: string;
@@ -79,6 +82,32 @@ export function CompareCard({
           <p className="text-sm text-contrast-400"> There are no other details available.</p>
         </div>
       )}
+    </div>
+  );
+}
+
+export function CompareCardSkeleton({ className }: { className?: string }) {
+  return (
+    <div
+      className={clsx(
+        'flex w-full animate-pulse flex-col divide-y divide-contrast-100 @container',
+        className,
+      )}
+    >
+      <div className="mb-2 space-y-4 pb-4">
+        <ProductCardSkeleton />
+        <div className="h-14 w-full rounded-full bg-contrast-100" />
+      </div>
+
+      <div className="space-y-4 py-4">
+        <div className="h-[1lh] rounded-md bg-contrast-100 font-mono text-xs uppercase" />
+        <div className="h-5 w-12 rounded-md bg-contrast-100" />
+      </div>
+
+      <div className="space-y-4 py-4">
+        <div className="h-[1lh] rounded-md bg-contrast-100 font-mono text-xs" />
+        <div className="h-[1lh] rounded-md bg-contrast-100 text-sm" />
+      </div>
     </div>
   );
 }
