b2ba1d385e7949a0b1f92c316bdf6173d91ebb87
Alan Pledger <apledger3@gmail.com>
2024-11-09 14:45:02 -0500

    fix: compare and button styles

--- core/vibes/soul/sections/featured-products-carousel/index.tsx
+++ core/vibes/soul/sections/featured-products-carousel/index.tsx
@@ -7,7 +7,7 @@ type Link = {
 };
 
 type Props = {
-  title: string;
+  title?: string;
   description?: string;
   cta?: Link;
   products: CarouselProduct[];
@@ -19,13 +19,14 @@ export function FeaturedProductsCarousel({ title, description, cta, products }: 
       <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
         <div className="mb-6 flex w-full flex-row flex-wrap justify-between gap-x-8 text-foreground @4xl:mb-8 @4xl:items-end">
           <div className="flex-1">
-            <h2 className="font-heading text-2xl font-medium leading-none @xl:text-3xl @4xl:text-4xl">
+            <h2 className="font-heading text-2xl leading-none @xl:text-3xl @4xl:text-4xl">
               {title}
             </h2>
             {description != null && description !== '' && (
-              <p className="mt-3 max-w-xl text-contrast-500">{description}</p>
+              <p className="mt-3 max-w-xl leading-relaxed text-contrast-500">{description}</p>
             )}
           </div>
+
           {cta != null && cta.href !== '' && cta.label !== '' && (
             <AnimatedLink className="mr-3" label={cta.label} link={{ href: cta.href }} />
           )}
