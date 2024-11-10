9904a653f9b0b9364b65fda3e70b38b744292fea
Alan Pledger <apledger3@gmail.com>
2024-11-10 16:40:47 -0500

    feat: add section layout

---
+++ core/vibes/soul/sections/section-layout/index.tsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx';
+
+export function SectionLayout({
+  className,
+  children,
+  containerSize = '2xl',
+}: {
+  className?: string;
+  children: React.ReactNode;
+  containerSize?: 'lg' | 'xl' | '2xl';
+}) {
+  return (
+    <section className={clsx('@container', className)}>
+      <div
+        className={clsx(
+          'mx-auto px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20',
+          {
+            lg: 'max-w-screen-lg',
+            xl: 'max-w-screen-xl',
+            '2xl': 'max-w-screen-2xl',
+          }[containerSize],
+        )}
+      >
+        {children}
+      </div>
+    </section>
+  );
+}
