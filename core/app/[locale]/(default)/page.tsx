a91e9da782a89497a1b13e94fbeb81051aae0a90
Fikri Karim <fkfikrikarim@gmail.com>
2024-11-11 12:44:03 +0700

    feat: make homepage visually editable

--- core/app/[locale]/(default)/page.tsx
+++ core/app/[locale]/(default)/page.tsx
@@ -1,97 +1,26 @@
-import { removeEdgesAndNodes } from '@bigcommerce/catalyst-client';
-import { getTranslations, setRequestLocale } from 'next-intl/server';
+import { Page as MakeswiftPage } from '@makeswift/runtime/next';
+import { getSiteVersion } from '@makeswift/runtime/next/server';
+import { notFound } from 'next/navigation';
 
-import { FeaturedImage } from '@/vibes/soul/sections/featured-image';
-// import { Subscribe } from '@/vibes/soul/sections/subscribe';
-import { getSessionCustomerAccessToken } from '~/auth';
-import { client } from '~/client';
-import { graphql } from '~/client/graphql';
-import { revalidate } from '~/client/revalidate-target';
-import { FeaturedProductsList } from '~/components/featured-product-list';
-import { FeaturedProductsListFragment } from '~/components/featured-product-list/fragment';
-import { FeaturedProductsCarousel } from '~/components/featured-products-carousel';
-import { FeaturedProductsCarouselFragment } from '~/components/featured-products-carousel/fragment';
-import { Slideshow } from '~/components/slideshow';
-import { LocaleType } from '~/i18n/routing';
-
-import image from './_images/featured1.jpg';
-
-const HomePageQuery = graphql(
-  `
-    query HomePageQuery {
-      site {
-        newestProducts(first: 8) {
-          edges {
-            node {
-              ...FeaturedProductsCarouselFragment
-            }
-          }
-        }
-        featuredProducts(first: 6) {
-          edges {
-            node {
-              ...FeaturedProductsListFragment
-            }
-          }
-        }
-      }
-    }
-  `,
-  [FeaturedProductsCarouselFragment, FeaturedProductsListFragment],
-);
+import { defaultLocale } from '~/i18n/routing';
+import { client } from '~/lib/makeswift/client';
+import '~/lib/makeswift/components';
 
 interface Props {
   params: {
-    locale: LocaleType;
+    locale: string;
   };
 }
 
 export default async function Home({ params: { locale } }: Props) {
-  setRequestLocale(locale);
-
-  const t = await getTranslations('Home');
-  const customerAccessToken = await getSessionCustomerAccessToken();
-
-  const { data } = await client.fetch({
-    document: HomePageQuery,
-    customerAccessToken,
-    fetchOptions: customerAccessToken ? { cache: 'no-store' } : { next: { revalidate } },
+  const snapshot = await client.getPageSnapshot('/', {
+    siteVersion: await getSiteVersion(),
+    locale: locale === defaultLocale ? undefined : locale,
   });
 
-  const featuredProducts = removeEdgesAndNodes(data.site.featuredProducts);
-  const newestProducts = removeEdgesAndNodes(data.site.newestProducts);
+  if (snapshot == null) return notFound();
 
-  return (
-    <>
-      <Slideshow />
-
-      <FeaturedProductsCarousel products={newestProducts} title={t('Carousel.title')} />
-
-      <FeaturedImage
-        cta={{ href: '/shop-all', label: t('FeaturedImage.cta') }}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
-        image={{
-          src: image.src,
-          alt: t('FeaturedImage.altText'),
-          blurDataUrl: image.blurDataURL,
-        }}
-        title={t('FeaturedImage.title')}
-      />
-
-      <FeaturedProductsList
-        cta={{ href: '/#', label: t('List.cta') }}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        products={featuredProducts}
-        title={t('List.title')}
-      />
-
-      {/* TODO: Wire up action */}
-      {/* <Subscribe
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-        title={t('Subscribe.title')}
-      /> */}
-    </>
-  );
+  return <MakeswiftPage snapshot={snapshot} />;
 }
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
