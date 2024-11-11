b00aa1a1f3a22738148bfff350a4630b9c6652ac
Alan Pledger <apledger3@gmail.com>
2024-11-10 19:54:03 -0500

    feat: use streamable patterns in carousels

--- core/vibes/soul/primitives/card-carousel/index.tsx
+++ core/vibes/soul/primitives/card-carousel/index.tsx
@@ -1,3 +1,6 @@
+import clsx from 'clsx';
+import { Suspense } from 'react';
+
 import { Card, CardProps, CardSkeleton } from '@/vibes/soul/primitives/card';
 import {
   Carousel,
@@ -7,33 +10,47 @@ import {
   CarouselScrollbar,
 } from '@/vibes/soul/primitives/carousel';
 
+import { Streamable } from '../../lib/streamable';
+import { mapStreamable } from '../../lib/streamable/server';
+
 export type Card = CardProps & {
   id: string;
 };
 
 type Props = {
-  cards: Card[];
+  cards: Streamable<Card[]>;
   textContrast?: 'light' | 'dark';
   className?: string;
   emptyStateMessage?: string;
 };
 
-export function CardCarousel({ cards, textContrast, className, emptyStateMessage }: Props) {
-  if (cards.length === 0) {
-    return <CardCarouselEmptyState className={className} message={emptyStateMessage} />;
-  }
-
+export function CardCarousel({
+  cards: streamableCards,
+  textContrast,
+  className,
+  emptyStateMessage = 'No items found',
+}: Props) {
   return (
     <Carousel className={className}>
       <CarouselContent className="mb-10">
-        {cards.map((card) => (
-          <CarouselItem
-            className="basis-full @md:basis-1/2 @lg:basis-1/3 @2xl:basis-1/4"
-            key={card.id}
-          >
-            <Card {...card} textContrast={textContrast} />
-          </CarouselItem>
-        ))}
+        <Suspense
+          fallback={<CardCarouselSkeleton className={className} message={emptyStateMessage} />}
+        >
+          {mapStreamable(streamableCards, (cards) => {
+            if (cards.length === 0) {
+              return <CardCarouselSkeleton className={className} message={emptyStateMessage} />;
+            }
+
+            return cards.map((card) => (
+              <CarouselItem
+                className="basis-full @md:basis-1/2 @lg:basis-1/3 @2xl:basis-1/4"
+                key={card.id}
+              >
+                <Card {...card} textContrast={textContrast} />
+              </CarouselItem>
+            ));
+          })}
+        </Suspense>
       </CarouselContent>
       <div className="flex w-full items-center justify-between">
         <CarouselScrollbar />
@@ -43,17 +60,24 @@ export function CardCarousel({ cards, textContrast, className, emptyStateMessage
   );
 }
 
-export function CardCarouselEmptyState({
+export function CardCarouselSkeleton({
   className,
-  message = 'No items found',
+  message,
+  count = 8,
 }: {
   className?: string;
   message?: string;
+  count?: number;
 }) {
   return (
     <Carousel className={className}>
-      <CarouselContent className="relative mb-10 [mask-image:radial-gradient(circle,transparent,black)]">
-        {Array.from({ length: 8 }).map((_, index) => (
+      <CarouselContent
+        className={clsx(
+          'relative mb-10',
+          message && message !== '' && '[mask-image:radial-gradient(circle,transparent,black)]',
+        )}
+      >
+        {Array.from({ length: count }).map((_, index) => (
           <CarouselItem
             className="basis-full @md:basis-1/2 @lg:basis-1/3 @2xl:basis-1/4"
             key={index}
@@ -67,27 +91,6 @@ export function CardCarouselEmptyState({
         <CarouselButtons />
       </div>
       <div className="absolute inset-0 flex items-center justify-center text-lg">{message}</div>
-    </Carousel>
-  );
-}
-
-export function CardCarouselSkeleton({ className }: Props) {
-  return (
-    <Carousel className={className}>
-      <CarouselContent className="mb-10">
-        {Array.from({ length: 8 }).map((_, index) => (
-          <CarouselItem
-            className="basis-full @md:basis-1/2 @lg:basis-1/3 @2xl:basis-1/4"
-            key={index}
-          >
-            <CardSkeleton />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <div className="flex w-full items-center justify-between">
-        <CarouselScrollbar />
-        <CarouselButtons />
-      </div>
     </Carousel>
   );
 }
