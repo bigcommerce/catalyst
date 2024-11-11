a7f9aa8f219b7dc9abeb180ee806d0d56494e136
Alan Pledger <apledger3@gmail.com>
2024-11-11 17:44:15 -0500

    fix: remove registered bc image

--- core/lib/makeswift/components.ts
+++ core/lib/makeswift/components.ts
@@ -14,7 +14,6 @@ import '~/makeswift/components/products-carousel/products-carousel.makeswift';
 import '~/makeswift/components/products-list/products-list.makeswift';
 // import '~/makeswift/components/inline-email-form/inline-email-form.makeswift';
 import '~/makeswift/components/icon/icon.makeswift';
-import '~/makeswift/components/image/image.makeswift';
 import '~/makeswift/components/slideshow/slideshow.makeswift';
 // import '~/makeswift/components/featured-image/featured-image.makeswift';
 import '~/makeswift/components/sticky-sidebar-layout/sticky-sidebar-layout.makeswift';
