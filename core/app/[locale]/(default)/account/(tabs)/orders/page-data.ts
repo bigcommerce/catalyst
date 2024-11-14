be374cfaeec0af12bee1af4b5f69fcc4a04844fc
Alexander Saiannyi <alexander.saiannyi@bigcommerce.com>
2024-11-14 14:12:29 +0100

    refactor(core): add filters to orders query

--- core/app/[locale]/(default)/account/(tabs)/orders/page-data.ts
+++ core/app/[locale]/(default)/account/(tabs)/orders/page-data.ts
@@ -31,9 +31,15 @@ const OrderShipmentFragment = graphql(`
 
 const CustomerAllOrders = graphql(
   `
-    query CustomerAllOrders($after: String, $before: String, $first: Int, $last: Int) {
+    query CustomerAllOrders(
+      $after: String
+      $before: String
+      $first: Int
+      $last: Int
+      $filters: OrdersFiltersInput
+    ) {
       customer {
-        orders(after: $after, before: $before, first: $first, last: $last) {
+        orders(after: $after, before: $before, first: $first, last: $last, filters: $filters) {
           pageInfo {
             ...PaginationFragment
           }
@@ -241,20 +247,37 @@ const addProductAttributesToShippingConsignments = async (consignments: Shipping
   return shippingConsignments;
 };
 
+type OrdersFiltersInput = VariablesOf<typeof CustomerAllOrders>['filters'];
+type OrderStatus = NonNullable<OrdersFiltersInput>['status'];
+type OrderDateRange = NonNullable<OrdersFiltersInput>['dateRange'];
+
 interface CustomerOrdersArgs {
   after?: string;
   before?: string;
   limit?: number;
+  filterByStatus?: OrderStatus;
+  filterByDateRange?: OrderDateRange;
 }
 
 export const getCustomerOrders = cache(
-  async ({ before = '', after = '', limit = 2 }: CustomerOrdersArgs) => {
+  async ({
+    before = '',
+    after = '',
+    filterByStatus,
+    filterByDateRange,
+    limit = 2,
+  }: CustomerOrdersArgs) => {
     const customerAccessToken = await getSessionCustomerAccessToken();
     const paginationArgs = before ? { last: limit, before } : { first: limit, after };
-
+    const filtersArgs = {
+      filters: {
+        ...(filterByDateRange && { dateRange: filterByDateRange }),
+        ...(filterByStatus && { status: filterByStatus }),
+      },
+    };
     const response = await client.fetch({
       document: CustomerAllOrders,
-      variables: { ...paginationArgs },
+      variables: { ...paginationArgs, ...filtersArgs },
       customerAccessToken,
       fetchOptions: { cache: 'no-store' },
     });
