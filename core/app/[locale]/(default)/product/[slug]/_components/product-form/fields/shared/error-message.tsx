6299a2ff9e8f055bbfee66eb48f10f32cd059aee
Jorge Moya <jorge.moya@bigcommerce.com>
2024-11-18 15:35:14 -0600

    fix: eslint issues

--- core/app/[locale]/(default)/product/[slug]/_components/product-form/fields/shared/error-message.tsx
+++ core/app/[locale]/(default)/product/[slug]/_components/product-form/fields/shared/error-message.tsx
@@ -1,3 +1,3 @@
 export const ErrorMessage = ({ children }: { children: React.ReactNode }) => (
-  <p className="mt-2 text-xs font-normal text-error-secondary">{children}</p>
+  <p className="text-error-secondary mt-2 text-xs font-normal">{children}</p>
 );
