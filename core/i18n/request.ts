a6c99f8731ff771a0ea0201962f3da4384eb871d
Riccardo Tempesta <riccardo.tempesta@gmail.com>
2024-11-19 20:20:52 +0100

    Do not import two times the same loacale when fallback is the same as locale

--- core/i18n/request.ts
+++ core/i18n/request.ts
@@ -1,15 +1,23 @@
+import deepmerge from 'deepmerge';
 import { notFound } from 'next/navigation';
 import { getRequestConfig } from 'next-intl/server';
 
 import { fallbackLocale, locales } from './routing';
 
-import deepmerge from 'deepmerge';
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const locale = await requestLocale;
 
   if (!locale || !locales.includes(locale)) {
     notFound();
+  }
+
+  if (locale === fallbackLocale) {
+    return {
+      locale,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+      messages: (await import(`../messages/${locale}.json`)).default,
+    };
   }
 
   return {
