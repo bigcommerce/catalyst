d60f9035ca1bbcd5ff547fef7dd410399c0657db
Jorge Moya <jorge.moya@bigcommerce.com>
2024-11-21 13:59:47 -0600

    feat(core): add FeaturedProductsList component and use in Homepage (#1673)

--- core/app/[locale]/(default)/layout.tsx
+++ core/app/[locale]/(default)/layout.tsx
@@ -20,9 +20,7 @@ export default async function DefaultLayout({ params, children }: Props) {
         <Header cart={<Cart />} />
       </Suspense>
 
-      <main className="flex-1 px-4 2xl:container sm:px-10 lg:px-12 2xl:mx-auto 2xl:px-0">
-        {children}
-      </main>
+      <main>{children}</main>
 
       <Suspense>
         <Footer />
