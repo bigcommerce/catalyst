fa6b0475d0a264291d418e0b5ea86b7251933ff5
Jorge Moya <jorge.moya@bigcommerce.com>
2024-11-22 16:36:32 -0600

    feat(core): add subscribe form to Homepage

---
+++ core/vibes/soul/primitives/inline-email-form/index.tsx
@@ -0,0 +1,80 @@
+'use client';
+
+import { getFormProps, getInputProps, SubmissionResult, useForm } from '@conform-to/react';
+import { parseWithZod } from '@conform-to/zod';
+import { clsx } from 'clsx';
+import { ArrowRight } from 'lucide-react';
+import { useActionState, useEffect } from 'react';
+
+import { ErrorMessage } from '../../form/error-message';
+import { Button } from '../button';
+
+import { schema } from './schema';
+
+type Action<State, Payload> = (
+  prevState: Awaited<State>,
+  formData: Payload,
+) => State | Promise<State>;
+
+export function InlineEmailForm({
+  className,
+  action,
+  submitLabel = 'Submit',
+  placeholder = 'Enter your email',
+}: {
+  className?: string;
+  placeholder?: string;
+  submitLabel?: string;
+  action: Action<SubmissionResult | null, FormData>;
+}) {
+  const [lastResult, formAction, isPending] = useActionState(action, null);
+
+  const [form, fields] = useForm({
+    lastResult,
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema });
+    },
+    shouldValidate: 'onSubmit',
+    shouldRevalidate: 'onInput',
+  });
+
+  useEffect(() => {
+    if (lastResult?.error) {
+      console.log(lastResult.error);
+    }
+  }, [lastResult]);
+
+  const { errors = [] } = fields.email;
+
+  return (
+    <form {...getFormProps(form)} action={formAction} className={clsx('space-y-2', className)}>
+      <div
+        className={clsx(
+          'relative rounded-xl border bg-background text-base transition-colors duration-200 focus-within:border-primary focus:outline-none',
+          errors.length ? 'border-error' : 'border-black',
+        )}
+      >
+        <input
+          {...getInputProps(fields.email, { type: 'email' })}
+          className="placeholder-contrast-gray-500 h-14 w-full bg-transparent pl-5 pr-16 text-foreground placeholder:font-normal focus:outline-none"
+          key={fields.email.id}
+          placeholder={placeholder}
+        />
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 pr-2">
+          <Button
+            aria-label={submitLabel}
+            loading={isPending}
+            size="icon"
+            type="submit"
+            variant="secondary"
+          >
+            <ArrowRight size={20} strokeWidth={1.5} />
+          </Button>
+        </div>
+      </div>
+      {errors.map((error, index) => (
+        <ErrorMessage key={index}>{error}</ErrorMessage>
+      ))}
+    </form>
+  );
+}
---
+++ core/vibes/soul/sections/subscribe/index.tsx
@@ -0,0 +1,58 @@
+import { SubmissionResult } from '@conform-to/react';
+import { clsx } from 'clsx';
+
+import { InlineEmailForm } from '@/vibes/soul/primitives/inline-email-form';
+import { BcImage as Image } from '~/components/bc-image';
+
+type Action<State, Payload> = (state: Awaited<State>, payload: Payload) => State | Promise<State>;
+
+export function Subscribe({
+  action,
+  image,
+  title,
+  description,
+  placeholder,
+}: {
+  action: Action<SubmissionResult | null, FormData>;
+  image?: { src: string; alt: string };
+  title: string;
+  description?: string;
+  placeholder?: string;
+}) {
+  return (
+    <section className="bg-primary-shadow @container">
+      <div className="flex flex-col items-start @4xl:flex-row @4xl:items-stretch">
+        {image && (
+          <div className="relative min-h-96 w-full bg-primary/10 @4xl:flex-1">
+            <Image
+              alt={image.alt}
+              className="object-cover"
+              fill
+              sizes="(max-width: 680px) 100vw, 50vw"
+              src={image.src}
+            />
+          </div>
+        )}
+
+        <div className="w-full flex-1">
+          <div
+            className={clsx(
+              'flex w-full flex-col gap-10 px-4 py-10 @xl:px-6 @xl:py-14 @4xl:gap-16 @4xl:px-8 @4xl:py-20',
+              image != null
+                ? '@4xl:max-w-4xl'
+                : 'mx-auto max-w-screen-2xl @4xl:flex-row @4xl:items-center',
+            )}
+          >
+            <div className="flex-1">
+              <h2 className="mb-4 font-heading text-2xl font-medium leading-none text-primary-highlight @xl:text-3xl @4xl:text-4xl">
+                {title}
+              </h2>
+              <p className="text-primary-highlight opacity-75">{description}</p>
+            </div>
+            <InlineEmailForm action={action} className="flex-1" placeholder={placeholder} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
