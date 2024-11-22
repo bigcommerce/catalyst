b7331e88a5edea77ede9b0014680efc551665892
Jorge Moya <jorge.moya@bigcommerce.com>
2024-11-22 12:10:02 -0600

    chore: bump components to include latest Streamable util (#1681)

---
+++ core/vibes/soul/lib/streamable.tsx
@@ -0,0 +1,33 @@
+import { Suspense, use } from 'react';
+
+export type Streamable<T> = T | Promise<T>;
+
+export function useStreamable<T>(streamable: Streamable<T>): T {
+  return streamable instanceof Promise ? use(streamable) : streamable;
+}
+
+function UseStreamable<T>({
+  value,
+  children,
+}: {
+  value: Streamable<T>;
+  children(value: T): React.ReactNode;
+}) {
+  return children(useStreamable(value));
+}
+
+export function Stream<T>({
+  value,
+  fallback,
+  children,
+}: {
+  value: Streamable<T>;
+  fallback?: React.ReactNode;
+  children(value: T): React.ReactNode;
+}) {
+  return (
+    <Suspense fallback={fallback}>
+      <UseStreamable value={value}>{children}</UseStreamable>
+    </Suspense>
+  );
+}
--- core/vibes/soul/lib/streamable/streamable.ts
+++
@@ -1,7 +0,0 @@
-import { use } from 'react';
-
-export type Streamable<T> = T | Promise<T>;
-
-export function useStreamable<T>(streamable: Streamable<T>): T {
-  return streamable instanceof Promise ? use(streamable) : streamable;
-}
