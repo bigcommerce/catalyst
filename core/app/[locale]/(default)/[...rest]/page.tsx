4c248f20488e9b8d5954f1065cb96334f1243e02
Fikri Karim <fkfikrikarim@gmail.com>
2024-12-02 22:08:45 +0700

    feat: integrate makeswift with catalyst

--- core/app/[locale]/(default)/[...rest]/page.tsx
+++ core/app/[locale]/(default)/[...rest]/page.tsx
@@ -1,5 +1,47 @@
+import { Page as MakeswiftPage } from '@makeswift/runtime/next';
+import { getSiteVersion } from '@makeswift/runtime/next/server';
 import { notFound } from 'next/navigation';
 
-export default function CatchAllPage() {
-  notFound();
+import { defaultLocale, locales } from '~/i18n/routing';
+import { client } from '~/lib/makeswift/client';
+import { MakeswiftProvider } from '~/lib/makeswift/provider';
+
+export async function generateStaticParams() {
+  const pages = await client.getPages().toArray();
+
+  return pages
+    .filter((page) => page.path !== '/')
+    .flatMap((page) =>
+      locales.map((locale) => ({
+        rest: page.path.split('/').filter((segment) => segment !== ''),
+        locale: locale === defaultLocale ? undefined : locale,
+      })),
+    );
 }
+
+interface Props {
+  params: Promise<{
+    locale: string;
+    rest: string[];
+  }>;
+}
+
+export default async function CatchAllPage({ params }: Props) {
+  const { rest, locale } = await params;
+  const path = `/${rest.join('/')}`;
+
+  const snapshot = await client.getPageSnapshot(path, {
+    siteVersion: getSiteVersion(),
+    locale: locale === defaultLocale ? undefined : locale,
+  });
+
+  if (snapshot == null) return notFound();
+
+  return (
+    <MakeswiftProvider>
+      <MakeswiftPage snapshot={snapshot} />
+    </MakeswiftProvider>
+  );
+}
+
+export const runtime = 'nodejs';
--- core/app/[locale]/layout.tsx
+++ core/app/[locale]/layout.tsx
@@ -1,3 +1,4 @@
+import { DraftModeScript } from '@makeswift/runtime/next/server';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
@@ -108,6 +109,9 @@ export default async function RootLayout({ params, children }: Props) {
 
   return (
     <html lang={locale}>
+      <head>
+        <DraftModeScript appOrigin={process.env.MAKESWIFT_APP_ORIGIN} />
+      </head>
       <body className={`${inter.variable} ${dm_serif_text.variable} ${roboto_mono.variable}`}>
         <Notifications />
         <NextIntlClientProvider locale={locale} messages={messages}>
