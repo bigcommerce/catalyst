6a5d55fdc0d3bda89b28a25bee9c0b35548517bc
Alan Pledger <apledger3@gmail.com>
2024-12-05 14:36:02 -0500

    feat: add error message for sign in

--- core/vibes/soul/form/form-status/index.tsx
+++ core/vibes/soul/form/form-status/index.tsx
@@ -1,5 +1,5 @@
-import { clsx } from 'clsx'
-import { CheckCircle, CircleAlert } from 'lucide-react'
+import { clsx } from 'clsx';
+import { CheckCircle, CircleAlert } from 'lucide-react';
 
 export function FormStatus({
   className,
@@ -11,18 +11,18 @@ export function FormStatus({
     <div
       {...rest}
       className={clsx(
-        'flex items-center gap-3 rounded-xl bg-error-highlight px-4 py-3 text-xs',
+        'flex items-center gap-3 rounded-xl bg-error-highlight px-4 py-3 text-sm',
         {
           error: 'bg-error-highlight text-error-shadow',
           success: 'bg-success-highlight text-success-shadow',
         }[type],
-        className
+        className,
       )}
     >
-      {type === 'error' && <CircleAlert strokeWidth={1.5} size={20} />}
-      {type === 'success' && <CheckCircle strokeWidth={1.5} size={20} />}
+      {type === 'error' && <CircleAlert className="shrink-0" strokeWidth={1.5} size={20} />}
+      {type === 'success' && <CheckCircle className="shrink-0" strokeWidth={1.5} size={20} />}
 
       {children}
     </div>
-  )
+  );
 }
