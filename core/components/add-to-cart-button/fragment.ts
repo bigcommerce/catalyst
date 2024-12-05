ae1033b7c1a1f3f9578b90fe32e8523bed75e520
Eoin Conway <eoinconway@live.com>
2024-12-05 12:27:07 +0000

    check for variant stock levels before disabling 'add to cart' button

--- core/components/add-to-cart-button/fragment.ts
+++ core/components/add-to-cart-button/fragment.ts
@@ -3,6 +3,7 @@ import { graphql } from '~/client/graphql';
 export const AddToCartButtonFragment = graphql(`
   fragment AddToCartButtonFragment on Product {
     inventory {
+      hasVariantInventory
       isInStock
     }
     availabilityV2 {
