1e875a86a5118d22214fd3d11694699b7ccfcdc7
Jorge Moya <jorge.moya@bigcommerce.com>
2024-12-05 16:29:46 -0600

    chore: bump buttons and add alert and toaster

--- core/vibes/soul/primitives/button-link/index.tsx
+++ core/vibes/soul/primitives/button-link/index.tsx
@@ -3,8 +3,8 @@ import { clsx } from 'clsx';
 import { Link } from '~/components/link';
 
 export type Props = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
-  variant?: 'primary' | 'secondary' | 'tertiary';
-  size?: 'large' | 'medium' | 'small' | 'icon';
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'ghost';
+  size?: 'large' | 'medium' | 'small' | 'x-small' | 'icon' | 'icon-small';
   href: string;
 };
 
@@ -20,7 +20,7 @@ export function ButtonLink({
     <Link
       {...props}
       className={clsx(
-        'relative z-0 inline-flex h-fit select-none items-center justify-center overflow-hidden rounded-full border text-center font-medium leading-normal after:absolute after:inset-0 after:-z-10 after:-translate-x-[105%] after:rounded-full after:transition-[opacity,transform] after:duration-300 after:[animation-timing-function:cubic-bezier(0,0.25,0,1)] hover:after:translate-x-0 focus-visible:outline-none focus-visible:ring-2',
+        'relative z-0 inline-flex h-fit select-none items-center justify-center overflow-hidden rounded-full border text-center font-semibold leading-normal after:absolute after:inset-0 after:-z-10 after:-translate-x-[105%] after:rounded-full after:transition-[opacity,transform] after:duration-300 after:[animation-timing-function:cubic-bezier(0,0.25,0,1)] hover:after:translate-x-0 focus-visible:outline-none focus-visible:ring-2',
         {
           primary:
             'border-primary bg-primary text-foreground ring-foreground after:bg-background/40',
@@ -28,12 +28,16 @@ export function ButtonLink({
             'border-foreground bg-foreground text-background ring-primary after:bg-background',
           tertiary:
             'border-contrast-200 bg-background text-foreground ring-primary after:bg-contrast-100',
+          ghost:
+            'border-transparent bg-transparent text-foreground ring-primary after:bg-foreground/5',
         }[variant],
         {
-          icon: 'p-2.5 text-sm',
-          small: 'gap-x-2 px-4 py-2.5 text-sm',
-          medium: 'gap-x-2.5 px-5 py-3 text-base',
-          large: 'gap-x-3 px-6 py-4 text-base',
+          'icon-small': 'min-h-8 p-1.5 text-xs',
+          icon: 'min-h-10 p-2.5 text-sm',
+          'x-small': 'min-h-8 gap-x-2 px-3 py-1.5 text-xs',
+          small: 'min-h-10 gap-x-2 px-4 py-2.5 text-sm',
+          medium: 'min-h-12 gap-x-2.5 px-5 py-3 text-base',
+          large: 'min-h-14 gap-x-3 px-6 py-4 text-base',
         }[size],
         className,
       )}
