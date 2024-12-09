c2b82af6e97514e85d2355bf413bf5b469e54485
Miguel Oller <maoa22@gmail.com>
2024-12-09 00:02:07 -0500

    feat: initial set up of category pages with Soul

--- core/app/[locale]/(default)/(faceted)/category/[slug]/_components/sub-categories.tsx
+++ core/app/[locale]/(default)/(faceted)/category/[slug]/_components/sub-categories.tsx
@@ -1,7 +1,4 @@
-import { useTranslations } from 'next-intl';
-
-import { FragmentOf, graphql } from '~/client/graphql';
-import { Link } from '~/components/link';
+import { graphql } from '~/client/graphql';
 
 export const CategoryTreeFragment = graphql(`
   fragment CategoryTreeFragment on Site {
@@ -22,32 +19,3 @@ export const CategoryTreeFragment = graphql(`
     }
   }
 `);
-
-type FragmentResult = FragmentOf<typeof CategoryTreeFragment>;
-type CategoryTree = FragmentResult['categoryTree'];
-
-interface Props {
-  categoryTree: CategoryTree;
-}
-
-export function SubCategories({ categoryTree }: Props) {
-  const t = useTranslations('FacetedGroup.MobileSideNav');
-
-  if (!categoryTree[0]?.children?.length) {
-    return null;
-  }
-
-  return (
-    <div className="mb-8">
-      <h3 className="mb-3 text-2xl font-bold">{t('subCategories')}</h3>
-
-      <ul className="flex flex-col gap-4">
-        {categoryTree[0].children.map((category) => (
-          <li key={category.entityId}>
-            <Link href={category.path}>{category.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
