4d1371d79cc71145e14972352ae77c64480ec9f0
Alan Pledger <apledger3@gmail.com>
2024-12-10 17:12:27 -0500

    feat: update sign up form to soul

--- core/lib/utils.ts
+++ core/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function exists<T>(value: T | null | undefined): value is T {
+  return value != null;
+}
