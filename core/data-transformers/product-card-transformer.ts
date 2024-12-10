89282e8abbda27f6145f647d9eec0e01163016c1
Jorge Moya <jorge.moya@bigcommerce.com>
2024-12-10 11:13:39 -0600

    feat: stream in products for carousel/featured in Home (#1726)

---
+++ core/data-transformers/product-card-transformer.ts
@@ -0,0 +1,24 @@
+import { ResultOf } from 'gql.tada';
+import { getFormatter } from 'next-intl/server';
+
+import { CardProduct } from '@/vibes/soul/primitives/product-card';
+import { ExistingResultType } from '~/client/util';
+import { ProductCardFragment } from '~/components/product-card/fragment';
+
+import { pricesTransformer } from './prices-transformer';
+
+export const productCardTransformer = (
+  products: Array<ResultOf<typeof ProductCardFragment>>,
+  format: ExistingResultType<typeof getFormatter>,
+): CardProduct[] => {
+  return products.map((product) => ({
+    id: product.entityId.toString(),
+    title: product.name,
+    href: product.path,
+    image: product.defaultImage
+      ? { src: product.defaultImage.url, alt: product.defaultImage.altText }
+      : undefined,
+    price: pricesTransformer(product.prices, format),
+    subtitle: product.brand?.name ?? undefined,
+  }));
+};
