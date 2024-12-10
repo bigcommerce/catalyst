296d40d7cad2d72144816f85300b7a9ef1f84894
Jorge Moya <jorge.moya@bigcommerce.com>
2024-12-10 10:59:28 -0600

    feat(core): add subscribe form to Homepage (#1682)

---
+++ core/components/subscribe/_actions/subscribe.ts
@@ -0,0 +1,25 @@
+'use server';
+
+import { SubmissionResult } from '@conform-to/react';
+import { parseWithZod } from '@conform-to/zod';
+import { getTranslations } from 'next-intl/server';
+
+import { schema } from '@/vibes/soul/primitives/inline-email-form/schema';
+
+export const subscribe = async (
+  _lastResult: { lastResult: SubmissionResult | null },
+  formData: FormData,
+) => {
+  const t = await getTranslations('Components.Subscribe');
+
+  const submission = parseWithZod(formData, { schema });
+
+  if (submission.status !== 'success') {
+    return { lastResult: submission.reply() };
+  }
+
+  // Simulate a network request
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return { lastResult: submission.reply(), successMessage: t('success') };
+};
