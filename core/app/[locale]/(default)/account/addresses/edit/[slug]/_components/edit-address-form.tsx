71d2d3727e6260250d6605a86c5c0bfedfb55402
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-12-11 13:32:05 -0500

    feat(core): update address actions to use toasts (#1740)

--- core/app/[locale]/(default)/account/addresses/edit/[slug]/_components/edit-address-form.tsx
+++ core/app/[locale]/(default)/account/addresses/edit/[slug]/_components/edit-address-form.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { AlertCircle, Check } from 'lucide-react';
 import { useTranslations } from 'next-intl';
-import { MouseEvent, useEffect, useRef, useState } from 'react';
+import { MouseEvent, useRef, useState } from 'react';
 import { useFormStatus } from 'react-dom';
+import { toast } from 'react-hot-toast';
 
 import {
   Checkboxes,
@@ -33,15 +35,9 @@ import {
 import { Link } from '~/components/link';
 import { Button } from '~/components/ui/button';
 import { Form, FormSubmit } from '~/components/ui/form';
-import { Message } from '~/components/ui/message';
 import { useRouter } from '~/i18n/routing';
 
-import {
-  AccountState as FormStatus,
-  useAccountStatusContext,
-} from '../../../../_components/account-status-provider';
 import { Modal } from '../../../../_components/modal';
-import { SubmitMessagesList } from '../../../../_components/submit-messages-list';
 import { deleteAddress } from '../../../_actions/delete-address';
 import { updateAddress } from '../_actions/update-address';
 import { CustomerEditAddressQueryResult } from '../page';
@@ -113,15 +109,9 @@ export const EditAddressForm = ({
   isAddressRemovable,
 }: EditAddressProps) => {
   const form = useRef<HTMLFormElement>(null);
-  const [formStatus, setFormStatus] = useState<FormStatus | null>(null);
   const t = useTranslations('Account.Addresses.Edit.Form');
 
   const router = useRouter();
-  const { setAccountState } = useAccountStatusContext();
-
-  useEffect(() => {
-    setAccountState({ status: 'idle', messages: [''] });
-  }, [setAccountState]);
 
   const [textInputValid, setTextInputValid] = useState<Record<string, boolean>>({});
   const [passwordValid, setPasswordValid] = useState<Record<string, boolean>>({});
@@ -177,250 +167,241 @@ export const EditAddressForm = ({
   };
 
   const onSubmit = async (formData: FormData) => {
-    const submit = await updateAddress(formData, address.entityId);
+    const { status, message } = await updateAddress(formData, address.entityId);
 
-    if (submit.status === 'success') {
-      setAccountState({
-        status: 'success',
-        messages: submit.messages || [''],
+    if (status === 'error') {
+      toast.error(message, {
+        icon: <AlertCircle className="text-error-secondary" />,
       });
-
-      router.push('/account/addresses');
 
       return;
     }
 
-    if (submit.status === 'error') {
-      setFormStatus({ status: 'error', messages: submit.messages || [''] });
-    }
-
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
+    toast.success(message, {
+      icon: <Check className="text-success-secondary" />,
     });
+
+    router.push('/account/addresses', { scroll: true });
   };
 
   const onDeleteAddress = async () => {
-    const submit = await deleteAddress(address.entityId);
+    const { status, message } = await deleteAddress(address.entityId);
 
-    if (submit.status === 'success') {
-      setAccountState({ status: submit.status, messages: [submit.message] });
+    if (status === 'error') {
+      toast.error(message, {
+        icon: <AlertCircle className="text-error-secondary" />,
+      });
+
+      return;
     }
 
-    if (submit.status === 'error') {
-      setAccountState({ status: submit.status, messages: [submit.message] });
-    }
+    toast.success(message, {
+      icon: <Check className="text-success-secondary" />,
+    });
 
-    router.push('/account/addresses');
+    router.push('/account/addresses', { scroll: true });
   };
 
   return (
-    <>
-      {formStatus && formStatus.status !== 'idle' && (
-        <Message className="mx-auto mb-8 w-full" variant={formStatus.status}>
-          <SubmitMessagesList messages={formStatus.messages} />
-        </Message>
-      )}
-      <Form action={onSubmit} onClick={preSubmitFieldsValidation} ref={form}>
-        <div className="grid grid-cols-1 gap-y-3 lg:grid-cols-2 lg:gap-x-6">
-          {addressFields.map((field) => {
-            const fieldId = field.entityId;
-            const fieldName = createFieldName(field, 'address');
-            const key = FieldNameToFieldId[fieldId];
-            const defaultCustomField = address.formFields.find(
-              ({ entityId }) => entityId === fieldId,
-            );
-            const defaultValue = (isExistedField(key) && address[key]) || undefined;
+    <Form action={onSubmit} onClick={preSubmitFieldsValidation} ref={form}>
+      <div className="grid grid-cols-1 gap-y-3 lg:grid-cols-2 lg:gap-x-6">
+        {addressFields.map((field) => {
+          const fieldId = field.entityId;
+          const fieldName = createFieldName(field, 'address');
+          const key = FieldNameToFieldId[fieldId];
+          const defaultCustomField = address.formFields.find(
+            ({ entityId }) => entityId === fieldId,
+          );
+          const defaultValue = (isExistedField(key) && address[key]) || undefined;
 
-            switch (field.__typename) {
-              case 'TextFormField': {
-                const previousTextValue =
-                  getPreviouslySubmittedValue(defaultCustomField).TextFormField;
+          switch (field.__typename) {
+            case 'TextFormField': {
+              const previousTextValue =
+                getPreviouslySubmittedValue(defaultCustomField).TextFormField;
 
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <Text
-                      defaultValue={defaultValue ?? previousTextValue}
-                      field={field}
-                      isValid={textInputValid[fieldId]}
-                      name={fieldName}
-                      onChange={handleTextInputValidation}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              case 'MultilineTextFormField': {
-                const previousMultiTextValue =
-                  getPreviouslySubmittedValue(defaultCustomField).MultilineTextFormField;
-
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <MultilineText
-                      defaultValue={previousMultiTextValue}
-                      field={field}
-                      isValid={multiTextValid[fieldId]}
-                      name={fieldName}
-                      onChange={handleMultiTextValidation}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              case 'NumberFormField': {
-                const previousNumberValue =
-                  getPreviouslySubmittedValue(defaultCustomField).NumberFormField;
-
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <NumbersOnly
-                      defaultValue={previousNumberValue}
-                      field={field}
-                      isValid={numbersInputValid[fieldId]}
-                      name={fieldName}
-                      onChange={handleNumbersInputValidation}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              case 'CheckboxesFormField': {
-                const previousCheckboxesValue =
-                  getPreviouslySubmittedValue(defaultCustomField).CheckboxesFormField;
-
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <Checkboxes
-                      defaultValue={previousCheckboxesValue}
-                      field={field}
-                      isValid={checkboxesValid[fieldId]}
-                      name={fieldName}
-                      onValidate={setCheckboxesValid}
-                      options={field.options}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              case 'DateFormField': {
-                const previousDateValue =
-                  getPreviouslySubmittedValue(defaultCustomField).DateFormField;
-
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <DateField
-                      defaultValue={previousDateValue}
-                      field={field}
-                      isValid={datesValid[fieldId]}
-                      name={fieldName}
-                      onChange={handleDatesValidation}
-                      onValidate={setDatesValid}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              case 'RadioButtonsFormField': {
-                const previousMultipleChoiceValue =
-                  getPreviouslySubmittedValue(defaultCustomField).MultipleChoiceFormField;
-
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <RadioButtons
-                      defaultValue={previousMultipleChoiceValue}
-                      field={field}
-                      isValid={radioButtonsValid[fieldId]}
-                      name={fieldName}
-                      onChange={handleRadioButtonsChange}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              case 'PicklistFormField': {
-                const isCountrySelector = fieldId === FieldNameToFieldId.countryCode;
-                const previousMultipleChoiceValue =
-                  getPreviouslySubmittedValue(defaultCustomField).MultipleChoiceFormField;
-                const picklistOptions = isCountrySelector
-                  ? countries.map(({ name, code }) => ({ label: name, entityId: code }))
-                  : field.options;
-
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <Picklist
-                      defaultValue={isCountrySelector ? defaultValue : previousMultipleChoiceValue}
-                      field={field}
-                      isValid={picklistValid[fieldId]}
-                      name={fieldName}
-                      onChange={isCountrySelector ? handleCountryChange : undefined}
-                      onValidate={setPicklistValid}
-                      options={picklistOptions}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              case 'PicklistOrTextFormField': {
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <PicklistOrText
-                      defaultValue={
-                        fieldId === FieldNameToFieldId.stateOrProvince ? defaultValue : undefined
-                      }
-                      field={field}
-                      key={countryStates.length}
-                      name={fieldName}
-                      options={countryStates.map(({ name }) => ({ entityId: name, label: name }))}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              case 'PasswordFormField': {
-                const previousPasswordValue =
-                  getPreviouslySubmittedValue(defaultCustomField).PasswordFormField;
-
-                return (
-                  <FieldWrapper fieldId={fieldId} key={fieldId}>
-                    <Password
-                      defaultValue={previousPasswordValue}
-                      field={field}
-                      isValid={passwordValid[fieldId]}
-                      name={fieldName}
-                      onChange={handlePasswordValidation}
-                    />
-                  </FieldWrapper>
-                );
-              }
-
-              default:
-                return null;
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <Text
+                    defaultValue={defaultValue ?? previousTextValue}
+                    field={field}
+                    isValid={textInputValid[fieldId]}
+                    name={fieldName}
+                    onChange={handleTextInputValidation}
+                  />
+                </FieldWrapper>
+              );
             }
-          })}
-        </div>
 
-        <div className="mt-8 flex flex-col justify-stretch gap-2 md:flex-row md:justify-between md:gap-0">
-          <FormSubmit asChild>
-            <SubmitButton messages={{ submit: t('submit'), submitting: t('submitting') }} />
-          </FormSubmit>
-          <Button asChild className="items-center px-8 md:ms-6 md:w-fit" variant="secondary">
-            <Link href="/account/addresses">{t('cancel')}</Link>
-          </Button>
-          <Modal
-            actionHandler={onDeleteAddress}
-            confirmationText={t('confirmDeleteAddress')}
-            title={t('deleteModalTitle')}
+            case 'MultilineTextFormField': {
+              const previousMultiTextValue =
+                getPreviouslySubmittedValue(defaultCustomField).MultilineTextFormField;
+
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <MultilineText
+                    defaultValue={previousMultiTextValue}
+                    field={field}
+                    isValid={multiTextValid[fieldId]}
+                    name={fieldName}
+                    onChange={handleMultiTextValidation}
+                  />
+                </FieldWrapper>
+              );
+            }
+
+            case 'NumberFormField': {
+              const previousNumberValue =
+                getPreviouslySubmittedValue(defaultCustomField).NumberFormField;
+
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <NumbersOnly
+                    defaultValue={previousNumberValue}
+                    field={field}
+                    isValid={numbersInputValid[fieldId]}
+                    name={fieldName}
+                    onChange={handleNumbersInputValidation}
+                  />
+                </FieldWrapper>
+              );
+            }
+
+            case 'CheckboxesFormField': {
+              const previousCheckboxesValue =
+                getPreviouslySubmittedValue(defaultCustomField).CheckboxesFormField;
+
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <Checkboxes
+                    defaultValue={previousCheckboxesValue}
+                    field={field}
+                    isValid={checkboxesValid[fieldId]}
+                    name={fieldName}
+                    onValidate={setCheckboxesValid}
+                    options={field.options}
+                  />
+                </FieldWrapper>
+              );
+            }
+
+            case 'DateFormField': {
+              const previousDateValue =
+                getPreviouslySubmittedValue(defaultCustomField).DateFormField;
+
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <DateField
+                    defaultValue={previousDateValue}
+                    field={field}
+                    isValid={datesValid[fieldId]}
+                    name={fieldName}
+                    onChange={handleDatesValidation}
+                    onValidate={setDatesValid}
+                  />
+                </FieldWrapper>
+              );
+            }
+
+            case 'RadioButtonsFormField': {
+              const previousMultipleChoiceValue =
+                getPreviouslySubmittedValue(defaultCustomField).MultipleChoiceFormField;
+
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <RadioButtons
+                    defaultValue={previousMultipleChoiceValue}
+                    field={field}
+                    isValid={radioButtonsValid[fieldId]}
+                    name={fieldName}
+                    onChange={handleRadioButtonsChange}
+                  />
+                </FieldWrapper>
+              );
+            }
+
+            case 'PicklistFormField': {
+              const isCountrySelector = fieldId === FieldNameToFieldId.countryCode;
+              const previousMultipleChoiceValue =
+                getPreviouslySubmittedValue(defaultCustomField).MultipleChoiceFormField;
+              const picklistOptions = isCountrySelector
+                ? countries.map(({ name, code }) => ({ label: name, entityId: code }))
+                : field.options;
+
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <Picklist
+                    defaultValue={isCountrySelector ? defaultValue : previousMultipleChoiceValue}
+                    field={field}
+                    isValid={picklistValid[fieldId]}
+                    name={fieldName}
+                    onChange={isCountrySelector ? handleCountryChange : undefined}
+                    onValidate={setPicklistValid}
+                    options={picklistOptions}
+                  />
+                </FieldWrapper>
+              );
+            }
+
+            case 'PicklistOrTextFormField': {
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <PicklistOrText
+                    defaultValue={
+                      fieldId === FieldNameToFieldId.stateOrProvince ? defaultValue : undefined
+                    }
+                    field={field}
+                    key={countryStates.length}
+                    name={fieldName}
+                    options={countryStates.map(({ name }) => ({ entityId: name, label: name }))}
+                  />
+                </FieldWrapper>
+              );
+            }
+
+            case 'PasswordFormField': {
+              const previousPasswordValue =
+                getPreviouslySubmittedValue(defaultCustomField).PasswordFormField;
+
+              return (
+                <FieldWrapper fieldId={fieldId} key={fieldId}>
+                  <Password
+                    defaultValue={previousPasswordValue}
+                    field={field}
+                    isValid={passwordValid[fieldId]}
+                    name={fieldName}
+                    onChange={handlePasswordValidation}
+                  />
+                </FieldWrapper>
+              );
+            }
+
+            default:
+              return null;
+          }
+        })}
+      </div>
+
+      <div className="mt-8 flex flex-col justify-stretch gap-2 md:flex-row md:justify-between md:gap-0">
+        <FormSubmit asChild>
+          <SubmitButton messages={{ submit: t('submit'), submitting: t('submitting') }} />
+        </FormSubmit>
+        <Button asChild className="items-center px-8 md:ms-6 md:w-fit" variant="secondary">
+          <Link href="/account/addresses">{t('cancel')}</Link>
+        </Button>
+        <Modal
+          actionHandler={onDeleteAddress}
+          confirmationText={t('confirmDeleteAddress')}
+          title={t('deleteModalTitle')}
+        >
+          <Button
+            className="ms-auto items-center px-8 md:w-fit"
+            disabled={!isAddressRemovable}
+            variant="subtle"
           >
-            <Button
-              className="ms-auto items-center px-8 md:w-fit"
-              disabled={!isAddressRemovable}
-              variant="subtle"
-            >
-              {t('deleteButton')}
-            </Button>
-          </Modal>
-        </div>
-      </Form>
-    </>
+            {t('deleteButton')}
+          </Button>
+        </Modal>
+      </div>
+    </Form>
   );
 };
