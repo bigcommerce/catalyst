86736af3932059c9cd5a9558736c3937afecfdf6
Hunter Garrett <hunter@hunterbecton.com>
2024-12-13 15:37:13 -0500

    feat: update Breadcrumbs to use AnimatedLink and new skeleton design (#1754)

---
+++ core/vibes/soul/primitives/breadcrumbs/index.tsx
@@ -0,0 +1,66 @@
+import { clsx } from 'clsx';
+import { ChevronRight } from 'lucide-react';
+
+import { AnimatedLink } from '@/vibes/soul/primitives/animated-link';
+
+export interface Breadcrumb {
+  label: string;
+  href: string;
+}
+
+export interface BreadcrumbsProps {
+  breadcrumbs: Breadcrumb[];
+  className?: string;
+}
+
+export function Breadcrumbs({ breadcrumbs, className }: BreadcrumbsProps) {
+  return (
+    <nav aria-label="breadcrumb" className={clsx(className)}>
+      <ol className="flex flex-wrap items-center gap-x-1.5 text-sm @xl:text-base">
+        {breadcrumbs.map(({ label, href }, idx) => {
+          if (idx < breadcrumbs.length - 1) {
+            return (
+              <li className="inline-flex items-center gap-x-1.5" key={idx}>
+                <AnimatedLink label={label} link={{ href }} />
+                <ChevronRight
+                  aria-hidden="true"
+                  className="text-contrast-500"
+                  size={20}
+                  strokeWidth={1}
+                />
+              </li>
+            );
+          }
+
+          return (
+            <li className="inline-flex items-center text-contrast-500" key={idx}>
+              <span aria-current="page" aria-disabled="true" role="link">
+                {label}
+              </span>
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+}
+
+export function BreadcrumbsSkeleton({ className }: { className?: string }) {
+  return (
+    <div
+      className={clsx('flex animate-pulse flex-wrap items-center gap-x-1.5 text-base', className)}
+    >
+      <div className="flex h-[1lh] items-center">
+        <span className="block h-[1.25ex] w-[4ch] rounded bg-contrast-100" />
+      </div>
+      <ChevronRight aria-hidden="true" className="text-contrast-200" size={20} strokeWidth={1} />
+      <div className="flex h-[1lh] items-center">
+        <span className="block h-[1.25ex] w-[6ch] rounded bg-contrast-100" />
+      </div>
+      <ChevronRight aria-hidden="true" className="text-contrast-200" size={20} strokeWidth={1} />
+      <div className="flex h-[1lh] items-center">
+        <span className="block h-[1.25ex] w-[6ch] rounded bg-contrast-100" />
+      </div>
+    </div>
+  );
+}
