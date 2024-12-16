b11d31a8469e5cb3aced26f87c74873af026d511
Chancellor Clark <chancellor.clark@bigcommerce.com>
2024-12-16 15:09:08 -0500

    feat(core): add account settings soul components

--- core/app/[locale]/(default)/account/settings/_actions/update-customer.ts
+++ core/app/[locale]/(default)/account/settings/_actions/update-customer.ts
@@ -1,13 +1,17 @@
 'use server';
 
+import { BigCommerceAPIError } from '@bigcommerce/catalyst-client';
+import { parseWithZod } from '@conform-to/zod';
 import { expireTag } from 'next/cache';
 import { getTranslations } from 'next-intl/server';
 
+import { updateAccountSchema } from '@/vibes/soul/sections/account-settings-section/schema';
 import { getSessionCustomerAccessToken } from '~/auth';
 import { client } from '~/client';
-import { graphql, VariablesOf } from '~/client/graphql';
+import { graphql } from '~/client/graphql';
 import { TAGS } from '~/client/tags';
-import { parseAccountFormData } from '~/components/form-fields/shared/parse-fields';
+
+import { UpdateAccountAction } from '../../../../../../vibes/soul/sections/account-settings-section/update-account-form';
 
 const UpdateCustomerMutation = graphql(`
   mutation UpdateCustomerMutation($input: UpdateCustomerInput!) {
@@ -40,68 +44,59 @@ const UpdateCustomerMutation = graphql(`
   }
 `);
 
-type AddCustomerAddressInput = VariablesOf<typeof UpdateCustomerMutation>['input'];
-
-const isUpdateCustomerInput = (data: unknown): data is AddCustomerAddressInput => {
-  if (
-    typeof data === 'object' &&
-    data !== null &&
-    ('firstName' in data ||
-      'lastName' in data ||
-      'email' in data ||
-      'phone' in data ||
-      'company' in data ||
-      'formFields' in data)
-  ) {
-    return true;
-  }
-
-  return false;
-};
-
-interface UpdateCustomerResponse {
-  status: 'success' | 'error';
-  message: string;
-}
-
-export const updateCustomer = async (formData: FormData): Promise<UpdateCustomerResponse> => {
-  const t = await getTranslations('Account.Settings.UpdateCustomer');
+export const updateCustomer: UpdateAccountAction = async (prevState, formData) => {
+  const t = await getTranslations('Register');
   const customerAccessToken = await getSessionCustomerAccessToken();
 
-  const parsed = parseAccountFormData(formData);
+  const submission = parseWithZod(formData, { schema: updateAccountSchema });
 
-  if (!isUpdateCustomerInput(parsed)) {
+  if (submission.status !== 'success') {
     return {
-      status: 'error',
-      message: t('Errors.inputError'),
+      account: prevState.account,
+      lastResult: submission.reply(),
     };
   }
 
-  const response = await client.fetch({
-    document: UpdateCustomerMutation,
-    customerAccessToken,
-    fetchOptions: { cache: 'no-store' },
-    variables: {
-      input: parsed,
-    },
-  });
-
-  const result = response.data.customer.updateCustomer;
-
-  if (result.errors.length > 0) {
-    result.errors.forEach((error) => {
-      throw new Error(error.message);
+  try {
+    const response = await client.fetch({
+      document: UpdateCustomerMutation,
+      customerAccessToken,
+      variables: {
+        input: submission.value,
+      },
+      fetchOptions: { cache: 'no-store' },
     });
-  }
 
-  if (!result.customer) {
+    const result = response.data.customer.updateCustomer;
+
+    if (result.errors.length > 0) {
+      return {
+        account: prevState.account,
+        lastResult: submission.reply({ formErrors: result.errors.map((error) => error.message) }),
+      };
+    }
+
+    expireTag(TAGS.customer);
+
     return {
-      status: 'error',
-      message: t('Errors.notFound'),
+      account: submission.value,
+      successMessage: t('successfulUpdate'),
+      lastResult: submission.reply(),
+    };
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+
+    if (error instanceof BigCommerceAPIError) {
+      return {
+        account: prevState.account,
+        lastResult: submission.reply({ formErrors: [t('Errors.apiError')] }),
+      };
+    }
+
+    return {
+      account: prevState.account,
+      lastResult: submission.reply({ formErrors: [t('Errors.error')] }),
     };
   }
-
-  expireTag(TAGS.customer);
-
-  return { status: 'success', message: t('successMessage') };
 };
---
+++ core/vibes/soul/sections/account-settings-section/update-account-form.tsx
@@ -0,0 +1,136 @@
+'use client';
+
+import { getFormProps, getInputProps, SubmissionResult, useForm } from '@conform-to/react';
+import { getZodConstraint, parseWithZod } from '@conform-to/zod';
+import { useActionState, useEffect, useOptimistic, useTransition } from 'react';
+import { z } from 'zod';
+
+import { Input } from '@/vibes/soul/form/input';
+import { Button } from '@/vibes/soul/primitives/button';
+import { toast } from '@/vibes/soul/primitives/toaster';
+
+import { updateAccountSchema } from './schema';
+
+type Action<S, P> = (state: Awaited<S>, payload: P) => S | Promise<S>;
+
+export type UpdateAccountAction = Action<State, FormData>;
+
+export type Account = z.infer<typeof updateAccountSchema>;
+
+interface State {
+  account: Account;
+  successMessage?: string;
+  lastResult: SubmissionResult | null;
+}
+
+interface Props {
+  action: UpdateAccountAction;
+  account: Account;
+  firstNameLabel?: string;
+  lastNameLabel?: string;
+  emailLabel?: string;
+  companyLabel?: string;
+  submitLabel?: string;
+}
+
+export function UpdateAccountForm({
+  action,
+  account,
+  firstNameLabel = 'First name',
+  lastNameLabel = 'Last name',
+  emailLabel = 'Email',
+  companyLabel = 'Company',
+  submitLabel = 'Update',
+}: Props) {
+  const [state, formAction] = useActionState(action, { account, lastResult: null });
+  const [pending, startTransition] = useTransition();
+
+  const [optimisticState, setOptimisticState] = useOptimistic<State, FormData>(
+    state,
+    (prevState, formData) => {
+      const intent = formData.get('intent');
+      const submission = parseWithZod(formData, { schema: updateAccountSchema });
+
+      if (submission.status !== 'success') return prevState;
+
+      switch (intent) {
+        case 'update': {
+          return {
+            ...prevState,
+            account: submission.value,
+          };
+        }
+
+        default:
+          return prevState;
+      }
+    },
+  );
+
+  const [form, fields] = useForm({
+    lastResult: state.lastResult,
+    defaultValue: optimisticState.account,
+    constraint: getZodConstraint(updateAccountSchema),
+    shouldValidate: 'onBlur',
+    shouldRevalidate: 'onInput',
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: updateAccountSchema });
+    },
+  });
+
+  useEffect(() => {
+    if (state.lastResult?.status === 'success' && typeof state.successMessage === 'string') {
+      toast.success(state.successMessage);
+    }
+  }, [state]);
+
+  return (
+    <form
+      {...getFormProps(form)}
+      action={(formData) => {
+        startTransition(() => {
+          formAction(formData);
+          setOptimisticState(formData);
+        });
+      }}
+      className="space-y-5"
+    >
+      <div className="flex gap-5">
+        <Input
+          {...getInputProps(fields.firstName, { type: 'text' })}
+          errors={fields.firstName.errors}
+          key={fields.firstName.id}
+          label={firstNameLabel}
+        />
+        <Input
+          {...getInputProps(fields.lastName, { type: 'text' })}
+          errors={fields.lastName.errors}
+          key={fields.lastName.id}
+          label={lastNameLabel}
+        />
+      </div>
+      <Input
+        {...getInputProps(fields.email, { type: 'text' })}
+        errors={fields.email.errors}
+        key={fields.email.id}
+        label={emailLabel}
+      />
+      <Input
+        {...getInputProps(fields.company, { type: 'text' })}
+        errors={fields.company.errors}
+        key={fields.company.id}
+        label={companyLabel}
+      />
+      <Button
+        loading={pending}
+        name="intent"
+        size="small"
+        type="submit"
+        value="update"
+        variant="secondary"
+      >
+        {submitLabel}
+      </Button>
+    </form>
+  );
+}
