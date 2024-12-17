154c74e896f1742fff2fada77f70e9bb3b01c09e
Alan Pledger <apledger3@gmail.com>
2024-12-17 16:10:16 -0500

    feat: add CSS variables to ProductCard

--- core/vibes/soul/form/checkbox/index.tsx
+++ core/vibes/soul/form/checkbox/index.tsx
@@ -11,9 +11,41 @@ import { FieldError } from '@/vibes/soul/form/field-error';
 type Props = Omit<React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>, 'id'> & {
   label?: React.ReactNode;
   errors?: string[];
+  colorScheme?: 'light' | 'dark';
 };
 
-export function Checkbox({ label, errors, className, ...rest }: Props) {
+// eslint-disable-next-line valid-jsdoc
+/**
+ * This component supports various CSS variables for theming. Here's a comprehensive list, along
+ * with their default values:
+ *
+ * ```css
+ *  :root {
+ *    --checkbox-focus: hsl(var(--primary));
+ *    --checkbox-light-label: hsl(var(--foreground));
+ *    --checkbox-light-error: hsl(var(--error));
+ *    --checkbox-light-unchecked-border: hsl(var(--contrast-200));
+ *    --checkbox-light-unchecked-border-hover: hsl(var(--contrast-300));
+ *    --checkbox-light-unchecked-background: hsl(var(--background));
+ *    --checkbox-light-unchecked-icon: hsl(var(--foreground));
+ *    --checkbox-light-checked-border: hsl(var(--foreground));
+ *    --checkbox-light-checked-border-hover: hsl(var(--foreground));
+ *    --checkbox-light-checked-background: hsl(var(--foreground));
+ *    --checkbox-light-checked-icon: hsl(var(--background));
+ *    --checkbox-dark-label: hsl(var(--background));
+ *    --checkbox-dark-error: hsl(var(--error));
+ *    --checkbox-dark-unchecked-border: hsl(var(--contrast-400));
+ *    --checkbox-dark-unchecked-border-hover: hsl(var(--contrast-300));
+ *    --checkbox-dark-unchecked-background: hsl(var(--foreground));
+ *    --checkbox-dark-unchecked-icon: hsl(var(--background));
+ *    --checkbox-dark-checked-border: hsl(var(--background));
+ *    --checkbox-dark-checked-border-hover: hsl(var(--background));
+ *    --checkbox-dark-checked-background: hsl(var(--foreground));
+ *    --checkbox-dark-checked-icon: hsl(var(--foreground));
+ *  }
+ * ```
+ */
+export function Checkbox({ label, errors, className, colorScheme = 'light', ...rest }: Props) {
   const id = React.useId();
 
   return (
@@ -22,20 +54,36 @@ export function Checkbox({ label, errors, className, ...rest }: Props) {
         <CheckboxPrimitive.Root
           {...rest}
           className={clsx(
-            'flex h-5 w-5 items-center justify-center rounded-md border transition-colors duration-150 focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-primary data-[state=checked]:border-foreground data-[state=unchecked]:border-contrast-200 data-[state=checked]:bg-foreground data-[state=unchecked]:bg-background',
-            errors && errors.length > 0
-              ? 'border-error'
-              : 'border-contrast-200 hover:border-contrast-300 focus:border-contrast-300',
+            'flex h-5 w-5 items-center justify-center rounded-md border transition-colors duration-150 focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-[var(--checkbox-focus,hsl(var(--primary)))]',
+            {
+              light:
+                errors && errors.length > 0
+                  ? 'border-[var(--checkbox-light-error,hsl(var(--error)))]'
+                  : 'data-[state=checked]:border-[var(--checkbox-light-checked-border,hsl(var(--foreground)))] data-[state=unchecked]:border-[var(--checkbox-light-unchecked-border,hsl(var(--contrast-200)))] data-[state=checked]:bg-[var(--checkbox-light-checked-background,hsl(var(--foreground)))] data-[state=unchecked]:bg-[var(--checkbox-light-unchecked-background,hsl(var(--background)))] data-[state=checked]:text-[var(--checkbox-light-checked-text,hsl(var(--background)))] data-[state=unchecked]:text-[var(--checkbox-light-unchecked-text,hsl(var(--foreground)))] data-[state=checked]:hover:border-[var(--checkbox-light-checked-border-hover,hsl(var(--foreground)))] data-[state=unchecked]:hover:border-[var(--checkbox-light-unchecked-border-hover,hsl(var(--contrast-300)))]',
+              dark:
+                errors && errors.length > 0
+                  ? 'border-[var(--checkbox-dark-error,hsl(var(--error)))]'
+                  : 'data-[state=checked]:border-[var(--checkbox-dark-checked-border,hsl(var(--background)))] data-[state=unchecked]:border-[var(--checkbox-dark-unchecked-border,hsl(var(--contrast-400)))] data-[state=checked]:bg-[var(--checkbox-dark-checked-background,hsl(var(--foreground)))] data-[state=unchecked]:bg-[var(--checkbox-dark-unchecked-background,hsl(var(--foreground)))] data-[state=checked]:text-[var(--checkbox-dark-checked-text,hsl(var(--background)))] data-[state=unchecked]:text-[var(--checkbox-dark-unchecked-text,hsl(var(--background)))] data-[state=checked]:hover:border-[var(--checkbox-dark-checked-border-hover,hsl(var(--background)))] data-[state=unchecked]:hover:border-[var(--checkbox-dark-unchecked-border-hover,hsl(var(--contrast-300)))]',
+            }[colorScheme],
           )}
           id={id}
         >
           <CheckboxPrimitive.Indicator>
-            <Check className="h-4 w-4" color="white" />
+            <Check className="h-4 w-4" color="currentColor" />
           </CheckboxPrimitive.Indicator>
         </CheckboxPrimitive.Root>
 
         {label != null && label !== '' && (
-          <LabelPrimitive.Root className="cursor-pointer font-body text-sm" htmlFor={id}>
+          <LabelPrimitive.Root
+            className={clsx(
+              'cursor-pointer font-body text-sm',
+              {
+                light: 'text-[var(--checkbox-light-label,hsl(var(--foreground)))]',
+                dark: 'text-[var(--checkbox-dark-label,hsl(var(--background)))]',
+              }[colorScheme],
+            )}
+            htmlFor={id}
+          >
             {label}
           </LabelPrimitive.Root>
         )}
--- core/vibes/soul/primitives/price-label/index.tsx
+++ core/vibes/soul/primitives/price-label/index.tsx
@@ -16,27 +16,85 @@ export type Price = string | PriceRange | PriceSale;
 
 interface Props {
   className?: string;
+  colorScheme?: 'light' | 'dark';
   price: Price;
 }
 
-export function PriceLabel({ className, price }: Props) {
+// eslint-disable-next-line valid-jsdoc
+/**
+ * This component supports various CSS variables for theming. Here's a comprehensive list, along
+ * with their default values:
+ *
+ * ```css
+ * :root {
+ *   --price-light-text: hsl(var(--foreground));
+ *   --price-light-sale-text: hsl(var(--foreground));
+ *   --price-dark-text: hsl(var(--background));
+ *   --price-dark-sale-text: hsl(var(--background));
+ * }
+ * ```
+ */
+export function PriceLabel({ className, colorScheme = 'light', price }: Props) {
   if (typeof price === 'string') {
-    return <span className={clsx('block font-semibold', className)}>{price}</span>;
+    return (
+      <span
+        className={clsx(
+          'block font-semibold',
+          {
+            light: 'text-[var(--price-light-text,hsl(var(--foreground)))]',
+            dark: 'text-[var(--price-dark-text,hsl(var(--background)))]',
+          }[colorScheme],
+          className,
+        )}
+      >
+        {price}
+      </span>
+    );
   }
 
   switch (price.type) {
     case 'range':
       return (
-        <span className={clsx('block font-semibold', className)}>
-          {price.minValue}–{price.maxValue}
+        <span
+          className={clsx(
+            'block font-semibold',
+            {
+              light: 'text-[var(--price-light-text,hsl(var(--foreground)))]',
+              dark: 'text-[var(--price-dark-text,hsl(var(--background)))]',
+            }[colorScheme],
+            className,
+          )}
+        >
+          {price.minValue}
+          &nbsp;&ndash;&nbsp;
+          {price.maxValue}
         </span>
       );
 
     case 'sale':
       return (
         <span className={clsx('block font-semibold', className)}>
-          <span className="font-normal text-contrast-400 line-through">{price.previousValue}</span>{' '}
-          <span className="text-error">{price.currentValue}</span>
+          <span
+            className={clsx(
+              'font-normal line-through opacity-50',
+              {
+                light: 'text-[var(--price-light-text,hsl(var(--foreground)))]',
+                dark: 'text-[var(--price-dark-text,hsl(var(--background)))]',
+              }[colorScheme],
+            )}
+          >
+            {price.previousValue}
+          </span>{' '}
+          <span
+            className={clsx(
+              {
+                light: 'text-[var(--price-light-sale-text,hsl(var(--foreground)))]',
+                dark: 'text-[var(--price-dark-sale-text,hsl(var(--background)))]',
+              }[colorScheme],
+            )}
+          >
+            {price.currentValue}
+          </span>
         </span>
       );
 
