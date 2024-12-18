f76e7587cf868874b0beed8f0a58da4eb4d3ce8d
Alan Pledger <apledger3@gmail.com>
2024-12-18 14:50:52 -0500

    feat: add variables to StickySidebarLayout

--- core/vibes/soul/sections/sticky-sidebar-layout/index.tsx
+++ core/vibes/soul/sections/sticky-sidebar-layout/index.tsx
@@ -1,19 +1,32 @@
 import { clsx } from 'clsx';
 
+/**
+ * This component supports various CSS variables for theming. Here's a comprehensive list, along
+ * with their default values:
+ *
+ * ```css
+ * :root {
+ *   --section-max-width-medium: 768px;
+ *   --section-max-width-lg: 1024px;
+ *   --section-max-width-xl: 1280px;
+ *   --section-max-width-2xl: 1536px;
+ * }
+ * ```
+ */
 export function StickySidebarLayout({
   className,
   sidebar,
   children,
   sidebarSize = '1/3',
-  sidebarPosition = 'left',
+  sidebarPosition = 'before',
   containerSize = '2xl',
 }: {
   className?: string;
   sidebar: React.ReactNode;
   children: React.ReactNode;
-  containerSize?: 'lg' | 'xl' | '2xl';
+  containerSize?: 'md' | 'lg' | 'xl' | '2xl';
   sidebarSize?: '1/4' | '1/3' | '1/2' | 'small' | 'medium' | 'large';
-  sidebarPosition?: 'left' | 'right';
+  sidebarPosition?: 'before' | 'after';
 }) {
   return (
     <section className={clsx('group/pending @container', className)}>
@@ -21,16 +34,17 @@ export function StickySidebarLayout({
         className={clsx(
           'mx-auto flex flex-col items-stretch gap-x-16 gap-y-10 px-4 py-10 @xl:px-6 @xl:py-14 @4xl:flex-row @4xl:px-8 @4xl:py-20',
           {
-            lg: 'max-w-screen-lg',
-            xl: 'max-w-screen-xl',
-            '2xl': 'max-w-screen-2xl',
+            md: 'max-w-screen-[var(--section-max-width-md,768px)]',
+            lg: 'max-w-screen-[var(--section-max-width-lg,1024px)]',
+            xl: 'max-w-screen-[var(--section-max-width-xl,1280px)]',
+            '2xl': 'max-w-screen-[var(--section-max-width-2xl,1536px)]',
           }[containerSize],
         )}
       >
         <div
           className={clsx(
             'shrink-0',
-            sidebarPosition === 'right' ? 'order-2' : 'order-1',
+            sidebarPosition === 'after' ? 'order-2' : 'order-1',
             {
               '1/3': '@4xl:w-1/3',
               '1/2': '@4xl:w-1/2',
@@ -45,7 +59,7 @@ export function StickySidebarLayout({
         </div>
         <div
           className={clsx(
-            sidebarPosition === 'right' ? 'order-1' : 'order-2',
+            sidebarPosition === 'after' ? 'order-1' : 'order-2',
             {
               '1/3': '@4xl:w-2/3',
               '1/2': '@4xl:w-1/2',
