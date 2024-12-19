e462065b2eb6a75c080797f6a640e708261813b5
jordanarldt <jordan.arldt@bigcommerce.com>
2024-12-19 16:47:46 -0600

    feat: update pageInfoTransformer

--- core/app/[locale]/(default)/blog/page.tsx
+++ core/app/[locale]/(default)/blog/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
 import { FeaturedBlogPostList } from '@/vibes/soul/sections/featured-blog-post-list';
-import { pageInfoTransformer } from '~/data-transformers/page-info-transformer';
+import { defaultPageInfo, pageInfoTransformer } from '~/data-transformers/page-info-transformer';
 
 import { getBlog, getBlogMetaData, getBlogPosts } from './page-data';
 
@@ -29,7 +29,7 @@ async function getPaginationInfo(searchParamsPromise: Promise<SearchParams>) {
   const searchParams = await searchParamsPromise;
   const blogPosts = await getBlogPosts(searchParams);
 
-  return pageInfoTransformer(blogPosts?.pageInfo);
+  return pageInfoTransformer(blogPosts?.pageInfo ?? defaultPageInfo);
 }
 
 export default async function Blog(props: Props) {
