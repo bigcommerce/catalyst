9aaa32e31c49f002cf01d0c21cfe67022b5e0b3d
Aleksey Gurtovoy <agurtovoy@acm.org>
2024-12-20 01:32:41 -0600

    feat: component theming

---
+++ core/lib/makeswift/controls/font-tokens.ts
@@ -0,0 +1,47 @@
+import { Font, Select } from '@makeswift/runtime/controls';
+
+export const fontFamilyTokens = {
+  heading: Font({
+    label: 'Heading',
+    variant: false,
+    defaultValue: { fontFamily: 'var(--font-family-dm-serif-text)' },
+  }),
+  body: Font({
+    label: 'Body',
+    variant: false,
+    defaultValue: { fontFamily: 'var(--font-family-inter)' },
+  }),
+  accent: Font({
+    label: 'Accent',
+    variant: false,
+    defaultValue: { fontFamily: 'var(--font-family-roboto-mono)' },
+  }),
+};
+
+type FontFamilyToken = keyof typeof fontFamilyTokens;
+export type FontFamilyCssVar = `var(--font-family-${FontFamilyToken})`;
+
+const fontFamilyCssVar = (token: FontFamilyToken): FontFamilyCssVar =>
+  `var(--font-family-${token})`;
+
+const fontFamilyOption = (token: FontFamilyToken): { label: string; value: FontFamilyCssVar } => ({
+  label: `${fontFamilyTokens[token].config.label}`,
+  value: fontFamilyCssVar(token),
+});
+
+export const FontFamily = ({
+  label,
+  defaultValue,
+}: {
+  label: string;
+  defaultValue: FontFamilyCssVar;
+}) =>
+  Select({
+    label,
+    options: [fontFamilyOption('heading'), fontFamilyOption('body'), fontFamilyOption('accent')],
+    defaultValue,
+  });
+
+FontFamily.Heading = fontFamilyCssVar('heading');
+FontFamily.Body = fontFamilyCssVar('body');
+FontFamily.Accent = fontFamilyCssVar('accent');
