20be7c9cf8d5c9ab037455a7a5077d34f07f0331
Aleksey Gurtovoy <agurtovoy@acm.org>
2024-12-20 01:32:41 -0600

    feat: component theming

---
+++ core/lib/makeswift/components/site-theme/components/index.ts
@@ -0,0 +1,25 @@
+import { accordion } from './accordion';
+import { button } from './button';
+import { card } from './card';
+import { carousel } from './carousel';
+import { footer } from './footer';
+import { header } from './header';
+import { logo } from './logo';
+import { priceLabel } from './price-label';
+import { productCard } from './product-card';
+import { section } from './section';
+import { slideshow } from './slide-show';
+
+export default {
+  accordion,
+  button,
+  card,
+  carousel,
+  footer,
+  header,
+  logo,
+  priceLabel,
+  productCard,
+  section,
+  slideshow,
+};
