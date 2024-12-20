d1288f0eb50c094ad74a35cb69902afa5a5ba590
Fikri Karim <fkfikrikarim@gmail.com>
2024-12-21 03:04:42 +0700

    feat: add Makeswift site header

--- core/components/header/index.tsx
+++ core/components/header/index.tsx
@@ -2,7 +2,6 @@ import { cookies } from 'next/headers';
 import { getTranslations } from 'next-intl/server';
 import { cache } from 'react';
 
-import { HeaderSection } from '@/vibes/soul/sections/header-section';
 import { LayoutQuery } from '~/app/[locale]/(default)/query';
 import { getSessionCustomerAccessToken } from '~/auth';
 import { client } from '~/client';
@@ -10,6 +9,7 @@ import { graphql, readFragment } from '~/client/graphql';
 import { revalidate } from '~/client/revalidate-target';
 import { TAGS } from '~/client/tags';
 import { logoTransformer } from '~/data-transformers/logo-transformer';
+import { SiteHeader as HeaderSection } from '~/lib/makeswift/components/site-header/site-header';
 
 import { search } from './_actions/search';
 import { HeaderFragment } from './fragment';
