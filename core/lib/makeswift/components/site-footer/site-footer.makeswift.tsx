bf0530810d9623e9bb2031fda8c031d4e5d55fdd
Fikri Karim <fkfikrikarim@gmail.com>
2024-12-23 20:56:28 +0700

    feat: add Makeswift site footer

---
+++ core/lib/makeswift/components/site-footer/site-footer.makeswift.tsx
@@ -0,0 +1,43 @@
+import { Checkbox, Image, Link, List, Number, Shape, TextInput } from '@makeswift/runtime/controls';
+
+import { runtime } from '~/lib/makeswift/runtime';
+
+import { MakeswiftFooter } from './site-footer.client';
+
+export const COMPONENT_TYPE = 'catalyst-makeswift-footer';
+
+runtime.registerComponent(MakeswiftFooter, {
+  type: COMPONENT_TYPE,
+  label: 'Site Footer',
+  hidden: true,
+  props: {
+    logo: Shape({
+      type: {
+        show: Checkbox({ label: 'Show logo', defaultValue: true }),
+        src: Image({ label: 'Logo' }),
+        width: Number({ label: 'Logo width', suffix: 'px' }),
+        alt: TextInput({ label: 'Alt text', defaultValue: 'Logo alt' }),
+      },
+    }),
+    sections: List({
+      label: 'Footer group',
+      type: Shape({
+        type: {
+          title: TextInput({ label: 'Heading', defaultValue: 'Heading' }),
+          links: List({
+            label: 'Links',
+            type: Shape({
+              type: {
+                label: TextInput({ label: 'Text', defaultValue: 'Text' }),
+                link: Link({ label: 'URL' }),
+              },
+            }),
+            getItemLabel: (item) => item?.label ?? 'Text',
+          }),
+        },
+      }),
+      getItemLabel: (item) => item?.title ?? 'Heading',
+    }),
+    copyright: TextInput({ label: 'Copyright text' }),
+  },
+});
