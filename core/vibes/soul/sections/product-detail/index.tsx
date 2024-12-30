2b44e24ddca49aa9b5bd88d0fb2fc441f70944c7
Alan Pledger <apledger3@gmail.com>
2024-12-30 11:02:28 -0500

    fix: reduce gap on pdp

--- core/vibes/soul/sections/product-detail/index.tsx
+++ core/vibes/soul/sections/product-detail/index.tsx
@@ -62,7 +62,7 @@ export function ProductDetail<F extends Field>({
         <Stream fallback={<ProductDetailSkeleton />} value={streamableProduct}>
           {(product) =>
             product && (
-              <div className="grid grid-cols-1 items-stretch gap-x-6 gap-y-8 @2xl:grid-cols-2 @5xl:gap-x-12">
+              <div className="grid grid-cols-1 items-stretch gap-x-8 gap-y-8 @2xl:grid-cols-2 @5xl:gap-x-12">
                 <div className="hidden @2xl:block">
                   <Stream fallback={<ProductGallerySkeleton />} value={product.images}>
                     {(images) => <ProductGallery images={images} />}
