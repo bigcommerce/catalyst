eb83ba4358a54181bb668c39d4a7caea0d8b9e54
Kathiravan Balu <kathir@arizon.digital>
2024-12-30 12:23:23 +0530

    fixing login issue

--- core/app/[locale]/(default)/(auth)/login/page.tsx
+++ core/app/[locale]/(default)/(auth)/login/page.tsx
@@ -123,32 +123,19 @@ export default async function Login({ params }: Props) {
             </div>
           </div>
         </div>
-      <div className="m-auto mt-[20px] flex w-[92%] flex-col gap-[20px] px-0 xl:px-12 2xl:mx-auto 2xl:px-0">
-        <ComponentsBreadcrumbs
-          className="login-div login-breadcrumb mx-auto mt-[0.5rem] hidden w-[80%] px-[1px] xl:block"
-          breadcrumbs={breadcrumbs}
-        />
-        <div className="login-div login-div-one mx-auto mt-3 flex w-full flex-col justify-between gap-0 px-[1px] text-center md:mt-6 lg:mt-0 xl:w-[80%] xl:flex-row xl:gap-24 xl:text-left">
-          <h2 className="text-[24px] font-normal text-[#353535] xl:text-[34px]">Rewards Program</h2>
-          <div className="flex flex-col xl:flex-row flex-wrap content-center items-center justify-center gap-[10px] px-0 pb-0 pt-[10px]">
-            <p className="flex items-center text-center text-[16px] xl:text-[20px] font-medium tracking-[0.15px] text-[#002A37]">
-              Shopping for a Business?
-            </p>
+        <div className="m-auto mt-[20px] flex w-[92%] flex-col gap-[20px] px-0 xl:px-12 2xl:mx-auto 2xl:px-0">
+          <ComponentsBreadcrumbs
+            className="login-div login-breadcrumb mx-auto mt-[0.5rem] hidden w-[80%] px-[1px] xl:block"
+            breadcrumbs={breadcrumbs}
+          />
+          <div className="login-div login-div-one mx-auto mt-3 flex w-full flex-col justify-between gap-0 px-[1px] text-center md:mt-6 lg:mt-0 xl:w-[80%] xl:flex-row xl:gap-24 xl:text-left">
+            <h2 className="text-[24px] font-normal text-[#353535] xl:text-[34px]">Rewards Program</h2>
+            <div className="flex flex-col xl:flex-row flex-wrap content-center items-center justify-center gap-[10px] px-0 pb-0 pt-[10px]">
+              <p className="flex items-center text-center text-[16px] xl:text-[20px] font-medium tracking-[0.15px] text-[#002A37]">
+                Shopping for a Business?
+              </p>
 
-            <button className="box-border flex max-w-[258px] flex-row items-center justify-center gap-1 px-[10px] py-[2px] rounded border border-[#4EAECC] bg-white xl:px-6 xl:py-2 xl:border-[#B4DDE9]">
-              <div className="flex items-center text-[14px] font-medium tracking-wide text-[#002A37]">
-                <Link className="text-[16px]" href="/trade-account/trade-step1/">
-                  OPEN A TRADE ACCOUNT
-                </Link>
-              </div>
-            </button>
-          </div>
-        </div>
-        <div className="login-div font-weight-[500] mx-auto w-[80%] px-[1px] text-center text-[20px] tracking-[0.15px] xl:text-left">
-          Log In
-        </div>
-
-              <button className="box-border flex max-w-[258px] flex-row items-center justify-center gap-1 rounded border border-[#4EAECC] bg-white px-6 py-2 xl:border-[#B4DDE9]">
+              <button className="box-border flex max-w-[258px] flex-row items-center justify-center gap-1 px-[10px] py-[2px] rounded border border-[#4EAECC] bg-white xl:px-6 xl:py-2 xl:border-[#B4DDE9]">
                 <div className="flex items-center text-[14px] font-medium tracking-wide text-[#002A37]">
                   <Link className="text-[16px]" href="/trade-account/trade-step1/">
                     OPEN A TRADE ACCOUNT
@@ -161,76 +148,87 @@ export default async function Login({ params }: Props) {
             Log In
           </div>
 
-
-          <div className="login-div mx-auto mb-6 flex w-full flex-col justify-center lg:gap-[4.5rem] xl:w-[80%] xl:flex-row">
-            <div className="login-first-child login-logo mx-[1px] flex w-[calc(90vw)] flex-grow flex-col self-center xl:w-[calc(35.28vw)] xl:self-auto">
-              <LoginForm {...imageIconList} passwordHide={passwordHide} />
+          <button className="box-border flex max-w-[258px] flex-row items-center justify-center gap-1 rounded border border-[#4EAECC] bg-white px-6 py-2 xl:border-[#B4DDE9]">
+            <div className="flex items-center text-[14px] font-medium tracking-wide text-[#002A37]">
+              <Link className="text-[16px]" href="/trade-account/trade-step1/">
+                OPEN A TRADE ACCOUNT
+              </Link>
             </div>
-            <div className="login-sec-child mx-[1px] mt-[7em] flex w-[calc(90vw)] flex-grow flex-col gap-[20px] self-center rounded-[5px] p-0 sm:mt-[7em] xl:mt-0 xl:w-[calc(35.28vw)] xl:items-center xl:self-auto">
-              <div className="flex w-full flex-col gap-[20px] rounded-[5px] bg-[#008BB7] p-[40px] text-white">
-                <h3 className="flex items-center justify-center text-center text-[34px] font-bold tracking-[0.25px]">
-                  {t('CreateAccount.heading')}
-                </h3>
+          </button>
+        </div>
+      </div>
+      <div className="login-div font-weight-[500] mx-auto w-[80%] px-[1px] text-center text-[20px] tracking-[0.15px] xl:text-left">
+        Log In
+      </div>
 
-                <div className="item-center flex flex-row justify-center">
-                  <Button
-                    asChild
-                    className="flex h-[42px] w-fit items-center justify-center gap-[5px] rounded-[3px] bg-white p-[5px] px-[10px] text-[#03465c] hover:text-[#03465c]"
-                  >
-                    <Link href="/register">
-                      {' '}
-                      <img src={person} alt="" /> {t('CreateAccount.createLink')}
-                    </Link>
-                  </Button>
-                </div>
-                <ul className="flex list-disc flex-col gap-[20px]">
-                  <li className="flex list-none flex-row items-center gap-[10px]">
-                    <img className="mt-[5px] self-start" src={checkCircle} alt="" />
-                    <p className="text-[19px] font-normal tracking-[0.15px]">
-                      {t('CreateAccount.fastCheckout')}
-                    </p>
-                  </li>
-                  <li className="flex list-none flex-row items-center gap-[10px]">
-                    <img className="mt-[5px] self-start" src={checkCircle} alt="" />
-                    <p className="text-[19px] font-normal tracking-[0.15px]">
-                      {t('CreateAccount.multipleAddresses')}
-                    </p>
-                  </li>
-                  <li className="flex list-none flex-row items-center gap-[10px]">
-                    <img className="mt-[5px] self-start" src={checkCircle} alt="" />
-                    <p className="text-[19px] font-normal tracking-[0.15px]">
-                      {t('CreateAccount.ordersHistory')}
-                    </p>
-                  </li>
-                  <li className="flex list-none flex-row items-center gap-[10px]">
-                    <img className="mt-[5px] self-start" src={checkCircle} alt="" />
-                    <p className="text-[19px] font-normal tracking-[0.15px]">
-                      {t('CreateAccount.ordersTracking')}
-                    </p>
-                  </li>
-                  <li className="flex list-none flex-row items-center gap-[10px]">
-                    <img className="mt-[5px] self-start" src={checkCircle} alt="" />
-                    <p className="text-[19px] font-normal tracking-[0.15px]">
-                      {t('CreateAccount.wishlists')}
-                    </p>
-                  </li>
-                  <li className="flex list-none flex-row items-center gap-[10px]">
-                    <img className="mt-[5px] self-start" src={checkCircle} alt="" />
-                    <p className="text-[19px] font-normal tracking-[0.15px]">
-                      {t('CreateAccount.exclusiveAccess')}
-                    </p>
-                  </li>
-                </ul>
-              </div>
-              <div className="flex flex-col items-center gap-[20px] rounded-[5px] border border-black p-[20px_40px]">
-                <h3 className="flex items-center text-center text-[24px] font-normal text-[#353535]">
-                  Current Offer Banner Placeholder
-                </h3>
-                <p className="flex items-center text-center text-[16px] font-medium tracking-[0.5px] text-[#353535]">
-                  Optional placeholder for if we want to advertise a specific member promotion
+
+      <div className="login-div mx-auto mb-6 flex w-full flex-col justify-center lg:gap-[4.5rem] xl:w-[80%] xl:flex-row">
+        <div className="login-first-child login-logo mx-[1px] flex w-[calc(90vw)] flex-grow flex-col self-center xl:w-[calc(35.28vw)] xl:self-auto">
+          <LoginForm {...imageIconList} passwordHide={passwordHide} />
+        </div>
+        <div className="login-sec-child mx-[1px] mt-[7em] flex w-[calc(90vw)] flex-grow flex-col gap-[20px] self-center rounded-[5px] p-0 sm:mt-[7em] xl:mt-0 xl:w-[calc(35.28vw)] xl:items-center xl:self-auto">
+          <div className="flex w-full flex-col gap-[20px] rounded-[5px] bg-[#008BB7] p-[40px] text-white">
+            <h3 className="flex items-center justify-center text-center text-[34px] font-bold tracking-[0.25px]">
+              {t('CreateAccount.heading')}
+            </h3>
+
+            <div className="item-center flex flex-row justify-center">
+              <Button
+                asChild
+                className="flex h-[42px] w-fit items-center justify-center gap-[5px] rounded-[3px] bg-white p-[5px] px-[10px] text-[#03465c] hover:text-[#03465c]"
+              >
+                <Link href="/register">
+                  {' '}
+                  <img src={person} alt="" /> {t('CreateAccount.createLink')}
+                </Link>
+              </Button>
+            </div>
+            <ul className="flex list-disc flex-col gap-[20px]">
+              <li className="flex list-none flex-row items-center gap-[10px]">
+                <img className="mt-[5px] self-start" src={checkCircle} alt="" />
+                <p className="text-[19px] font-normal tracking-[0.15px]">
+                  {t('CreateAccount.fastCheckout')}
                 </p>
-              </div>
-            </div>
+              </li>
+              <li className="flex list-none flex-row items-center gap-[10px]">
+                <img className="mt-[5px] self-start" src={checkCircle} alt="" />
+                <p className="text-[19px] font-normal tracking-[0.15px]">
+                  {t('CreateAccount.multipleAddresses')}
+                </p>
+              </li>
+              <li className="flex list-none flex-row items-center gap-[10px]">
+                <img className="mt-[5px] self-start" src={checkCircle} alt="" />
+                <p className="text-[19px] font-normal tracking-[0.15px]">
+                  {t('CreateAccount.ordersHistory')}
+                </p>
+              </li>
+              <li className="flex list-none flex-row items-center gap-[10px]">
+                <img className="mt-[5px] self-start" src={checkCircle} alt="" />
+                <p className="text-[19px] font-normal tracking-[0.15px]">
+                  {t('CreateAccount.ordersTracking')}
+                </p>
+              </li>
+              <li className="flex list-none flex-row items-center gap-[10px]">
+                <img className="mt-[5px] self-start" src={checkCircle} alt="" />
+                <p className="text-[19px] font-normal tracking-[0.15px]">
+                  {t('CreateAccount.wishlists')}
+                </p>
+              </li>
+              <li className="flex list-none flex-row items-center gap-[10px]">
+                <img className="mt-[5px] self-start" src={checkCircle} alt="" />
+                <p className="text-[19px] font-normal tracking-[0.15px]">
+                  {t('CreateAccount.exclusiveAccess')}
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col items-center gap-[20px] rounded-[5px] border border-black p-[20px_40px]">
+            <h3 className="flex items-center text-center text-[24px] font-normal text-[#353535]">
+              Current Offer Banner Placeholder
+            </h3>
+            <p className="flex items-center text-center text-[16px] font-medium tracking-[0.5px] text-[#353535]">
+              Optional placeholder for if we want to advertise a specific member promotion
+            </p>
           </div>
         </div>
       </div>
