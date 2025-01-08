3c26e545416e817e6a27dbb2cd1f5a921d04ed18
Chancellor Clark <chancellor.clark@bigcommerce.com>
2025-01-08 15:44:54 -0500

    feat(core): update order details page with VIBES

---
+++ core/app/[locale]/(default)/account/orders/[id]/page.tsx
@@ -0,0 +1,39 @@
+import { notFound } from 'next/navigation';
+import { getFormatter, getTranslations } from 'next-intl/server';
+
+import { OrderDetailsSection } from '@/vibes/soul/sections/order-details-section';
+import { orderDetailsTransformer } from '~/data-transformers/order-details-transformer';
+
+import { getCustomerOrderDetails } from './page-data';
+
+interface Props {
+  params: Promise<{
+    id: string;
+    locale: string;
+  }>;
+}
+
+export default async function Order(props: Props) {
+  const { id, locale } = await props.params;
+  const t = await getTranslations('Account.OrderDetails');
+  const format = await getFormatter();
+
+  const order = await getCustomerOrderDetails({
+    id: Number(id),
+  });
+
+  if (!order) {
+    notFound();
+  }
+
+  return (
+    <OrderDetailsSection
+      order={orderDetailsTransformer(order, t, format)}
+      prevHref={`/${locale}/account/orders`}
+      shipmentAddressLabel={t('shippingAddressLabel')}
+      shipmentMethodLabel={t('shippingMethodLabel')}
+      summaryTotalLabel={t('summaryTotalLabel')}
+      title={t('title', { orderNumber: id })}
+    />
+  );
+}
