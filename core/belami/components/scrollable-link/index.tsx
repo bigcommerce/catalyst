05c9f56d26b27e53174ab52f36cd15398c3f6a30
dimonets <dmitry.yeskin@gmail.com>
2025-01-08 12:37:49 -0800

    Another test of scrollable link

--- core/belami/components/scrollable-link/index.tsx
+++ core/belami/components/scrollable-link/index.tsx
@@ -3,11 +3,11 @@
 import { PropsWithChildren } from 'react';
 import { useRouter } from 'next/navigation';
  
-export function ScrollableLink({ children, href, replace = false, className } : PropsWithChildren<{ href: string, replace?: boolean, prefetch?: boolean, className?: string }>) {
+export function ScrollableLink({ children, href, replace = false, scroll = true, className } : PropsWithChildren<{ href: string, replace?: boolean, scroll?: boolean, prefetch?: boolean, className?: string }>) {
   const router = useRouter();
  
   return (<>
-    <button type="button" onClick={() => replace ? router.replace(href, {scroll: false}) : router.push(href, {scroll: false})} className={className}>
+    <button type="button" onClick={() => replace ? router.replace(href, {scroll: scroll}) : router.push(href, {scroll: scroll})} className={className}>
       {children}
     </button>
   </>)
