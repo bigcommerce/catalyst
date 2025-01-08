fcdfe32eed0caf7250e83d43bb904c457afa00f5
Chancellor Clark <chancellor.clark@bigcommerce.com>
2025-01-08 15:45:29 -0500

    refactor(core): remove product path fetching in favor of line item path

--- core/data-transformers/orders-transformer.ts
+++ core/data-transformers/orders-transformer.ts
@@ -1,62 +1,13 @@
-import { removeEdgesAndNodes } from '@bigcommerce/catalyst-client';
 import { getFormatter } from 'next-intl/server';
 
 import { Order } from '@/vibes/soul/sections/order-list-section/order-list-item';
 import { getCustomerOrders } from '~/app/[locale]/(default)/account/orders/page-data';
-import { client } from '~/client';
-import { graphql } from '~/client/graphql';
-import { revalidate } from '~/client/revalidate-target';
 import { ExistingResultType } from '~/client/util';
 
-const ProductAttributes = graphql(`
-  query ProductAttributes($entityIds: [Int!], $first: Int!) {
-    site {
-      products(entityIds: $entityIds, first: $first) {
-        edges {
-          node {
-            entityId
-            path
-          }
-        }
-      }
-    }
-  }
-`);
-
-export const ordersTransformer = async (
+export const ordersTransformer = (
   orders: ExistingResultType<typeof getCustomerOrders>['orders'],
   format: ExistingResultType<typeof getFormatter>,
-): Promise<Order[]> => {
-  const productIds = orders.reduce<number[]>((acc, order) => {
-    // In the future we probably want to include downloads, email, and pickup consignments
-    const shippingConsignment =
-      order.consignments.shipping?.flatMap((consignment) =>
-        removeEdgesAndNodes(consignment.lineItems),
-      ) ?? [];
-
-    return [
-      ...acc,
-      ...shippingConsignment.map((lineItem) => {
-        return lineItem.productEntityId;
-      }),
-    ];
-  }, []);
-
-  // We need to fetch the product urls separately as our GQL API doesn't have a node
-  // for these urls. In order to make it efficient, we aggregate all the products for
-  // all the orders on the page and fetch them all at once.
-  const { data } = await client.fetch({
-    document: ProductAttributes,
-    // We only display the first 5 orders on the page and we truncate the amount of
-    // products are shown per order. The max products shown per order is 8 based on
-    // the largest screen size, so we can safely fetch 50 products and feel comfortable.
-    // 50 is the max we can fetch at once.
-    variables: { entityIds: productIds, first: 50 },
-    fetchOptions: { next: { revalidate } },
-  });
-
-  const productPaths = removeEdgesAndNodes(data.site.products);
-
+): Order[] => {
   return orders.map((order) => {
     return {
       id: order.entityId.toString(),
@@ -68,14 +19,11 @@ export const ordersTransformer = async (
       }),
       lineItems:
         order.consignments.shipping?.flatMap((consignment) => {
-          return removeEdgesAndNodes(consignment.lineItems).map((lineItem) => {
-            const productPath = productPaths.find(
-              (product) => product.entityId === lineItem.productEntityId,
-            );
-
+          return consignment.lineItems.map((lineItem) => {
             return {
               id: lineItem.entityId.toString(),
-              href: productPath?.path ?? `/product/${lineItem.productEntityId}`,
+              href:
+                lineItem.baseCatalogProduct?.path ?? `/product/${String(lineItem.productEntityId)}`,
               title: lineItem.name,
               subtitle: lineItem.brand ?? undefined,
               price: format.number(lineItem.subTotalListPrice.value, {
