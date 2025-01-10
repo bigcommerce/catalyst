b62c3e5141c92217046eff1f27cf8b4c19fb5823
Fikri Karim <fkfikrikarim@gmail.com>
2025-01-11 00:46:07 +0700

    fix: temp fix on Page to solve non-published pages are not editable

--- core/lib/makeswift/page.tsx
+++ core/lib/makeswift/page.tsx
@@ -1,12 +1,18 @@
 import { Page as MakeswiftPage } from '@makeswift/runtime/next';
 import { notFound } from 'next/navigation';
+import { connection } from 'next/server';
 
 import { getPageSnapshot } from './client';
 
 export async function Page({ path, locale }: { path: string; locale: string }) {
   const snapshot = await getPageSnapshot({ path, locale });
 
-  if (snapshot == null) return notFound();
+  if (snapshot == null) {
+    // This is a temporary solution to fix the issue where non-published pages are not editable in the builder.
+    await connection();
+
+    return notFound();
+  }
 
   return <MakeswiftPage snapshot={snapshot} />;
 }
