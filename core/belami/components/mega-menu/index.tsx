bac2a0a8405206e0d389a26c0fc802ca8d63941d
dimonets <dmitry.yeskin@gmail.com>
2025-01-15 14:03:08 -0800

    Removed suspense and skeleton from Mega Menu component

--- core/belami/components/mega-menu/index.tsx
+++ core/belami/components/mega-menu/index.tsx
@@ -6,31 +6,11 @@ export function MegaMenu({
   variant,
   menuItems,
   secondaryMenuItems,
-  classNames,
-  loadingMessage = 'Loading...',
-}: MegaMenuProps & {
-  loadingMessage?: string;
-}) {
+  classNames
+}: MegaMenuProps) {
   return (
     <>
-    {/* <Suspense fallback={<MegaMenuSkeleton classNames={classNames} message={loadingMessage} />}> */}
       {(variant === 'default') && <MegaMenuDefault menuItems={menuItems} secondaryMenuItems={secondaryMenuItems} classNames={classNames} />}
-    {/* </Suspense> */}
     </>
   );
-}
-
-export function MegaMenuSkeleton({
-  classNames,
-  message
-}: {
-  classNames?: {
-    root?: string;
-    mainMenu?: string;
-    secondaryMenu?: string;
-  },
-  message?: string;
-}) {
-  return <></>;
-  //return <div className={classNames?.root}>{message}</div>;
 }
