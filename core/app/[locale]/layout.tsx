8ed7bc1199004de46b3caa8c3e360297f35e6afe
dimonets <dmitry.yeskin@gmail.com>
2025-01-17 11:13:07 -0800

    Disabled SiteVibes refreshes on every page

--- core/app/[locale]/layout.tsx
+++ core/app/[locale]/layout.tsx
@@ -142,7 +142,9 @@ export default async function RootLayout({ params, children }: Props) {
           src="https://app.sitevibes.com/js/pixel.js?key=e0feae51-26fd-453a-8e67-f9a1a74c8d69"
           strategy="afterInteractive"
         />
+        {/*
         <SiteVibesIntegration />
+        */}
         <Notifications />
         <NextIntlClientProvider locale={locale} messages={messages}>
           <NuqsAdapter>
