23f30d32ca264d6c96581ff067842b02124ccfbb
RahaviSekar <rahavi@arizon.digital>
2025-01-21 12:08:00 +0530

    Warranty section update

--- core/components/ui/form/radio-group/radio-group.tsx
+++ core/components/ui/form/radio-group/radio-group.tsx
@@ -24,11 +24,11 @@ const RadioGroup = ({ children, className, error = false, items, ...props }: Pro
         const { label, value, ...itemProps } = item;
 
         return (
-          <div className="mb-3 flex w-full gap-2" key={`${id}-${value}`}>
+          <div className="mb-3 flex items-center w-full gap-2" key={`${id}-${value}`}>
             <RadioGroupPrimitive.Item
               {...itemProps}
               className={cn(
-                'hover:border-secondary focus-visible:hover:border-secondary radix-state-checked:hover:border-secondary flex h-[20px] w-[22px] items-center justify-center rounded-full border-2 border-gray-800 bg-white bg-opacity-100 focus-visible:border-primary focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary/20 disabled:pointer-events-none disabled:bg-gray-100 radix-state-checked:border-primary radix-state-checked:bg-white radix-state-checked:disabled:border-gray-400 radix-state-checked:disabled:bg-gray-400',
+                'hover:border-secondary focus-visible:hover:border-secondary radix-state-checked:hover:border-secondary flex h-[18px] w-[20px] items-center justify-center rounded-full border-2 border-gray-800 bg-white bg-opacity-100 focus-visible:border-primary focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-primary/20 disabled:pointer-events-none disabled:bg-gray-100 radix-state-checked:border-primary radix-state-checked:bg-white radix-state-checked:disabled:border-gray-400 radix-state-checked:disabled:bg-gray-400',
                 error &&
                   'border-error-secondary focus-visible:border-error-secondary focus-visible:ring-error-secondary/20 radix-state-checked:border-error-secondary radix-state-checked:bg-error-secondary hover:border-error focus-visible:hover:border-error disabled:border-gray-200 radix-state-checked:hover:border-error radix-state-checked:hover:bg-error',
               )}
