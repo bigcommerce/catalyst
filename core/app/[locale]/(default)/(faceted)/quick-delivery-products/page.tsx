590d2a67d8d463965bf9aca3ae40a7184e020ec7
dimonets <dmitry.yeskin@gmail.com>
2025-01-23 15:14:45 -0800

    Connected quick delivery products to Makeswift

--- core/app/[locale]/(default)/(faceted)/quick-delivery-products/page.tsx
+++ core/app/[locale]/(default)/(faceted)/quick-delivery-products/page.tsx
@@ -1,4 +1,4 @@
-import { getFormatter, getTranslations } from 'next-intl/server';
+import { getFormatter, getTranslations, setRequestLocale } from 'next-intl/server';
 
 import { getSessionCustomerAccessToken } from '~/auth';
 
@@ -7,6 +7,18 @@ import { getActivePromotions } from '~/belami/lib/fetch-promotions';
 import { Breadcrumbs } from '~/components/breadcrumbs';
 import { QuickDeliveryProducts } from './quick-delivery-products';
 
+import { Page as MakeswiftPage } from '@makeswift/runtime/next';
+import { getSiteVersion } from '@makeswift/runtime/next/server';
+import { defaultLocale } from '~/i18n/routing';
+import { client } from '~/lib/makeswift/client';
+import '~/lib/makeswift/components';
+
+interface Props {
+  params: Promise<{
+    locale: string;
+  }>;
+  searchParams: Promise<Record<string, string | string[] | undefined>>;
+}
 
 export async function generateMetadata() {
   const t = await getTranslations('QuickDeliveryProducts');
@@ -16,12 +28,24 @@ export async function generateMetadata() {
   };
 }
 
-export default async function QuickDeliveryProductsPage() {
+export default async function QuickDeliveryProductsPage(props: Props) {
+
+  const searchParams = await props.searchParams;
+  const params = await props.params;
 
   const customerAccessToken = await getSessionCustomerAccessToken();
   const useDefaultPrices = !customerAccessToken;
 
+  const { locale } = params;
+
+  setRequestLocale(locale);
+
   const t = await getTranslations('QuickDeliveryProducts');
+
+  const snapshot = await client.getPageSnapshot('/quick-delivery-products', {
+    siteVersion: await getSiteVersion(),
+    locale: locale === defaultLocale ? undefined : locale,
+  });
 
   const promotions = await getActivePromotions(true);
 
@@ -31,6 +55,11 @@ export default async function QuickDeliveryProductsPage() {
       <div className="md:mb-8 lg:flex lg:flex-row lg:items-center lg:justify-between">
         <h1 className="mb-4 text-4xl font-black lg:mb-0 lg:text-5xl">{t('title')}</h1>
       </div>
+
+      {!!snapshot &&
+        <MakeswiftPage snapshot={snapshot} />
+      }
+
       <QuickDeliveryProducts promotions={promotions} useDefaultPrices={useDefaultPrices} />
     </div>
   );
