ba3a904b66d0903153396a4357d75b01108e36ca
Nathan Booker <nathan.booker@bigcommerce.com>
2025-01-25 10:45:46 -0600

    Add currency code into makeswift components

--- core/app/api/products/[entityId]/route.ts
+++ core/app/api/products/[entityId]/route.ts
@@ -3,11 +3,12 @@ import { NextRequest, NextResponse } from 'next/server';
 import { getSessionCustomerAccessToken } from '~/auth';
 import { client } from '~/client';
 import { graphql, ResultOf } from '~/client/graphql';
+import { getPreferredCurrencyCode } from '~/lib/currency';
 import { MakeswiftProductFragment } from '~/lib/makeswift/utils/use-bc-product-to-vibes-product/fragment';
 
 const GetProduct = graphql(
   `
-    query GetProduct($entityId: Int!) {
+    query GetProduct($entityId: Int!, $currencyCode: currencyCode) {
       site {
         product(entityId: $entityId) {
           ...MakeswiftProductFragment
@@ -25,12 +26,13 @@ export const GET = async (
   { params }: { params: Promise<{ entityId: string }> },
 ): Promise<NextResponse<GetProductResponse>> => {
   const customerAccessToken = await getSessionCustomerAccessToken();
+  const currencyCode = await getPreferredCurrencyCode();
   const { entityId } = await params;
 
   const { data } = await client.fetch({
     document: GetProduct,
     customerAccessToken,
-    variables: { entityId: parseInt(entityId, 10) },
+    variables: { entityId: parseInt(entityId, 10), currencyCode },
   });
 
   return NextResponse.json(data.site.product);
