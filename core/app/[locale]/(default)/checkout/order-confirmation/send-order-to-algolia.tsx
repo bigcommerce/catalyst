d2244f596428d4aed6be5c93ecd3e87e964fcdbe
dimonets <dmitry.yeskin@gmail.com>
2025-01-27 15:17:46 -0800

    Added order purchase tracking for Algolia

---
+++ core/app/[locale]/(default)/checkout/order-confirmation/send-order-to-algolia.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { useEffect } from 'react';
+import aa from 'search-insights';
+
+aa('init', {
+  appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || '',
+  apiKey: process.env.NEXT_PUBLIC_ALGOLIA_API_KEY || '',
+});
+
+const indexName: string = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME || '';
+
+interface Props {
+    lineItems: any;
+}
+
+export const SendOrderToAlgolia = ({
+  lineItems
+}: Props) => {
+
+  useEffect(() => {
+    if (lineItems && lineItems.length > 0) {
+      aa('purchasedObjectIDs', {
+        eventName: 'Product Purchased',
+        index: indexName,
+        objectIDs: lineItems.map((item: any) => String(item.entityId)).join(','), // List of product IDs
+        objectData: lineItems.map((item: any) => ({
+          objectID: String(item.entityId),
+          price: item.subTotalListPrice?.value,
+          quantity: item.quantity,
+        })), // List of product data
+        currency: lineItems[0]?.subTotalListPrice?.currencyCode || 'USD',
+      });
+  }
+  }, [lineItems]);
+
+  return <></>;
+}
