6c08959c95e1a232cd2add0251b8fb5f69b4aaee
Jorge Moya <jorge.moya@bigcommerce.com>
2025-01-28 13:12:19 -0600

    fix: remove ppr from catch all

--- core/app/[locale]/(default)/[...rest]/page.tsx
+++ core/app/[locale]/(default)/[...rest]/page.tsx
@@ -3,3 +3,7 @@ import { notFound } from 'next/navigation';
 export default function CatchAllPage() {
   notFound();
 }
+
+export const dynamic = 'force-dynamic';
+
+export const experimental_ppr = false;
