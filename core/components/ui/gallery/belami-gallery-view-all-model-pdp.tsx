b0d7a78e43fce48d4f389315757b01b8330023e0
Team Arizon <team@arizon.digital>
2025-01-28 16:57:14 +0530

    P2-494-pdp-gallery

--- core/components/ui/gallery/belami-gallery-view-all-model-pdp.tsx
+++ core/components/ui/gallery/belami-gallery-view-all-model-pdp.tsx
@@ -233,22 +233,22 @@ const GalleryModel = ({
   return (
     <div className="modal-overlay fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
       <div className="modal-content relative flex h-full w-full flex-col bg-white p-0 lg:p-[30px] xl:flex-row">
-        <div className="gallery-slider relative flex h-[65%] w-full items-center justify-center overflow-hidden sm:h-[50%] xl:h-full xl:w-[60%] 2xl:w-[70%] xl:py-[20px]">
+        <div className="gallery-slider relative flex h-[65%] w-full items-center justify-center overflow-hidden sm:h-[50%] xl:h-full xl:w-[60%] xl:py-[20px] 2xl:w-[70%]">
           <button
             aria-label="Previous item"
-            className="absolute left-0 xl:left-8 top-1/2 -translate-y-1/2 transform p-2 hover:bg-opacity-50 focus:outline-none"
+            className="absolute left-0 top-1/2 -translate-y-1/2 transform p-2 hover:bg-opacity-50 focus:outline-none xl:left-8"
             onClick={handlePrevious}
           >
             <ChevronLeft size={36} className="hover:text-gray-300" />
           </button>
 
-          <div className="relative flex w-[80%] xl:w-[80%] h-[300px] xl:h-[100%] items-center justify-center">
+          <div className="relative flex h-[300px] w-[80%] items-center justify-center xl:h-[100%] xl:w-[80%]">
             {renderMediaContent()}
           </div>
 
           <button
             aria-label="Next item"
-            className="absolute right-0 xl:right-8 top-1/2 -translate-y-1/2 transform p-2 hover:bg-opacity-50 focus:outline-none"
+            className="absolute right-0 top-1/2 -translate-y-1/2 transform p-2 hover:bg-opacity-50 focus:outline-none xl:right-8"
             onClick={handleNext}
           >
             <ChevronRight size={36} className="hover:text-gray-300" />
@@ -268,15 +268,15 @@ const GalleryModel = ({
                 onClick={() => onSelectIndex(index)}
               >
                 {item.type === 'video' ? (
-                  <div className="relative h-12 w-[100%] xl:w-[6em] md:!h-[8em] lg:!h-[10em] xl:!h-[6em] Images-Videos object-fill">
+                  <div className="Images-Videos relative h-12 w-[100%] object-fill md:!h-[8em] lg:!h-[10em] xl:!h-[6em] xl:w-[7em] 2xl:w-[6em]">
                     {isYoutubeUrl(item.url) ? (
                       <img
                         src={getYoutubeThumbnailUrl(item.url || '') || ''}
                         alt={item.title || 'Video thumbnail'}
-                        className="h-full w-full object-cover gallery-img"
+                        className="gallery-img h-full w-full object-cover"
                       />
                     ) : (
-                      <video className="h-full w-full object-cover gallery-video">
+                      <video className="gallery-video h-full w-full object-cover">
                         <source src={item.url} type="video/mp4" />
                       </video>
                     )}
@@ -306,7 +306,7 @@ const GalleryModel = ({
                 ) : (
                   <BcImage
                     alt={item.altText || ''}
-                    className="Images-Gallery h-12 w-[100%] xl:w-28 object-fill md:!h-[8em] lg:!h-[10em] xl:!h-[6em]"
+                    className="Images-Gallery h-12 w-[100%] object-fill md:!h-[8em] lg:!h-[10em] xl:!h-[6em] xl:w-28"
                     src={item.src || ''}
                     width={96}
                     height={96}
