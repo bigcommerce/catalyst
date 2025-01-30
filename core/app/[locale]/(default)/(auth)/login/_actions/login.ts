b1d1fa42bbc2a6b29d172628e28bfd124092dda3
Jorge Moya <jorge.moya@bigcommerce.com>
2025-01-30 15:43:51 -0600

    fix: remove nested conditionals

--- core/app/[locale]/(default)/(auth)/login/_actions/login.ts
+++ core/app/[locale]/(default)/(auth)/login/_actions/login.ts
@@ -43,14 +43,13 @@ export const login = async (_lastResult: SubmissionResult | null, formData: Form
       });
     }
 
-    if (error instanceof AuthError) {
-      if (error.name === 'CallbackRouteError') {
-        if (error.cause && error.cause.err?.message.includes('Invalid credentials')) {
-          return submission.reply({ formErrors: [t('Form.invalidCredentials')] });
-        }
-      }
-
-      return submission.reply({ formErrors: [error.name] });
+    if (
+      error instanceof AuthError &&
+      error.name === 'CallbackRouteError' &&
+      error.cause &&
+      error.cause.err?.message.includes('Invalid credentials')
+    ) {
+      return submission.reply({ formErrors: [t('Form.invalidCredentials')] });
     }
 
     return submission.reply({ formErrors: [t('Form.somethingWentWrong')] });
