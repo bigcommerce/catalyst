d82924290df106d63c0f771e4c3c76213b83f4a1
Jorge Moya <jorge.moya@bigcommerce.com>
2025-01-31 11:32:28 -0600

    fix: revert header

--- core/vibes/soul/primitives/navigation/index.tsx
+++ core/vibes/soul/primitives/navigation/index.tsx
@@ -320,7 +320,7 @@ export const Navigation = forwardRef(function Navigation<S extends SearchResult>
     >
       <div
         className={clsx(
-          'flex items-center justify-between gap-1 bg-[var(--nav-background,hsl(var(--background)))] py-2 pl-3 pr-2 transition-shadow @4xl:rounded-2xl @4xl:px-2 @4xl:pl-6 @4xl:pr-2.5',
+          'flex min-h-16 items-center justify-between gap-1 bg-[var(--nav-background,hsl(var(--background)))] py-2 pl-3 pr-2 transition-shadow @4xl:rounded-2xl @4xl:px-2 @4xl:pl-6 @4xl:pr-2.5',
           isFloating
             ? 'shadow-xl ring-1 ring-[var(--nav-floating-border,hsl(var(--foreground)/10%))]'
             : 'shadow-none ring-0',
@@ -423,7 +423,7 @@ export const Navigation = forwardRef(function Navigation<S extends SearchResult>
         {/* Top Level Nav Links */}
         <ul
           className={clsx(
-            'hidden min-h-11 items-center gap-1 @4xl:flex @4xl:flex-1',
+            'hidden gap-1 @4xl:flex @4xl:flex-1',
             {
               left: '@4xl:justify-start',
               center: '@4xl:justify-center',
