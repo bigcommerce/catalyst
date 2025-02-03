dca9a7edadd5ef51b70302a761e328be0332006f
dimonets <dmitry.yeskin@gmail.com>
2025-02-03 10:27:40 -0800

    Integrated DataDog

--- core/app/[locale]/layout.tsx
+++ core/app/[locale]/layout.tsx
@@ -27,6 +27,7 @@ import { Open_Sans, Roboto_Mono } from 'next/font/google';
 
 import Script from 'next/script';
 import { SiteVibesIntegration } from "~/belami/components/sitevibes";
+import { DatadogInit } from "~/belami/components/datadog";
 import '~/lib/makeswift/components';
 import { MakeswiftProvider } from '~/lib/makeswift/provider';
 
@@ -139,6 +140,7 @@ export default async function RootLayout({ params, children }: Props) {
         <DraftModeScript />
       </head>
       <body className="flex h-screen min-w-[375px] flex-col">
+        <DatadogInit />
         <Script
           id="sv-pixel-script"
           src="https://app.sitevibes.com/js/pixel.js?key=e0feae51-26fd-453a-8e67-f9a1a74c8d69"
