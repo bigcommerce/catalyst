281be0071a7e992ce53e94d7c90525a11bdb8746
Aleksey Gurtovoy <agurtovoy@acm.org>
2025-02-06 21:32:50 -0600

    fix: configure NextAuth cookies to work inside the builder

--- core/auth/index.ts
+++ core/auth/index.ts
@@ -172,6 +172,16 @@ async function authorize(credentials: unknown): Promise<User | null> {
   }
 }
 
+const partitionedCookie = (name?: string) =>
+  ({
+    ...(name !== undefined ? { name } : {}),
+    options: {
+      partitioned: true,
+      secure: true,
+      sameSite: 'none',
+    },
+  }) as const;
+
 const config = {
   session: {
     strategy: 'jwt',
@@ -232,6 +242,16 @@ const config = {
       authorize,
     }),
   ],
+  // configure NextAuth cookies to work inside of the Makeswift Builder's canvas
+  cookies: {
+    sessionToken: partitionedCookie(),
+    callbackUrl: partitionedCookie(),
+    csrfToken: partitionedCookie(),
+    pkceCodeVerifier: partitionedCookie(),
+    state: partitionedCookie(),
+    nonce: partitionedCookie(),
+    webauthnChallenge: partitionedCookie(),
+  },
 } satisfies NextAuthConfig;
 
 const { handlers, auth, signIn: nextAuthSignIn, signOut } = NextAuth(config);
