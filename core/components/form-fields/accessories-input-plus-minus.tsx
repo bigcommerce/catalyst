8344028667269bdd474b392a3d4a04c83b0b44f9
RahaviSekar <rahavi@arizon.digital>
2025-02-12 15:21:18 +0530

    update accessories-page

--- core/components/form-fields/accessories-input-plus-minus.tsx
+++ core/components/form-fields/accessories-input-plus-minus.tsx
@@ -9,7 +9,6 @@ import toast from "react-hot-toast";
 import { useTranslations } from "next-intl";
 import { Link } from '~/components/link';
 import { getCartData } from "../get-cart-items";
-import { getReferralIdCookie } from "~/app/[locale]/(default)/sales-buddy/_actions/referral";
 
 export const AccessoriesInputPlusMinus = ({ accessories, data }: { accessories: any, data: any }) => {
   const [quantity, setQuantity] = useState<any>(accessories?.prodQuantity);
@@ -72,8 +71,6 @@ export const AccessoriesInputPlusMinus = ({ accessories, data }: { accessories: 
             variantId: accessories?.variantEntityId,
             quantity: quantity,
           };
-          let cookieValue = await getReferralIdCookie();
-          const referrerId = cookieValue?.value;
           let cartMetaFields: any = await GetCartMetaFields(cartId, 'accessories_data');
           let getCartMetaLineItems = cartMetaFields?.find((item: any) => item?.key == lineItemId);
           if (cartMetaFields?.length == 0 || !getCartMetaLineItems) {
