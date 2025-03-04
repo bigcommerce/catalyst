285121caaf71ba777257f812c37be446f208717a
Hunter Garrett <hunter@hunterbecton.com>
2025-03-03 20:39:33 -0500

    feat: add default Tailwind typography styles (#2007)

--- core/tailwind.config.js
+++ core/tailwind.config.js
@@ -8,6 +8,75 @@ const config = {
   ],
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            h1: {
+              color: 'hsl(var(--foreground))',
+              fontFamily: 'var(--font-family-heading)',
+            },
+            h2: {
+              color: 'hsl(var(--foreground))',
+              fontFamily: 'var(--font-family-heading)',
+            },
+            h3: {
+              color: 'hsl(var(--foreground))',
+              fontFamily: 'var(--font-family-heading)',
+            },
+            h4: {
+              color: 'hsl(var(--foreground))',
+              fontFamily: 'var(--font-family-heading)',
+            },
+            h5: {
+              color: 'hsl(var(--foreground))',
+              fontFamily: 'var(--font-family-heading)',
+            },
+            h6: {
+              color: 'hsl(var(--foreground))',
+              fontFamily: 'var(--font-family-heading)',
+            },
+            p: {
+              color: 'hsl(var(--foreground))',
+              fontFamily: 'var(--font-family-body)',
+            },
+            a: {
+              color: 'color-mix(in oklab, hsl(var(--primary)), black 15%)',
+              textDecoration: 'none',
+              '&:hover': {
+                textDecoration: 'underline',
+              },
+            },
+            ul: {
+              color: 'hsl(var(--contrast-500))',
+              fontFamily: 'var(--font-family-body)',
+            },
+            ol: {
+              color: 'hsl(var(--contrast-500))',
+              fontFamily: 'var(--font-family-body)',
+            },
+            strong: {
+              fontWeight: '600',
+            },
+            blockquote: {
+              borderLeftColor: 'hsl(var(--contrast-300))',
+              p: {
+                color: 'hsl(var(--contrast-500))',
+                fontStyle: 'normal',
+                fontWeight: '400',
+              },
+            },
+            code: {
+              color: 'hsl(var(--contrast-500))',
+              fontFamily: 'var(--font-family-mono)',
+            },
+            pre: {
+              color: 'hsl(var(--background))',
+              backgroundColor: 'hsl(var(--foreground))',
+              fontFamily: 'var(--font-family-mono)',
+            },
+          },
+        },
+      },
       colors: {
         primary: {
           DEFAULT: 'hsl(var(--primary))',
@@ -164,6 +233,7 @@ const config = {
     require('tailwindcss-radix')(),
     require('tailwindcss-animate'),
     require('@tailwindcss/container-queries'),
+    require('@tailwindcss/typography'),
   ],
 };
 
