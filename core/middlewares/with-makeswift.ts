34ac728e50ad976e0004e3e503d832bbcfa0349b
arvinpoddar <arvin.poddar@bigcommerce.com>
2025-03-06 11:03:27 -0500

    refactor: replace Makeswift middleware internals (#2014)
    Swap the internals of the Makeswift middleware decorator to use experimental
    utilities from the Makeswift, which will hide Makeswift internals and avoid
    having to maintain a bespoke draft mode route.

--- core/middlewares/with-makeswift.ts
+++ core/middlewares/with-makeswift.ts
@@ -1,53 +1,36 @@
-import { NextRequest } from 'next/server';
-import { parse as parseSetCookie } from 'set-cookie-parser';
+import { unstable_createMakeswiftDraftRequest } from '@makeswift/runtime/next/middleware';
 
 import { routing } from '~/i18n/routing';
 
 import { MiddlewareFactory } from './compose-middlewares';
+
+if (!process.env.MAKESWIFT_SITE_API_KEY) {
+  throw new Error('MAKESWIFT_SITE_API_KEY is not set');
+}
+
+const MAKESWIFT_SITE_API_KEY = process.env.MAKESWIFT_SITE_API_KEY;
 
 const localeCookieName = ({ localeCookie }: { localeCookie?: boolean | { name?: string } }) =>
   (typeof localeCookie === 'object' ? localeCookie.name : undefined) ?? 'NEXT_LOCALE';
 
 export const withMakeswift: MiddlewareFactory = (middleware) => {
   return async (request, event) => {
-    const apiKey =
-      request.nextUrl.searchParams.get('x-makeswift-draft-mode') ??
-      request.headers.get('x-makeswift-draft-mode');
+    const draftRequest = await unstable_createMakeswiftDraftRequest(
+      request,
+      MAKESWIFT_SITE_API_KEY,
+    );
 
-    if (apiKey === process.env.MAKESWIFT_SITE_API_KEY) {
-      const response = await fetch(new URL('/api/makeswift/draft-mode', request.nextUrl.origin), {
-        headers: {
-          'x-makeswift-api-key': apiKey,
-        },
-      });
-
-      const cookies = parseSetCookie(response.headers.get('set-cookie') || '');
-      const prerenderBypassValue = cookies.find((c) => c.name === '__prerender_bypass')?.value;
-
-      if (!prerenderBypassValue) {
-        return middleware(request, event);
-      }
-
-      // https://github.com/vercel/next.js/issues/52967#issuecomment-1644675602
-      // if we don't pass request twice, headers are stripped
-      const proxiedRequest = new NextRequest(request, request);
-
-      proxiedRequest.cookies.set('__prerender_bypass', prerenderBypassValue);
-      proxiedRequest.cookies.set(
-        'x-makeswift-draft-data',
-        JSON.stringify({ makeswift: true, siteVersion: 'Working' }),
-      );
-
+    if (draftRequest != null) {
       if (routing.localeCookie) {
         // If the i18n middleware is configured to use a cookie, it will first try to derive the
         // locale from the existing request cookie before attempting to match the URL against the
         // locale routes. The locale switcher in the Makeswift Builder expects the host to always
         // determine the locale from the URL, though, so we have to erase the cookie from the
         // proxied request to force that behavior.
-        proxiedRequest.cookies.delete(localeCookieName(routing));
+        draftRequest.cookies.delete(localeCookieName(routing));
       }
 
-      return middleware(proxiedRequest, event);
+      return middleware(draftRequest, event);
     }
 
     return middleware(request, event);
