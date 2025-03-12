a7700dd385980bb24812338f3dc4abf2bd4023a8
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-03-12 14:15:25 +0100

    feat: Improve add to quote functionality
    - Match button size with add to cart
    - Set a loading state

--- core/b2b/types.ts
+++ core/b2b/types.ts
@@ -29,7 +29,7 @@ declare global {
               quantity?: number;
               selectedOptions?: B2BProductOption[];
             }>,
-          ) => void;
+          ) => Promise<void>;
         };
       };
       callbacks?: {
