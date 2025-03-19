51caad228926efaeffbbf9e98e65cb79e13c2478
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-03-19 17:16:13 +0100

    refactor: separate dev/production script loading

--- core/b2b/scripts.tsx
+++ core/b2b/scripts.tsx
@@ -5,61 +5,15 @@ import Script from 'next/script';
 
 import { useB2BAuth } from './use-b2b-auth';
 
-export function B2BProductionScripts({
-  storeHash,
-  channelId,
-  token,
-  localBuyerPortalHost,
-}: {
+interface Props {
   storeHash: string;
   channelId: string;
   token?: string;
-  localBuyerPortalHost?: string;
-}) {
+  environment: 'staging' | 'production';
+}
+
+export function B2BProductionScripts({ storeHash, channelId, token, environment }: Props) {
   useB2BAuth(token);
-
-  if (localBuyerPortalHost) {
-    return (
-      <>
-        <Script id="b2b-react-refresh" strategy="beforeInteractive" type="module">
-          {`
-              import RefreshRuntime from '${localBuyerPortalHost}/@react-refresh'
-              RefreshRuntime.injectIntoGlobalHook(window)
-              window.$RefreshReg$ = () => {}
-              window.$RefreshSig$ = () => (type) => type
-              window.__vite_plugin_react_preamble_installed__ = true
-          `}
-        </Script>
-        <Script
-          id="b2b-vite-client"
-          src={`${localBuyerPortalHost}/@vite/client`}
-          strategy="beforeInteractive"
-          type="module"
-        />
-
-        <Script id="b2b-config">
-          {`
-              window.B3 = {
-                setting: {
-                  store_hash: '${storeHash}',
-                  channel_id: ${channelId},
-                  platform: 'catalyst',
-                  cart_url: '/cart',
-                },
-              };
-          `}
-        </Script>
-        <Script
-          data-channelid={storeHash}
-          data-storehash={channelId}
-          src={`${localBuyerPortalHost}/src/main.ts`}
-          type="module"
-        />
-      </>
-    );
-  }
-  const isStagingEnabled = process.env.STAGING_B2B_CDN_ORIGIN === 'true'
-  const environment = isStagingEnabled ? 'staging' : 'production'
 
   return (
     <>
@@ -82,6 +36,53 @@ export function B2BProductionScripts({
         src={`https://cdn.bundleb2b.net/b2b/${environment}/storefront/headless.js`}
         type="module"
       />
+    </>
+  );
+}
+
+interface DevProps {
+  storeHash: string;
+  channelId: string;
+  hostname: string;
+  token?: string;
+}
+
+export function B2BDevScripts({ hostname, storeHash, channelId, token }: DevProps) {
+  useB2BAuth(token);
+
+  const src = `${hostname}/src/main.ts`;
+
+  return (
+    <>
+      <Script id="b2b-react-refresh" strategy="beforeInteractive" type="module">
+        {`
+              import RefreshRuntime from '${hostname}/@react-refresh'
+              RefreshRuntime.injectIntoGlobalHook(window)
+              window.$RefreshReg$ = () => {}
+              window.$RefreshSig$ = () => (type) => type
+              window.__vite_plugin_react_preamble_installed__ = true
+          `}
+      </Script>
+      <Script
+        id="b2b-vite-client"
+        src={`${hostname}/@vite/client`}
+        strategy="beforeInteractive"
+        type="module"
+      />
+
+      <Script id="b2b-config">
+        {`
+              window.B3 = {
+                setting: {
+                  store_hash: '${storeHash}',
+                  channel_id: ${channelId},
+                  platform: 'catalyst',
+                  cart_url: '/cart',
+                },
+              };
+          `}
+      </Script>
+      <Script data-channelid={storeHash} data-storehash={channelId} src={src} type="module" />
     </>
   );
 }
