dd473ab0abb628da300a20509045a4e96066e2c7
jon.mears <jon.mears@leighton.com>
2025-03-24 14:22:43 +0000

    better implementation with checks

--- core/app/[locale]/_components/gtm.tsx
+++ core/app/[locale]/_components/gtm.tsx
@@ -8,14 +8,14 @@ const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || '';
 
 declare global {
   interface Window {
-    dataLayer: unknown[][];
+    dataLayer?: Array<Record<string, unknown> | [string, ...unknown[]]>;
   }
 }
 
 type GtagArgs = ['js', Date] | ['config', string, Record<string, unknown>];
 
 function gtag(...args: GtagArgs) {
-  window.dataLayer.push(args);
+  if (window.dataLayer) window.dataLayer.push(args);
 }
 
 export default function GTM() {
@@ -23,6 +23,10 @@ export default function GTM() {
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
+      if (!window.dataLayer) {
+        window.dataLayer = [];
+      }
+
       gtag('js', new Date());
       gtag('config', GTM_ID, {
         page_path: pathname,
