013462f72166bd02894297261d8e92b1f068f272
Jorge Moya <jorge.moya@bigcommerce.com>
2025-04-01 10:52:28 -0500

    fix: revert default variables error component

--- core/vibes/soul/sections/error/index.tsx
+++ core/vibes/soul/sections/error/index.tsx
@@ -7,7 +7,12 @@ interface Props {
   ctaAction?: () => void | Promise<void>;
 }
 
-export function Error({ title, subtitle, ctaLabel, ctaAction }: Props) {
+export function Error({
+  title = 'Something went wrong!',
+  subtitle = 'Please try again or contact our support team for assistance.',
+  ctaLabel = 'Try again',
+  ctaAction,
+}: Props) {
   return (
     <section className="@container">
       <div className="mx-auto max-w-3xl px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20">
