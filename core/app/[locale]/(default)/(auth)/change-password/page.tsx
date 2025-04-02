008445d97e5da60b29242c20981d0e38048acd3d
Miguel Oller <maoa22@gmail.com>
2025-04-02 14:20:25 +0100

    refactor: /change-password for `dynamicIO`

--- core/app/[locale]/(default)/(auth)/change-password/page.tsx
+++ core/app/[locale]/(default)/(auth)/change-password/page.tsx
@@ -1,17 +1,20 @@
-/* eslint-disable react/jsx-no-bind */
 import { Metadata } from 'next';
-import { getLocale, getTranslations } from 'next-intl/server';
+import { getTranslations } from 'next-intl/server';
+import { createLoader, parseAsString, SearchParams } from 'nuqs/server';
 
 import { ResetPasswordSection } from '@/vibes/soul/sections/reset-password-section';
 import { redirect } from '~/i18n/routing';
 
 import { changePassword } from './_actions/change-password';
 
+const loadSearchParams = createLoader({
+  c: parseAsString,
+  t: parseAsString,
+});
+
 interface Props {
-  searchParams: Promise<{
-    c?: string;
-    t?: string;
-  }>;
+  params: Promise<{ locale: string }>;
+  searchParams: Promise<SearchParams>;
 }
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -22,10 +25,10 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default async function ChangePassword({ searchParams }: Props) {
-  const { c: customerEntityId, t: token } = await searchParams;
+export default async function ChangePassword({ params, searchParams }: Props) {
+  const { locale } = await params;
+  const { c: customerEntityId, t: token } = await loadSearchParams(searchParams);
   const t = await getTranslations('ChangePassword');
-  const locale = await getLocale();
 
   if (!customerEntityId || !token) {
     return redirect({ href: '/login', locale });
@@ -33,6 +36,7 @@ export default async function ChangePassword({ searchParams }: Props) {
 
   return (
     <ResetPasswordSection
+      // eslint-disable-next-line react/jsx-no-bind
       action={changePassword.bind(null, { customerEntityId, token })}
       confirmPasswordLabel={t('Form.confirmPasswordLabel')}
       newPasswordLabel={t('Form.newPasswordLabel')}
