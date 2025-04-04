22b2708ba13df931e0052b3e6f30c9d55fb3bbb8
Nathan Booker <nathan.booker@bigcommerce.com>
2025-04-04 16:23:07 +0100

    Bump options and option values to 50/50

--- core/app/[locale]/(default)/product/[slug]/page-data.ts
+++ core/app/[locale]/(default)/product/[slug]/page-data.ts
@@ -18,7 +18,7 @@ const MultipleChoiceFieldFragment = graphql(`
     displayName
     displayStyle
     isRequired
-    values(first: 10) {
+    values(first: 50) {
       edges {
         node {
           entityId
@@ -125,7 +125,7 @@ export const ProductFormFragment = graphql(
           }
         }
       }
-      productOptions(first: 10) {
+      productOptions(first: 50) {
         edges {
           node {
             __typename
