8ea3e4fed7cc8a803d6abc68ad3d60b58872d610
Ignacio Catalina <ignacio.catalina@bigcommerce.com>
2025-04-10 14:44:03 +0200

    refactor: Simplify integration points with product-details-form
    Use an onSubmit listener and move most of the logic outside the
    product-details-form component.
    
    The ideal solution here is to patch the form action at the root of the
    component tree, but that's proven complicated at this point. Will try to
    amend it in the future.

---
+++ core/b2b/use-product-details.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { useState } from 'react';
+
+import { useB2BQuoteEnabled } from '~/b2b/use-b2b-quote-enabled';
+import { useSDK } from '~/b2b/use-b2b-sdk';
+import { useB2bShoppingListEnabled } from '~/b2b/use-b2b-shopping-list-enabled';
+
+import { Field } from '../vibes/soul/sections/product-detail/schema';
+
+import { mapToB2BProductOptions } from './map-to-b2b-product-options';
+
+interface ProductOption {
+  field: Field;
+  value?: string | number;
+}
+
+interface Data {
+  productId: string;
+  sku: string;
+  quantity: number;
+  selectedOptions: ProductOption[];
+}
+
+export const useAddToQuote = () => {
+  const isQuotesEnabled = useB2BQuoteEnabled();
+  const [isAddingToQuote, setLoading] = useState(false);
+  const sdk = useSDK();
+
+  const addProductsToQuote = async ({ selectedOptions, productId, quantity, sku }: Data) => {
+    setLoading(true);
+
+    try {
+      await sdk?.utils?.quote?.addProducts([
+        {
+          sku,
+          productEntityId: Number(productId),
+          quantity,
+          selectedOptions: selectedOptions.map(mapToB2BProductOptions),
+        },
+      ]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    isQuotesEnabled,
+    isAddingToQuote,
+    addProductsToQuote,
+  };
+};
+
+export const useAddToShoppingList = () => {
+  const isShoppingListEnabled = useB2bShoppingListEnabled();
+  const [isAddingToShoppingList, setLoading] = useState(false);
+  const sdk = useSDK();
+
+  const addProductToShoppingList = async ({ selectedOptions, productId, quantity, sku }: Data) => {
+    setLoading(true);
+
+    try {
+      await sdk?.utils?.shoppingList?.addProductFromPage({
+        sku,
+        productEntityId: Number(productId),
+        quantity,
+        selectedOptions: selectedOptions.map(mapToB2BProductOptions),
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    isShoppingListEnabled,
+    isAddingToShoppingList,
+    addProductToShoppingList,
+  };
+};
