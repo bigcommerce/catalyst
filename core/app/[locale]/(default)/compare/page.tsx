4a9a34b72ccca6d6a485e68219efd3d47cafb60a
Jorge Moya <jorge.moya@bigcommerce.com>
2025-04-21 13:08:47 -0500

    chore: add changeset

--- core/app/[locale]/(default)/compare/page.tsx
+++ core/app/[locale]/(default)/compare/page.tsx
@@ -105,4 +105,6 @@ export default async function Compare(props: Props) {
   );
 }
 
+// Disabled to circumvent a bug in Next.js and PPR
+// More info: https://github.com/vercel/next.js/issues/59407
 export const experimental_ppr = false;
