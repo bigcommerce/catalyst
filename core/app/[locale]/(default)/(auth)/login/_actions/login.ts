dc790c6b4a395bea63bee7a6e834edcb4e89b42b
Jorge Moya <jorge.moya@bigcommerce.com>
2025-05-01 11:06:05 -0500

    fix(core): remove error.name check for login auth error

--- core/app/[locale]/(default)/(auth)/login/_actions/login.ts
+++ core/app/[locale]/(default)/(auth)/login/_actions/login.ts
@@ -45,7 +45,6 @@ export const login = async (
 
     if (
       error instanceof AuthError &&
-      error.name === 'CallbackRouteError' &&
       error.cause &&
       error.cause.err?.message.includes('Invalid credentials')
     ) {
