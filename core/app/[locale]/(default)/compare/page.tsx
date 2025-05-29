1198f79b40163be2e234a24f518b1364b2995d5b
Jorge Moya <jorge.moya@bigcommerce.com>
2025-05-29 13:07:56 -0500

    refactor(core): refactor compare for dynamicIO

--- core/app/[locale]/(default)/compare/page.tsx
+++ core/app/[locale]/(default)/compare/page.tsx
@@ -1,17 +1,17 @@
 import { removeEdgesAndNodes } from '@bigcommerce/catalyst-client';
 import { Metadata } from 'next';
 import { getFormatter, getTranslations, setRequestLocale } from 'next-intl/server';
-import { cache } from 'react';
 import * as z from 'zod';
 
 import { Streamable } from '@/vibes/soul/lib/streamable';
-import { CompareProduct } from '@/vibes/soul/primitives/compare-card';
 import { CompareSection } from '@/vibes/soul/sections/compare-section';
+import { getSessionCustomerAccessToken } from '~/auth';
 import { pricesTransformer } from '~/data-transformers/prices-transformer';
+import { getPreferredCurrencyCode } from '~/lib/currency';
 
 import { addToCart } from './_actions/add-to-cart';
 import { CompareAnalyticsProvider } from './_components/compare-analytics-provider';
-import { getCompareData } from './page-data';
+import { getComparedProducts } from './page-data';
 
 const CompareParamsSchema = z.object({
   ids: z
@@ -29,49 +29,6 @@ const CompareParamsSchema = z.object({
     })
     .transform((value) => value?.map((id) => parseInt(id, 10))),
 });
-
-const getProducts = cache(async (productIds: number[] = []): Promise<CompareProduct[]> => {
-  const t = await getTranslations('Compare');
-
-  const products = await getCompareData(productIds);
-  const format = await getFormatter();
-
-  return products.map((product) => ({
-    id: product.entityId.toString(),
-    title: product.name,
-    href: product.path,
-    image: product.defaultImage
-      ? { src: product.defaultImage.url, alt: product.defaultImage.altText }
-      : undefined,
-    price: pricesTransformer(product.prices, format),
-    subtitle: product.brand?.name ?? undefined,
-    rating: product.reviewSummary.averageRating,
-    description: <div dangerouslySetInnerHTML={{ __html: product.description }} />,
-    customFields: [
-      { name: t('sku'), value: product.sku },
-      { name: t('weight'), value: `${product.weight?.value} ${product.weight?.unit}` },
-      ...removeEdgesAndNodes(product.customFields).map(({ name, value }) => ({ name, value })),
-    ],
-    hasVariants: removeEdgesAndNodes(product.productOptions).length > 0,
-    isPreorder: product.availabilityV2.status === 'Preorder',
-    disabled: product.availabilityV2.status === 'Unavailable' || !product.inventory.isInStock,
-  }));
-});
-
-const getAnalyticsData = async (productIds: number[] = []) => {
-  const products = await getCompareData(productIds);
-
-  return products.map((product) => {
-    return {
-      id: product.entityId,
-      name: product.name,
-      sku: product.sku,
-      brand: product.brand?.name ?? '',
-      price: product.prices?.price.value ?? 0,
-      currency: product.prices?.price.currencyCode ?? '',
-    };
-  });
-};
 
 interface Props {
   params: Promise<{ locale: string }>;
@@ -97,12 +54,63 @@ export default async function Compare(props: Props) {
 
   const t = await getTranslations('Compare');
 
-  const searchParams = await props.searchParams;
-  const parsed = CompareParamsSchema.parse(searchParams);
-  const productIds = parsed.ids?.filter((id) => !Number.isNaN(id));
+  const streamableProducts = Streamable.from(async () => {
+    const customerAccessToken = await getSessionCustomerAccessToken();
+    const currencyCode = await getPreferredCurrencyCode();
+
+    const searchParams = await props.searchParams;
+    const parsed = CompareParamsSchema.parse(searchParams);
+    const productIds = parsed.ids?.filter((id) => !Number.isNaN(id));
+
+    const products = await getComparedProducts(productIds, currencyCode, customerAccessToken);
+    const format = await getFormatter();
+
+    return products.map((product) => ({
+      id: product.entityId.toString(),
+      title: product.name,
+      href: product.path,
+      image: product.defaultImage
+        ? { src: product.defaultImage.url, alt: product.defaultImage.altText }
+        : undefined,
+      price: pricesTransformer(product.prices, format),
+      subtitle: product.brand?.name ?? undefined,
+      rating: product.reviewSummary.averageRating,
+      description: <div dangerouslySetInnerHTML={{ __html: product.description }} />,
+      customFields: [
+        { name: t('sku'), value: product.sku },
+        { name: t('weight'), value: `${product.weight?.value} ${product.weight?.unit}` },
+        ...removeEdgesAndNodes(product.customFields).map(({ name, value }) => ({ name, value })),
+      ],
+      hasVariants: removeEdgesAndNodes(product.productOptions).length > 0,
+      isPreorder: product.availabilityV2.status === 'Preorder',
+      disabled: product.availabilityV2.status === 'Unavailable' || !product.inventory.isInStock,
+    }));
+  });
+
+  const streamableAnalyticsData = Streamable.from(async () => {
+    const customerAccessToken = await getSessionCustomerAccessToken();
+    const currencyCode = await getPreferredCurrencyCode();
+
+    const searchParams = await props.searchParams;
+    const parsed = CompareParamsSchema.parse(searchParams);
+    const productIds = parsed.ids?.filter((id) => !Number.isNaN(id));
+
+    const products = await getComparedProducts(productIds, currencyCode, customerAccessToken);
+
+    return products.map((product) => {
+      return {
+        id: product.entityId,
+        name: product.name,
+        sku: product.sku,
+        brand: product.brand?.name ?? '',
+        price: product.prices?.price.value ?? 0,
+        currency: product.prices?.price.currencyCode ?? '',
+      };
+    });
+  });
 
   return (
-    <CompareAnalyticsProvider data={Streamable.from(() => getAnalyticsData(productIds))}>
+    <CompareAnalyticsProvider data={streamableAnalyticsData}>
       <CompareSection
         addToCartAction={addToCart}
         addToCartLabel={t('addToCart')}
@@ -114,7 +122,7 @@ export default async function Compare(props: Props) {
         noRatingsLabel={t('noRatings')}
         otherDetailsLabel={t('otherDetails')}
         previousLabel={t('previous')}
-        products={Streamable.from(() => getProducts(productIds))}
+        products={streamableProducts}
         ratingLabel={t('rating')}
         title={t('title')}
         viewOptionsLabel={t('viewOptions')}
