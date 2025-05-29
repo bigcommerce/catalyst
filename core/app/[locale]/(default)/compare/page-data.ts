a601f7e5d91e21bd9001ecfdf65708ba803a68e4
Jorge Moya <jorge.moya@bigcommerce.com>
2025-05-29 13:58:51 -0500

    refactor(core): refactor compare for dynamicIO (#2347)

--- core/app/[locale]/(default)/compare/page-data.ts
+++ core/app/[locale]/(default)/compare/page-data.ts
@@ -1,85 +1,77 @@
 import { removeEdgesAndNodes } from '@bigcommerce/catalyst-client';
 import { cache } from 'react';
 
-import { getSessionCustomerAccessToken } from '~/auth';
 import { client } from '~/client';
 import { graphql } from '~/client/graphql';
 import { revalidate } from '~/client/revalidate-target';
+import { CurrencyCode } from '~/components/header/fragment';
 import { ProductCardFragment } from '~/components/product-card/fragment';
-import { getPreferredCurrencyCode } from '~/lib/currency';
 
 export const MAX_COMPARE_LIMIT = 10;
 
-export const CompareCardFragment = graphql(
+const ComparedProductsQuery = graphql(
   `
-    fragment CompareCardFragment on Product {
-      ...ProductCardFragment
-      description
-      sku
-      weight {
-        value
-        unit
-      }
-      condition
-      customFields {
-        edges {
-          node {
-            entityId
-            name
-            value
-          }
-        }
-      }
-      productOptions(first: 1) {
-        edges {
-          node {
-            entityId
-          }
-        }
-      }
-      inventory {
-        isInStock
-      }
-      availabilityV2 {
-        status
-      }
-    }
-  `,
-  [ProductCardFragment],
-);
-
-const ComparePageQuery = graphql(
-  `
-    query ComparePageQuery($entityIds: [Int!], $first: Int, $currencyCode: currencyCode) {
+    query ComparedProductsQuery($entityIds: [Int!], $first: Int, $currencyCode: currencyCode) {
       site {
         products(entityIds: $entityIds, first: $first) {
           edges {
             node {
-              ...CompareCardFragment
+              ...ProductCardFragment
               description
+              sku
+              weight {
+                value
+                unit
+              }
+              condition
+              customFields {
+                edges {
+                  node {
+                    entityId
+                    name
+                    value
+                  }
+                }
+              }
+              productOptions(first: 1) {
+                edges {
+                  node {
+                    entityId
+                  }
+                }
+              }
+              inventory {
+                isInStock
+              }
+              availabilityV2 {
+                status
+              }
             }
           }
         }
       }
     }
   `,
-  [CompareCardFragment],
+  [ProductCardFragment],
 );
 
-export const getCompareData = cache(async (productIds: number[] = []) => {
-  const customerAccessToken = await getSessionCustomerAccessToken();
-  const currencyCode = await getPreferredCurrencyCode();
+export const getComparedProducts = cache(
+  async (productIds: number[] = [], currencyCode?: CurrencyCode, customerAccessToken?: string) => {
+    if (productIds.length === 0) {
+      return [];
+    }
 
-  const { data } = await client.fetch({
-    document: ComparePageQuery,
-    variables: {
-      entityIds: productIds,
-      first: productIds.length ? MAX_COMPARE_LIMIT : 0,
-      currencyCode,
-    },
-    customerAccessToken,
-    fetchOptions: customerAccessToken ? { cache: 'no-store' } : { next: { revalidate } },
-  });
+    const { data } = await client.fetch({
+      document: ComparedProductsQuery,
+      variables: {
+        entityIds: productIds,
+        first: productIds.length ? MAX_COMPARE_LIMIT : 0,
+        currencyCode,
+      },
+      customerAccessToken,
+      fetchOptions: customerAccessToken ? { cache: 'no-store' } : { next: { revalidate } },
+    });
 
-  return removeEdgesAndNodes(data.site.products);
-});
+    return removeEdgesAndNodes(data.site.products);
+  },
+);
