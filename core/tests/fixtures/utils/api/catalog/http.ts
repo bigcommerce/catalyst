ce366b5ec18f7457c2bb4d3d0013d4a098051628
jordanarldt <jordan.arldt@bigcommerce.com>
2025-06-06 12:39:15 -0500

    Rewrite e2e tests for not-found and search pages

--- core/tests/fixtures/utils/api/catalog/http.ts
+++ core/tests/fixtures/utils/api/catalog/http.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
+import { testEnv } from '~/tests/environment';
+
 import { httpClient } from '../client';
 import { apiResponseSchema } from '../schema';
 
-import { CatalogApi, Product } from '.';
+import { Brand, CatalogApi, Category, Product } from '.';
 
 const ProductSchema = z
   .object({
@@ -29,11 +31,77 @@ const ProductSchema = z
     }),
   );
 
+const CategorySchema = z
+  .object({
+    category_id: z.number(),
+    parent_id: z.number(),
+    name: z.string(),
+    description: z.string(),
+    url: z.object({
+      path: z.string(),
+    }),
+  })
+  .transform(
+    (data): Category => ({
+      categoryId: data.category_id,
+      parentId: data.parent_id,
+      name: data.name,
+      description: data.description,
+      path: data.url.path,
+    }),
+  );
+
+const BrandSchema = z
+  .object({
+    id: z.number(),
+    name: z.string(),
+    custom_url: z.object({
+      url: z.string(),
+    }),
+  })
+  .transform(
+    (data): Brand => ({
+      id: data.id,
+      name: data.name,
+      path: data.custom_url.url,
+    }),
+  );
+
 export const catalogHttpClient: CatalogApi = {
   getProductById: async (id: number): Promise<Product> => {
     const resp = await httpClient
       .get(`/v3/catalog/products/${id}`)
       .parse(apiResponseSchema(ProductSchema));
+
+    return resp.data;
+  },
+  getCategories: async (filters = {}): Promise<Category[]> => {
+    const trees = await httpClient
+      .get(`/v3/catalog/trees?channel_id:in=${testEnv.BIGCOMMERCE_CHANNEL_ID ?? 1}`)
+      .parse(apiResponseSchema(z.array(z.object({ id: z.number() }))));
+
+    const params = new URLSearchParams({
+      is_visible: 'true',
+      'tree_id:in': trees.data.map((tree) => tree.id).join(','),
+      ...(filters.ids ? { 'id:in': filters.ids.join(',') } : {}),
+      ...(filters.nameLike ? { 'name:like': filters.nameLike } : {}),
+    });
+
+    const resp = await httpClient
+      .get(`/v3/catalog/trees/categories?${params}`)
+      .parse(apiResponseSchema(z.array(CategorySchema)));
+
+    return resp.data;
+  },
+  getBrands: async (filters = {}): Promise<Brand[]> => {
+    const params = new URLSearchParams({
+      ...(filters.ids ? { 'id:in': filters.ids.join(',') } : {}),
+      ...(filters.nameLike ? { 'name:like': filters.nameLike } : {}),
+    });
+
+    const resp = await httpClient
+      .get(`/v3/catalog/brands${params.size ? `?${params}` : ''}`)
+      .parse(apiResponseSchema(z.array(BrandSchema)));
 
     return resp.data;
   },
--- core/tests/fixtures/utils/api/catalog/index.ts
+++ core/tests/fixtures/utils/api/catalog/index.ts
@@ -8,8 +8,24 @@ export interface Product {
   readonly path: string;
 }
 
+export interface Category {
+  readonly categoryId: number;
+  readonly parentId: number;
+  readonly name: string;
+  readonly description: string;
+  readonly path: string;
+}
+
+export interface Brand {
+  readonly id: number;
+  readonly name: string;
+  readonly path: string;
+}
+
 export interface CatalogApi {
   getProductById: (id: number) => Promise<Product>;
+  getCategories: (filters?: { nameLike?: string; ids?: number[] }) => Promise<Category[]>;
+  getBrands: (filters?: { nameLike?: string; ids?: number[] }) => Promise<Brand[]>;
 }
 
 export { catalogHttpClient } from './http';
