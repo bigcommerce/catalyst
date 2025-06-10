087fcbbcec16a73b332c81cded39b58c622089b3
Matthew Volk <matt.volk@bigcommerce.com>
2025-06-10 12:37:55 -0500

    feat: add disabled styles to checkbox input and label

--- core/vibes/soul/form/checkbox/index.tsx
+++ core/vibes/soul/form/checkbox/index.tsx
@@ -32,6 +32,9 @@ export interface CheckboxProps extends ComponentPropsWithoutRef<typeof CheckboxP
  *    --checkbox-light-checked-border-hover: hsl(var(--foreground));
  *    --checkbox-light-checked-background: hsl(var(--foreground));
  *    --checkbox-light-checked-icon: hsl(var(--background));
+ *    --checkbox-light-disabled-border: hsl(var(--contrast-200));
+ *    --checkbox-light-disabled-background: hsl(var(--contrast-100));
+ *    --checkbox-light-disabled-icon: hsl(var(--contrast-300));
  *    --checkbox-dark-label: hsl(var(--background));
  *    --checkbox-dark-error: hsl(var(--error));
  *    --checkbox-dark-unchecked-border: hsl(var(--contrast-400));
@@ -42,6 +45,9 @@ export interface CheckboxProps extends ComponentPropsWithoutRef<typeof CheckboxP
  *    --checkbox-dark-checked-border-hover: hsl(var(--background));
  *    --checkbox-dark-checked-background: hsl(var(--foreground));
  *    --checkbox-dark-checked-icon: hsl(var(--foreground));
+ *    --checkbox-dark-disabled-border: hsl(var(--contrast-200));
+ *    --checkbox-dark-disabled-background: hsl(var(--contrast-100));
+ *    --checkbox-dark-disabled-icon: hsl(var(--contrast-300));
  *    --checkbox-font-family: var(--font-family-body);
  *  }
  * ```
@@ -68,16 +74,30 @@ export function Checkbox({
           {...props}
           aria-labelledby={id !== undefined ? `${id}-label` : `${generatedId}-label`}
           className={clsx(
-            'flex h-5 w-5 items-center justify-center rounded-md border transition-colors duration-150 focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-[var(--checkbox-focus,hsl(var(--primary)))]',
+            'peer flex h-5 w-5 items-center justify-center rounded-md border transition-colors duration-150 focus-visible:outline-0 focus-visible:ring-2 focus-visible:ring-[var(--checkbox-focus,hsl(var(--primary)))] disabled:cursor-not-allowed',
             {
               light:
                 errors && errors.length > 0
                   ? 'border-[var(--checkbox-light-error,hsl(var(--error)))]'
-                  : 'data-[state=checked]:border-[var(--checkbox-light-checked-border,hsl(var(--foreground)))] data-[state=unchecked]:border-[var(--checkbox-light-unchecked-border,hsl(var(--contrast-200)))] data-[state=checked]:bg-[var(--checkbox-light-checked-background,hsl(var(--foreground)))] data-[state=unchecked]:bg-[var(--checkbox-light-unchecked-background,hsl(var(--background)))] data-[state=checked]:text-[var(--checkbox-light-checked-text,hsl(var(--background)))] data-[state=unchecked]:text-[var(--checkbox-light-unchecked-text,hsl(var(--foreground)))] data-[state=checked]:hover:border-[var(--checkbox-light-checked-border-hover,hsl(var(--foreground)))] data-[state=unchecked]:hover:border-[var(--checkbox-light-unchecked-border-hover,hsl(var(--contrast-300)))]',
+                  : clsx(
+                      // Disabled states
+                      'disabled:border-[var(--checkbox-light-disabled-border,hsl(var(--contrast-200)))] disabled:bg-[var(--checkbox-light-disabled-background,hsl(var(--contrast-100)))] disabled:text-[var(--checkbox-light-disabled-icon,hsl(var(--contrast-300)))]',
+                      // Normal states
+                      'enabled:data-[state=checked]:border-[var(--checkbox-light-checked-border,hsl(var(--foreground)))] enabled:data-[state=unchecked]:border-[var(--checkbox-light-unchecked-border,hsl(var(--contrast-200)))] enabled:data-[state=checked]:bg-[var(--checkbox-light-checked-background,hsl(var(--foreground)))] enabled:data-[state=unchecked]:bg-[var(--checkbox-light-unchecked-background,hsl(var(--background)))] enabled:data-[state=checked]:text-[var(--checkbox-light-checked-text,hsl(var(--background)))] enabled:data-[state=unchecked]:text-[var(--checkbox-light-unchecked-text,hsl(var(--foreground)))]',
+                      // Hover states (only apply when checkbox is enabled)
+                      'enabled:data-[state=checked]:hover:border-[var(--checkbox-light-checked-border-hover,hsl(var(--foreground)))] enabled:data-[state=unchecked]:hover:border-[var(--checkbox-light-unchecked-border-hover,hsl(var(--contrast-300)))]',
+                    ),
               dark:
                 errors && errors.length > 0
                   ? 'border-[var(--checkbox-dark-error,hsl(var(--error)))]'
-                  : 'data-[state=checked]:border-[var(--checkbox-dark-checked-border,hsl(var(--background)))] data-[state=unchecked]:border-[var(--checkbox-dark-unchecked-border,hsl(var(--contrast-400)))] data-[state=checked]:bg-[var(--checkbox-dark-checked-background,hsl(var(--foreground)))] data-[state=unchecked]:bg-[var(--checkbox-dark-unchecked-background,hsl(var(--foreground)))] data-[state=checked]:text-[var(--checkbox-dark-checked-text,hsl(var(--background)))] data-[state=unchecked]:text-[var(--checkbox-dark-unchecked-text,hsl(var(--background)))] data-[state=checked]:hover:border-[var(--checkbox-dark-checked-border-hover,hsl(var(--background)))] data-[state=unchecked]:hover:border-[var(--checkbox-dark-unchecked-border-hover,hsl(var(--contrast-300)))]',
+                  : clsx(
+                      // Disabled states
+                      'disabled:border-[var(--checkbox-dark-disabled-border,hsl(var(--contrast-200)))] disabled:bg-[var(--checkbox-dark-disabled-background,hsl(var(--contrast-100)))] disabled:text-[var(--checkbox-dark-disabled-icon,hsl(var(--contrast-300)))]',
+                      // Normal states
+                      'enabled:data-[state=checked]:border-[var(--checkbox-dark-checked-border,hsl(var(--background)))] enabled:data-[state=unchecked]:border-[var(--checkbox-dark-unchecked-border,hsl(var(--contrast-400)))] enabled:data-[state=checked]:bg-[var(--checkbox-dark-checked-background,hsl(var(--foreground)))] enabled:data-[state=unchecked]:bg-[var(--checkbox-dark-unchecked-background,hsl(var(--foreground)))] enabled:data-[state=checked]:text-[var(--checkbox-dark-checked-text,hsl(var(--background)))] enabled:data-[state=unchecked]:text-[var(--checkbox-dark-unchecked-text,hsl(var(--background)))]',
+                      // Hover states (only apply when checkbox is enabled)
+                      'enabled:data-[state=checked]:hover:border-[var(--checkbox-dark-checked-border-hover,hsl(var(--background)))] enabled:data-[state=unchecked]:hover:border-[var(--checkbox-dark-unchecked-border-hover,hsl(var(--contrast-300)))]',
+                    ),
             }[colorScheme],
           )}
           id={id ?? generatedId}
@@ -90,7 +110,7 @@ export function Checkbox({
         {label != null && label !== '' && (
           <LabelPrimitive.Root
             className={clsx(
-              'cursor-pointer text-sm',
+              'cursor-pointer text-sm peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
               {
                 light: 'text-[var(--checkbox-light-label,hsl(var(--foreground)))]',
                 dark: 'text-[var(--checkbox-dark-label,hsl(var(--background)))]',
