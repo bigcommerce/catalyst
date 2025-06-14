ba3530ddad84be8e83f03c74db965f99a127bcc2
joudepic <joud@epicdesignlabs.com>
2025-06-14 23:59:39 +0300

    return to old slider

--- core/lib/makeswift/components/logo-carousel/logo-carousel.makeswift.tsx
+++ core/lib/makeswift/components/logo-carousel/logo-carousel.makeswift.tsx
@@ -11,11 +11,9 @@ import {
 import dynamic from 'next/dynamic';
 
 import { breakpoints, runtime } from '~/lib/makeswift/runtime';
-// import 'react-multi-carousel/lib/styles.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import 'react-multi-carousel/lib/styles.css';
 
-const Slider = dynamic(() => import('react-slick'), { ssr: true });
+const Carousel = dynamic(() => import('react-multi-carousel'), { ssr: true });
 
 interface LogoInterface {
   imageSrc?: string;
@@ -70,7 +68,23 @@ runtime.registerComponent(
     };
 
     return (
-      <Slider dots={true} infinite={true} speed={500} slidesToShow={1} slidesToScroll={1}>
+      <Carousel
+        swipeable={swipeable}
+        draggable={draggable}
+        showDots={showdots}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={infinite}
+        deviceType={'desktop'} // This is important for SSR. It should match the device type you want to render.
+        autoPlaySpeed={1000}
+        keyBoardControl={keyBoardControl}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={['tablet', 'mobile']}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+      >
         {logos.map((logo: LogoInterface, index: number) => (
           <div key={index} className="px-6 py-6">
             <a
@@ -87,45 +101,8 @@ runtime.registerComponent(
             </a>
           </div>
         ))}
-      </Slider>
+      </Carousel>
     );
-
-    // return (
-    //   <Carousel
-    //     swipeable={swipeable}
-    //     draggable={draggable}
-    //     showDots={showdots}
-    //     responsive={responsive}
-    //     ssr={true} // means to render carousel on server-side.
-    //     infinite={infinite}
-    //     deviceType={'desktop'} // This is important for SSR. It should match the device type you want to render.
-    //     autoPlaySpeed={1000}
-    //     keyBoardControl={keyBoardControl}
-    //     customTransition="all .5"
-    //     transitionDuration={500}
-    //     containerClass="carousel-container"
-    //     removeArrowOnDeviceType={['tablet', 'mobile']}
-    //     dotListClass="custom-dot-list-style"
-    //     itemClass="carousel-item-padding-40-px"
-    //   >
-    //     {logos.map((logo: LogoInterface, index: number) => (
-    //       <div key={index} className="px-6 py-6">
-    //         <a
-    //           href={logo.link?.href ?? '#'}
-    //           target={logo.link?.target}
-    //           rel={logo.link?.target === '_blank' ? 'noopener noreferrer' : undefined}
-    //           className="block h-full w-full"
-    //         >
-    //           <img
-    //             src={logo.imageSrc ?? ''}
-    //             alt={logo.imageAlt ?? ''}
-    //             className="h-full w-full object-contain"
-    //           />
-    //         </a>
-    //       </div>
-    //     ))}
-    //   </Carousel>
-    // );
   },
   {
     type: 'primitive-logo-carousel',
