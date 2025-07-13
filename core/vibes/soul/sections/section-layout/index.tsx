d435236f1a310e93172584b232f01346f0e7c732
joudepic <joud@epicdesignlabs.com>
2025-07-13 23:56:33 +0300

    add options to the section padding

--- core/vibes/soul/sections/section-layout/index.tsx
+++ core/vibes/soul/sections/section-layout/index.tsx
@@ -18,18 +18,26 @@ export function SectionLayout({
   className,
   children,
   containerSize = '2xl',
+  paddingOptionsLargeDesktop,
+  paddingOptionsDesktop,
+  paddingOptionsTablet,
+  paddingOptionsMobile,
   hideOverflow = false,
 }: {
   className?: string;
   children: React.ReactNode;
   containerSize?: 'md' | 'lg' | 'xl' | '2xl';
   hideOverflow?: boolean;
+  paddingOptionsLargeDesktop: string;
+  paddingOptionsDesktop: string;
+  paddingOptionsTablet: string;
+  paddingOptionsMobile: string;
 }) {
   return (
     <section className={clsx('@container', hideOverflow && 'overflow-hidden', className)}>
       <div
         className={clsx(
-          'mx-auto px-4 py-10 @xl:px-6 @xl:py-14 @4xl:px-8 @4xl:py-20',
+          `@4xl:py-${paddingOptionsLargeDesktop} mx-auto px-4 py-${paddingOptionsMobile} @md:py-${paddingOptionsTablet} @xl:px-6 @xl:py-${paddingOptionsDesktop} @4xl:px-8`,
           {
             md: 'max-w-[var(--section-max-width-md,768px)]',
             lg: 'max-w-[var(--section-max-width-lg,1024px)]',
