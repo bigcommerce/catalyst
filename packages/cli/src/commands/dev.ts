0a137b2d34ed0ba6e89b6387bd687c95c2ff6cb6
Jorge Moya <jorge.moya@bigcommerce.com>
2025-07-25 14:30:45 -0500

    refactor: run local binary

--- packages/cli/src/commands/dev.ts
+++ packages/cli/src/commands/dev.ts
@@ -1,14 +1,31 @@
 import { Command } from 'commander';
 import consola from 'consola';
 import { execa } from 'execa';
+import { join } from 'node:path';
 
 export const dev = new Command('dev')
-  .description('Start the development server')
-  .option('-p, --port <number>', 'Port to run the development server on', '3000')
-  .option('--root-dir <rootDir>', 'Root directory to run development server from.', process.cwd())
+  .description('Start the Catalyst development server.')
+  .option(
+    '-p, --port <number>',
+    'Port number to run the development server on (default: 3000).',
+    '3000',
+  )
+  .option('--root-dir <rootDir>', 'Root directory of your Catalyst project.', process.cwd())
   .action(async (opts) => {
     try {
-      await execa('npm', ['run', 'dev'], { stdio: 'inherit', cwd: opts.rootDir });
+      const dotenvBin = join(opts.rootDir, 'node_modules', '.bin', 'dotenv');
+
+      await execa({
+        stdio: 'inherit',
+        cwd: opts.rootDir,
+      })`${dotenvBin} -e .env.local -- node ./scripts/generate.cjs`;
+
+      const nextBin = join(opts.rootDir, 'node_modules', '.bin', 'next');
+
+      await execa({
+        stdio: 'inherit',
+        cwd: opts.rootDir,
+      })`${nextBin} dev`;
     } catch (error) {
       consola.error(error);
       process.exit(1);
