31b037546f2430feaa7c355bc4849a24909bba70
joudepic <joud@epicdesignlabs.com>
2025-07-28 00:31:04 +0300

    more fixes

--- core/app/[locale]/layout.tsx
+++ core/app/[locale]/layout.tsx
@@ -116,13 +116,6 @@ export default async function RootLayout({ params, children }: Props) {
                   <CookieNotifications {...toastNotificationCookieData} />
                 )}
                 {children}
-                <button
-                  className="qn-button qn-hover-button qn-hover-button-expanded"
-                  id="qn-dialog-toggle"
-                  type="button"
-                >
-                  Finish Quote Request
-                </button>
               </Providers>
             </NuqsAdapter>
           </NextIntlClientProvider>
--- core/app/providers.tsx
+++ core/app/providers.tsx
@@ -4,17 +4,11 @@ import { PropsWithChildren } from 'react';
 
 import { Toaster } from '@/vibes/soul/primitives/toaster';
 import { CartProvider } from '~/components/header/cart-provider';
-import { QuoteNinjaCustomerSync } from '~/components/QuoteNinjaCustomerSync';
 
-interface ProvidersProps extends PropsWithChildren {
-  customer?: any;
-}
-
-export function Providers({ children, customer }: ProvidersProps) {
+export function Providers({ children }: PropsWithChildren) {
   return (
     <>
       <Toaster position="top-right" />
-      {customer && <QuoteNinjaCustomerSync customer={customer} />}
       <CartProvider>{children}</CartProvider>
     </>
   );
