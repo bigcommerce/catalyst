78651c18fe8abbd39715439ece7bf8677e6d8d72
Matthew Volk <matt.volk@bigcommerce.com>
2025-08-14 23:18:55 -0500

    refactor: proxy all valid next start and dev options

--- packages/cli/tests/commands/start.spec.ts
+++ packages/cli/tests/commands/start.spec.ts
@@ -5,6 +5,10 @@ import { expect, test, vi } from 'vitest';
 import { start } from '../../src/commands/start';
 import { program } from '../../src/program';
 
+vi.mock('node:fs', () => ({
+  existsSync: vi.fn(() => true),
+}));
+
 vi.mock('execa', () => ({
   execa: vi.fn(() => Promise.resolve({})),
   __esModule: true,
@@ -14,31 +18,17 @@ test('properly configured Command instance', () => {
   expect(start).toBeInstanceOf(Command);
   expect(start.name()).toBe('start');
   expect(start.description()).toBe('Start your Catalyst storefront in optimized production mode.');
-  expect(start.options).toEqual(
-    expect.arrayContaining([
-      expect.objectContaining({ flags: '-p, --port <number>', defaultValue: '3000' }),
-      expect.objectContaining({ flags: '--root-dir <path>', defaultValue: process.cwd() }),
-    ]),
-  );
 });
 
 test('calls execa with Next.js production optimized server', async () => {
-  await program.parseAsync([
-    'node',
-    'catalyst',
-    'start',
-    '-p',
-    '3001',
-    '--root-dir',
-    '/path/to/root',
-  ]);
+  await program.parseAsync(['node', 'catalyst', 'start', '-p', '3001']);
 
   expect(execa).toHaveBeenCalledWith(
-    '/path/to/root/node_modules/.bin/next',
+    'node_modules/.bin/next',
     ['start', '-p', '3001'],
     expect.objectContaining({
       stdio: 'inherit',
-      cwd: '/path/to/root',
+      cwd: process.cwd(),
     }),
   );
 });
