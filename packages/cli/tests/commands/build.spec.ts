53a70157cf782e5390f1fc99d63642c762ad2b1a
Matthew Volk <matt.volk@bigcommerce.com>
2025-08-19 16:30:39 -0500

    refactor: proxy next build if framework is nextjs

--- packages/cli/tests/commands/build.spec.ts
+++ packages/cli/tests/commands/build.spec.ts
@@ -1,7 +1,21 @@
 import { Command } from 'commander';
-import { expect, test } from 'vitest';
+import { execa } from 'execa';
+import { expect, test, vi } from 'vitest';
 
 import { build } from '../../src/commands/build';
+import { program } from '../../src/program';
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+vi.spyOn(process, 'exit').mockImplementation(() => null as never);
+
+vi.mock('node:fs', () => ({
+  existsSync: vi.fn(() => true),
+}));
+
+vi.mock('execa', () => ({
+  execa: vi.fn(() => Promise.resolve({ stdout: '' })),
+  __esModule: true,
+}));
 
 test('properly configured Command instance', () => {
   expect(build).toBeInstanceOf(Command);
@@ -11,5 +25,18 @@ test('properly configured Command instance', () => {
       expect.objectContaining({ long: '--framework' }),
       expect.objectContaining({ long: '--project-uuid' }),
     ]),
+  );
+});
+
+test('calls execa with Next.js build if framework is nextjs', async () => {
+  await program.parseAsync(['node', 'catalyst', 'build', '--framework', 'nextjs', '--debug']);
+
+  expect(execa).toHaveBeenCalledWith(
+    'node_modules/.bin/next',
+    ['build', '--debug'],
+    expect.objectContaining({
+      stdio: 'inherit',
+      cwd: process.cwd(),
+    }),
   );
 });
