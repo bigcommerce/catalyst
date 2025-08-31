095e16c09b3f3fbb30a6000c0008e13957cf9721
Nathan Booker <nathan.booker@bigcommerce.com>
2025-08-30 23:48:47 -0500

    SST initial setup

---
+++ core/sst.config.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "core",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs("MyWeb");
+  },
+});
