baf07ca89fdbb65bebf5926738b72690e4e6db60
Jorge Moya <jorge.moya@bigcommerce.com>
2025-09-05 10:02:25 -0500

    chore(core): removes unused exports (#2577)

--- core/app/[locale]/(default)/compare/_components/compare-analytics-provider.tsx
+++ core/app/[locale]/(default)/compare/_components/compare-analytics-provider.tsx
@@ -31,7 +31,7 @@ export function CompareAnalyticsProvider(
   );
 }
 
-export function CompareAnalyticsProviderResolved({
+function CompareAnalyticsProviderResolved({
   children,
   data,
 }: PropsWithChildren<{ data: Streamable<AddToCartContext[]> }>) {
--- core/lib/cart/add-cart-line-item.ts
+++ core/lib/cart/add-cart-line-item.ts
@@ -30,21 +30,3 @@ export const addCartLineItem = async (
     fetchOptions: { cache: 'no-store' },
   });
 };
-
-export function assertAddCartLineItemErrors(
-  response: Awaited<ReturnType<typeof addCartLineItem>>,
-): asserts response is Awaited<ReturnType<typeof addCartLineItem>> {
-  if (typeof response === 'object' && 'errors' in response && Array.isArray(response.errors)) {
-    response.errors.forEach((error) => {
-      if (error.message.includes('Not enough stock:')) {
-        // This removes the item id from the message. It's very brittle, but it's the only
-        // solution to do it until our API returns a better error message.
-        throw new Error(
-          error.message.replace('Not enough stock: ', '').replace(/\(\w.+\)\s{1}/, ''),
-        );
-      }
-
-      throw new Error(error.message);
-    });
-  }
-}
