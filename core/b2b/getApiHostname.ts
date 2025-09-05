7e796c2492946ee9786ff8ff0b97c0a57b302b54
Victor Campos <victor.campos@bigcommerce.com>
2025-09-05 12:09:38 -0600

    feat: temp b2b staging support

--- core/b2b/getApiHostname.ts
+++ core/b2b/getApiHostname.ts
@@ -7,12 +7,17 @@ const ENV = z
     env: z.object({
       B2B_API_HOST: z.string().default(DEFAULT_API_HOST),
       NODE_ENV: z.enum(['development', 'production']).default('production'),
+      STAGING_B2B_CDN_ORIGIN: z.string().optional(),
     }),
   })
   .transform(({ env }) => env);
 
 export const getAPIHostname = () => {
-  const { B2B_API_HOST, NODE_ENV } = ENV.parse(process);
+  const { B2B_API_HOST, NODE_ENV, STAGING_B2B_CDN_ORIGIN } = ENV.parse(process);
+
+  if (STAGING_B2B_CDN_ORIGIN === "true") {
+    return 'https://api-b2b.staging.zone';
+  }
 
   if (NODE_ENV === 'production') {
     return DEFAULT_API_HOST;
