ae929ecd4fcf643cd78eebb94a1415ba61975868
mxplantae <80371957+mxplantae@users.noreply.github.com>
2025-09-21 14:37:19 -0600

    update Falgs.ts

--- core/flags.ts
+++ core/flags.ts
@@ -1,11 +1,13 @@
 import { statsigAdapter, type StatsigUser } from '@flags-sdk/statsig';
 import { flag, dedupe } from 'flags/next';
-import type { Identify } from 'flags';
 
-export const identify = dedupe((async () => ({
-  // implement the identify() function to add any additional user properties you'd like, see docs.statsig.com/concepts/user
-  userID: '1234', // for example, set userID
-})) satisfies Identify<StatsigUser>);
+export const identify = dedupe(
+  async () =>
+    ({
+      // implement the identify() function to add any additional user properties you'd like, see docs.statsig.com/concepts/user
+      userID: '1234', // for example, set userID
+    }) satisfies StatsigUser,
+);
 
 export const createFeatureFlag = (key: string) =>
   flag<boolean, StatsigUser>({
@@ -15,4 +17,3 @@ export const createFeatureFlag = (key: string) =>
     }),
     identify,
   });
-
