68e31140ab31385ff7c8d34312b894650caddf18
mxplantae <80371957+mxplantae@users.noreply.github.com>
2025-09-21 16:47:55 -0600

    core

--- core/dynamic-statsig-provider.tsx
+++ core/dynamic-statsig-provider.tsx
@@ -1,21 +1,24 @@
-"use client";
+'use client';
 
-import type { Statsig } from "@flags-sdk/statsig";
-import { StatsigProvider, useClientBootstrapInit } from "@statsig/react-bindings";
-import { StatsigAutoCapturePlugin } from "@statsig/web-analytics";
-import type { ReactNode } from "react";
+import type { Statsig } from '@flags-sdk/statsig';
+import { StatsigProvider, useClientBootstrapInit } from '@statsig/react-bindings';
+import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
+import type { ReactNode } from 'react';
 
-export function DynamicStatsigProvider({ children, datafile, }: {
+export function DynamicStatsigProvider({
+  children,
+  datafile,
+}: {
   children: ReactNode;
   datafile: Awaited<ReturnType<typeof Statsig.getClientInitializeResponse>>;
 }) {
-  if (!datafile) throw new Error("Missing datafile");
+  if (!datafile) throw new Error('Missing datafile');
 
   const client = useClientBootstrapInit(
     process.env.NEXT_PUBLIC_STATSIG_CLIENT_KEY as string,
     datafile.user,
     JSON.stringify(datafile),
-    { plugins: [new StatsigAutoCapturePlugin()] } // Optional, will add autocaptured web analytics events to Statsig
+    { plugins: [new StatsigAutoCapturePlugin()] }, // Optional, will add autocaptured web analytics events to Statsig
   );
 
   return (
--- core/next.config.ts
+++ core/next.config.ts
@@ -118,7 +118,6 @@ export default async (): Promise<NextConfig> => {
         },
       ];
     },
-
   };
 
   // Apply withNextIntl to the config
