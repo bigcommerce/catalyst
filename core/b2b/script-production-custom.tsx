b179535b6615ba32872d93593c524db829349a46
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-09-30 13:19:56 -0500

    Add support for deployed custom B2B Buyer Portal based on env vars

---
+++ core/b2b/script-production-custom.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import Script from 'next/script';
+
+import { useB2BAuth } from './use-b2b-auth';
+import { useB2BCart } from './use-b2b-cart';
+
+/**
+ * Use these vars if using build hashes in B2B Buyer Portal files.
+ */
+const hashIndex = undefined;
+const hashIndexLegacy = undefined;
+const hashPolyfills = undefined;
+
+interface Props {
+  storeHash: string;
+  channelId: string;
+  token?: string;
+  cartId?: string | null;
+  prodUrl: string;
+}
+
+export function ScriptProductionCustom({ 
+  cartId, 
+  storeHash, 
+  channelId, 
+  token,
+  prodUrl,
+}: Props) {
+  useB2BAuth(token);
+  useB2BCart(cartId);
+
+  return (
+    <>
+      <Script>
+        {`
+        window.b3CheckoutConfig = {
+          routes: {
+            dashboard: '/#/dashboard',
+          },
+        }
+        window.B3 = {
+          setting: {
+            store_hash: '${storeHash}',  
+            channel_id: ${channelId},
+          },
+        }
+        `}
+      </Script>
+      <Script
+        type="module"
+        crossOrigin=""
+        src={`${prodUrl}/index${hashIndex ? `.${hashIndex}` : ''}.js`}
+      ></Script>
+      <Script
+        noModule
+        crossOrigin=""
+        src={`${prodUrl}/polyfills-legacy${hashPolyfills ? `.${hashPolyfills}` : ''}.js`}
+      ></Script>
+      <Script
+        noModule
+        crossOrigin=""
+        src={`${prodUrl}/index-legacy${hashIndexLegacy ? `.${hashIndexLegacy}` : ''}.js`}
+      ></Script>
+    </>
+  );
+}
