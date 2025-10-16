ab06c1b819f4b29974628bad4318d7a6041b022c
Victor Campos <140021227+bc-victor@users.noreply.github.com>
2025-10-16 13:35:10 -0600

    fix: B2B-3725 add required b2b headers (#2631)

--- core/b2b/client.ts
+++ core/b2b/client.ts
@@ -11,10 +11,17 @@ interface LoginWithB2BParams {
 
 const ENV = z
   .object({
-    env: z.object({
-      B2B_API_TOKEN: z.string(),
-      BIGCOMMERCE_CHANNEL_ID: z.string(),
-    }),
+    env: z.union([
+      z.object({
+        BIGCOMMERCE_CHANNEL_ID: z.string(),
+        B2B_API_TOKEN: z.string(),
+      }),
+      z.object({
+        BIGCOMMERCE_CHANNEL_ID: z.string(),
+        BIGCOMMERCE_STORE_HASH: z.string(),
+        BIGCOMMERCE_ACCESS_TOKEN: z.string(),
+      }),
+    ]),
   })
   .transform(({ env }) => env);
 
@@ -29,17 +36,27 @@ const B2BTokenResponseSchema = z.object({
 });
 
 export async function loginWithB2B({ customerId, customerAccessToken }: LoginWithB2BParams) {
-  const { B2B_API_TOKEN, BIGCOMMERCE_CHANNEL_ID } = ENV.parse(process);
+  const env = ENV.parse(process);
+  const BIGCOMMERCE_CHANNEL_ID = env.BIGCOMMERCE_CHANNEL_ID;
+  const headers: HeadersInit = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  if ('BIGCOMMERCE_ACCESS_TOKEN' in env) {
+    headers['X-Auth-Token'] = env.BIGCOMMERCE_ACCESS_TOKEN;
+    headers['X-Store-Hash'] = env.BIGCOMMERCE_STORE_HASH;
+  } else if ('B2B_API_TOKEN' in env) {
+    headers['authToken'] = env.B2B_API_TOKEN;
+    console.warn('This is deprecated in favour or BIGCOMMERCE_ACCESS_TOKEN, read https://support.bigcommerce.com/s/article/Store-API-Accounts?language=en_US')
+  } else {
+    throw new Error('No B2B API token or BigCommerce token found in environment variables.');
+  } 
 
   const apiHost = getAPIHostname();
-
   const response = await fetch(`${apiHost}/api/io/auth/customers/storefront`, {
     method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      authToken: B2B_API_TOKEN,
-    },
+    headers,
     body: JSON.stringify({
       channelId: BIGCOMMERCE_CHANNEL_ID,
       customerId,
