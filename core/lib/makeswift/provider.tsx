99a60c9973db0e545dafa4be4bdec544788a2de2
Fikri Karim <fkfikrikarim@gmail.com>
2025-10-21 12:35:15 +0700

    debug: add provider that will cause a flicker
    https://docs.makeswift.com/developer/guides/troubleshooting/layout-shift

--- core/lib/makeswift/provider.tsx
+++ core/lib/makeswift/provider.tsx
@@ -1,9 +1,12 @@
 'use client';
 
 import { ReactRuntimeProvider, RootStyleRegistry, type SiteVersion } from '@makeswift/runtime/next';
+import { createContext, useEffect, useState } from 'react';
 
 import { runtime } from '~/lib/makeswift/runtime';
 import '~/lib/makeswift/components';
+
+const FlickerContext = createContext(0);
 
 export function MakeswiftProvider({
   children,
@@ -12,9 +15,15 @@ export function MakeswiftProvider({
   children: React.ReactNode;
   siteVersion: SiteVersion | null;
 }) {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => setValue(1), []);
+
   return (
     <ReactRuntimeProvider runtime={runtime} siteVersion={siteVersion}>
-      <RootStyleRegistry enableCssReset={false}>{children}</RootStyleRegistry>
+      <FlickerContext.Provider value={value}>
+        <RootStyleRegistry enableCssReset={false}>{children}</RootStyleRegistry>
+      </FlickerContext.Provider>
     </ReactRuntimeProvider>
   );
 }
