1d49247e466f132d65bb17e612250a6aceedda91
Jorge Moya <jorge.moya@bigcommerce.com>
2025-12-03 12:40:13 -0600

    chore: revert middleware

--- core/middleware.ts
+++ core/middleware.ts
@@ -5,7 +5,7 @@ import { withChannelId } from './middlewares/with-channel-id';
 import { withIntl } from './middlewares/with-intl';
 import { withRoutes } from './middlewares/with-routes';
 
-export const proxy = composeMiddlewares(
+export const middleware = composeMiddlewares(
   withAuth,
   withIntl,
   withAnalyticsCookies,
