d836509c39abf0ef811dc82639c92098ec326d12
Jorge Moya <jorge.moya@bigcommerce.com>
2026-01-27 16:45:16 -0600

    chore: add comments

--- core/app/not-found.tsx
+++ core/app/not-found.tsx
@@ -1,6 +1,8 @@
 import { defaultLocale } from '~/i18n/locales';
 import { redirect } from '~/i18n/routing';
 
+// For scenarios where a redirect to `notFound` is used outside the `[locale]` path.
+// We still need to redirect to the `[locale]/not-found` path so that the page is rendered with the correct locale.
 export default function Page() {
   redirect({ href: '/not-found', locale: defaultLocale });
 }
