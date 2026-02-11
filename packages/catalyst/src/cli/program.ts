1d2a505cf0795437f8168db1363d456877532b5e
jamesqquick <james.q.quick@gmail.com>
2026-02-11 12:58:28 -0500

    add --env-file flag allowing user to pass path to environment variable file to load

--- packages/catalyst/src/cli/program.ts
+++ packages/catalyst/src/cli/program.ts
@@ -1,5 +1,7 @@
 import { Command } from 'commander';
 import { colorize } from 'consola/utils';
+import { config } from 'dotenv';
+import { resolve } from 'node:path';
 
 import PACKAGE_INFO from '../../package.json';
 
@@ -16,12 +18,33 @@ import { consola } from './lib/logger';
 
 export const program = new Command();
 
+function loadEnvFilePreHook(thisCommand: Command) {
+  let root: typeof thisCommand | undefined = thisCommand;
+
+  while (root.parent) {
+    root = root.parent as typeof thisCommand;
+  }
+
+  const opts = root.opts() as { envFile?: string };
+  const envFile = opts.envFile;
+
+  if (envFile) {
+    const resolvedPath = resolve(process.cwd(), envFile);
+
+    config({ path: resolvedPath, override: true });
+  }
+}
+
 consola.log(colorize('cyanBright', `◢ ${PACKAGE_INFO.name} v${PACKAGE_INFO.version}\n`));
 
 program
   .name(PACKAGE_INFO.name)
   .version(PACKAGE_INFO.version)
   .description('CLI tool for Catalyst development')
+  .option(
+    '--env-file <path>',
+    'Path to environment file to load (relative to current working directory)',
+  )
   .addCommand(version)
   .addCommand(dev)
   .addCommand(start)
@@ -30,5 +53,6 @@ program
   .addCommand(project)
   .addCommand(auth)
   .addCommand(telemetry)
+  .hook('preAction', loadEnvFilePreHook)
   .hook('preAction', telemetryPreHook)
   .hook('postAction', telemetryPostHook);
