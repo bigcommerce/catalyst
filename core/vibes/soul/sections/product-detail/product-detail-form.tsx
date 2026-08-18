0ae3a8e2eae6290be7d0d36109bf1ac3dd2aab95
Animesh Singh <animeshsingh1987@gmail.com>
2026-08-18 23:14:19 +0000

    feat(back): BACK-669 Display error message when quantity exceeds ATS (#3179)

--- core/vibes/soul/sections/product-detail/product-detail-form.tsx
+++ core/vibes/soul/sections/product-detail/product-detail-form.tsx
@@ -195,7 +195,13 @@ export function ProductDetailForm<F extends Field>({
       backorderMessage,
       showQuantityOnBackorder,
       unlimitedBackorder,
-    } = backorderDisplayData || { availableForBackorder: 0, availableOnHand: 0 };
+    } = backorderDisplayData || {
+      availableForBackorder: 0,
+      availableOnHand: 0,
+      backorderMessage: null,
+      showQuantityOnBackorder: false,
+      unlimitedBackorder: false,
+    };
 
     if (!showQuantityOnBackorder && !backorderMessage) {
       return undefined;
@@ -203,8 +209,23 @@ export function ProductDetailForm<F extends Field>({
 
     const orderQuantity = Number(formFields.quantity.value);
 
+    if (orderQuantity > availableForBackorder + availableOnHand) {
+      return {
+        backOrderErrorMessage: t('maxPurchasableQuantity', {
+          quantity: availableForBackorder + availableOnHand,
+        }),
+        backorderQuantityMessage: t('backorderQuantity', {
+          quantity: unlimitedBackorder
+            ? orderQuantity - availableOnHand
+            : Math.min(orderQuantity - availableOnHand, availableForBackorder),
+        }),
+        backorderInfoMessage: undefined,
+      };
+    }
+
     if (Number.isNaN(orderQuantity) || orderQuantity <= availableOnHand) {
       return {
+        backOrderErrorMessage: undefined,
         backorderQuantityMessage: undefined,
         backorderInfoMessage: undefined,
       };
@@ -212,12 +233,14 @@ export function ProductDetailForm<F extends Field>({
 
     if (!showQuantityOnBackorder) {
       return {
+        backOrderErrorMessage: undefined,
         backorderQuantityMessage: undefined,
         backorderInfoMessage: backorderMessage ?? undefined,
       };
     }
 
     return {
+      backOrderErrorMessage: undefined,
       backorderQuantityMessage: t('backorderQuantity', {
         quantity: unlimitedBackorder
           ? orderQuantity - availableOnHand
@@ -313,9 +336,20 @@ export function ProductDetailForm<F extends Field>({
               required
               value={quantityControl.value}
             />
-            <SubmitButton disabled={ctaDisabled}>{ctaLabel}</SubmitButton>
+            <SubmitButton disabled={ctaDisabled || !!backorderMessages?.backOrderErrorMessage}>
+              {ctaLabel}
+            </SubmitButton>
             {additionalActions}
           </div>
+
+          {!!backorderMessages?.backOrderErrorMessage && (
+            <FormStatus
+              className="ease-initial duration-400 opacity-100 transition-opacity"
+              type="error"
+            >
+              {backorderMessages.backOrderErrorMessage}
+            </FormStatus>
+          )}
         </div>
       </form>
     </FormProvider>
